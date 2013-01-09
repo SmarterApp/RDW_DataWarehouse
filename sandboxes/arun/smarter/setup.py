21582c1672bf3c642c972dddb7fe35a0dc60acc9
Eugene Jen <ejen@ejen-mac.wgenhq.net>
2013-01-08 19:38:48 -0500

    fix pep8

--- sandboxes/arun/smarter/setup.py
+++ sandboxes/arun/smarter/setup.py
@@ -14,18 +14,18 @@ requires = [
     'pyramid_debugtoolbar',
     'zope.sqlalchemy',
     'waitress',
-    ]
+]
 
 setup(name='smarter',
       version='0.0',
       description='smarter',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
       author='',
       author_email='',
       url='',
@@ -40,5 +40,4 @@ setup(name='smarter',
       main = smarter:main
       [console_scripts]
       initialize_smarter_db = smarter.scripts.initializedb:main
-      """,
-      )
+      """,)

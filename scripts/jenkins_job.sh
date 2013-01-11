8332e37ca03463fd8adbeb66af89273c6806a23c
Eugene Jen <ejen@ejen-mac.wgenhq.net>
2013-01-11 15:27:42 -0500

    just make the script runnable under jenkins server. I may generalize this a little bit so we can run client side code check before commit

--- scripts/jenkins_job.sh
+++ scripts/jenkins_job.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-export PROJ=edware
+export EDWARE=edware
 export EDWARE_GITHUB_URI=ssh://git@github.wgenhq.net/Ed-Ware-SBAC/edware
 
-if [ ! -d $HOME/prjs/$PROJ ]; then
-    mkdir -p $HOME/prjs/
-    cd $HOME/prjs
+if [ ! -d $HOME/workspace/repos/$EDWARE ]; then
+    mkdir -p $HOME/workspace/repos
+    cd $HOME/prjs/workspace/repos/$EDWARE
     git clone $EDWARE_GITHUB_URI
 fi
 
-if [ -d $HOME/prjs/$PROJ ]; then
+if [ -d $HOME/workspace/repos/$EDWARE ]; then
 
-    cd $HOME/prjs/$PROJ
+    cd $HOME/workspace/repos/$EDWARE
     git pull $EDWARE_GITHUB_URI
 
 # run the py.test --pep8
-cd $HOME/prjs/$PROJ
+cd $HOME/workspace/repos/$EDWARE
 find . -type f |grep ".py$" | xargs  py.test --pep8
 # run rhino jslint
 

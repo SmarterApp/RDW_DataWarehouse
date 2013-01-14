284fc37802d1e21534f24ad07348aac194099e40
Adam Oren <aoren@wgen.net>
2013-01-14 13:15:22 -0500

    US12152: new test config

---
+++ edapi/edapi/report_config_tests.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 14, 2013
+
+@author: aoren
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testTextLengthValidation(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test']
+    unittest.main()

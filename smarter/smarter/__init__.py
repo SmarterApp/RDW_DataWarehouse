06b3a62e0388389cd01eabc7be5b5df597d84483
Andrew Brien <abrien@wgen.net>
2013-01-14 15:49:49 -0500

    Adding 2 new pages (single student report, and the class list).  Also adding a few new javascript files: data tables, etc.

--- smarter/smarter/__init__.py
+++ smarter/smarter/__init__.py
@@ -27,7 +27,10 @@ def main(global_config, **settings):
     # splita's code
     config.add_route('comparepopulation', '/comparepopulation')
     config.add_route('getcomparepopulation', '/getcomparepopulation')
-    
     config.add_route('report', '/report/{name}')
+    # routing for individual student report
+    config.add_route('indiv_student', '/indiv_student_report')
+    #routing for class report
+    config.add_route('class_report', '/class_report')
     config.scan()
     return config.make_wsgi_app()
--- smarter/smarter/views.py
+++ smarter/smarter/views.py
@@ -133,4 +133,91 @@ def get_selection(request):
     report = request.matchdict['name']
     return {'result': sm.get_selector(sm, reportName=report)}
 
+# Individual Student Report
+@view_config(route_name='indiv_student', renderer='templates/reports/individual_student.pt')
+def individual_student_report(request):
+    id = request.params['id']
+
+    f_name = "First_Name"
+    m_name = "Middle_Name"
+    l_name = "Last_Name"
+
+    if id == "00001":
+        f_name = "Andrew"
+        m_name = "Murphy"
+        l_name = "Brien"
+    elif id == "00002":
+        f_name = "Seth"
+        m_name = "A"
+        l_name = "Wimberly"
+    elif id == "00003":
+        f_name = "Scott"
+        m_name = "B"
+        l_name = "MacGibbon"
+
+    return {'first_name': f_name, 'middle_name': m_name, 'last_name': l_name, 'assmt_name': 'English Language Arts', 'assmt_period': 'Fall 2012', 'claim_name_1': 'Composition', 'claim_score_1': 100, 'claim_name_2': 'Comprehension', 'claim_score_2': 90, 'claim_name_3': 'Grammar', 'claim_score_3': 80, 'claim_name_4': 'Vocabulary', 'claim_score_4': 70, 'assmt_ovr_score': 85}
+
+# Class Report
+@view_config(route_name='class_report', renderer='templates/reports/class.pt')
+def class_report(request):
+
+    """
+    json =
+    [
+        {
+            "middle_name": "SMITH",
+            "subject": "ELA",
+            "time_period": "BOY",
+            "assessment_score": 62.0,
+            "student_id": 2881,
+            "year_range": "2012-2013",
+            "first_name": "MARY"
+        },
+        {
+            "middle_name": "SMITH",
+            "subject": "MATH",
+            "time_period": "MOY",
+            "assessment_score": 67.0,
+            "student_id": 2881,
+            "year_range": "2012-2013",
+            "first_name": "MARY"
+        },
+        {
+            "middle_name": "SMITH",
+            "subject": "ELA",
+            "time_period": "EOY",
+            "assessment_score": 52.0,
+            "student_id": 2881,
+            "year_range": "2012-2013",
+            "first_name": "MARY"
+        },
+        {
+            "middle_name": "SMITH",
+            "subject": "MATH",
+            "time_period": "BOY",
+            "assessment_score": 64.0,
+            "student_id": 2881,
+            "year_range": "2013-2014",
+            "first_name": "MARY"
+        },
+        {
+            "middle_name": "SMITH",
+            "subject": "ELA",
+            "time_period": "MOY",
+            "assessment_score": 67.0,
+            "student_id": 2881,
+            "year_range": "2013-2014",
+            "first_name": "MARY"
+        },
+        {
+            "middle_name": "SMITH",
+            "subject": "ELA",
+            "time_period": "EOY",
+            "assessment_score": 62.0,
+            "student_id": 2881,
+            "year_range": "2013-2014",
+            "first_name": "MARY"
+        }
+    ]
+"""
 

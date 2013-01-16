bf471f71fe0c92cb6c973aa15af2721a0d737e99
Adam Oren <aoren@wgen.net>
2013-01-16 11:15:40 -0500

    S12152: comments

--- edapi/edapi/views.py
+++ edapi/edapi/views.py
@@ -3,35 +3,37 @@ Created on Jan 14, 2013
 
 @author: aoren
 '''
-from edapi.repository.report_config_repository import IReportConfigRepository
-from zope import component
 from edapi.utils import generate_report_config, generate_report
 from pyramid.httpexceptions import HTTPNotFound, HTTPPreconditionFailed
 
+# handle the OPTIONS verb for the report resource
 def get_report_config(request):
+    # gets the name of the report from the URL
     name = request.matchdict['name']
+    # find the report configuration in the repository
     report_config = generate_report_config(name)
+    # if we cannot find the report configuration in the repository, we return 404 error
     if (report_config is None):
         return HTTPNotFound()
     return report_config
 
 def generate_report_get(request):
-    Request = request
+    # gets the name of the report from the URL
     reportName = request.matchdict['name'] 
-    report_config = Request.GET
+    report_config = request.GET
     return generate_report(reportName, report_config)
 
 def generate_report_post(request):
+    # if the media type is not application/json, the request is rejected.
     if (request.content_type != 'application/json'):
         return HTTPNotFound()
-    Request = request 
     
     try:
         # basic check that it is a correct json, if not an exception will get raised when accessing json_body.
-        report_config = Request.json_body
+        report_config = request.json_body
         #break
     except:
         return HTTPPreconditionFailed()
-    
+    # gets the name of the report from the URL
     reportName = request.matchdict['name']
     return generate_report(reportName, report_config)

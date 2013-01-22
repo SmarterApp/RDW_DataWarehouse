14936adfe57a4dde6eaada2ca9a58eca4411f8d5
Scott MacGibbon <smacgibbon@wgen.net>
2013-01-22 09:34:07 -0500

    First draft 0.1 of schema generation.

--- sql/init/create_star.py
+++ sql/init/create_star.py
@@ -24,9 +24,9 @@ import os
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
-from sqlalchemy import create_engine, Sequence, Table, Column, BigInteger, Integer, String 
-from sqlalchemy import MetaData, ForeignKey, select
-from sqlalchemy.schema import CreateTable
+from sqlalchemy import create_engine, Sequence, Table, Column, Index
+from sqlalchemy import BigInteger, SmallInteger, String, Date
+from sqlalchemy import MetaData, ForeignKey
 
 __all__     = []
 __version__ = 0.1
@@ -46,6 +46,11 @@ class CLIError(Exception):
         return self.msg
     def __unicode__(self):
         return self.msg
+
+# Dump out all SQL statements (from http://www.sqlalchemy.org/trac/wiki/FAQ#HowcanIgettheCREATETABLEDROPTABLEoutputasastring)
+def dummySQLDump(sql, *multiparams, **params):
+    dummyEngine = create_engine(DBDRIVER + '://')
+    print(sql.compile(dialect = dummyEngine.dialect))
 
 def getDBConnectionStrings(userName, password, dbServer, databaseName):
     global DBDRIVER
@@ -87,24 +92,163 @@ def createTables(dbConnectionString, schemaName):
     engine      = create_engine(dbConnectionString)
 
     metadata    = MetaData(schema = schemaName)
-
-    users       = Table('users', metadata,
-                        Column('id',            BigInteger,     Sequence('user_id_seq'),    primary_key=True),
-                        Column('first_name',    String(128)),
-                        Column('last_name',     String(256)),
-                        )
-
-    addresses  = Table('addresses', metadata,
-                       Column('id',             Integer,        Sequence('address_iq_seq'), primary_key=True),
-                       Column('user_id',        None,           ForeignKey('users.id')),
-                       Column('email_address',  String(256),    nullable=False)  
-                       )
     
+    # For PR, Guam, US VI... etc
+    country     = Table('dim_country', metadata,
+                        Column('country_id',    String(2),                                  primary_key = True),
+                        Column('country',       String(64),     nullable = False),
+                        )
+    
+    Index('dim_country_idx', country.c.country_id, unique = True)
+
+    # Two-letter state - some countries have 3 or more, but two will do for US
+    state_prov  = Table('dim_state', metadata,
+                        Column('state_id',      String(2),                                  primary_key = True),
+                        Column('state',         String(32),     nullable = False)
+                        )
+    
+    Index('dim_state_idx', state_prov.c.state_id, unique = True)
+
+    schools     = Table('dim_school', metadata,
+                        Column('school_id',     BigInteger,     Sequence('school_id_seq'),  primary_key = True),
+                        Column('things',        String(256)),
+                        )
+    
+    Index('dim_school_idx', schools.c.school_id, unique = True)
+    
+    where_taken = Table('dim_where_taken', metadata,
+                        Column('place_id',      BigInteger,     Sequence('place_id_seq'),   primary_key = True),
+                        Column('address_1',     String(32),     nullable = False),
+                        Column('address_2',     String(32),     nullable = True),
+                        Column('address_3',     String(32),     nullable = True),
+                        Column('city',          String(32),     nullable = False),
+                        Column('state',         None,           ForeignKey('dim_state.state_id')),
+                        Column('country',       None,           ForeignKey('dim_country.country_id'))
+                        )
+    
+    Index('dim_where_taken_idx', where_taken.c.place_id, unique = True)
+    
+    grades      = Table('dim_grade', metadata,
+                        Column('grade_id',      String(2),      primary_key = True),
+                        Column('grade_desc',    String(32)),
+                        )
+    
+    Index('dim_grade_idx', grades.c.grade_id, unique = True)
+    
+    classes     = Table('dim_class', metadata,
+                        Column('class_id',      BigInteger,     Sequence('class_id_seq'),   primary_key = True),
+                        Column('things',        String(128)),
+                        )
+    
+    Index('dim_class_idx', classes.c.class_id, unique = True)
+    
+    sections    = Table('dim_section', metadata,
+                        Column('section_id',    BigInteger,     Sequence('section_id_seq'), primary_key = True),
+                        Column('things',        String(128)),
+                        Column('class_id',      None,           ForeignKey('dim_class.class_id')),
+                        )
+    
+    Index('dim_section_idx', sections.c.section_id, unique = True)
+    
+    students    = Table('dim_student', metadata,
+                        Column('student_id',    BigInteger,     Sequence('student_id_seq'), primary_key = True),
+                        Column('school_id',     None,           ForeignKey('dim_school.school_id')),
+                        # Teacher?
+                        )
+    
+    Index('dim_student_idx', students.c.student_id, unique = True)
+    
+    parents     = Table('dim_parent', metadata,
+                        Column('parent_uniq_id',BigInteger,     Sequence('parent_uniq_id_seq'), primary_key = True),
+                        Column('parent_id',     BigInteger,     nullable = False),
+                        Column('first_name',    String(128),    nullable = False),
+                        Column('last_name',     String(256),    nullable = False),
+                        Column('student_id',    None,           ForeignKey('dim_student.student_id')),
+                        )
+    
+    Index('dim_parent_uniq_idx',parents.c.parent_uniq_id,   unique = True)
+    Index('dim_parent_id_idx',  parents.c.parent_id,        unique = False)
+    Index('dim_parent_student_idx', parents.c.parent_id, parents.c.student_id, unique = True)
+    
+    stdnt_tmp   = Table('dim_stdnt_tmprl_data', metadata,
+                        Column('stdnt_tmprl_id',BigInteger,     Sequence('sdnt_tmprl_seq'), primary_key = True),
+                        Column('student_id',    None,           ForeignKey('dim_student.student_id')),
+                        Column('effective_date',Date,           nullable = False),
+                        Column('end_date',      Date,           nullable = False),
+                        Column('grade_id',      None,           ForeignKey('dim_grade.grade_id')),
+                        Column('school_id',     None,           ForeignKey('dim_school.school_id')),
+                        Column('class_id',      None,           ForeignKey('dim_class.class_id')),
+                        Column('section_id',    None,           ForeignKey('dim_section.section_id'))
+                        )
+    
+    Index('dim_stdnt_tmprl_data_idx', stdnt_tmp.c.stdnt_tmprl_id,  unique = True)
+    
+    assessment_type = Table(
+                        'dim_asmt_type',   metadata,
+                        Column('asmt_type_id',  BigInteger,     Sequence('asmt_type_seq'),  primary_key = True),
+                        Column('asmt_subject',  String(16),     nullable = False),
+                        Column('asmt_type',     String(16),     nullable = False),
+                        Column('asmt_period',   String(16),     nullable = False),
+                        Column('asmt_version',  String(16),     nullable = False),
+                        Column('asmt_grade',    None,           ForeignKey('dim_grade.grade_id'))
+                            )
+    
+    Index('dim_asmt_type_idx', assessment_type.c.asmt_type_id, unique = True)
+                
+    assessment_outcome = Table(
+                               'fact_asmt_outcome', metadata,
+                        Column('asmnt_outcome_id',  BigInteger, Sequence('assmt_outcome_seq'),  primary_key = True),
+                        Column('asmt_type_id',      None,       ForeignKey('dim_asmt_type.asmt_type_id')),
+                        Column('student_id',        None,       ForeignKey('dim_student.student_id')),
+                        Column('stdnt_tmprl_id',    None,       ForeignKey('dim_stdnt_tmprl_data.stdnt_tmprl_id')),
+                        Column('date_taken',        Date,       nullable = False),
+                        Column('date_taken_day',    SmallInteger,   nullable = False),
+                        Column('date_taken_month',  SmallInteger,   nullable = False),
+                        Column('date_taken_year',   SmallInteger,   nullable = False),
+                        Column('where_taken_id',    None,       ForeignKey('dim_where_taken.place_id')),
+                        Column('asmt_score',        SmallInteger,   nullable = False),
+                        Column('asmt_claim_1_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_2_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_3_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_4_score',SmallInteger,   nullable = True),
+                        Column('asmt_create_date',  Date,       nullable = False),
+                        )
+    
+    Index('fact_asmt_outcome_idx', assessment_outcome.c.asmnt_outcome_id, unique = True)
+    
+    # Clone the assessment outcome, and add some columns
+    # assessment_outcome_hist = assessment_outcome.name('hist_fact_asmt_outcome')
+    # assessment_outcome_hist.append_column('hist_create_date', Date, nullable = False)
+    
+    hist_assessment_outcome = Table(
+                               'hist_asmt_outcome', metadata,
+                        Column('asmnt_outcome_id',  BigInteger, primary_key = True), # NOTE sequence deleted
+                        Column('asmt_type_id',      None,       ForeignKey('dim_asmt_type.asmt_type_id')),
+                        Column('student_id',        None,       ForeignKey('dim_student.student_id')),
+                        Column('stdnt_tmprl_id',    None,       ForeignKey('dim_stdnt_tmprl_data.stdnt_tmprl_id')),
+                        Column('date_taken',        Date,           nullable = False),
+                        Column('date_taken_day',    SmallInteger,   nullable = False),
+                        Column('date_taken_month',  SmallInteger,   nullable = False),
+                        Column('date_taken_year',   SmallInteger,   nullable = False),
+                        Column('where_taken_id',    None,       ForeignKey('dim_where_taken.place_id')),
+                        Column('asmt_score',        SmallInteger,   nullable = False),
+                        Column('asmt_claim_1_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_2_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_3_score',SmallInteger,   nullable = True),
+                        Column('asmt_claim_4_score',SmallInteger,   nullable = True),
+                        Column('asmt_create_date',  Date,           nullable = False),
+                        #
+                        Column('hist_create_date',  Date,           nullable = False),
+                        )
+    
+    Index('hist_asmt_outcome_idx', hist_assessment_outcome.c.asmnt_outcome_id, unique = True)
+
     metadata.create_all(engine)
     
-    print(CreateTable(users))
-    print(CreateTable(addresses))
-    
+    if VERBOSE:
+        dummyEngine = create_engine(DBDRIVER + '://', strategy='mock', executor = dummySQLDump)
+        metadata.create_all(dummyEngine, checkfirst=False)
+
     return
 
 def main(argv=None): # IGNORE:C0111
@@ -125,7 +269,7 @@ def main(argv=None): # IGNORE:C0111
     program_shortdesc       = __import__('__main__').__doc__.split("\n")[1]
     program_license         = '''%s
 
-  Created by smacgibbon on %s.
+  Created by Wireless Generation (a division of Amplify) on %s.
   Copyright 2013 Wireless Generation. All rights reserved.
   
   Licensed under the Apache License 2.0

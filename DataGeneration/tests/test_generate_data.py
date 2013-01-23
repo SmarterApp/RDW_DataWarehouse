f064380ef5addb508a0bb7097275d2a55488ee0a
lichen <lichen@wgen.net>
2013-01-23 14:11:29 -0500

    add unit test cases

--- DataGeneration/tests/test_generate_data.py
+++ DataGeneration/tests/test_generate_data.py
@@ -1,6 +1,7 @@
-import unittest
+import unittest, math
 import generate_data
-from entities import Student, School
+from entities import Student
+from generate_data import subjects
 
 class TestGenerateData(unittest.TestCase):
     
@@ -161,8 +162,7 @@ class TestGenerateData(unittest.TestCase):
         print(sequence)
         print(gen_seq)
         
-        
-        
+            
     def test_list_to_chucks(self):
         list1 = [9, 8, 62, 123, 345, 1, 2, 98 ]
         size1 = 3
@@ -225,10 +225,71 @@ class TestGenerateData(unittest.TestCase):
         stu_tea_ratio = round(len(stu_list) / len(tea_list))  
         
         expected_classes = generate_data.create_classes_for_grade(stu_list, tea_list, stu_tea_ratio)
+        
+        devided = len(expected_classes) / len(generate_data.subjects)
+        first_part = devided * (len(generate_data.subjects) - 1)
         for i in range(len(expected_classes)):
-            if(i < len(expected_classes) / 2):
-                self.assertEqual(expected_classes[i].title, generate_data.subjects[0] + " " + str(i))
+            if(i < first_part):
+                self.assertEqual(expected_classes[i].title, generate_data.subjects[math.floor((int)(i / devided))] + " " + str((int)(i % devided)))
+            else:
+                self.assertEqual(expected_classes[i].title, generate_data.subjects[len(generate_data.subjects) - 1] + " " + str((int)(i % devided)))
 
+
+    def test_create_classes_for_grade_samllstudents(self):
+        stu_num = 20
+        stu_list = make_stus_or_teas(stu_num)
+        tea_list = make_stus_or_teas(1)
+        stu_tea_ratio = round(len(stu_list) / len(tea_list))  
+        
+        expected_classes = generate_data.create_classes_for_grade(stu_list, tea_list, stu_tea_ratio)
+        self.assertEqual(len(expected_classes), len(generate_data.subjects))
+        for i in range(len(expected_classes)):
+            self.assertEqual(expected_classes[i].title, subjects[i] + " " + str(0))
+    
+    
+    def test_create_one_class_severalsections(self):
+        sub_name = "Math"
+        class_count = 2
+        distribute_stu_inaclass = make_stus_or_teas(45)
+        tea_list = make_stus_or_teas(10)
+        stu_tea_ratio = 15
+        
+        expected_class = generate_data.create_one_class(sub_name, class_count, distribute_stu_inaclass, tea_list, stu_tea_ratio)
+        expected_sec_num = math.floor(45 // stu_tea_ratio)
+        
+        self.assertEqual(expected_class.sub_name, sub_name)
+        self.assertEqual(expected_class.title, sub_name + " " + str(class_count))
+        self.assertEqual(len(expected_class.section_stu_map), expected_sec_num)
+       
+        for key, value in expected_class.section_stu_map.items():
+            self.assertEqual(len(value), 15)
+        
+        self.assertEqual(len(expected_class.section_tea_map), expected_sec_num)
+        for key, value in expected_class.section_tea_map.items():
+            self.assertEqual(len(value), 1)
+
+    def test_create_one_class_onesection(self):
+        sub_name = "Math"
+        class_count = 2
+        distribute_stu_inaclass = make_stus_or_teas(45)
+        tea_list = make_stus_or_teas(5)
+        stu_tea_ratio = 78
+        
+        expected_class = generate_data.create_one_class(sub_name, class_count, distribute_stu_inaclass, tea_list, stu_tea_ratio)
+        expected_sec_num = 1
+        
+        self.assertEqual(expected_class.sub_name, sub_name)
+        self.assertEqual(expected_class.title, sub_name + " " + str(class_count))
+        self.assertEqual(len(expected_class.section_stu_map), expected_sec_num)
+       
+        for key, value in expected_class.section_stu_map.items():
+            self.assertEqual(len(value), 45)
+        
+        self.assertEqual(len(expected_class.section_tea_map), expected_sec_num)
+        for key, value in expected_class.section_tea_map.items():
+            self.assertEqual(len(value), 1)
+         
+        
 def make_stus_or_teas(count):
     student_list = []
     while(count > 0):

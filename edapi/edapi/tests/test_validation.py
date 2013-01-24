f4483f4599fdeee33eebf6749c125e4d7d77a8ff
Adam Oren <aoren@wgen.net>
2013-01-24 09:59:09 -0500

    US12607: added 3 unit tests for validation

--- edapi/edapi/tests/test_validation.py
+++ edapi/edapi/tests/test_validation.py
@@ -4,11 +4,12 @@ Created on Jan 14, 2013
 @author: aoren
 '''
 from edapi.tests.test_reports import TestReport
-from edapi import utils
+from edapi import utils, add_report_config
 from unittest.mock import MagicMock
 import unittest
 from edapi.utils import Validator
 from edapi.exceptions import InvalidParameterError
+from edapi.tests.dummy import Dummy
  
 class TestReportConfig(unittest.TestCase):
 
@@ -33,20 +34,44 @@ class TestReportConfig(unittest.TestCase):
         validator.validate_params_schema = MagicMock(return_value=(False, None))
         validator.fix_types = MagicMock(return_value=(False, None))
         self.assertRaises(InvalidParameterError, utils.generate_report, registry, "test", params, validator)
-        
-    def test_validate_params(self):
-        params = {}
-        params_config = {}
+
+    def test_validate_integer_param(self):
+        report_name = "test"
+        config = {"id": {"type": "integer","required": True}}
+        registry = {}
+        registry[report_name] = { "params": config, "reference" : (Dummy,  Dummy.some_func)}
+        params = {"id" : 123}
         validator = Validator()
-        validator.validate_params(params_config, "report_name", params)
+        response = validator.validate_params_schema(registry, report_name, params)
+        self.assertEqual(response[0], True)
+        
+        pass
+    
+    def test_validate_regex_param(self):
+        report_name = "test"
+        config = {"id": {"type" : "string","pattern" : "^[a-z]$","required": True}}
+        registry = {}
+        registry[report_name] = { "params": config, "reference" : (Dummy,  Dummy.some_func)}
+        params = {"id" : "a"}
+        validator = Validator()
+        response = validator.validate_params_schema(registry, report_name, params)
+        self.assertEqual(response[0], True)
+        
         pass
         
-    def test_validate_string_param(self):
-        params = {}
-        params_config = {}
+    def test_invalidate_regex_param(self):
+        report_name = "test"
+        config = {"id": {"type" : "string","pattern" : "^[a-z]$","required": True}}
+        registry = {}
+        registry[report_name] = { "params": config, "reference" : (Dummy,  Dummy.some_func)}
+        params = {"id" : "1"}
         validator = Validator()
-        validator.validate_params(params_config, "report_name", params)
+        response = validator.validate_params_schema(registry, report_name, params)
+        self.assertIn("does not match regular expression", str(response[1]))
+        self.assertEqual(response[0], False)
+        
         pass
+            
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test']

101f9b3b863246f46ac0c91cb2f3c5dcf84b720b
Doris Ip <dip@wgen.net>
2013-01-25 13:55:46 -0500

    last pep8 fixes

--- smarter/smarter/reports/list_of_students_report.py
+++ smarter/smarter/reports/list_of_students_report.py
@@ -12,29 +12,29 @@ from sqlalchemy.sql.expression import func
 
 
 @report_config(name="list_of_students",
-               params={"districtId": {
-                                      "type": "integer",
-                                      "required": True
-                                      },
-                       "schoolId": {
-                                   "type": "integer",
-                                   "required": True
-                                   },
-                       "asmtGrade": {
-                                    "type": "integer",
-                                    "required": True
-                                    },
-                       "asmtSubject": {
-                                      "type": "array",
-                                      "required": False,
-                                      "minLength": 0,
-                                      "maxLength": 2,
-                                      "items":{
-                                               "type": "string"
-                                               }
-                                      }
-                       }
-               )
+            params={
+                "districtId": {
+                    "type": "integer",
+                    "required": True
+                },
+                "schoolId": {
+                    "type": "integer",
+                    "required": True
+                },
+                "asmtGrade": {
+                    "type": "integer",
+                    "required": True
+                },
+                "asmtSubject": {
+                    "type": "array",
+                    "required": False,
+                    "minLength": 0,
+                    "maxLength": 2,
+                    "items": {
+                        "type": "string"
+                    }
+                }
+            })
 def get_list_of_students_report(params, connector=None):
 
     # if connector is not supplied, use DBConnector
@@ -45,7 +45,7 @@ def get_list_of_students_report(params, connector=None):
     schoolId = params['schoolId']
     asmtGrade = params['asmtGrade']
 
-    # asmtSubject is optional. 
+    # asmtSubject is optional.
     # TODO: make sure get it as an array
     asmtSubject = None
     if 'asmtSubject' in params:
@@ -53,8 +53,6 @@ def get_list_of_students_report(params, connector=None):
 
     # get sql session
     connector.open_session()
-
-
     '''
     Output:
     Student last name
@@ -73,23 +71,21 @@ def get_list_of_students_report(params, connector=None):
 
     #TODO: find out where can we get enrollment grade
     #TODO: missing dim_teacher from the DB
-
-
     #I use label function as experimental.  to eliminate ambiguous column namez
     if isinstance(dim_student, Table) and isinstance(dim_stdnt_tmprl_data, Table) and isinstance(dim_grade, Table) and isinstance(fact_asmt_outcome, Table) and isinstance(dim_asmt_type, Table):
         query = Query([dim_student.c.first_name.label('first_name'),
-                            func.substr(dim_student.c.middle_name,1,1).label('middle_name'),
-                            dim_student.c.last_name.label('last_name'),
-                            dim_asmt_type.c.asmt_grade.label('asmt_grade'),
-                            dim_asmt_type.c.asmt_subject.label('asmt_subject'),
-                            fact_asmt_outcome.c.asmt_score.label('asmt_score'),
-                            fact_asmt_outcome.c.asmt_claim_1_name.label('asmt_claim_1_name'),
-                            fact_asmt_outcome.c.asmt_claim_2_name.label('asmt_claim_2_name'),
-                            fact_asmt_outcome.c.asmt_claim_3_name.label('asmt_claim_3_name'),
-                            fact_asmt_outcome.c.asmt_claim_4_name.label('asmt_claim_4_name'),
-                            fact_asmt_outcome.c.asmt_claim_1_score.label('asmt_claim_1_score'),
-                            fact_asmt_outcome.c.asmt_claim_2_score.label('asmt_claim_2_score'),
-                            fact_asmt_outcome.c.asmt_claim_3_score.label('asmt_claim_3_score'),
-                            fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score')])\
-                            .join(dim_student, dim_student.c.student_id == fact_asmt_outcome.c.student_id)\
-                            .join(dim_asmt_type, dim_asmt_type.c.asmt_type_id == fact_asmt_outcome.c.asmt_type_id)
+                    func.substr(dim_student.c.middle_name, 1, 1).label('middle_name'),
+                    dim_student.c.last_name.label('last_name'),
+                    dim_asmt_type.c.asmt_grade.label('asmt_grade'),
+                    dim_asmt_type.c.asmt_subject.label('asmt_subject'),
+                    fact_asmt_outcome.c.asmt_score.label('asmt_score'),
+                    fact_asmt_outcome.c.asmt_claim_1_name.label('asmt_claim_1_name'),
+                    fact_asmt_outcome.c.asmt_claim_2_name.label('asmt_claim_2_name'),
+                    fact_asmt_outcome.c.asmt_claim_3_name.label('asmt_claim_3_name'),
+                    fact_asmt_outcome.c.asmt_claim_4_name.label('asmt_claim_4_name'),
+                    fact_asmt_outcome.c.asmt_claim_1_score.label('asmt_claim_1_score'),
+                    fact_asmt_outcome.c.asmt_claim_2_score.label('asmt_claim_2_score'),
+                    fact_asmt_outcome.c.asmt_claim_3_score.label('asmt_claim_3_score'),
+                    fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score')])\
+            .join(dim_student, dim_student.c.student_id == fact_asmt_outcome.c.student_id)\
+            .join(dim_asmt_type, dim_asmt_type.c.asmt_type_id == fact_asmt_outcome.c.asmt_type_id)

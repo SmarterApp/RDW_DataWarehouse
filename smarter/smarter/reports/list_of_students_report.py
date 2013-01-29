03a096b94dfa5a8e5b6fc1dcf5e835cbafbe5c81
Takashi Osako <tosako@wgen.net>
2013-01-29 15:42:56 -0500

    US12499: list of Students SQL Query

--- smarter/smarter/reports/list_of_students_report.py
+++ smarter/smarter/reports/list_of_students_report.py
@@ -5,11 +5,11 @@ Created on Jan 24, 2013
 '''
 
 from edapi.utils import report_config
-from sqlalchemy.orm.query import Query
 from sqlalchemy.schema import Table
 from sqlalchemy.sql.expression import func
 from database.connector import DBConnector
-
+from sqlalchemy.sql import select
+from sqlalchemy.sql import and_
 
 @report_config(
     name="list_of_students",
@@ -23,14 +23,13 @@ from database.connector import DBConnector
             "required": True
         },
         "asmtGrade": {
-            "type": "integer",
+            "type": "string",
+            "maxLength": 2,
             "required": True
         },
         "asmtSubject": {
             "type": "array",
             "required": False,
-            "minLength": 0,
-            "maxLength": 2,
             "items": {
                 "type": "string"
             }
@@ -52,7 +51,7 @@ def get_list_of_students_report(params, connector=None):
         asmtSubject = params['asmtSubject']
 
     # get sql session
-    connector.open_session()
+    connector.open_connection()
     '''
     Output:
     Student last name
@@ -69,10 +68,36 @@ def get_list_of_students_report(params, connector=None):
     fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
     dim_asmt_type = connector.get_table('dim_asmt_type')
 
+    query = None
     #TODO: find out where can we get enrollment grade
     #TODO: missing dim_teacher from the DB
     #I use label function as experimental.  to eliminate ambiguous column namez
     if isinstance(dim_student, Table) and isinstance(dim_stdnt_tmprl_data, Table) and isinstance(dim_grade, Table) and isinstance(fact_asmt_outcome, Table) and isinstance(dim_asmt_type, Table):
+        query = select([dim_student.c.first_name.label('first_name'),
+                    func.substr(dim_student.c.middle_name, 1, 1).label('middle_name'),
+                    dim_student.c.last_name.label('last_name'),
+                    dim_asmt_type.c.asmt_grade.label('asmt_grade'),
+                    dim_asmt_type.c.asmt_subject.label('asmt_subject'),
+                    fact_asmt_outcome.c.asmt_score.label('asmt_score'),
+                    fact_asmt_outcome.c.asmt_claim_1_name.label('asmt_claim_1_name'),
+                    fact_asmt_outcome.c.asmt_claim_2_name.label('asmt_claim_2_name'),
+                    fact_asmt_outcome.c.asmt_claim_3_name.label('asmt_claim_3_name'),
+                    fact_asmt_outcome.c.asmt_claim_4_name.label('asmt_claim_4_name'),
+                    fact_asmt_outcome.c.asmt_claim_1_score.label('asmt_claim_1_score'),
+                    fact_asmt_outcome.c.asmt_claim_2_score.label('asmt_claim_2_score'),
+                    fact_asmt_outcome.c.asmt_claim_3_score.label('asmt_claim_3_score'),
+                    fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score')], 
+                       from_obj=[dim_student\
+                                  .join(fact_asmt_outcome,dim_student.c.student_id == fact_asmt_outcome.c.student_id)\
+                                  .join(dim_asmt_type, dim_asmt_type.c.asmt_type_id == fact_asmt_outcome.c.asmt_type_id)\
+                                  .join(dim_stdnt_tmprl_data, dim_stdnt_tmprl_data.c.student_id == dim_student.c.student_id)])\
+                    .where(dim_stdnt_tmprl_data.c.school_id == schoolId).where(and_(dim_asmt_type.c.asmt_grade == asmtGrade))
+                    
+        if asmtSubject is not None:
+            query.wher(dim_grade.c.asmt_subject.in_(asmtSubject))
+       
+
+        '''
         query = Query([dim_student.c.first_name.label('first_name'),
                     func.substr(dim_student.c.middle_name, 1, 1).label('middle_name'),
                     dim_student.c.last_name.label('last_name'),
@@ -89,3 +114,8 @@ def get_list_of_students_report(params, connector=None):
                     fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score')])\
             .join(dim_student, dim_student.c.student_id == fact_asmt_outcome.c.student_id)\
             .join(dim_asmt_type, dim_asmt_type.c.asmt_type_id == fact_asmt_outcome.c.asmt_type_id)
+        '''
+    result = connector.get_result(query)
+    connector.close_connection()
+
+    return result

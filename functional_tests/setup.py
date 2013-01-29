4e344361c5fd71465975ac1850a2861ca2d510ac
Doris Ip <dip@wgen.net>
2013-01-29 15:35:06 -0500

    remove unnecessary stuff from setup.py

--- functional_tests/setup.py
+++ functional_tests/setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 install_requires = [
     'behave',  # BDD tool

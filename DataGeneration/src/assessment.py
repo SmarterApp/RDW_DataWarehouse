ea3b095a746cbd64745899d0585239a58cba65a3
Seth Wimberly <swimberly@wgen.net>
2013-01-30 13:59:13 -0500

    Adding some method documentation

--- DataGeneration/src/assessment.py
+++ DataGeneration/src/assessment.py
@@ -20,6 +20,8 @@ def generate_assmts_for_students(total, grade, state):
     total -- integer number of students
     grade -- integer for the grade to generate scores
     state -- current state
+    Returns a dictionary that contains 12 items where the key is a string: '<year>_<type>_<period>'
+    and the value is a list of score objects
     '''
     year = date.today().year
     periods = ['BOY', 'MOY', 'EOY']
@@ -35,6 +37,8 @@ def generate_assmts_for_students(total, grade, state):
                 string = "%s_%s_%s" % (year, subject, period)
                 scores[string] = score
 
+    return scores
+
 
 def generate_assmt_scores(state, asmt_type, year, period, grade, total):
     '''
@@ -43,16 +47,16 @@ def generate_assmt_scores(state, asmt_type, year, period, grade, total):
     # validate parameters
     if(total <= 0 or (int)(grade) < 0 or (int)(grade) > 12):
         return []
-    
+
     # change the grade to get data
     if(0 <= (int)(grade) <= 6):
         grade = '4'
     else:
         grade = '8'
-        
+
     # get statistical data. Average score, standard deviation, and four percentage numbers of levels
     stat_avg, stat_sd, stat_levles = get_stat_data(state, asmt_type, year, period, grade)
-    
+
     # generate list
     overallscore_list = []
     socre_withclaims_list = []
@@ -61,20 +65,21 @@ def generate_assmt_scores(state, asmt_type, year, period, grade, total):
         overallscore_list = py1.makeup_core(stat_avg, stat_sd, MIN_ASSMT_SCORE, MAX_ASSMT_SCORE, total)
         # generate scores for claims
         socre_withclaims_list = generate_allscores(overallscore_list, stat_levles, asmt_type, grade)
-        
+
     # print("Total number of ", len(socre_withclaims_list), " scores are generated")
     return socre_withclaims_list
+
 
 def get_stat_data(state, asmt_type, year, period, grade):
     '''
     Main function to get statistical assessment data from database
     '''
-        
+
     # connect to db
     db = get_db_conn()
     query = "select * from assmt_raw_stat where state = '" + state + "' and subject = '" + asmt_type + "' and year = '" + year + "' and grade = '" + grade + "'"
     row = db.prepare(query)
-    
+
     stat_avg = None
     stat_sd = None
     stat_levles = None
@@ -82,19 +87,20 @@ def get_stat_data(state, asmt_type, year, period, grade):
         stat_avg = r[4]
         stat_sd = r[6]
         stat_levles = [r[8], r[10], r[12], r[14]]
-        
+
     db.close()
     print("Close db connection")
-    
+
     return stat_avg, stat_sd, stat_levles
-    
+
+
 def generate_allscores(score_list, levels, asmt_type, grade):
     scores = []
     if(score_list is not None and len(score_list) > 0):
         score_list.sort()
         # generate absolute count for each of levels
         level_count = perc_to_count(levels, len(score_list))
-        
+
         start = 0
         for l in range(len(level_count)):
             end = start + level_count[l]
@@ -103,11 +109,12 @@ def generate_allscores(score_list, levels, asmt_type, grade):
                 claims_score = generate_claims(total_score, asmt_type, grade)
                 scores.append(Score(total_score, claims_score, l))
             start = end
-    
+
     scores_list = list(scores)
     random.shuffle(scores_list)
-    
+
     return scores_list
+
 
 def generate_claims(total_score, asmt_type, grade):
     claim_scores = {}
@@ -119,8 +126,9 @@ def generate_claims(total_score, asmt_type, grade):
                 c_sores = perc_to_count(ass_grade.get('claim_percs'), total_score)
                 claim_scores = dict(zip(ass_grade.get('claim_names'), c_sores))
 
-    return claim_scores 
-   
+    return claim_scores
+
+
 def perc_to_count(perc, total):
     count = []
     if(total > 0):

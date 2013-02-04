978afeccb5da90889689e8a2ccdbd6326f510668
Adam Oren <aoren@wgen.net>
2013-02-04 15:36:17 -0500

    US12667: added properties to FT

---
+++ functional_tests/frontend/test_base.py
@@ -0,0 +1,29 @@
+'''
+Created on Feb 4, 2013
+
+@author: aoren
+'''
+import unittest
+from selenium import webdriver
+import configparser
+import os
+
+
+class EdTestBase(unittest.TestCase):
+
+    config = configparser.ConfigParser()
+    test_config_path = os.getcwd() + '../test.ini'
+    config.read(test_config_path)
+
+    driver_name = config['DEFAULT']['driver']
+
+    def get_driver(self):
+        if self.driver_name == 'firefox':
+            return webdriver.Firefox()
+        elif self.driver_name == 'chrome':
+            return webdriver.ChromeOptions()
+        else:
+            return webdriver.Firefox()
+
+    def default_config(self):
+        return self.config['DEFAULT']

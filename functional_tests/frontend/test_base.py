ddb131c62f85c060521a84355a721fb21bca6641
Adam Oren <aoren@wgen.net>
2013-02-04 15:56:39 -0500

    US12667: fixed path

--- functional_tests/frontend/test_base.py
+++ functional_tests/frontend/test_base.py
@@ -12,7 +12,11 @@ import os
 class EdTestBase(unittest.TestCase):
 
     config = configparser.ConfigParser()
-    test_config_path = os.getcwd() + '../test.ini'
+    test_config_path = os.getcwd() + '/../../test.ini'
+
+    if not os.path.exists(test_config_path):
+        raise IOError(test_config_path)
+
     config.read(test_config_path)
 
     driver_name = config['DEFAULT']['driver']

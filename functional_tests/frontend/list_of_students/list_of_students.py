e0138cf6a46be04753d5f84667752e24ff512b06
Nidhi Paroha <nparoha@wgen.net>
2013-02-05 11:37:52 -0500

    Updating the latest version of list of student functional tests

--- functional_tests/frontend/list_of_students/list_of_students.py
+++ functional_tests/frontend/list_of_students/list_of_students.py
@@ -1,7 +1,12 @@
+'''
+Created on Feb 5, 2013
+
+@author: nparoha
+'''
 import unittest
-import time
 from selenium import webdriver
-from test.test_base import EdTestBase
+from frontend.test_base import EdTestBase
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class ListOfStudents (EdTestBase):
@@ -10,11 +15,12 @@ class ListOfStudents (EdTestBase):
     ''' test_open_website: Open webpage '''
     def setUp(self):
         self.driver = webdriver.Firefox()
-        time.sleep(5)
         self.driver.get("http://localhost:6543/assets/html/gridDemo.html#")
-        time.sleep(10)
+        try:
+            WebDriverWait(self.driver, 10).until(lambda driver: self.driver.find_element_by_id("gbox_gridTable"))
+        except:
+            raise AssertionError("Web page did not load correctly.")
         print("Opened web page")
-        # TODO: Check with isDisplayed() function
 
     ### test_page_headers: Test page headers: Logo, User and Log out ###
     def test_page_headers(self):
@@ -52,19 +58,55 @@ class ListOfStudents (EdTestBase):
 
     ### test_grid_headers_row1: Validate the grid has subject headers: Math and ELA ###
     def test_grid_headers_row1(self):
-        pass
+        print("Test Case: LOS: Validate that the subject headers are displayed on the grid")
+        grid = self.driver.find_element_by_class_name("ui-jqgrid-hbox")
+        grid_header = grid.find_element_by_class_name("jqg-second-row-header").find_elements_by_class_name("ui-th-column-header")
+        header_list = []
+        for each in grid_header:
+            header_list.append(each.text)
+        assert "Math" in header_list, "Math not displayed in the grid header"
+        print("Passed Scenario: Math subject header found.")
+        assert "ELA" in header_list, "Math not displayed in the grid header"
+        print("Passed Scenario: ELA subject header found.")
+        assert header_list.index("Math") < header_list.index("ELA"), "Math and ELA appear in an incorrect order"
+        print("Passed Scenario: Math and ELA appear in the correct order")
 
     ### test_grid_headers_row2: Validate that the column headers: Student Name, Grade, Teacher, Measures  are displayed ###
+    def test_grid_headers_row2(self):
+        print("Test Case: LOS: Validate that the grid headers are displayed correctly")
+        grid_header = self.driver.find_elements_by_class_name("jqg-third-row-header")
+
+        self._helper_match_headers(grid_header, "gridTable_student_full_name", "Student")
+        self._helper_match_headers(grid_header, "gridTable_enrollment_grade", "Grade")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.teacher_full_name", "Teacher")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_grade", "Grade")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_score", "Overall")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_claim_1_score", "Concepts and Procedures")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_claim_2_score", "Problem Solving")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_claim_3_score", "Communicating Reasoning")
+        self._helper_match_headers(grid_header, "gridTable_assessments.MATH.asmt_claim_4_score", "Modeling and Data Analysis")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.teacher_full_name", "Teacher")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_grade", "Grade")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_score", "Overall")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_claim_1_score", "Reading")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_claim_2_score", "Writing")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_claim_3_score", "Speaking & Listening")
+        self._helper_match_headers(grid_header, "gridTable_assessments.ELA.asmt_claim_4_score", "Research & Inquiry")
+
+    ###_helper_match_headers: Helper function to validate the grid header###
+    def _helper_match_headers(self, grid_header, key, value):
+        header_list = grid_header
+        key = key
+        value = value
+        for each in header_list:
+            assert each.find_element_by_id(key).text == value, ("Header ' %s ' not found on the page") % value
+            print("Passed Scenario: Header '" + value + "' found")
 
     ### test_grid_contents: Validate total number of students displayed on the page ###
 
-    ### test_student1_data: Validate student 1's data ###
-
     ### test_grid_headers_row2_sortable: Validate the default sorting and that the following headers are sortable: LastName and each of the measures ###
-    # ## test_student2_data: Validate student n/2's data ### (n is total # of students on the page)
-    ### test_student3_data: Validate student n's data ###
 
-    # # test_footer: Validate that footer is displayed and the text is valid ##
+    ### test_footer: Validate that footer is displayed and the text is valid ###
     def test_footer(self):
         print("Test Case: LOS: Validate page footer")
         footer = self.driver.find_element_by_id("footer")

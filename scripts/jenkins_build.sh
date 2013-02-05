33179539345a764e6765b180de6685bfde9a92ba
Doris Ip <dip@wgen.net>
2013-02-05 16:46:06 -0500

    add export display env

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -139,6 +139,7 @@ function run_functional_tests {
     cd "$WORKSPACE/functional_tests"
 
     sed -i.bak 's/port = 6543/port = 80/g' test.ini
+    export DISPLAY=:6.0
 
     nosetests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
 

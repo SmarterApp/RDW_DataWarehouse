1a61ef720c3093808a19df743546adee2e44f1d9
lichen <lichen@wgen.net>
2013-02-06 10:12:25 -0500

    change school type generation

--- DataGeneration/tests/test_assessment.py
+++ DataGeneration/tests/test_assessment.py
@@ -41,6 +41,39 @@ class TestAssessment(unittest.TestCase):
             self.assertEqual(calculate_overall, score.overall)
             self.assertEqual(sorted(score.claims.keys()), sorted(expected_claimnames))
 
+    # test generate_allscores()
+    def test_generate_allscores(self):
+        score_list = [387, 381, 276, 488, 123, 298]
+        levels = []
+        asmt_type = 'ELA'
+        grade = '4'
+
+        generated_allscores = assessment.generate_allscores(score_list, levels, asmt_type, grade)
+
+        self.assertEqual(len(generated_allscores), len(score_list))
+        for i in range(len(generated_allscores)):
+            self.assertEqual(generated_allscores[i].overall, score_list[i])
+
+    # test generate_claims(total_score, asmt_type, grade)
+    def test_generate_claims(self):
+        total_score = []
+        asmt_type = 'Math'
+        grade = '4'
+
+    # test perc_to_count(perc, total)
+    def test_perc_to_count1(self):
+        perc = [40, 35, 10, 15]
+        total = 548
+
+        generated_counts = assessment.perc_to_count(perc, total)
+        self.assertEqual(generated_counts, [219, 192, 55, 82])
+
+    def test_perc_to_count2(self):
+        perc = [40, 35, 10, 15]
+        total = 56
+
+        generated_counts = assessment.perc_to_count(perc, total)
+        self.assertEqual(generated_counts, [22, 20, 6, 8])
 
 if __name__ == "__main__":
     unittest.main()

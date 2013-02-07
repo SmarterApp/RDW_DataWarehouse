59ac66dad186f22db739dc078d6a61bb7cb0d3ff
Takashi Osako <tosako@wgen.net>
2013-02-07 17:10:55 -0500

    Added helpers for LOS

---
+++ smarter/smarter/reports/helpers/list_of_districts.py
@@ -0,0 +1,42 @@
+'''
+Created on Feb 2, 2013
+
+@author: tosako
+'''
+from edapi.utils import report_config
+from database.connector import DBConnector
+from sqlalchemy.sql import select
+
+
+__state_code = 'state_code'
+__dim_district = 'dim_district'
+
+
+@report_config(name="list_of_districts",
+               params={
+                    __state_code: {
+                    "type": "string",
+                    "maxLength": 2,
+                    "required": True
+                    }
+               }
+               )
+def get_districts(params, connector=None):
+
+    # if connector is not supplied, use DBConnector
+    if connector is None:
+        connector = DBConnector()
+
+    state_code = params[__state_code]
+
+    dim_district = connector.get_table(__dim_district)
+
+    query = None
+
+    if dim_district is not None:
+        query = select([dim_district.c.district_id.lable('district_id'),
+                        dim_district.c.district_name.label('district_name')])
+        query = query.where(dim_district.c.state_code == state_code)
+    result = connector.get_result(query)
+    connector.close_connection()
+    return result
---
+++ smarter/smarter/reports/helpers/list_of_schools.py
@@ -0,0 +1,47 @@
+'''
+Created on Feb 2, 2013
+
+@author: tosako
+'''
+from edapi.utils import report_config
+from database.connector import DBConnector
+from sqlalchemy.sql import select
+#######################
+# This function is available after branch edware_schema_20130201 is merged into master
+#######################
+
+
+__district_name = 'district_name'
+__dim_school = 'dim_school'
+
+
+#######################
+# TODO: should we have a district id in order to seachr school id?
+#######################
+
+
+@report_config(name="list_of_schools",
+               params={
+                    __district_name: {
+                    "type": "string",
+                    "required": True
+                    }
+               }
+               )
+def get_schools(params, connector=None):
+
+    # if connector is not supplied, use DBConnector
+    if connector is None:
+        connector = DBConnector()
+    district_name = params[__district_name]
+    dim_school = connector.get_table(__dim_school)
+
+    query = None
+
+    if dim_school is not None:
+        query = select([dim_school.c.school_id.lable('school_id'),
+                        dim_school.c.school_name.label('school_name')])
+        query = query.where(dim_school.c.district_name == district_name)
+    result = connector.get_result(query)
+    connector.close_connection()
+    return result

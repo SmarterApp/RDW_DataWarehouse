6d15ae05695eae7f0539f34ed62dd0ae15d3769d
Doris Ip <dip@wgen.net>
2013-02-07 20:05:54 -0500

    open sql connection first

--- smarter/smarter/reports/helpers/list_of_grades.py
+++ smarter/smarter/reports/helpers/list_of_grades.py
@@ -17,12 +17,16 @@ def get_grades(params, connector=None):
     # if connector is not supplied, use DBConnector
     if connector is None:
         connector = DBConnector()
+
+    # get sql session
+    connector.open_connection()
+
     dim_grade = connector.get_table(__dim_grade)
 
     query = None
 
     if dim_grade is not None:
-        query = select([dim_grade.c.grade_id.lable('grade_id'),
+        query = select([dim_grade.c.grade_id.label('grade_id'),
                         dim_grade.c.grade_code.label('grade_code'),
                         dim_grade.c.grade_desc.label('grade_desc')])
     result = connector.get_result(query)

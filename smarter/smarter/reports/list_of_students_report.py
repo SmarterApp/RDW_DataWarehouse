a9a27cc6b617723542cd5c6d634bf2af83bf4bce
Takashi Osako <tosako@wgen.net>
2013-02-09 17:08:31 -0500

    refactored List of student UT

--- smarter/smarter/reports/list_of_students_report.py
+++ smarter/smarter/reports/list_of_students_report.py
@@ -88,144 +88,146 @@ def get_list_of_students_report(params, connector=None):
     fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
 
     students = {}
-    query = None
-    if isinstance(dim_student, Table) and isinstance(dim_teacher, Table) and isinstance(dim_asmt, Table) and isinstance(fact_asmt_outcome, Table):
 
-        query = select([dim_student.c.student_id.label('student_id'),
-                        dim_student.c.first_name.label('student_first_name'),
-                        func.substr(dim_student.c.middle_name, 1, 1).label('student_middle_name'),
-                        dim_student.c.last_name.label('student_last_name'),
-                        fact_asmt_outcome.c.enrl_grade_id.label('enrollment_grade'),
-                        dim_teacher.c.first_name.label('teacher_first_name'),
-                        dim_teacher.c.last_name.label('teacher_last_name'),
-                        fact_asmt_outcome.c.asmt_grade_id.label('asmt_grade'),
-                        dim_asmt.c.asmt_subject.label('asmt_subject'),
-                        fact_asmt_outcome.c.asmt_score.label('asmt_score'),
-                        fact_asmt_outcome.c.asmt_score_range_min.label('asmt_score_range_min'),
-                        fact_asmt_outcome.c.asmt_score_range_max.label('asmt_score_range_max'),
-                        fact_asmt_outcome.c.asmt_perf_lvl.label('asmt_perf_lvl'),
-                        dim_asmt.c.asmt_claim_1_name.label('asmt_claim_1_name'),
-                        dim_asmt.c.asmt_claim_2_name.label('asmt_claim_2_name'),
-                        dim_asmt.c.asmt_claim_3_name.label('asmt_claim_3_name'),
-                        dim_asmt.c.asmt_claim_4_name.label('asmt_claim_4_name'),
-                        fact_asmt_outcome.c.asmt_claim_1_score.label('asmt_claim_1_score'),
-                        fact_asmt_outcome.c.asmt_claim_2_score.label('asmt_claim_2_score'),
-                        fact_asmt_outcome.c.asmt_claim_3_score.label('asmt_claim_3_score'),
-                        fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score'),
-                        dim_asmt.c.asmt_claim_1_score_min.label('asmt_claim_1_score_min'),
-                        dim_asmt.c.asmt_claim_2_score_min.label('asmt_claim_2_score_min'),
-                        dim_asmt.c.asmt_claim_3_score_min.label('asmt_claim_3_score_min'),
-                        dim_asmt.c.asmt_claim_4_score_min.label('asmt_claim_4_score_min'),
-                        dim_asmt.c.asmt_claim_1_score_max.label('asmt_claim_1_score_max'),
-                        dim_asmt.c.asmt_claim_2_score_max.label('asmt_claim_2_score_max'),
-                        dim_asmt.c.asmt_claim_3_score_max.label('asmt_claim_3_score_max'),
-                        dim_asmt.c.asmt_claim_4_score_max.label('asmt_claim_4_score_max')],
-                       from_obj=[dim_student
-                                 .join(fact_asmt_outcome, dim_student.c.student_id == fact_asmt_outcome.c.student_id)
-                                 .join(dim_asmt, dim_asmt.c.asmt_id == fact_asmt_outcome.c.asmt_id)
-                                 .join(dim_teacher, dim_teacher.c.teacher_id == fact_asmt_outcome.c.teacher_id)])
-        query = query.where(fact_asmt_outcome.c.school_id == schoolId)
-        query = query.where(and_(fact_asmt_outcome.c.asmt_grade_id == asmtGrade))
-        query = query.where(and_(fact_asmt_outcome.c.district_id == districtId))
+    query = select([dim_student.c.student_id.label('student_id'),
+                    dim_student.c.first_name.label('student_first_name'),
+                    func.substr(dim_student.c.middle_name, 1, 1).label('student_middle_name'),
+                    dim_student.c.last_name.label('student_last_name'),
+                    fact_asmt_outcome.c.enrl_grade_id.label('enrollment_grade'),
+                    dim_teacher.c.first_name.label('teacher_first_name'),
+                    dim_teacher.c.last_name.label('teacher_last_name'),
+                    fact_asmt_outcome.c.asmt_grade_id.label('asmt_grade'),
+                    dim_asmt.c.asmt_subject.label('asmt_subject'),
+                    fact_asmt_outcome.c.asmt_score.label('asmt_score'),
+                    fact_asmt_outcome.c.asmt_score_range_min.label('asmt_score_range_min'),
+                    fact_asmt_outcome.c.asmt_score_range_max.label('asmt_score_range_max'),
+                    fact_asmt_outcome.c.asmt_perf_lvl.label('asmt_perf_lvl'),
+                    dim_asmt.c.asmt_claim_1_name.label('asmt_claim_1_name'),
+                    dim_asmt.c.asmt_claim_2_name.label('asmt_claim_2_name'),
+                    dim_asmt.c.asmt_claim_3_name.label('asmt_claim_3_name'),
+                    dim_asmt.c.asmt_claim_4_name.label('asmt_claim_4_name'),
+                    fact_asmt_outcome.c.asmt_claim_1_score.label('asmt_claim_1_score'),
+                    fact_asmt_outcome.c.asmt_claim_2_score.label('asmt_claim_2_score'),
+                    fact_asmt_outcome.c.asmt_claim_3_score.label('asmt_claim_3_score'),
+                    fact_asmt_outcome.c.asmt_claim_4_score.label('asmt_claim_4_score'),
+                    dim_asmt.c.asmt_claim_1_score_min.label('asmt_claim_1_score_min'),
+                    dim_asmt.c.asmt_claim_2_score_min.label('asmt_claim_2_score_min'),
+                    dim_asmt.c.asmt_claim_3_score_min.label('asmt_claim_3_score_min'),
+                    dim_asmt.c.asmt_claim_4_score_min.label('asmt_claim_4_score_min'),
+                    dim_asmt.c.asmt_claim_1_score_max.label('asmt_claim_1_score_max'),
+                    dim_asmt.c.asmt_claim_2_score_max.label('asmt_claim_2_score_max'),
+                    dim_asmt.c.asmt_claim_3_score_max.label('asmt_claim_3_score_max'),
+                    dim_asmt.c.asmt_claim_4_score_max.label('asmt_claim_4_score_max')],
+                   from_obj=[dim_student
+                             .join(fact_asmt_outcome, dim_student.c.student_id == fact_asmt_outcome.c.student_id)
+                             .join(dim_asmt, dim_asmt.c.asmt_id == fact_asmt_outcome.c.asmt_id)
+                             .join(dim_teacher, dim_teacher.c.teacher_id == fact_asmt_outcome.c.teacher_id)])
+    query = query.where(fact_asmt_outcome.c.school_id == schoolId)
+    query = query.where(and_(fact_asmt_outcome.c.asmt_grade_id == asmtGrade))
+    query = query.where(and_(fact_asmt_outcome.c.district_id == districtId))
 
-        if asmtSubject is not None:
-            query = query.where(dim_asmt.c.asmt_subject.in_(asmtSubject))
+    if asmtSubject is not None:
+        query = query.where(dim_asmt.c.asmt_subject.in_(asmtSubject))
 
-        results = connector.get_result(query)
+    query = query.order_by(dim_student.c.first_name).order_by(dim_student.c.last_name)
 
-        # Formatting data for Front End
-        for result in results:
-            student_id = result['student_id']
-            student = {}
-            assessments = {}
-            if student_id in students:
-                student = students[student_id]
-                assessments = student['assessments']
-            else:
-                student['student_id'] = result['student_id']
-                student['student_first_name'] = result['student_first_name']
-                student['student_middle_name'] = result['student_middle_name']
-                student['student_last_name'] = result['student_last_name']
-                student['student_full_name'] = result['student_first_name'] + ' ' + result['student_middle_name'] + ' ' + result['student_last_name']
-                student['enrollment_grade'] = result['enrollment_grade']
+    results = connector.get_result(query)
 
-            assessment = {}
-            assessment['teacher_first_name'] = result['teacher_first_name']
-            assessment['teacher_last_name'] = result['teacher_last_name']
-            assessment['teacher_full_name'] = result['teacher_first_name'] + ' ' + result['teacher_last_name']
-            assessment['asmt_grade'] = result['asmt_grade']
-            assessment['asmt_score'] = result['asmt_score']
-            assessment['asmt_score_range_min'] = result['asmt_score_range_min']
-            assessment['asmt_score_range_max'] = result['asmt_score_range_max']
-            assessment['asmt_perf_lvl'] = result['asmt_perf_lvl']
-            assessment['asmt_claim_1_name'] = result['asmt_claim_1_name']
-            assessment['asmt_claim_2_name'] = result['asmt_claim_2_name']
-            assessment['asmt_claim_3_name'] = result['asmt_claim_3_name']
-            assessment['asmt_claim_4_name'] = result['asmt_claim_4_name']
-            assessment['asmt_claim_1_score'] = result['asmt_claim_1_score']
-            assessment['asmt_claim_2_score'] = result['asmt_claim_2_score']
-            assessment['asmt_claim_3_score'] = result['asmt_claim_3_score']
-            assessment['asmt_claim_4_score'] = result['asmt_claim_4_score']
-            assessment['asmt_claim_1_score_min'] = result['asmt_claim_1_score_min']
-            assessment['asmt_claim_2_score_min'] = result['asmt_claim_2_score_min']
-            assessment['asmt_claim_3_score_min'] = result['asmt_claim_3_score_min']
-            assessment['asmt_claim_4_score_min'] = result['asmt_claim_4_score_min']
-            assessment['asmt_claim_1_score_max'] = result['asmt_claim_1_score_max']
-            assessment['asmt_claim_2_score_max'] = result['asmt_claim_2_score_max']
-            assessment['asmt_claim_3_score_max'] = result['asmt_claim_3_score_max']
-            assessment['asmt_claim_4_score_max'] = result['asmt_claim_4_score_max']
+    # Formatting data for Front End
+    for result in results:
+        student_id = result['student_id']
+        student = {}
+        assessments = {}
+        if student_id in students:
+            student = students[student_id]
+            assessments = student['assessments']
+        else:
+            student['student_id'] = result['student_id']
+            student['student_first_name'] = result['student_first_name']
+            student['student_middle_name'] = result['student_middle_name']
+            student['student_last_name'] = result['student_last_name']
+            student['student_full_name'] = result['student_first_name'] + ' ' + result['student_middle_name'] + ' ' + result['student_last_name']
+            student['enrollment_grade'] = result['enrollment_grade']
 
-            assessments[result['asmt_subject']] = assessment
-            student['assessments'] = assessments
+        assessment = {}
+        assessment['teacher_first_name'] = result['teacher_first_name']
+        assessment['teacher_last_name'] = result['teacher_last_name']
+        assessment['teacher_full_name'] = result['teacher_first_name'] + ' ' + result['teacher_last_name']
+        assessment['asmt_grade'] = result['asmt_grade']
+        assessment['asmt_score'] = result['asmt_score']
+        assessment['asmt_score_range_min'] = result['asmt_score_range_min']
+        assessment['asmt_score_range_max'] = result['asmt_score_range_max']
+        assessment['asmt_perf_lvl'] = result['asmt_perf_lvl']
+        assessment['asmt_claim_1_name'] = result['asmt_claim_1_name']
+        assessment['asmt_claim_2_name'] = result['asmt_claim_2_name']
+        assessment['asmt_claim_3_name'] = result['asmt_claim_3_name']
+        assessment['asmt_claim_4_name'] = result['asmt_claim_4_name']
+        assessment['asmt_claim_1_score'] = result['asmt_claim_1_score']
+        assessment['asmt_claim_2_score'] = result['asmt_claim_2_score']
+        assessment['asmt_claim_3_score'] = result['asmt_claim_3_score']
+        assessment['asmt_claim_4_score'] = result['asmt_claim_4_score']
+        assessment['asmt_claim_1_score_min'] = result['asmt_claim_1_score_min']
+        assessment['asmt_claim_2_score_min'] = result['asmt_claim_2_score_min']
+        assessment['asmt_claim_3_score_min'] = result['asmt_claim_3_score_min']
+        assessment['asmt_claim_4_score_min'] = result['asmt_claim_4_score_min']
+        assessment['asmt_claim_1_score_max'] = result['asmt_claim_1_score_max']
+        assessment['asmt_claim_2_score_max'] = result['asmt_claim_2_score_max']
+        assessment['asmt_claim_3_score_max'] = result['asmt_claim_3_score_max']
+        assessment['asmt_claim_4_score_max'] = result['asmt_claim_4_score_max']
 
-            students[student_id] = student
+        assessments[result['asmt_subject']] = assessment
+        student['assessments'] = assessments
+
+        students[student_id] = student
 
     # including assessments and cutpoints to returning JSON
-    results = {}
+    los_results = {}
     assessments = []
-    for key, value in students.items():
-        assessments.append(value)
-    results['assessments'] = assessments
-    results['cutpoints'] = get_cut_points(connector, asmtGrade, asmtSubject)
+
+    # keep them in orders from result set
+    for result in results:
+        assessments.append(students[result['student_id']])
+
+    los_results['assessments'] = assessments
+    los_results['cutpoints'] = __get_cut_points(connector, asmtGrade, asmtSubject)
     #TODO - restructure this method
     #       make sure connection always closed even on error
     connector.close_connection()
-    return results
+    return los_results
 
 
 # returning cutpoints in JSON.
-def get_cut_points(connector, asmtGrade, asmtSubject):
+def __get_cut_points(connector, asmtGrade, asmtSubject):
     cutpoints = {}
     dim_asmt = connector.get_table('dim_asmt')
 
     # construct the query
-    if isinstance(dim_asmt, Table):
-        query = select([dim_asmt.c.asmt_subject.label("asmt_subject"),
-                        dim_asmt.c.asmt_perf_lvl_name_1.label("asmt_cut_point_name_1"),
-                        dim_asmt.c.asmt_perf_lvl_name_2.label("asmt_cut_point_name_2"),
-                        dim_asmt.c.asmt_perf_lvl_name_3.label("asmt_cut_point_name_3"),
-                        dim_asmt.c.asmt_perf_lvl_name_4.label("asmt_cut_point_name_4"),
-                        dim_asmt.c.asmt_cut_point_1.label("asmt_cut_point_1"),
-                        dim_asmt.c.asmt_cut_point_2.label("asmt_cut_point_2"),
-                        dim_asmt.c.asmt_cut_point_3.label("asmt_cut_point_3"),
-                        dim_asmt.c.asmt_cut_point_4.label("asmt_cut_point_4")],
-                       from_obj=[dim_asmt])
-        query = query.where(dim_asmt.c.asmt_grade == asmtGrade)
-        if asmtSubject is not None:
-            query = query.where(dim_asmt.c.asmt_subject.in_(asmtSubject))
+    query = select([dim_asmt.c.asmt_subject.label("asmt_subject"),
+                    dim_asmt.c.asmt_perf_lvl_name_1.label("asmt_cut_point_name_1"),
+                    dim_asmt.c.asmt_perf_lvl_name_2.label("asmt_cut_point_name_2"),
+                    dim_asmt.c.asmt_perf_lvl_name_3.label("asmt_cut_point_name_3"),
+                    dim_asmt.c.asmt_perf_lvl_name_4.label("asmt_cut_point_name_4"),
+                    dim_asmt.c.asmt_cut_point_1.label("asmt_cut_point_1"),
+                    dim_asmt.c.asmt_cut_point_2.label("asmt_cut_point_2"),
+                    dim_asmt.c.asmt_cut_point_3.label("asmt_cut_point_3"),
+                    dim_asmt.c.asmt_cut_point_4.label("asmt_cut_point_4")],
+                   from_obj=[dim_asmt])
+    query = query.where(dim_asmt.c.asmt_grade == asmtGrade)
+    if asmtSubject is not None:
+        query = query.where(dim_asmt.c.asmt_subject.in_(asmtSubject))
 
-        # run it and format the results
-        results = connector.get_result(query)
-        for result in results:
-            cutpoint = {}
-            cutpoint["asmt_cut_point_name_1"] = result["asmt_cut_point_name_1"]
-            cutpoint["asmt_cut_point_name_2"] = result["asmt_cut_point_name_2"]
-            cutpoint["asmt_cut_point_name_3"] = result["asmt_cut_point_name_3"]
-            cutpoint["asmt_cut_point_name_4"] = result["asmt_cut_point_name_4"]
-            cutpoint["asmt_cut_point_1"] = result["asmt_cut_point_1"]
-            cutpoint["asmt_cut_point_2"] = result["asmt_cut_point_2"]
-            cutpoint["asmt_cut_point_3"] = result["asmt_cut_point_3"]
-            cutpoint["asmt_cut_point_4"] = result["asmt_cut_point_4"]
-            cutpoints[result["asmt_subject"]] = cutpoint
+    # run it and format the results
+    results = connector.get_result(query)
+    for result in results:
+        cutpoint = {}
+        cutpoint["asmt_cut_point_name_1"] = result["asmt_cut_point_name_1"]
+        cutpoint["asmt_cut_point_name_2"] = result["asmt_cut_point_name_2"]
+        cutpoint["asmt_cut_point_name_3"] = result["asmt_cut_point_name_3"]
+        cutpoint["asmt_cut_point_name_4"] = result["asmt_cut_point_name_4"]
+        cutpoint["asmt_cut_point_1"] = result["asmt_cut_point_1"]
+        cutpoint["asmt_cut_point_2"] = result["asmt_cut_point_2"]
+        cutpoint["asmt_cut_point_3"] = result["asmt_cut_point_3"]
+        cutpoint["asmt_cut_point_4"] = result["asmt_cut_point_4"]
+        cutpoints[result["asmt_subject"]] = cutpoint
 
     return cutpoints
--- smarter/smarter/tests/reports/test_list_of_students.py
+++ smarter/smarter/tests/reports/test_list_of_students.py
@@ -4,24 +4,33 @@ Created on Feb 4, 2013
 @author: tosako
 '''
 import unittest
-from .test_connector import TestConnector
 from smarter.reports.list_of_students_report import get_list_of_students_report
+from database.tests.unittest_with_sqlite import Unittest_with_sqlite
 
 
-class Test(unittest.TestCase):
+class Test(Unittest_with_sqlite):
 
     def testReport(self):
-        testConnector = TestConnector()
         testParam = {}
-        testParam['districtId'] = 4
-        testParam['schoolId'] = 3
+        testParam['districtId'] = 1
+        testParam['schoolId'] = 1
         testParam['asmtGrade'] = 1
-        testParam['asmtSubject'] = ['MATH']
-        results = get_list_of_students_report(testParam, connector=testConnector)
-        assert('cutpoints' in results), "returning JSON must have cutpoints"
-        assert('assessments' in results), "returning JSON must have assessments"
-        pass
+        testParam['asmtSubject'] = ['ELA', 'MATH']
+        results = get_list_of_students_report(testParam)
 
+        self.assertTrue('cutpoints' in results, "returning JSON must have cutpoints")
+        self.assertTrue('assessments' in results, "returning JSON must have assessments")
+
+        cutpoints = results['cutpoints']
+        self.assertEqual(2, len(cutpoints), "cutpoints are ELA and MATH")
+        self.assertTrue('ELA' in cutpoints, 'ELA')
+        self.assertTrue('MATH' in cutpoints, 'MATH')
+
+        assessments = results['assessments']
+        self.assertEqual(3, len(assessments), "3 assessments")
+        self.assertEqual("Adam", assessments[0]['student_first_name'], "student_first_name")
+        self.assertEqual("Doris", assessments[1]['student_first_name'], "student_first_name")
+        self.assertEqual("Takashi", assessments[2]['student_first_name'], "student_first_name")
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testReport']

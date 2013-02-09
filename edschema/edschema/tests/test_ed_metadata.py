5f51bf62e719d1ba5d7a0155c684d701f9cc09e3
Takashi Osako <tosako@wgen.net>
2013-02-08 19:02:11 -0500

    Updated UT

--- edschema/edschema/tests/test_ed_metadata.py
+++ edschema/edschema/tests/test_ed_metadata.py
@@ -26,18 +26,23 @@ class Test(unittest.TestCase):
         cls.__metadata = MetaData()
         cls.__metadata.reflect(bind=__engine)
 
-    def test_total_tables(self):
+    def test_number_of_tables(self):
         # check number of tables
         self.assertEqual(15, len(self.__metadata.tables), "Number of table does not match")
 
+    # Test dim_district data
     def test_dim_district(self):
         self.assertTrue('dim_district' in self.__metadata.tables, "missing dim_district")
         connector = DBConnector()
         connector.open_connection()
         dim_district = connector.get_table("dim_district")
         query = dim_district.select(dim_district.c.district_id == 1)
+        # check number of field in the table
+        self.assertEqual(7, len(dim_district.c), "Number of fields in dim_district")
         result = connector.get_result(query)
         self.assertEqual(1, result[0]['district_id'], 'district_id')
+        self.assertEqual("Daybreak District", result[0]['district_name'], "district_name")
+        connector.close_connection()
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']

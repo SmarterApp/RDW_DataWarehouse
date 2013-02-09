f2c54759bc622b29b5a98fa94d49dfee9b69d7c9
Doris Ip <dip@wgen.net>
2013-02-08 21:25:33 -0500

    Add Unit Test csv importer

--- edschema/database/tests/ed_sqlite.py
+++ edschema/database/tests/ed_sqlite.py
@@ -10,11 +10,13 @@ from zope import component
 from edschema.ed_metadata import generate_ed_metadata
 import csv
 from sqlalchemy.sql.expression import select
+import os
+import sqlite3
 
 
 # create sqlite from static metadata
 def create_sqlite():
-    __engine = create_engine('sqlite:///:memory:')
+    __engine = create_engine('sqlite:///:memory:', connect_args={'detect_types': sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES}, native_datetime=True)
     __metadata = generate_ed_metadata()
     __metadata.create_all(__engine)
     dbUtil = DbUtil(engine=__engine, metadata=__metadata)
@@ -24,12 +26,28 @@ def create_sqlite():
 def generate_data():
     dbconnector = DBConnector()
     connection = dbconnector.open_connection()
-
-    table = dbconnector.get_table('dim_district')
-
     here = os.path.abspath(os.path.dirname(__file__))
+    resources_dir = os.path.join(os.path.join(here, 'resources'))
 
-    with open(here + '/resources/dim_district.csv') as file:
-        reader = csv.DictReader(file, delimiter=',')
-        for row in reader:
-            connection.execute(table.insert().values(**row))
+    resources = ['dim_country',
+                 'dim_state',
+                 'dim_district',
+                 'dim_school',
+                 'dim_where_taken',
+                 'dim_grade',
+                 'dim_student',
+                 'dim_teacher',
+                 'dim_asmt',
+                 'fact_asmt_outcome'
+                 ]
+
+    for resource in resources:
+        table = dbconnector.get_table(resource)
+        with open(os.path.join(resources_dir, resource + ".csv")) as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for row in reader:
+                new_row = {}
+                for field_name in row.keys():
+                    clean_field_name = field_name.rstrip()
+                    new_row[clean_field_name] = row[field_name]
+                connection.execute(table.insert().values(**new_row))

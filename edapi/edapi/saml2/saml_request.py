ac1168ad544780d959592d033dea2d2ef6afd8cc
Doris Ip <dip@wgen.net>
2013-02-13 19:47:17 -0500

    generate saml request dynamically

---
+++ edapi/edapi/saml2/saml_request.py
@@ -0,0 +1,48 @@
+from xml.dom.minidom import Document
+import uuid
+from time import gmtime, strftime
+'''
+Created on Feb 13, 2013
+
+@author: dip
+'''
+import zlib
+import urllib
+import base64
+
+# Create XML SAML Auth Request
+# returns a byte string of a SAML AuthnRequest 
+def get_auth_request():
+    doc = Document()
+    
+    samlp_Auth_Request = doc.createElement('samlp:AuthnRequest')
+    samlp_Auth_Request.setAttribute('xmlns:samlp', "urn:oasis:names:tc:SAML:2.0:protocol")
+    samlp_Auth_Request.setAttribute('xmlns:saml', "urn:oasis:names:tc:SAML:2.0:assertion")
+    # UUID based on host ID and current time, or use 4 to get random
+    samlp_Auth_Request.setAttribute('ID', str(uuid.uuid1()))
+    samlp_Auth_Request.setAttribute('Version', "2.0")
+    samlp_Auth_Request.setAttribute('IssueInstant', strftime("%Y-%m-%dT%H:%M:%S", gmtime()))
+    samlp_Auth_Request.setAttribute('AssertionConsumerServiceIndex', "0")
+    samlp_Auth_Request.setAttribute("AttributeConsumingServiceIndex", "0")
+
+    saml_issuer = doc.createElement("saml:Issuer")
+    saml_issuer_text = doc.createTextNode("http://localhost:6543/sp.xml")
+    saml_issuer.appendChild(saml_issuer_text)
+    samlp_Auth_Request.appendChild(saml_issuer)
+
+    samlp_name_id_policy = doc.createElement("samlp:NameIDPolicy")
+    samlp_name_id_policy.setAttribute("AllowCreate", "true")
+    samlp_name_id_policy.setAttribute("Format", "urn:oasis:names:tc:SAML:2.0:nameid-format:transient")
+    samlp_Auth_Request.appendChild(samlp_name_id_policy)
+
+    doc.appendChild(samlp_Auth_Request)
+
+    # This will strip out the xml version text
+    data = doc.documentElement.toxml('utf-8')
+    
+    return encode_saml_request(data)
+
+def encode_saml_request(data):
+    compressed = zlib.compress(data)
+    encoded = base64.b64encode(compressed[2:-4])
+    return urllib.parse.urlencode({'SAMLRequest': encoded})
--- edapi/edapi/saml2/views.py
+++ edapi/edapi/saml2/views.py
@@ -1,9 +1,7 @@
 from pyramid.security import NO_PERMISSION_REQUIRED, forget
 from pyramid.httpexceptions import HTTPFound
-import base64
-import zlib
 from pyramid.view import view_config, forbidden_view_config
-import urllib
+from edapi.saml2.saml_request import get_auth_request
 '''
 Created on Feb 13, 2013
 
@@ -15,26 +13,9 @@ Created on Feb 13, 2013
 #TODO for accessign a view that user aren't allowed to do
 @forbidden_view_config(renderer='json')
 def login(request):
-    data = '''<samlp:AuthnRequest
-xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
-xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
-ID="aaf23196-1773-2113-474a-fe114412ab72"
-Version="2.0"
-IssueInstant="2013-02-12T23:02:59"
-AssertionConsumerServiceIndex="0"
-AttributeConsumingServiceIndex="0">
-<saml:Issuer>http://localhost:6543/sp.xml</saml:Issuer>
-<samlp:NameIDPolicy
-AllowCreate="true"
-Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient"/>
-</samlp:AuthnRequest>'''
-
     url = 'http://edwappsrv4.poc.dum.edwdc.net:18080/opensso/SSORedirect/metaAlias/idp?%s'
-    compressed = zlib.compress(data.encode())
-    encoded = base64.b64encode(compressed[2:-4])
-    params = urllib.parse.urlencode({'SAMLRequest': encoded})
-    final_url = url % params
-    return HTTPFound(location=final_url)
+    params = get_auth_request()
+    return HTTPFound(location=url % params)
 
 
 @view_config(route_name='logout')

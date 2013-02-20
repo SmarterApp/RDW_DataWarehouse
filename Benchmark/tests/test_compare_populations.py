05896b1b0c5c17ac0d9fd92e89fd2edec056217e
Smitha Pitla <spitla@wgen.net>
2013-02-20 12:52:30 -0500

    Added test case for grades within a school
    Former-commit-id: cf14129b7344bc81afbbfe19bd9df6f3cb003189

--- Benchmark/tests/test_compare_populations.py
+++ Benchmark/tests/test_compare_populations.py
@@ -1,5 +1,6 @@
 import compare_populations_districts_within_state
 import compare_populations_schools_within_district
+import compare_populations_grades_within_school
 import unittest
 
 class ComparePopulations(unittest.TestCase):
@@ -11,6 +12,10 @@ class ComparePopulations(unittest.TestCase):
     def test_compare_populations_schools_within_district(self):
 
         expected_scorelist = compare_populations_schools_within_district.schools_in_a_district(161, 'SUMMATIVE', 'ELA')
+        
+    def test_compare_populations_grades_within_school(self):
+
+        expected_scorelist = compare_populations_grades_within_school.grades_in_a_school(161, 'SUMMATIVE', 'ELA')
         
 
    

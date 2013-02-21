0a2e14009809f496436903fa2c733cb035b18d34
Seth Wimberly <swimberly@wgen.net>
2013-02-21 15:09:06 -0500

    Updates and clean up to query code
    Former-commit-id: 5c96f47618d075f61a070ac4e355c6b9fb4b79a9

--- Benchmark/src/compare_populations_districts_within_state.py
+++ Benchmark/src/compare_populations_districts_within_state.py
@@ -1,46 +1,34 @@
 '''
 Created on Feb 15, 2013
 
-@author: Smitha Pitla
+@author: Smitha Pitla, swimberly
+
+Districts withing a state query
+
+Public interface:
+state_statistics(state_id, engine)
+districts_in_a_state(state_id, asmt_type, asmt_subject, engine)
+
+'state_statistics()' prints statistics for how long it takes for the query to return
+for each of the different types of queries:
+Summative-ELA, Summative-Math, Interim-ELA, Interim-Math
+
+'districts_in_a_state()': returns the result of running the query on the
+given parameters
 '''
+
 from sqlalchemy import create_engine
-import sys
-engine = create_engine('postgresql+psycopg2://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
 
 
-def districts_in_a_state(state_id, asmt_type, asmt_subject):
-    query = """
-    select dist.district_name, count(fact.student_id),
-    case when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
-    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
-    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
-    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
-    end
-    as performance_level
-    from
-    edware_star_20130212_fixture_3.dim_asmt asmt,
-    edware_star_20130212_fixture_3.dim_district dist,
-    edware_star_20130212_fixture_3.fact_asmt_outcome fact,
-    edware_star_20130212_fixture_3.dim_student stu
-    where asmt.asmt_id = fact.asmt_id
-    and dist.district_id = fact.district_id
-    and stu.student_id = fact.student_id
-    and fact.date_taken_year='2012'
-    and fact.state_id = '{state_id}'
-    and asmt.asmt_type = '{asmt_type}'
-    and asmt.asmt_subject = '{asmt_subject}'
-    group by dist.district_name, performance_level
-    """.format(state_id=state_id, asmt_type=asmt_type, asmt_subject=asmt_subject)
+def state_statistics(state_id, engine):
+    '''
+    Runs queries that print out the statistics and benchmarks for a state
+    INPUT:
+    state_id -- an id for a state from the database
+    '''
+    if state_id is None or engine is None:
+        raise ValueError('Bad Params for state_id or engine')
 
-    # print(state)
-    # resultset = engine.execute(query,{'state':state, 'year':year})
-    resultset = engine.execute(query)
-    results = resultset.fetchall()
-    # print(results)
-    return results
-
-
-def state_statistics(state_id):
     district_count_query = '''
     select count(*)
     from edware_star_20130212_fixture_3.dim_district district
@@ -86,32 +74,64 @@ def state_statistics(state_id):
     print('**** Benchmarks for Queries ****')
 
     start_time1 = time.time()
-    res1 = districts_in_a_state(state_id, 'SUMMATIVE', 'ELA')
+    districts_in_a_state(state_id, 'SUMMATIVE', 'ELA', engine)
     query_time = time.time() - start_time1
     print('Summative-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    res2 = districts_in_a_state(state_id, 'INTERIM', 'ELA')
+    districts_in_a_state(state_id, 'INTERIM', 'ELA', engine)
     query_time = time.time() - start_time1
     print('Interim-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    res3 = districts_in_a_state(state_id, 'SUMMATIVE', 'Math')
+    districts_in_a_state(state_id, 'SUMMATIVE', 'Math', engine)
     query_time = time.time() - start_time1
     print('Summative-Math:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    res4 = districts_in_a_state(state_id, 'INTERIM', 'Math')
+    districts_in_a_state(state_id, 'INTERIM', 'Math', engine)
     query_time = time.time() - start_time1
     print('Interim-Math:\t\t%6.2fs' % query_time)
 
 
+def districts_in_a_state(state_id, asmt_type, asmt_subject, engine):
+    query = """
+    select dist.district_name, count(fact.student_id),
+    case when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
+    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
+    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
+    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
+    end
+    as performance_level
+    from
+    edware_star_20130212_fixture_3.dim_asmt asmt,
+    edware_star_20130212_fixture_3.dim_district dist,
+    edware_star_20130212_fixture_3.fact_asmt_outcome fact,
+    edware_star_20130212_fixture_3.dim_student stu
+    where asmt.asmt_id = fact.asmt_id
+    and dist.district_id = fact.district_id
+    and stu.student_id = fact.student_id
+    and fact.date_taken_year='2012'
+    and fact.state_id = '{state_id}'
+    and asmt.asmt_type = '{asmt_type}'
+    and asmt.asmt_subject = '{asmt_subject}'
+    group by dist.district_name, performance_level
+    """.format(state_id=state_id, asmt_type=asmt_type, asmt_subject=asmt_subject)
+
+    # print(state)
+    # resultset = engine.execute(query,{'state':state, 'year':year})
+    resultset = engine.execute(query)
+    results = resultset.fetchall()
+    # print(results)
+    return results
+
 if __name__ == '__main__':
     import time
+
+    engine = create_engine('postgresql+psycopg2://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
     stime = time.time()
-    res = districts_in_a_state('DE', 'SUMMATIVE', 'ELA')
+    res = districts_in_a_state('DE', 'SUMMATIVE', 'ELA', engine)
     duration = time.time() - stime
+
     res.sort(key=lambda tup: tup[0])
-    state_statistics('DE')
-    # state_statistics(127)
-    # state_statistics(143)
+    state_statistics('DE', engine)

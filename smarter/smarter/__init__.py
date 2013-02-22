8636723d87357ab1c9c7a2537b642a003330058a
Anna Grebneva <agrebneva@gmail.com>
2013-02-22 14:09:17 -0500

    major cleanup

--- smarter/smarter/__init__.py
+++ smarter/smarter/__init__.py
@@ -43,32 +43,7 @@ def main(global_config, **settings):
 
     LessCSS(media_dir=parent_assets_dir + "/less", output_dir=parent_assets_dir + "/css", based=False)
 
-    config.add_static_view('static', 'static', cache_max_age=0, permission='view')
     config.add_static_view('assets', '../assets', cache_max_age=0, permission='view')
-
-    config.add_route('home', '/')
-    config.add_route('checkstatus', '/status')
-    #
-    config.add_route('comparing_populations', '/comparing_populations')
-    config.add_route('test1', '/test1')
-    config.add_route('template', '/template')
-    config.add_route('generateComparePopulations', '/comPopResults')
-    config.add_route('inputComparePopulations', '/comPop')
-    config.add_route('generateComparePopulationsAl', '/comPopResultsAl')
-    config.add_route('inputComparePopulationsAl', '/comPopAl')
-    config.add_route('datatojson2', '/datatojson2')
-    config.add_route('inputdata2', '/inputdata2')
-    # splita's code
-    config.add_route('comparepopulation', '/comparepopulation')
-    config.add_route('getcomparepopulation', '/getcomparepopulation')
-
-    # routing for individual student report
-    config.add_route('indiv_student', '/indiv_student_report')
-    # r routing for *bootstrapped* individual student report
-    config.add_route('indiv_student_bootstrap', '/indiv_student_report_bootstrap')
-    # routing for class report
-    config.add_route('class_report', '/class_report')
-    config.add_route('student_report', '/student_report')
 
     # scans smarter
     config.scan()

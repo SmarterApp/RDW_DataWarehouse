e98988568cbc7590df5aa08262ed98abd69d539b
Seth Wimberly <swimberly@wgen.net>
2013-02-22 12:04:55 -0500

    Update for methods to take a variable schema name as an argument adding setup.py and other attributes
    Former-commit-id: 8059c586702de28791ae29f404806bd795fe0991

--- Benchmark/src/compare_populations_grades_within_school.py
+++ Benchmark/src/compare_populations_grades_within_school.py
@@ -1,67 +1,54 @@
 '''
 Created on Feb 15, 2013
 
-@author: Smitha Pitla
+@author: Smitha Pitla, swimberly
+
+Grades withing a school query & benchmarks
+
+Public interface:
+school_statistics(state_id, engine, schema_name)
+grades_in_a_school(state_id, asmt_type, asmt_subject, engine, schema_name)
+
+Descriptions:
+'school_statistics()' prints statistics for how long it takes for the query to return
+for each of the different types of queries:
+Summative-ELA, Summative-Math, Interim-ELA, Interim-Math
+
+'grades_in_a_school()': returns the result of running the query on the
+given parameters
 '''
+
 from sqlalchemy import create_engine
-import sys
-engine = create_engine('postgresql+psycopg2://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
-#district=sys.argv[1]
 
 
-def grades_in_a_school(school_id, asmt_type, asmt_subject, engine):
-    query = """
-    select fact.asmt_grade_id, count(fact.student_id),
-    case
-    when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
-    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
-    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
-    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
-    end
-    as performance_level
-    from
-    edware_star_20130212_fixture_3.dim_asmt asmt,
-    edware_star_20130212_fixture_3.dim_school school,
-    edware_star_20130212_fixture_3.fact_asmt_outcome fact,
-    edware_star_20130212_fixture_3.dim_student stu
-    where asmt.asmt_id = fact.asmt_id
-    and school.school_id = fact.school_id
-    and stu.student_id = fact.student_id
-    and fact.date_taken_year='2012'
-    and fact.school_id = {school_id}
-    and asmt.asmt_type = '{asmt_type}'
-    and asmt.asmt_subject = '{asmt_subject}'
-    group by fact.asmt_grade_id, performance_level
-    """.format(school_id=school_id, asmt_type=asmt_type, asmt_subject=asmt_subject)
-    #print(district)
-    resultset = engine.execute(query)
-    results = resultset.fetchall()
-    #print(results)
-    return results
-
-
-def school_statistics(school_id):
+def school_statistics(school_id, engine, schema_name):
+    '''
+    Runs queries that print out the statistics and benchmarks for a school
+    INPUT:
+    state_id -- an id for a state from the database
+    engine -- the db engine created by a sqlAlchemy create_engine() statement
+    '''
 
     student_count_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_student student
+    from {schema}.dim_student student
     where student.school_id = {school_id}
-    '''.format(school_id=school_id)
+    '''.format(school_id=school_id, schema=schema_name)
 
     total_students_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_student
-    '''
+    from {schema}.dim_student
+    '''.format(schema=schema_name)
 
     total_dist_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_district
-    '''
+    from {schema}.dim_district
+    '''.format(schema=schema_name)
 
     total_schools_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_school
-    '''
+    from {schema}.dim_school
+    '''.format(schema=schema_name)
 
     start_time = time.time()
     stu_count_set = engine.execute(student_count_query)
@@ -79,38 +66,82 @@ def school_statistics(school_id):
     print('**** Benchmarks for Queries ****')
 
     start_time1 = time.time()
-    grades_in_a_school(school_id, 'SUMMATIVE', 'ELA')
+    grades_in_a_school(school_id, 'SUMMATIVE', 'ELA', engine, schema_name)
     query_time = time.time() - start_time1
     print('Summative-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    grades_in_a_school(school_id, 'INTERIM', 'ELA')
+    grades_in_a_school(school_id, 'INTERIM', 'ELA', engine, schema_name)
     query_time = time.time() - start_time1
     print('Interim-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    grades_in_a_school(school_id, 'SUMMATIVE', 'Math')
+    grades_in_a_school(school_id, 'SUMMATIVE', 'Math', engine, schema_name)
     query_time = time.time() - start_time1
     print('Summative-Math:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    grades_in_a_school(school_id, 'INTERIM', 'Math')
+    grades_in_a_school(school_id, 'INTERIM', 'Math', engine, schema_name)
     query_time = time.time() - start_time1
     print('Interim-Math:\t\t%6.2fs' % query_time)
+
+
+def grades_in_a_school(school_id, asmt_type, asmt_subject, engine, schema_name):
+    '''
+    Run a query for assessment performance for grades within a school.
+    INPUT:
+    state_id -- the id of the state that you want to use in the query (ie. 'DE' for deleware)
+    asmt_type -- the type of assessment to use in the query ('SUMMATIVE' or 'INTERIM')
+    asmt_subject -- the subject of assessment to use in the query ('ELA' or 'Math')
+    engine -- the db engine created by a sqlAlchemy create_engine() statement
+    RETURNS: result -- a list of tuples (grade, count, performance level)
+    '''
+
+    query = """
+    select fact.asmt_grade_id, count(fact.student_id),
+    case
+    when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
+    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
+    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
+    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
+    end
+    as performance_level
+    from
+    {schema}.dim_asmt asmt,
+    {schema}.dim_school school,
+    {schema}.fact_asmt_outcome fact,
+    {schema}.dim_student stu
+    where asmt.asmt_id = fact.asmt_id
+    and school.school_id = fact.school_id
+    and stu.student_id = fact.student_id
+    and fact.date_taken_year='2012'
+    and fact.school_id = {school_id}
+    and asmt.asmt_type = '{asmt_type}'
+    and asmt.asmt_subject = '{asmt_subject}'
+    group by fact.asmt_grade_id, performance_level
+    """.format(school_id=school_id, asmt_type=asmt_type, asmt_subject=asmt_subject, schema=schema_name)
+    # print(district)
+    resultset = engine.execute(query)
+    results = resultset.fetchall()
+    # print(results)
+    return results
 
 
 if __name__ == '__main__':
     import time
 
-    school_statistics(167)
-    school_statistics(169)
-    school_statistics(171)
-    school_statistics(173)
+    engine = create_engine('postgresql+psycopg2://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
+    schema_name = 'edware_star_20130212_fixture_3'
+
+    school_statistics(167, engine, schema_name)
+    school_statistics(169, engine, schema_name)
+    school_statistics(171, engine, schema_name)
+    school_statistics(173, engine, schema_name)
 
     stime = time.time()
-    result = grades_in_a_school(167, 'SUMMATIVE', 'ELA')
+    result = grades_in_a_school(173, 'SUMMATIVE', 'ELA', engine, schema_name)
     duration = time.time() - stime
-    result.sort(key=lambda tup: tup[0])
+    result.sort(key=lambda tup: int(tup[0]))
 
     for res in result:
         print(res)
--- Benchmark/src/compare_populations_schools_within_district.py
+++ Benchmark/src/compare_populations_schools_within_district.py
@@ -1,72 +1,53 @@
 '''
 Created on Feb 15, 2013
 
-@author: Smitha Pitla
+@author: Smitha Pitla, swimberly
+
+schools withing a district query
+
+Public interface:
+district_statistics(state_id, engine)
+schools_in_a_district(state_id, asmt_type, asmt_subject, engine)
+
+Descriptions:
+'district_statistics()' prints statistics for how long it takes for the query to return
+for each of the different types of queries:
+Summative-ELA, Summative-Math, Interim-ELA, Interim-Math
+
+'schools_in_a_district()': returns the result of running the query on the
+given parameters
 '''
+
 from sqlalchemy import create_engine
-import sys
-engine = create_engine('postgresql+psycopg2://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
-#district=sys.argv[1]
 
 
-def schools_in_a_district(district_id, asmt_type, asmt_subject):
-    query = """
-    select school.school_name, count(fact.student_id),
-    case
-    when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
-    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
-    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
-    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
-    end
-    as performance_level
-    from
-    edware_star_20130212_fixture_3.dim_asmt asmt,
-    edware_star_20130212_fixture_3.dim_school school,
-    edware_star_20130212_fixture_3.fact_asmt_outcome fact,
-    edware_star_20130212_fixture_3.dim_student stu
-    where asmt.asmt_id = fact.asmt_id
-    and school.school_id = fact.school_id
-    and stu.student_id = fact.student_id
-    and fact.date_taken_year='2012'
-    and fact.district_id = {district_id}
-    and asmt.asmt_type = '{asmt_type}'
-    and asmt.asmt_subject = '{asmt_subject}'
-    group by school.school_name, performance_level
-    """.format(district_id=district_id, asmt_type=asmt_type, asmt_subject=asmt_subject)
-    #print(district)
-    resultset = engine.execute(query)
-    results = resultset.fetchall()
-    #print(results)
-    return results
-
-
-def district_statistics(district_id):
+def district_statistics(district_id, engine, schema_name):
     school_count_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_school school
+    from {schema}.dim_school school
     where school.district_id = {district_id}
-    '''.format(district_id=district_id)
+    '''.format(district_id=district_id, schema=schema_name)
 
     student_count_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_student student
+    from {schema}.dim_student student
     where student.district_id = {district_id}
-    '''.format(district_id=district_id)
+    '''.format(district_id=district_id, schema=schema_name)
 
     total_students_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_student
-    '''
+    from {schema}.dim_student
+    '''.format(schema=schema_name)
 
     total_dist_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_district
-    '''
+    from {schema}.dim_district
+    '''.format(schema=schema_name)
 
     total_schools_query = '''
     select count(*)
-    from edware_star_20130212_fixture_3.dim_school
-    '''
+    from {schema}.dim_school
+    '''.format(schema=schema_name)
 
     start_time = time.time()
     school_count_set = engine.execute(school_count_query)
@@ -86,35 +67,79 @@ def district_statistics(district_id):
     print('**** Benchmarks for Queries ****')
 
     start_time1 = time.time()
-    schools_in_a_district(district_id, 'SUMMATIVE', 'ELA')
+    schools_in_a_district(district_id, 'SUMMATIVE', 'ELA', engine, schema_name)
     query_time = time.time() - start_time1
     print('Summative-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    schools_in_a_district(district_id, 'INTERIM', 'ELA')
+    schools_in_a_district(district_id, 'INTERIM', 'ELA', engine, schema_name)
     query_time = time.time() - start_time1
     print('Interim-ELA:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    schools_in_a_district(district_id, 'SUMMATIVE', 'Math')
+    schools_in_a_district(district_id, 'SUMMATIVE', 'Math', engine, schema_name)
     query_time = time.time() - start_time1
     print('Summative-Math:\t\t%6.2fs' % query_time)
 
     start_time1 = time.time()
-    schools_in_a_district(district_id, 'INTERIM', 'Math')
+    schools_in_a_district(district_id, 'INTERIM', 'Math', engine, schema_name)
     query_time = time.time() - start_time1
     print('Interim-Math:\t\t%6.2fs' % query_time)
+
+
+def schools_in_a_district(district_id, asmt_type, asmt_subject, engine, schema_name):
+    '''
+    Run a query for assessment performance for grades within a school.
+    INPUT:
+    state_id -- the id of the state that you want to use in the query (ie. 'DE' for deleware)
+    asmt_type -- the type of assessment to use in the query ('SUMMATIVE' or 'INTERIM')
+    asmt_subject -- the subject of assessment to use in the query ('ELA' or 'Math')
+    engine -- the db engine created by a sqlAlchemy create_engine() statement
+    RETURNS: results -- a list of tuples (school, count, performance level)
+    '''
+
+    query = """
+    select school.school_name, count(fact.student_id),
+    case
+    when fact.asmt_score <= asmt.asmt_cut_point_1 then asmt.asmt_perf_lvl_name_1
+    when fact.asmt_score > asmt.asmt_cut_point_1 and fact.asmt_score <= asmt.asmt_cut_point_2 then asmt.asmt_perf_lvl_name_2
+    when fact.asmt_score > asmt.asmt_cut_point_2 and fact.asmt_score <= asmt.asmt_cut_point_3 then asmt.asmt_perf_lvl_name_3
+    when fact.asmt_score > asmt.asmt_cut_point_3  then asmt.asmt_perf_lvl_name_4
+    end
+    as performance_level
+    from
+    {schema}.dim_asmt asmt,
+    {schema}.dim_school school,
+    {schema}.fact_asmt_outcome fact,
+    {schema}.dim_student stu
+    where asmt.asmt_id = fact.asmt_id
+    and school.school_id = fact.school_id
+    and stu.student_id = fact.student_id
+    and fact.date_taken_year='2012'
+    and fact.district_id = {district_id}
+    and asmt.asmt_type = '{asmt_type}'
+    and asmt.asmt_subject = '{asmt_subject}'
+    group by school.school_name, performance_level
+    """.format(district_id=district_id, asmt_type=asmt_type, asmt_subject=asmt_subject, schema=schema_name)
+    #print(district)
+    resultset = engine.execute(query)
+    results = resultset.fetchall()
+    #print(results)
+    return results
 
 
 if __name__ == '__main__':
     import time
 
-    district_statistics(161)
-    district_statistics(127)
-    district_statistics(143)
+    engine = create_engine('postgresql://postgres:postgres@monetdb1.poc.dum.edwdc.net:5432/edware')
+    schema_name = 'edware_star_20130212_fixture_3'
+
+    district_statistics(161, engine, schema_name)
+    district_statistics(127, engine, schema_name)
+    district_statistics(143, engine, schema_name)
 
     stime = time.time()
-    result = schools_in_a_district(161, 'SUMMATIVE', 'ELA')
+    result = schools_in_a_district(161, 'SUMMATIVE', 'ELA', engine, schema_name)
     duration = time.time() - stime
     result.sort(key=lambda tup: tup[0])
 

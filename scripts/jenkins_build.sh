a47081d2c85275e96e7b1800d7f5f8f9e353db1f
Doris Ip <dip@wgen.net>
2013-02-28 15:51:29 -0500

    temp fix jenkins with abs path

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -193,9 +193,11 @@ function create_sym_link_for_apache {
    #/usr/local/bin/lessc ${WORKSPACE}/assets/less/style.less ${WORKSPACE}/assets/css/style.css
     
     cd "$WORKSPACE/scripts"
-    ls
-    sed -i.bak "s/assets.directory = \/path\/assets/assets.directory = $WORKSPACE\/assets/g" compile_assets.ini 
-    sed -i.bak "s/smarter.directory = \/path\/smarter/smarter.directory = $WORKSPACE\/smarter/g" compile_assets.ini
+    echo $WORKSPACE > workspace.tmp 
+    sed -i.bak 's/\//\\\//g' workspace.tmp
+    
+    #sed -i.bak "s/assets.directory = \/path\/assets/assets.directory = $WORKSPACE\/assets/g" compile_assets.ini
+    #sed -i.bak "s/smarter.directory = \/path\/smarter/smarter.directory = $WORKSPACE\/smarter/g" compile_assets.ini
     python compile_assets.py
 }
 

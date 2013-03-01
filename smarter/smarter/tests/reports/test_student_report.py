bad24d3c1481379832b6dd8732f3f42a4e338ae3
Nidhi Paroha <nparoha@wgen.net>
2013-03-01 00:12:04 -0500

    Fixing broken unit tests

--- smarter/smarter/tests/reports/test_student_report.py
+++ smarter/smarter/tests/reports/test_student_report.py
@@ -74,9 +74,9 @@ class TestStudentReport(Unittest_with_sqlite):
         params = {"studentId": '286ee893-dad0-4833-ae6c-adef78a11567'}
         result = get_student_report(params)['context']
         self.assertEqual('NY', result['state_name'])
-        self.assertEqual('Sunset', result['district_name'])
+        self.assertEqual('Sunset School District', result['district_name'])
         self.assertEqual("2", result['grade'])
-        self.assertEqual("School Name 2", result['school_name'])
+        self.assertEqual("Sunset Central High", result['school_name'])
         self.assertEqual("Verda Herriman", result['student_name'])
 
 

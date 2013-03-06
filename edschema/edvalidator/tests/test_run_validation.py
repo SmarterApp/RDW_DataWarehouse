14b2ac99c9ef83b190bf2fc8fde6955d65c59891
Takashi Osako <tosako@wgen.net>
2013-03-05 22:06:22 -0500

    US13386: fix UT

--- edschema/edvalidator/tests/test_run_validation.py
+++ edschema/edvalidator/tests/test_run_validation.py
@@ -12,31 +12,31 @@ from edvalidator.tests.utils.metadata import generate_test_metadata
 class Test(unittest.TestCase):
 
     def test_validator(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('good_csv'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('good_csv'), verbose=False)
         self.assertEqual(0, rtn_code)
 
     def test_validator_with_missig_csv(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('missing_csv'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('missing_csv'), verbose=False)
         self.assertEqual(1, rtn_code)
 
     def test_validator_with_missig_csv_ignore_table(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=True, missing_field_ignore=False, dir_name=get_resource_dir('missing_csv'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=True, missing_field_ignore=False, dir_name=get_resource_dir('missing_csv'), verbose=False)
         self.assertEqual(0, rtn_code)
 
     def test_validator_with_missig_fields(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('missing_fields'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('missing_fields'), verbose=False)
         self.assertEqual(1, rtn_code)
 
     def test_validator_with_missig_fields_ignore_field(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=False, missing_field_ignore=True, dir_name=get_resource_dir('missing_fields'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=False, missing_field_ignore=True, dir_name=get_resource_dir('missing_fields'), verbose=False)
         self.assertEqual(0, rtn_code)
 
     def test_validator_directory_does_not_exist(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('no_such_dir'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=False, missing_field_ignore=False, dir_name=get_resource_dir('no_such_dir'), verbose=False)
         self.assertEqual(1, rtn_code)
 
     def test_validator_with_wrong_fields_order(self):
-        rtn_code = run_validation(metadata=generate_test_metadata(), force_foreign=True, missing_table_ignore=True, missing_field_ignore=False, dir_name=get_resource_dir('wrong_field_order'), verbose=False)
+        rtn_code = run_validation(metadata=generate_test_metadata(), missing_table_ignore=True, missing_field_ignore=False, dir_name=get_resource_dir('wrong_field_order'), verbose=False)
         self.assertEqual(1, rtn_code)
 
 

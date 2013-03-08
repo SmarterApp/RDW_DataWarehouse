d4afc885b2db32b6100ed598831997cc0be65682
lichen <lichen@wgen.net>
2013-03-07 23:40:07 -0500

    add optional command line arguments to 'where' clause to retrieve students
    Former-commit-id: 6a5248464755ba45ed14aa3d0b09d95bf1007a69

--- DataGeneration/src/get_list_of_students.py
+++ DataGeneration/src/get_list_of_students.py
@@ -3,12 +3,16 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.sql import select
 from sqlalchemy.schema import MetaData
 from datetime import datetime
+from sqlalchemy.sql import and_
 
 
 DBDRIVER = "postgresql+pypostgresql"
 
 
-def get_students_for_assessment(schema_name=None, bind=None):
+def get_students_for_assessment(schema_name=None, bind=None, parameters=None):
+    '''
+    Method to get list of students in the input schema with optional input parameters
+    '''
     # get schema object
     metadata = MetaData(schema=schema_name)
     metadata.reflect(bind=bind)
@@ -21,7 +25,7 @@ def get_students_for_assessment(schema_name=None, bind=None):
         dim_inst_hier = metadata.tables[schema_name + ".dim_inst_hier"]
     except KeyError as err:
         print("This table does not exist -- ", err)
-        return
+        return []
 
     # prepare the query
     try:
@@ -38,9 +42,25 @@ def get_students_for_assessment(schema_name=None, bind=None):
                                  .join(dim_section_subject, dim_student.c.section_id == dim_section_subject.c.section_id and dim_student.c.school_id == dim_section_subject.c.school_id)
                                  .join(dim_staff, dim_student.c.section_id == dim_staff.c.section_id and dim_student.c.school_id == dim_staff.c.school_id)
                                  .join(dim_inst_hier, dim_student.c.school_id == dim_inst_hier.c.school_id)])
+
+        # add where clause
+        if parameters is not None:
+            if 'state_code' in parameters.keys() and parameters['state_code']:
+                query = query.where(dim_student.c.state_code == parameters['state_code'])
+            if 'district_id' in parameters.keys() and parameters['district_id']:
+                query = query.where(and_(dim_student.c.district_id == parameters['district_id']))
+            if 'school_id' in parameters.keys() and parameters['school_id']:
+                query = query.where(and_(dim_student.c.school_id == parameters['school_id']))
+            if 'section_id' in parameters.keys() and parameters['section_id']:
+                query = query.where(and_(dim_student.c.section_id == parameters['section_id']))
+            if 'grade' in parameters.keys() and parameters['grade']:
+                query = query.where(and_(dim_student.c.grade == parameters['grade']))
+
     except AttributeError as err:
         print("This column does not exist -- ", err)
-        return
+        return []
+
+    # print(query)
 
     # execute the query
     students = []
@@ -65,18 +85,40 @@ def get_students_for_assessment(schema_name=None, bind=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create New Schema for EdWare')
+    # database related arguments
     parser.add_argument("-s", "--schema", help="set schema name.  required")
     parser.add_argument("-d", "--database", default="edware", help="set database name default[edware]")
     parser.add_argument("--host", default="127.0.0.1:5432", help="postgre host default[127.0.0.1:5432]")
     parser.add_argument("-u", "--user", default="postgres", help="postgre username default[edware]")
     parser.add_argument("-p", "--passwd", default="3423346", help="postgre password default[edware]")
-    args = parser.parse_args()
 
+    # query related arguments
+
+    parser.add_argument('--state_code', default=False, help='set state code.', required=False)
+    parser.add_argument('--district_id', default=False, help='set district id.', required=False)
+    parser.add_argument('--school_id', default=False, help='set school id.', required=False)
+    parser.add_argument('--section_id', default=False, help='set section id.', required=False)
+    parser.add_argument('--grade', default=False, help='set grade id.', required=False)
+
+    args = parser.parse_args()
+    print(args)
+    # get the value of database related arguments
     __schema = args.schema
     __database = args.database
     __host = args.host
     __user = args.user
     __passwd = args.passwd
+
+    # get the value of table related arguments
+
+    params = {'state_code': args.state_code if args.state_code else None,
+             'district_id': args.district_id if args.district_id else None,
+             'school_id': args.school_id if args.school_id else None,
+             'section_id': args.section_id if args.section_id else None,
+             'grade': args.grade if args.grade else None,
+             }
+
+    # print(params)
 
     if __schema is None:
         print("Please specify --schema option")
@@ -84,7 +126,6 @@ if __name__ == '__main__':
     __URL = DBDRIVER + "://" + __user + ":" + __passwd + "@" + __host + "/" + __database
     start_time = datetime.now()
 
-    print(__URL)
     print("DB Driver:" + DBDRIVER)
     print("     User:" + __user)
     print("  Password:" + __passwd)
@@ -95,7 +136,7 @@ if __name__ == '__main__':
     print("Getting list of students for assessment generation...")
 
     __engine = create_engine(__URL, echo=False)
-    student_list = get_students_for_assessment(schema_name=__schema, bind=__engine)
+    student_list = get_students_for_assessment(schema_name=__schema, bind=__engine, parameters=params)
     finish_time = datetime.now()
     print("Length of generated student list is ", len(student_list))
     print("Start at --  ", start_time)

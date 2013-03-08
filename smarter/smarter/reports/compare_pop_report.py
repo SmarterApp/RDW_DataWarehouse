75f92f02da72792d1013733d131471c840ba1613
Takashi Osako <tosako@wgen.net>
2013-03-08 18:36:59 -0500

    US12619: clean up and pep8

--- smarter/smarter/reports/compare_pop_report.py
+++ smarter/smarter/reports/compare_pop_report.py
@@ -135,49 +135,78 @@ def arrange_results(results, param_manager):
         calculate_percentage_for_summary(total_results[subject])
 
     # bind the results
-    # reverse map keys and values
-    arranged_results[Constants.SUBJECTS] = {v: k for k, v in subjects.items()}
     arranged_results[Constants.COLORS] = asmt_custom_metadata_results
     arranged_results[Constants.SUMMARY] = total_results
     arranged_results[Constants.RECORDS] = consolidated_results
+    # reverse map keys and values
+    arranged_results[Constants.SUBJECTS] = {v: k for k, v in subjects.items()}
     return arranged_results
 
 
 def process_result(read_from, subject_result, level_name):
+    '''
+    read_from: result set
+    process the result for each "level" names.
+    read the result, then pack data for
+    count, level, and a map of intervals
+    '''
+    # get interval array, if this is the first time.
+    # then get an empty arraay
     intervals = subject_result.get(Constants.INTERVALS, [])
     interval = {}
+
+    # get count for the level
     interval[Constants.COUNT] = read_from[level_name]
-    calulate_percentage(read_from, interval, level_name)
+
+    # calculate the percentage
+    calculate_percentage(read_from, interval, level_name)
     # strip out "level" from level_name
     interval[Constants.LEVEL] = int(level_name[5:])
+    # append to the end of intervals array
     intervals.append(interval)
+
+    # assign to "intervals" property
     subject_result[Constants.INTERVALS] = intervals
 
 
 def calculate_percentage_for_summary(total_result):
+    '''
+    calculate percentage for overall summary result sets
+    '''
     __total = total_result.get(Constants.TOTAL, 0)
     __intervals = total_result[Constants.INTERVALS]
     for index in range(len(__intervals)):
         __percentage = 0
         if __total != 0:
+            # use 0.5 to round up
             __percentage = int(__intervals[index][Constants.COUNT] / __total * 100 + 0.5)
         __intervals[index][Constants.PERCENTAGE] = __percentage
 
 
-def calulate_percentage(read_from, write_out, level_name):
+def calculate_percentage(read_from, write_out, level_name):
+    '''
+    calculate percentage for each records
+    '''
     __total = read_from.get(Constants.TOTAL, 0)
-    if __total == 0:
-        write_out[Constants.PERCENTAGE] = 0
-    write_out[Constants.PERCENTAGE] = int(read_from.get(level_name, 0) / __total * 100 + 0.5)
+    __percentage = 0
+    if __total != 0:
+        # use 0.5 to round up
+        __percentage = int(read_from.get(level_name, 0) / __total * 100 + 0.5)
+    write_out[Constants.PERCENTAGE] = __percentage
 
 
 def calculate_sum_for_total(read_from, write_out):
     '''
+    sum up for overall total and each levels
     read_from: subject record
+    write_out: total subject
     '''
+
+    # read record
     read_from_total = read_from[Constants.TOTAL]
     read_from_intervals = read_from[Constants.INTERVALS]
 
+    # read total record or initialize if it does not exist
     write_out_total = write_out.get(Constants.TOTAL, 0)
     write_out_intervals = write_out.get(Constants.INTERVALS, None)
 
@@ -187,16 +216,22 @@ def calculate_sum_for_total(read_from, write_out):
         for index in range(len(read_from_intervals)):
             write_out_intervals.append({})
 
+    # create information for level and total count
     for index in range(len(read_from_intervals)):
         write_out_intervals[index][Constants.LEVEL] = read_from_intervals[index][Constants.LEVEL]
         write_out_intervals[index][Constants.COUNT] = write_out_intervals[index].get(Constants.COUNT, 0) + read_from_intervals[index][Constants.COUNT]
 
+    # sum overall total
     write_out_total = write_out_total + read_from_total
+
     write_out[Constants.TOTAL] = write_out_total
     write_out[Constants.INTERVALS] = write_out_intervals
 
 
 class Parameters():
+    '''
+    placehold for input parameters
+    '''
     def __init__(self, params):
         self._state_id = params.get(Constants.STATEID, None)
         self._district_id = params.get(Constants.DISTRICTID, None)
@@ -216,19 +251,34 @@ class Parameters():
 
 
 class ParameterManager():
+    '''
+    Manager class for class Parameter
+    '''
     def __init__(self, parameters):
         self._parameters = parameters
 
     def is_state_view(self):
+        '''
+        return true if it is the state view
+        '''
         return self._parameters.state_id is not None and self._parameters.district_id is None and self._parameters.school_id is None
 
     def is_district_view(self):
+        '''
+        return true if it is the district view
+        '''
         return self._parameters.state_id is not None and self._parameters.district_id is not None and self._parameters.school_id is None
 
     def is_school_view(self):
+        '''
+        return true if it is the school vieww
+        '''
         return self._parameters.state_id is not None and self._parameters.district_id is not None and self._parameters.school_id is not None
 
     def get_name_of_field(self):
+        '''
+        return name of the field based on the view
+        '''
         __field_name = None
         if self.is_state_view():
             __field_name = Constants.DISTRICT_NAME
@@ -239,6 +289,9 @@ class ParameterManager():
         return __field_name
 
     def get_id_of_field(self):
+        '''
+        return id name of the field based on the view
+        '''
         __field_id = None
         if self.is_state_view():
             __field_id = Constants.DISTRICT_ID
@@ -250,15 +303,19 @@ class ParameterManager():
 
     @property
     def p(self):
+        '''
+        return parameters object
+        '''
         return self._parameters
 
 
 class QueryHelper():
-
+    '''
+    helper class to build sqlalchemy query based on the view
+    '''
     def __init__(self, connector, param_manager):
         self._param_manager = param_manager
-        # having a problme using property object at Table.
-        # using __get__ method is work around solutions
+        # get dim_inst_hier, dim_asmt, and fact_asmt_outcome tables
         self._dim_inst_hier = connector.get_table(Constants.DIM_INST_HIER)
         self._dim_asmt = connector.get_table(Constants.DIM_ASMT)
         self._fact_asmt_outcome = connector.get_table(Constants.FACT_ASMT_OUTCOME)
@@ -276,35 +333,44 @@ class QueryHelper():
         elif self._param_manager.is_school_view():
             columns = [self._fact_asmt_outcome.c.asmt_grade.label(Constants.ASMT_GRADE), self._dim_asmt.c.asmt_subject.label(Constants.ASMT_SUBJECT)]
 
-        # this is always static
-        columns_for_perf_level = [self._dim_asmt.c.asmt_custom_metadata.label(Constants.ASMT_CUSTOM_METADATA),
-                                  func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 1, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL1),
+        # these are static
+        # get information about bar colors
+        bar_widget_color_info = [self._dim_asmt.c.asmt_custom_metadata.label(Constants.ASMT_CUSTOM_METADATA), ]
+
+        # use pivot table for summarize from level1 to level5
+        columns_for_perf_level = [func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 1, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL1),
                                   func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 2, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL2),
                                   func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 3, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL3),
                                   func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 4, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL4),
                                   func.count(case([(self._fact_asmt_outcome.c.asmt_perf_lvl == 5, self._fact_asmt_outcome.c.student_id)])).label(Constants.LEVEL5),
                                   func.count(self._fact_asmt_outcome.c.student_id).label(Constants.TOTAL)]
-        return columns + columns_for_perf_level
+        return columns + bar_widget_color_info + columns_for_perf_level
 
     def build_from_obj(self):
+        '''
+        build join clause based on the view
+        '''
         from_obj = None
         # building join clause based on request
+        # used Constants.TRUE for pep8 E712 issue
         if self._param_manager.is_state_view():
             from_obj = [self._fact_asmt_outcome
-                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == True, self._fact_asmt_outcome.c.most_recent == True))
-                        .join(self._dim_inst_hier, and_(self._dim_inst_hier.c.inst_hier_rec_id == self._fact_asmt_outcome.c.inst_hier_rec_id, self._dim_inst_hier.c.most_recent == True))]
+                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == Constants.TRUE, self._fact_asmt_outcome.c.most_recent == Constants.TRUE))
+                        .join(self._dim_inst_hier, and_(self._dim_inst_hier.c.inst_hier_rec_id == self._fact_asmt_outcome.c.inst_hier_rec_id, self._dim_inst_hier.c.most_recent == Constants.TRUE))]
         elif self._param_manager.is_district_view():
             from_obj = [self._fact_asmt_outcome
-                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == True, self._fact_asmt_outcome.c.most_recent == True))
-                        .join(self._dim_inst_hier, and_(self._dim_inst_hier.c.inst_hier_rec_id == self._fact_asmt_outcome.c.inst_hier_rec_id, self._dim_inst_hier.c.most_recent == True))]
+                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == Constants.TRUE, self._fact_asmt_outcome.c.most_recent == Constants.TRUE))
+                        .join(self._dim_inst_hier, and_(self._dim_inst_hier.c.inst_hier_rec_id == self._fact_asmt_outcome.c.inst_hier_rec_id, self._dim_inst_hier.c.most_recent == Constants.TRUE))]
         elif self._param_manager.is_school_view():
             from_obj = [self._fact_asmt_outcome
-                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == True, self._fact_asmt_outcome.c.most_recent == True))]
+                        .join(self._dim_asmt, and_(self._dim_asmt.c.asmt_rec_id == self._fact_asmt_outcome.c.asmt_rec_id, self._dim_asmt.c.asmt_type == Constants.SUMMATIVE, self._dim_asmt.c.most_recent == Constants.TRUE, self._fact_asmt_outcome.c.most_recent == Constants.TRUE))]
         return from_obj
 
     def build_group_by(self):
+        '''
+        build group by clause based on the view
+        '''
         group_by = None
-        # building group by clause based on request
         if self._param_manager.is_state_view():
             group_by = self._dim_inst_hier.c.district_name, self._dim_inst_hier.c.district_id, self._dim_asmt.c.asmt_subject
         elif self._param_manager.is_district_view():
@@ -314,8 +380,10 @@ class QueryHelper():
         return group_by + (self._dim_asmt.c.asmt_custom_metadata,)
 
     def build_order_by(self):
+        '''
+        build order by clause based on the view
+        '''
         order_by = None
-        # building group by clause based on request
         if self._param_manager.is_state_view():
             order_by = self._dim_inst_hier.c.district_name, self._dim_asmt.c.asmt_subject.desc()
         elif self._param_manager.is_district_view():
@@ -325,6 +393,9 @@ class QueryHelper():
         return order_by
 
     def build_where(self):
+        '''
+        build where by clause based on the view
+        '''
         where = None
         # building group by clause based on request
         if self._param_manager.is_state_view():
@@ -337,6 +408,9 @@ class QueryHelper():
 
 
 class Constants():
+    '''
+    constants for this report
+    '''
     STATEID = 'stateId'
     DISTRICTID = 'districtId'
     SCHOOLID = 'schoolId'
@@ -372,3 +446,4 @@ class Constants():
     COLORS = 'colors'
     SUMMARY = 'summary'
     RECORDS = 'records'
+    TRUE = True

aa003dd41a944b985b42b6450581d2c46191bf20
Takashi Osako <tosako@wgen.net>
2013-03-11 17:38:17 -0400

    US12619: updated UT

--- smarter/smarter/tests/reports/test_compare_pop_report_Record.py
+++ smarter/smarter/tests/reports/test_compare_pop_report_Record.py
@@ -14,12 +14,18 @@ class Test(unittest.TestCase):
         self.assertEqual(0, len(record.subjects))
         self.assertIsNone(record.id)
         self.assertIsNone(record.name)
+        self.assertIsNone(record.state_id)
+        self.assertIsNone(record.district_id)
+        self.assertIsNone(record.school_id)
 
     def test_record(self):
-        record = Record('id1', 'name1')
+        record = Record(record_id='id1', name='name1', state_id='state1', district_id='district1', school_id='school1')
         self.assertEqual(0, len(record.subjects))
         self.assertEqual('id1', record.id)
         self.assertEqual('name1', record.name)
+        self.assertEqual('state1', record.state_id)
+        self.assertEqual('district1', record.district_id)
+        self.assertEqual('school1', record.school_id)
         subjects = {'hello': 'test'}
         self.subjects = subjects
         self.assertDictEqual(self.subjects, subjects)

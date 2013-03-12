137f60c46f53e13958f51e14cdcd18698b7ae8d5
Iqbal Gill <igill@wgen.net>
2013-03-12 17:34:05 -0400

    made pep 8 compliant and renamed to test/test_schemacheck.py

---
+++ test/test_schemacheck.py
@@ -0,0 +1,53 @@
+'''
+Created on Mar 12, 2013
+
+@author: igill
+'''
+from testbase import TestBase
+from sqlalchemy.engine import create_engine
+from sqlalchemy import event
+from sqlalchemy.schema import MetaData
+from sqlalchemy.schema import Table
+from edschema.ed_metadata import generate_ed_metadata
+
+
+class EdTestSchema(TestBase):
+
+    def __init__(self, *args, **kwargs):
+        TestBase.__init__(self, *args, **kwargs)
+        self.db_url = self.get_db_url('edware')
+        self.db_schema = self.get_schema_name('edware')
+
+    def setUp(self):
+        super(TestBase, self).setUp()
+        self.pgengine = create_engine(self.db_url)
+        self.connection = self.pgengine.connect()
+        self.live_db_metadata = MetaData(schema=self.db_schema)
+        self.live_db_metadata.reflect(bind=self.pgengine)
+        self.ed_metadata = generate_ed_metadata(schema_name=self.db_schema)
+
+    #in each table in the live db, if a column has a foreign key,
+    #check the data type matches primary column
+    def test_foreign_key_datatypes(self):
+        for table in self.live_db_metadata.tables:
+            columns = self.live_db_metadata.tables[table].columns
+            for column in columns.keys():
+                if len(list(columns[column].foreign_keys)) > 0:
+                    assert (str(columns[column].type) == str(list(columns[column].foreign_keys)[0].column.type))
+
+    #check that the ed_metadata matches
+    #the live metadata in tables,columns, and datatypes
+    def test_live_metadata_to_ed_metadata(self):
+        for table in self.live_db_metadata.tables:
+            assert table in self.ed_metadata.tables, "Unexpected table in live db"
+            live_columns = self.live_db_metadata.tables[table].columns
+            metadata_columns = self.ed_metadata.tables[table].columns
+            for column in live_columns.keys():
+                assert column in metadata_columns.keys(), "Unexpected column in live db"
+                if str(metadata_columns[column].type) == 'FLOAT':
+                    assert str(live_columns[column].type) == 'DOUBLE PRECISION'
+                else:
+                    assert (str(live_columns[column].type) == str(metadata_columns[column].type), "Column datatype mismatch")
+
+    def tearDown(self):
+        self.pgengine.dispose()

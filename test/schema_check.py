7a25fc4b20ac6590b2204ad31fe7ed2ba0ac6932
Eugene Jen <ejen@wgen.net>
2013-03-12 19:53:20 -0400

    Add setting up and destroy a test database that uses mismatched foreign key constraints that our tests has to check. See https://confluence.wgenhq.net/display/CS/Postgres+Schema+Rules

--- test/schema_check.py
+++ test/schema_check.py
@@ -8,6 +8,9 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy import event
 from sqlalchemy.schema import MetaData
 from sqlalchemy.schema import Table
+from sqlalchemy.schema import Column
+from sqlalchemy import String, Integer, Float, SmallInteger, Unicode, Date, DateTime
+from sqlalchemy import ForeignKey
 from edschema.ed_metadata import generate_ed_metadata
 from sqlalchemy.types import NULLTYPE,FLOAT
 from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
@@ -18,6 +21,8 @@ class EdTestSchema(TestBase):
         TestBase.__init__(self, *args, **kwargs)
         self.db_url = self.get_db_url('edware')
         self.db_schema=self.get_schema_name('edware')
+        self.qa_db_url = self.get_db_url('edware', mode='qa')
+        self.qa_db_schema = self.get_schema_name('edware', mode='qa')
         
     def setUp(self):
         super(TestBase, self).setUp()
@@ -47,3 +52,66 @@ class EdTestSchema(TestBase):
                
     def tearDown(self):
         self.pgengine.dispose()
+
+    def _generate_mismatch_metadata(self, engine, schema):
+        qa_metadata = MetaData(bind=engine, schema=schema)
+        # Here, we intentionally generated mismatched foreign keys among a sets of tables.
+        # So we can Meta Test tests above
+        # Please refer https://confluence.wgenhq.net/display/CS/Postgres+Schema+Rules for all cases
+        
+        tbl_string = Table('tbl_string', qa_metadata, Column('id', String(50), primary_key=True), schema=schema,)
+        tbl_unicode = Table('tbl_unicode', qa_metadata, Column('id', Unicode(50), primary_key=True), schema=schema,)
+        tbl_date = Table('tbl_date', qa_metadata, Column('id', Date, primary_key=True), schema=schema,)
+        tbl_datetime = Table('tbl_datetime', qa_metadata, Column('id', DateTime, primary_key=True), schema=schema,)
+        tbl_integer = Table('tbl_integer', qa_metadata, Column('id', Integer, primary_key=True), schema=schema,)
+        tbl_small_integer = Table('tbl_small_integer', qa_metadata, Column('id', SmallInteger, primary_key=True), schema=schema,)
+        tbl_float = Table('tbl_float', qa_metadata, Column('id', Float, primary_key=True), schema=schema,)
+        tbl_master = Table('tbl_master', qa_metadata,
+                           Column('id', Integer, primary_key=True),
+                           Column('string_short', String(5), ForeignKey(tbl_string.c.id)),
+                           Column('string_long', String(50), ForeignKey(tbl_string.c.id)),
+                           Column('unicode_string', Unicode(50), ForeignKey(tbl_string.c.id)),
+                           Column('string_unicode', String(50), ForeignKey(tbl_unicode.c.id)),
+                           Column('datetime_date', DateTime, ForeignKey(tbl_date.c.id)),     
+                           Column('date_datetime', Date, ForeignKey(tbl_datetime.c.id)),
+                           Column('small_integer_integer', SmallInteger, ForeignKey(tbl_integer.c.id)),
+                           Column('integer_small_integer', Integer, ForeignKey(tbl_small_integer.c.id)),
+                           Column('integer_float', Integer, ForeignKey(tbl_float.c.id)),
+                           schema=schema)
+        return qa_metadata
+
+    def create_mismatch_schema(self):
+        pgengine = create_engine(self.qa_db_url)
+        connection = pgengine.connect()
+        trans = connection.begin()
+        qa_metadata = self._generate_mismatch_metadata(pgengine, self.qa_db_schema)
+        try:
+            qa_metadata.create_all()
+        finally:
+            trans.commit()
+            connection.close()
+
+    def destroy_mismatch_schema(self):
+        # I can't use SQLAlchemy's metadata.drop_all to drop tables correctly due to drop cascade.
+        # I found this on google and tested it. http://www.executionunit.com/en/blog/2007/11/23/postgresql-sqlalchemy-dropping-all-tables-and-sequences/
+        pgengine = create_engine(self.qa_db_url)
+        qa_metadata = MetaData(bind=pgengine, schema=self.qa_db_schema)
+        #qa_metadata.drop_all()
+        connection = pgengine.connect()
+        trans = connection.begin()
+        sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='" + self.qa_db_schema + "'"
+        tables = connection.execute(sql)
+        for (table,) in tables:
+            try:
+                connection.execute("DROP TABLE %s CASCADE" % (self.qa_db_schema + '.' + table))
+            except Exception as e:
+                print(e)
+        sql = "SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema='" + self.qa_db_schema + "'"
+        seqs = connection.execute(sql)
+        for (seq,) in seqs:
+            try:
+                connection.execute("DROP SEQUENCE %s CASCADE" % (self.qa_db_schema + '.' + seq))
+            except Exception as e:
+                print(e)
+        trans.commit()
+        connection.close()

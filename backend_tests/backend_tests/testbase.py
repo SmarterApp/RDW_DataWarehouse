cd07291de34ae70e363f738fa9aae26d17649a8e
Iqbal Gill <igill@wgen.net>
2013-03-13 17:10:22 -0400

    pep8

--- backend_tests/backend_tests/testbase.py
+++ backend_tests/backend_tests/testbase.py
@@ -8,7 +8,7 @@ import configparser
 import os
 
 
-class TestBase(unittest.TestCase):    
+class TestBase(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
@@ -21,13 +21,13 @@ class TestBase(unittest.TestCase):
         self._config.read(test_config_path)
         self._qa_type_test_schema = 'qa_type_test'
 
-    def get_db_url(self, prefix, mode = None):
+    def get_db_url(self, prefix, mode=None):
         if mode is None:
             return self._config.get('app:main', prefix + '.db.main.url')
         else:
             return self._config.get('app:main', prefix + '.db.main.url')
 
-    def get_schema_name(self, prefix, mode = None):
+    def get_schema_name(self, prefix, mode=None):
         if mode is None:
             return self._config.get('app:main', prefix + '.schema_name')
         else:

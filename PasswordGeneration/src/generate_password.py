1db80a772f8421bc1cc7284bfc647d13822d3bd5
Lili Chen <lichen@wgen.net>
2013-04-08 12:28:30 -0400

    generate unique passwords
    Former-commit-id: ef931291e0e5b950002034156eac84ced2b83576

--- PasswordGeneration/src/generate_password.py
+++ PasswordGeneration/src/generate_password.py
@@ -8,13 +8,13 @@ import argparse
 import random
 import os
 import datetime
+import math
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 WORD_LIST = os.path.join(__location__, '..', 'word_lists', 'linuxwords.txt')
 DEFAULT_OUTPUT_FILE = os.path.join(__location__, '..', 'passwords.txt')
-SPECIAL_CHARS = '^!\$%&/=?+~#-_:;<>\\'
+SPECIAL_CHARS = '^!\$%&/=?+~#-\\_:;<>'
 MIN_LENGTH = 8
-MAX_NUMBER_OF_PASSWORD = 1000000
 
 
 def generate_password(number, output_file):
@@ -23,24 +23,38 @@ def generate_password(number, output_file):
     '''
     # cast to integer
     count = int(number)
-    # do check number
+
+    # the number should be greater than 0
     if count <= 0:
         print("The number should be greater than 0")
         raise ValueError
-    elif count > MAX_NUMBER_OF_PASSWORD:
-        print("The number should be less than", MAX_NUMBER_OF_PASSWORD)
-        raise ValueError
 
-    # get list of word (linux words, need to limit to 3 <= length <= 7)
+    # get list of word
     words_list = read_words()
+    # the words list should not be empty
     if len(words_list) == 0:
         print("Empty word list, please check", WORD_LIST)
         raise ValueError
 
-    # loop count times, passwords are generated in a list
-    generated_password_list = [generate_sigle_password(words_list) for _i in range(count)]
-    # write password list into file
-    write_into_file(generated_password_list, output_file)
+    # the input number should be less than half length of the words_list
+    half_number_of_words = math.ceil(len(words_list) / 2)
+    if count > half_number_of_words:
+        print("The input number", number, "is too big, it should be less than half number of words in", WORD_LIST)
+        raise ValueError
+
+    # get two word lists from words_list
+    # words in first_word_list are unique words selected from the first half of word_list
+    # words in second_word_list are unique words selected from the second half of word_list
+    first_word_list = random.sample(words_list[0: half_number_of_words], count)
+    second_word_list = random.sample(words_list[half_number_of_words:-1], count)
+
+    # generate single password count  number of times
+    # make words selected from first_word_list and second_word_list
+    # start with an upper case character and lower case for remaining characters
+    generated_password = [generate_single_password(first_word_list[i], second_word_list[i]) for i in range(count)]
+
+    # write into output file
+    write_into_file(generated_password, output_file)
 
 
 def read_words():
@@ -48,32 +62,28 @@ def read_words():
     Read all words in WORD_LIST, return as a list
     '''
     word_list = []
-    source_file_name = WORD_LIST
+    file_name = WORD_LIST
     try:
-        source_file = open(source_file_name, 'r')
-        lines = source_file.readlines()
+        target_file = open(file_name, 'r')
+        lines = target_file.readlines()
         for line in lines:
             word_list.append(line.strip())
-        source_file.close()
+        target_file.close()
     except FileNotFoundError as err:
         print("No file", err)
     return word_list
 
 
-def generate_sigle_password(words_list):
+def generate_single_password(first_word, second_word):
     '''
     Generate one single password as the pattern defined
     '''
-    # randomly pick two words in words_list
-    # words start with an upper case character and the remaining characters are lower case
-    first_word = random.choice(words_list).title()
-    second_word = random.choice(words_list).title()
     # generate one digit
     digit = generate_one_digit()
     # generate one special character
     special_char = generate_one_special_character()
     # group as a list
-    four_parts = [digit, special_char, first_word, second_word]
+    four_parts = [digit, special_char, first_word.title(), second_word.title()]
     # shuffle the four parts
     random.shuffle(four_parts)
     # password pattern is:
@@ -87,7 +97,6 @@ def generate_sigle_password(words_list):
 def generate_one_digit():
     '''
     Generate one digit between 1 and 10 (1<= x <10)
-    Return the string format of the digit
     '''
     return str(random.choice(range(1, 10)))
 
@@ -101,12 +110,11 @@ def generate_one_special_character():
 
 def write_into_file(generated_password, output_file):
     '''
-    Write given generated_password into the output_file
+    Write the given generated_password, which is a list into output_file
     Each item in generated_password is written as one line in output_file
     '''
     file = open(output_file, 'w')
     file.write("\n".join(generated_password))
-    file.close()
 
 
 if __name__ == '__main__':
--- PasswordGeneration/tests/test_generate_password.py
+++ PasswordGeneration/tests/test_generate_password.py
@@ -1,6 +1,7 @@
 import unittest
 import generate_password
 import os
+import re
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 FILES_FOR_TEST_PATH = os.path.join(__location__, 'files_for_test')
@@ -8,11 +9,12 @@ FILES_FOR_TEST_PATH = os.path.join(__location__, 'files_for_test')
 
 class TestGeneratePassword(unittest.TestCase):
 
-    def test_generate_password_negtive_number(self):
+    def test_generate_password_negative_number(self):
         self.assertRaises(ValueError, generate_password.generate_password, -10, '/output')
 
-    def test_generate_password_greater_than_max_number(self):
-        self.assertRaises(ValueError, generate_password.generate_password, generate_password.MAX_NUMBER_OF_PASSWORD + 10, '/output')
+    def test_generate_password_greater_than_half_of_words_list(self):
+        generate_password.WORD_LIST = os.path.join(FILES_FOR_TEST_PATH, 'word_list_10.txt')
+        self.assertRaises(ValueError, generate_password.generate_password, 8, '/output')
 
     def test_generate_password_invalid_words_list_file(self):
         generate_password.WORD_LIST = os.path.join(FILES_FOR_TEST_PATH, 'non_existing_file.txt')
@@ -20,6 +22,10 @@ class TestGeneratePassword(unittest.TestCase):
 
     def test_generate_password_empty_words_list(self):
         generate_password.WORD_LIST = os.path.join(FILES_FOR_TEST_PATH, 'empty_word_list.txt')
+        self.assertRaises(ValueError, generate_password.generate_password, 200, '/output')
+
+    def test_generate_password_one_word_in_words_list(self):
+        generate_password.WORD_LIST = os.path.join(FILES_FOR_TEST_PATH, 'one_word_list.txt')
         self.assertRaises(ValueError, generate_password.generate_password, 200, '/output')
 
     def test_generate_password_three_passwords(self):
@@ -37,28 +43,21 @@ class TestGeneratePassword(unittest.TestCase):
         os.remove(output_file)
 
     def test_generate_sigle_password_long_list(self):
-        words_list = ['oneword', 'zfgadfs', 'qdfafggt', 'lqpzxc', 'addfl', 'polqsf']
-        actual_password = generate_password.generate_sigle_password(words_list)
-
+        first_word = 'abcd'
+        second_word = 'deffgz'
+        actual_password = generate_password.generate_single_password(first_word, second_word)
         # verify that the actual password contains only one digit, one special character
-        actual_digit_index = -1
-        actual_special_char_index = -1
-        for i in range(len(actual_password)):
-            if actual_password[i].isdigit() and actual_digit_index == -1:
-                actual_digit_index = i
-            elif actual_password[i] in generate_password.SPECIAL_CHARS and actual_special_char_index == -1:
-                actual_special_char_index = i
-        self.assertTrue(-1 < actual_digit_index < len(actual_password))
-        self.assertTrue(-1 < actual_special_char_index < len(actual_password))
-        # verify that two words are selected from words_list(not necessary to be different)
+        actual_special_chars = re.findall(r'\W{1}|[_]{1}', actual_password)
+        self.assertTrue(len(actual_special_chars), 1)
+        self.assertTrue(actual_special_chars[0] in generate_password.SPECIAL_CHARS)
+        one_digit_expression = '[1-9]{1}'
+        actual_digit = re.findall(one_digit_expression, actual_password)
+        self.assertTrue(len(actual_digit), 1)
+        # verify that two words are either first_word, or second_word
         actual_password_copy = actual_password
-        rest_password = actual_password_copy.replace(str(actual_password[actual_digit_index]), '').replace(str(actual_password[actual_special_char_index]), '')
-        self.assertTrue(rest_password in [word.title() + wor.title() for word in words_list for wor in words_list])
-
-    def test_generate_sigle_password_one_word_list(self):
-        words_list = ['oneword']
-        actual_password = generate_password.generate_sigle_password(words_list)
-        self.assertEqual(len(actual_password), len(words_list[0]) * 2 + 2)
+        rest_password = actual_password_copy.replace(str(actual_special_chars[0]), '').replace(str(actual_digit[0]), '')
+        self.assertTrue(rest_password in [first_word.title() + second_word.title(), second_word.title() + first_word.title()])
+        self.assertEqual(len(actual_password), len(first_word) + len(second_word) + 2)
 
     def test_generate_one_special_character(self):
         special_char = generate_password.generate_one_special_character()

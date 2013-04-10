3102d41bb8d397d7b909429c6e9bbbdce6037bad
Takashi Osako <tosako@wgen.net>
2013-04-10 13:27:23 -0400

    Removed CHANGES.txt

--- edauth/setup.py
+++ edauth/setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 install_requires = [
     'pyramid >= 1.3.1',

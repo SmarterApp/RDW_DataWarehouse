2567e73fbcd92f23dad7fe35c69d1a64a404e6c3
Doris Ip <dip@wgen.net>
2013-04-19 14:31:08 -0400

    Add route for custom error page for 404

---
+++ smarter/smarter/views.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 19, 2013
+
+@author: dip
+'''
+from pyramid.view import notfound_view_config
+from pyramid.response import Response
+
+
+@notfound_view_config()
+def notfound_view_get(request):
+    '''
+    All Not found gets redirected here
+    '''
+    # TODO: waiting for static page
+    # TODO: we can separate  GET and POST request errors
+    return Response('TODO customize error', status='404 Not Found')

cf15c848191071526505b527c2a843ed100c8ae8
Doris Ip <dip@wgen.net>
2013-04-19 14:51:38 -0400

    Temp Disable 404 route

--- smarter/smarter/views.py
+++ smarter/smarter/views.py
@@ -3,15 +3,15 @@ Created on Apr 19, 2013
 
 @author: dip
 '''
-from pyramid.view import notfound_view_config
-from pyramid.response import Response
+#from pyramid.view import notfound_view_config
+#from pyramid.response import Response
 
 
-@notfound_view_config()
-def notfound_view_get(request):
-    '''
-    All Not found gets redirected here
-    '''
-    # TODO: waiting for static page
-    # TODO: we can separate  GET and POST request errors
-    return Response('TODO customize error', status='404 Not Found')
+#@notfound_view_config()
+#def notfound_view_get(request):
+#    '''
+#    All Not found gets redirected here
+#    '''
+#    # TODO: waiting for static page
+#    # TODO: we can separate  GET and POST request errors
+#    return Response('TODO customize error', status='404 Not Found')

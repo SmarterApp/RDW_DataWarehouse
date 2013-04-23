1a8c01946ac2bf5e3e349d11328b7b9686af6a92
Doris Ip <dip@wgen.net>
2013-04-23 17:06:19 -0400

    add logging into jenkins script

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -268,7 +268,8 @@ function build_rpm {
     
     scp /home/jenkins/rpmbuild/RPMS/x86_64/smarter-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm pynest@${PYNEST_SERVER}:/opt/wgen/rpms
     ssh pynest@${PYNEST_SERVER} "ln -sf /opt/wgen/rpms/smarter-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm /opt/wgen/rpms/smarter-latest.rpm"
-  
+    
+    echo "Upload to pulp" 
     pulp-admin content upload --dir /home/jenkins/rpmbuild/RPMS/x86_64 --repoid edware-el6-x86_64-upstream --nosig -v
 
     echo "Finished building RPM"

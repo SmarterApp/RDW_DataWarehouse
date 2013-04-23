a4d5ed7d5f1469cfa43c483d27aa384cfa5461b5
Seth Wimberly <swimberly@wgen.net>
2013-04-23 17:21:03 -0400

    Unit tests and epydocs
    Former-commit-id: 46c632edccb3e1308be4f8e36b4328f189f4f596

--- DataGeneration/tests/test_generate_data.py
+++ DataGeneration/tests/test_generate_data.py
@@ -6,10 +6,10 @@ Created on Apr 17, 2013
 import unittest
 import os
 from datetime import date
-from mock import MagicMock
 import generate_data
 import generate_entities
 from helper_entities_2 import District
+from entities_2 import Staff
 
 
 class Test(unittest.TestCase):
@@ -19,11 +19,16 @@ class Test(unittest.TestCase):
         self.perf_lvl_dist = {'ELA': {'3': {'percentages': [30, 34, 28, 9]}},
                               'Math': {'3': {'percentages': [14, 42, 37, 7]}}}
         self.score_details = {'min': 1200, 'max': 2400, 'cut_points': [1400, 1800, 2100]}
-        self.config_module = ConfigModule()
+        self.config_module = DummyClass()
         self.config_module.PERCENTAGES = 'percentages'
         self.config_module.MIN = 'min'
         self.config_module.MAX = 'max'
         self.config_module.CUT_POINTS = 'cut_points'
+        self.config_module.FROM_DATE = 'from_date'
+        self.config_module.MOST_RECENT = 'most_recent'
+        self.config_module.TO_DATE = 'to_date'
+        self.config_module.GRADES = 'grades'
+        self.config_module.get_temporal_information = self.get_temporal_information_mock
         generate_data.config_module = self.config_module
 
     def test_generate_name_list_dictionary(self):
@@ -69,14 +74,8 @@ class Test(unittest.TestCase):
             self.assertGreaterEqual(score, self.score_details['min'])
             self.assertLessEqual(score, self.score_details['max'])
 
-    def test_calcuate_claim_scores(self):
-        assmt = self.create_assessment()
-        res = generate_data.calculate_claim_scores(2105, assmt, 32, 8, -10, 25)
-        for claim in res:
-            print(claim.claim_score)
-
     def test_translate_scores_to_assessment_score(self):
-        assmt = self.create_assessment()
+        assmt = self.create_assessment(5, 'Math')
         scores = [2180, 1200, 2400, 1590, 1890, 1800, 2100, 1400]
         expected_pl = [4, 1, 4, 2, 3, 3, 4, 2]
         res = generate_data.translate_scores_to_assessment_score(scores, self.cut_points, assmt, 32, 8, -10, 25)
@@ -84,11 +83,26 @@ class Test(unittest.TestCase):
             self.assertEqual(res[i].perf_lvl, expected_pl[i])
             self.assertIn(res[i].perf_lvl, [1, 2, 3, 4])
 
-    def test_get_subset_of_students(self):
-        students = [object()] * 100
-        print(len(students))
-        res = generate_data.get_subset_of_students(students, .9)
-        self.assertEqual(len(res), 90)
+    def test_generate_students_from_institution_hierarchy(self):
+        number_of_students = 50
+        institution_hierarchy = DummyClass()
+        institution_hierarchy.state_code = 'GA'
+        institution_hierarchy.district_guid = 'dist123'
+        institution_hierarchy.school_guid = 'scho2343'
+        institution_hierarchy.school_name = 'school1'
+        grade = 10
+        section_guid = 'sect123'
+        street_names = ['Broadway', 'Wall', 'Spring', 'Houston', 'Washington', 'Main', 'Fulton', 'Chambers']
+
+        res = generate_data.generate_students_from_institution_hierarchy(number_of_students, institution_hierarchy, grade, section_guid, street_names)
+        self.assertEqual(len(res), number_of_students)
+        for student in res:
+            street_name = student.address_1.split()[1]
+            self.assertIn(street_name, street_names)
+            self.assertEqual(institution_hierarchy.state_code, student.state_code)
+            self.assertEqual(institution_hierarchy.district_guid, student.district_guid)
+            self.assertEqual(institution_hierarchy.school_guid, student.school_guid)
+            self.assertEqual(student.grade, grade)
 
     def test_set_students_rec_id_and_section_id(self):
         old_section_guid = 'old_id'
@@ -106,10 +120,141 @@ class Test(unittest.TestCase):
         for stu in students:
             self.assertEqual(stu.section_guid, 'new_id')
 
+    def test_generate_teaching_staff_from_institution_hierarchy(self):
+        num_of_staff = 50
+        institution_hierarchy = DummyClass()
+        institution_hierarchy.state_code = 'GA'
+        institution_hierarchy.district_guid = 'dict1234'
+        institution_hierarchy.school_guid = 'sch1234'
+        section_guid = 'sect234'
+        temp_data = self.get_temporal_information_mock()
+
+        res = generate_data.generate_teaching_staff_from_institution_hierarchy(num_of_staff, institution_hierarchy, section_guid)
+
+        self.assertEqual(len(res), num_of_staff)
+        for staff in res:
+            self.assertIsInstance(staff, Staff)
+            self.assertEqual(staff.hier_user_type, 'Teacher')
+            self.assertEqual(staff.to_date, temp_data['to_date'])
+            self.assertEqual(staff.from_date, temp_data['from_date'])
+            self.assertEqual(staff.most_recent, temp_data['most_recent'])
+            self.assertEqual(staff.state_code, 'GA')
+            self.assertEqual(staff.district_guid, 'dict1234')
+            self.assertEqual(staff.school_guid, 'sch1234')
+            self.assertEqual(staff.section_guid, 'sect234')
+
+    def test_generate_non_teaching_staff(self):
+        state_code = 'GA'
+        num_of_staff = 50
+        temp_data = self.get_temporal_information_mock()
+        district_guid = 'distguid'
+        school_guid = 'schoolguid'
+        res = generate_data.generate_non_teaching_staff(num_of_staff, state_code, district_guid, school_guid)
+        self.assertEqual(len(res), num_of_staff)
+        for staff in res:
+            self.assertIsInstance(staff, Staff)
+            self.assertEqual(staff.hier_user_type, 'Staff')
+            self.assertEqual(staff.to_date, temp_data['to_date'])
+            self.assertEqual(staff.from_date, temp_data['from_date'])
+            self.assertEqual(staff.most_recent, temp_data['most_recent'])
+            self.assertEqual(staff.state_code, state_code)
+            self.assertEqual(staff.district_guid, district_guid)
+            self.assertEqual(staff.school_guid, school_guid)
+
+    def test_calculate_number_of_schools(self):
+        res = generate_data.calculate_number_of_schools(100, 800, 200)
+        self.assertIsInstance(res, int)
+
+    def test_calculate_number_of_students(self):
+        res = generate_data.calculate_number_of_students(100, 800, 200)
+        self.assertIsInstance(res, int)
+
+    def test_calculate_number_of_sections(self):
+        # TODO: write more test when the method is implemented
+        res = generate_data.calculate_number_of_sections(15)
+        self.assertIsInstance(res, int)
+
+    def test_calcuate_claim_scores(self):
+        assmt = self.create_assessment(1, 'ELA')
+        res = generate_data.calculate_claim_scores(2105, assmt, 32, 8, -10, 25)
+        expected_claims = 3
+        self.assertEqual(len(res), expected_claims)
+        for claim in res:
+            self.assertGreaterEqual(claim.claim_score, assmt.asmt_claim_1_score_min)
+            self.assertLessEqual(claim.claim_score, assmt.asmt_claim_1_score_max)
+            self.assertGreaterEqual(claim.claim_score_interval_minimum, assmt.asmt_claim_1_score_min)
+            self.assertLessEqual(claim.claim_score_interval_maximum, assmt.asmt_claim_1_score_max)
+            self.assertGreaterEqual(claim.claim_score_interval_maximum, claim.claim_score)
+            self.assertLessEqual(claim.claim_score_interval_minimum, claim.claim_score)
+
+    def test_get_flat_grades_list(self):
+        school_config = {
+            'High School': {'grades': [11], 'students': {'min': 100, 'max': 500, 'avg': 300}},
+            'Jr. High School': {'grades': [9, 10, 11], 'students': {'min': 100, 'max': 500, 'avg': 300}},
+            'Middle School': {'grades': [6, 7, 8], 'students': {'min': 50, 'max': 200, 'avg': 150}},
+            'Elementary School': {'grades': [1, 3, 4, 5], 'students': {'min': 20, 'max': 70, 'avg': 60}}
+        }
+        expected_items = [11, 9, 10, 6, 7, 8, 1, 3, 4, 5]
+        res = generate_data.get_flat_grades_list(school_config)
+        self.assertEqual(len(res), len(expected_items))
+        for it in expected_items:
+            self.assertIn(it, res)
+        diffs = set(expected_items) ^ set(res)
+        self.assertFalse(diffs)
+
+    def test_select_assessment_from_list(self):
+        asmt_list = [self.create_assessment(1, 'Math'), self.create_assessment(2, 'Math'), self.create_assessment(3, 'Math'),
+                       self.create_assessment(1, 'ELA'), self.create_assessment(2, 'ELA'), self.create_assessment(3, 'ELA'),
+                       self.create_assessment(6, 'Math'), self.create_assessment(8, 'ELA'), self.create_assessment(12, 'Math')]
+        assessment = generate_data.select_assessment_from_list(asmt_list, 1, 'Math')
+        self.assertEqual(assessment.asmt_grade, 1)
+        self.assertEqual(assessment.asmt_subject, 'Math')
+
+        assessment = generate_data.select_assessment_from_list(asmt_list, 6, 'Math')
+        self.assertEqual(assessment.asmt_grade, 6)
+        self.assertEqual(assessment.asmt_subject, 'Math')
+
+        assessment = generate_data.select_assessment_from_list(asmt_list, 10, 'ELA')
+        self.assertIsNone(assessment)
+
+        assessment = generate_data.select_assessment_from_list([], 10, 'ELA')
+        self.assertIsNone(assessment)
+
+    def test_get_subset_of_students(self):
+        students = [object()] * 100
+        print(len(students))
+        res = generate_data.get_subset_of_students(students, .9)
+        self.assertEqual(len(res), 90)
+
+    # ----------------------
     # Helper functions
-    def create_assessment(self):
-        asmts = generate_entities.generate_assessments([5], self.cut_points, date.today(), True, date.today())
-        return asmts[0]
+    # ----------------------
+    def create_assessment(self, grade, subject):
+        assmt = DummyClass()
+        assmt.asmt_subject = subject
+        assmt.asmt_grade = grade
+        assmt.asmt_claim_1_name = 'claim1'
+        assmt.asmt_claim_1_score_min = self.score_details['min']
+        assmt.asmt_claim_1_score_max = self.score_details['max']
+        assmt.asmt_claim_1_score_weight = .15 if subject == 'Math' else .4
+        assmt.asmt_claim_2_name = 'claim2'
+        assmt.asmt_claim_2_score_min = self.score_details['min']
+        assmt.asmt_claim_2_score_max = self.score_details['max']
+        assmt.asmt_claim_2_score_weight = .1 if subject == 'Math' else .35
+        assmt.asmt_claim_3_name = 'claim3'
+        assmt.asmt_claim_3_score_min = self.score_details['min']
+        assmt.asmt_claim_3_score_max = self.score_details['max']
+        assmt.asmt_claim_3_score_weight = .3 if subject == 'Math' else .25
+        assmt.asmt_claim_4_name = 'claim4' if subject == 'Math' else None
+        assmt.asmt_claim_4_score_min = self.score_details['min'] if subject == 'Math' else None
+        assmt.asmt_claim_4_score_max = self.score_details['max'] if subject == 'Math' else None
+        assmt.asmt_claim_4_score_weight = .45 if subject == 'Math' else None
+        assmt.asmt_score_min = self.score_details['min']
+        assmt.asmt_score_max = self.score_details['max']
+        assmt.asmt_subject = 'Math'
+
+        #asmts = generate_entities.generate_assessments([5], self.cut_points, date.today(), True, date.today())
+        return assmt  # asmts[0]
 
     def create_test_name_files(self, file_count, name_count):
         list_name = 'unit_test_file_for_testing{num}'
@@ -126,8 +271,11 @@ class Test(unittest.TestCase):
         for name in file_list:
             os.remove(name)
 
+    def get_temporal_information_mock(self):
+        return {'from_date': '20120901', 'to_date': None, 'most_recent': True, 'date_taken_year': '2012', 'date_taken_month': ''}
 
-class ConfigModule(object):
+
+class DummyClass(object):
     pass
 
 

907245dec0e42177ce66a915874d490ad39c0b8e
David Wu <dwu@wgen.net>
2013-04-24 12:07:39 -0400

    unit test for session cleanup

--- edauth/edauth/tests/security/test_session_manager.py
+++ edauth/edauth/tests/security/test_session_manager.py
@@ -6,7 +6,7 @@ Created on Feb 15, 2013
 import unittest
 from edauth.security.session_manager import get_user_session, \
     create_new_user_session, update_session_access, delete_session, \
-    is_session_expired
+    is_session_expired, cleanup_sessions
 from edauth.security.roles import Roles
 import uuid
 from datetime import datetime, timedelta
@@ -15,6 +15,7 @@ from edauth.tests.test_helper.read_resource import create_SAMLResponse
 from database.sqlite_connector import create_sqlite, destroy_sqlite
 from edauth.persistence.persistence import generate_persistence
 from edauth.database.connector import EdauthDBConnection
+from sqlalchemy import select, func
 
 
 class TestSessionManager(unittest.TestCase):
@@ -83,6 +84,28 @@ class TestSessionManager(unittest.TestCase):
     def test_create_session_with_no_roles(self):
         session = create_new_user_session(create_SAMLResponse('SAMLResponse_no_memberOf.xml'))
         self.assertEquals(session.get_roles(), [Roles.get_invalid_role()], "no memberOf should have insert a role of none")
+
+    def test_cleanup_sessions(self):
+        num_sessions = 20
+        session_json = '{"roles": ["TEACHER"], "name": {"fullName": "Linda Kim"}, "uid": "linda.kim"}'
+        current_datetime = datetime.now()
+        expiration_datetime = current_datetime + timedelta(seconds=30)
+        with EdauthDBConnection() as connection:
+            user_session = connection.get_table('user_session')
+            # prepare mock sessions in database
+            for i in range(num_sessions):
+                connection.execute(user_session.insert(), session_id=str(uuid.uuid1()), session_context=session_json, last_access=current_datetime, expiration=expiration_datetime)
+
+            # check that sessions exist
+            query = select([func.count().label('count')]).select_from(user_session)
+            result = connection.get_result(query)
+            self.assertEqual(result[0]['count'], num_sessions, "session count incorrect")
+
+            cleanup_sessions()
+
+            # check that no sessions exist
+            result = connection.get_result(query)
+            self.assertEqual(result[0]['count'], 0, "session count incorrect")
 
 
 if __name__ == "__main__":

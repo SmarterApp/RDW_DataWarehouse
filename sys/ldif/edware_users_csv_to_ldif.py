6c11d899da150576fc1bf49f09f85c7349761521
James S. Becker III <jbecker@wgen.net>
2013-04-26 15:57:39 -0400

    converted passwds to all lower... passwd = fname[0:1].lower() + lname.lower() + "1234"

--- sys/ldif/edware_users_csv_to_ldif.py
+++ sys/ldif/edware_users_csv_to_ldif.py
@@ -146,7 +146,7 @@ def format_inetOrgPerson(_user, _uid, _email, _passwd):
         fname = user[1]
         lname = user[2]
     
-    passwd = fname[0:1] + lname + "1234"
+    passwd = fname[0:1].lower() + lname.lower() + "1234"
     
     strInetOrgPerson =  "dn: cn="+ _user +",ou=people,ou=environment,dc=edwdc,dc=net\n" + \
                         "objectClass: inetOrgPerson\n" + \

b54aa83f3ce7b4946582a95fd88411bf5688376b
Eugene Jen <ejen@wgen.net>
2013-05-02 13:13:55 -0400

    add some update to smooth starting rabbitmq and celery

---
+++ scripts/start_udl.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# I set up my virtualenv for python3.3 under ~/ejen/py33/bin
+
+source ~/py33/bin/activate
+cd ../src
+python start_udl.py
+
+
--- src/start_udl.py
+++ src/start_udl.py
@@ -3,9 +3,20 @@ import subprocess
 
 
 def start_rabbitmq(RABBITMQ_SERVER):
-    subprocess.call(["/usr/bin/sudo", RABBITMQ_SERVER])
+    try:    
+        subprocess.call(["sudo", RABBITMQ_SERVER])
+    except Exception as e:
+        print(e)
+
+def start_celery():
+    try:
+        subprocess.call(["celery", "-A", "udl_workers", "worker"])    
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
     RABBITMQ_SERVER='/opt/local/sbin/rabbitmq-server'
     start_rabbitmq(RABBITMQ_SERVER)
+    start_celery()
 

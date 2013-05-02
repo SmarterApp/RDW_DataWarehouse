c78e4520185ec6b6cb6e0a7ccd87d29d5588e306
Eugene Jen <ejen@wgen.net>
2013-05-02 13:36:36 -0400

    start rabbitmq at background

--- src/start_udl.py
+++ src/start_udl.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import subprocess
-
+import time
 
 def start_rabbitmq(RABBITMQ_SERVER):
     try:    
-        subprocess.call(["sudo", RABBITMQ_SERVER])
+        subprocess.call(["sudo " +  RABBITMQ_SERVER + " &"], shell=True)
     except Exception as e:
         print(e)
 
 def start_celery():
     try:
-        subprocess.call(["celery", "-A", "udl_workers", "worker"])    
+        subprocess.call(["celery -A udl_workers worker"], shell=True)    
     except Exception as e:
         print(e)
 
@@ -18,5 +18,7 @@ def start_celery():
 if __name__ == '__main__':
     RABBITMQ_SERVER='/opt/local/sbin/rabbitmq-server'
     start_rabbitmq(RABBITMQ_SERVER)
+    # wait for rabbit started
+    time.sleep(10)
     start_celery()
 

587d064c16d656b1d3e6620d6d67992c275ffda6
Eugene Jen <ejen@wgen.net>
2013-05-02 14:54:52 -0400

    add randomized delay for udl_workers to test delay features

--- src/udl_workers.py
+++ src/udl_workers.py
@@ -1,17 +1,25 @@
 from celery import Celery
+import time
+import random
 
 celery = Celery('udl_workers', backend='amqp://guest@localhost//', broker='amqp://guest@localhost//')
 
 @celery.task
 def W_file_splitter(msg):
+    # randomize delay seconds
+    time.sleep(random.random() * 10)
     return msg
 
 @celery.task
 def W_file_loader(msg):
+    # randomize delay second
+    time.sleep(random.random() * 10)
     return msg
 
 @celery.task
 def W_final_cleanup(msg):
+    # randomize delay seconds
+    time.sleep(random.random() * 10)
     return msg
 
 

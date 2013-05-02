86f6f224f142ee716774ce50dd1ae2c1b8fe3114
Eugene Jen <ejen@wgen.net>
2013-05-02 13:05:20 -0400

    Add initial task skelton

---
+++ src/udl_workers.py
@@ -0,0 +1,20 @@
+from celery import Celery
+
+celery = Celery('udl_workers', broker='amqp://guest@localhost//')
+
+@celery.task
+def W_file_splitter(msg):
+    return msg
+
+@celery.task
+def W_file_loader(msg):
+    return msg
+
+@celery.task
+def W_final_cleanup(msg):
+    return msg
+
+
+
+
+

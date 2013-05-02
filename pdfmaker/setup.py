caf114813d9039796e348380d61ae33716db247d
Doris Ip <dip@wgen.net>
2013-05-02 17:16:27 -0400

    initial pdfmaker project

---
+++ pdfmaker/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+install_requires = [
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'nose >= 1.2.1',
+    'coverage',
+    'virtualenv']  # for scaffolding tests
+
+
+docs_extras = [
+    'Sphinx',
+    'docutils',
+    'repoze.sphinx.autointerface']
+
+setup(name='pdfmaker',
+      version='0.1',
+      description='Generic Reporting Platform',
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application", ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=tests_require,
+      test_suite="nose.collector",
+      install_requires=install_requires,
+      extras_require={
+          'docs': docs_extras, },
+      entry_points="""\
+      """,
+      )

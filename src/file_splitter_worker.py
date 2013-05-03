4246724f5f3eeb8339b41582573f243fba86ce7e
Lili Chen <lichen@wgen.net>
2013-05-03 18:22:40 -0400

    move file_splitter_worker.py under src folder

--- src/file_splitter_worker.py
+++ src/file_splitter_worker.py
@@ -1,4 +1,4 @@
-from tasks import file_splitter_task
+from udl2.W_file_splitter import task
 import argparse
 import os
 
@@ -9,32 +9,39 @@ def main():
 
     # verify input parameters
     try:
-        if int(parm['parts']) <= 0:
+        if int(parm['parts']) < 0:
             print("The value for parts should be a positive integer")
             exit()
-        if not is_valid_file(parm['input_file']):
-            print('The input file is not valid')
+        if int(parm['number_of_rows']) < 0:
+            print("The value for number of rows should be a positive integer")
             exit()
-        # if not os.path.isdir(parm['output_path']):
-        #    os.makedirs(parm['output_path'])
+        if not is_valid_file(parm['input_file']):
+            print('The input file is invalid')
+            exit()
+        if not os.path.isdir(parm['output_path']):
+            os.makedirs(parm['output_path'])
+
     except Exception as e:
         print(e)
     # execute the file_splitter task
-    split_file_result = file_splitter_task.apply_async([parm], queue='Q_files_to_be_split')
+    split_file_result = task.apply_async([parm], queue='Q_files_to_be_split')
     print("Status", split_file_result.status)
 
 
 def get_parameters():
     parser = argparse.ArgumentParser(description='File Splitter Worker')
-    parser.add_argument("-p", "--parts", default=4, help="number of parts to be splitted")
+    parser.add_argument("-p", "--parts", type=int, default=0, help="number of parts to be splitted")
+    parser.add_argument("-n", "--number_of_rows", type=int, default=100, help="number of rows in each sub file")
     parser.add_argument("-i", "--input_file", required=True, help="input file")
     parser.add_argument("-o", "--output_path", required=True, help="output directory")
     args = parser.parse_args()
     conf = {
             'parts': args.parts,
+            'number_of_rows': args.number_of_rows,
             'input_file': args.input_file,
             'output_path': args.output_path
             }
+    print(conf)
     return conf
 
 
--- src/udl2/W_file_splitter.py
+++ src/udl2/W_file_splitter.py
@@ -14,14 +14,15 @@ logger = get_task_logger(__name__)
 @udl2.celery.celery.task(name="udl2.W_file_splitter.task")
 def task(msg):
     # place holder for file_splitter
-    # splitted_files = file_splitter_impl()
+    # split_files = file_splitter_impl()
     # the input parameter for method split_file TBD
-    splitted_files = file_splitter.split_file(msg)
-    number_of_files = len(splitted_files)
+    print('message is ', msg)
+    split_files = file_splitter.split_file(msg['input_file'])
+    number_of_files = len(split_files)
     time.sleep(random.random() * 10)
     logger.info(task.name)
     for i in range(0, number_of_files):
-        udl2.W_file_loader.task.apply_async([(msg + ' part %i of %i file %s passed after ' + task.name) % (i + 1, number_of_files, splitted_files[i])],
+        udl2.W_file_loader.task.apply_async([('part %i of %i file %s passed after ' + task.name) % (i + 1, number_of_files, split_files[i])],
                                                queue='Q_files_to_be_loaded',
                                                routing_key='udl2')
     return msg

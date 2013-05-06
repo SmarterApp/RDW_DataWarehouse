54fc4f64ec5bd38990dc5dffee0cdea36dbf7948
Andrew Brien <abrien@wgen.net>
2013-05-06 15:58:19 -0400

    File splitter now writes to designated work zone

--- src/driver.py
+++ src/driver.py
@@ -11,7 +11,9 @@ import time
 THIS_MODULE_PATH = os.path.abspath(__file__)
 SRC_DIRECTORY = os.path.dirname(THIS_MODULE_PATH)
 ROOT_DIRECTORY = os.path.dirname(SRC_DIRECTORY)
-LANDING_ZONE = os.path.join(ROOT_DIRECTORY, 'landing_zone')
+ZONES = os.path.join(ROOT_DIRECTORY, 'zones')
+LANDING_ZONE = os.path.join(ZONES, 'landing')
+WORK_ZONE = os.path.join(ZONES, 'work')
 DATAFILES = os.path.join(ROOT_DIRECTORY, 'datafiles')
 
 
@@ -19,7 +21,7 @@ def start_pipeline(file_path):
     landing_zone_file_name = create_unique_file_name(file_path)
     landing_zone_file_path = os.path.join(LANDING_ZONE, landing_zone_file_name)
     shutil.copy(file_path, landing_zone_file_path)
-    udl2.W_file_splitter.task.apply_async([{'input_file':landing_zone_file_path}], queue='Q_files_to_be_split')
+    udl2.W_file_splitter.task.apply_async([{'input_file':landing_zone_file_path, 'output_path': WORK_ZONE}], queue='Q_files_to_be_split')
     
     
 def create_unique_file_name(file_path):
@@ -32,17 +34,19 @@ def create_unique_file_name(file_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', dest='row_multiplier', type=int, default=1, help="number of times to muliply the rows in the seed file.")
-    parser.add_argument('-c', dest='column_multiplier', type=int, default=1, help="number of times to multiply the columns in the seed file.")
+    parser.add_argument('-r', dest='row_multiplier', required=False, type=int, default=1, help="number of times to muliply the rows in the seed file.")
+    parser.add_argument('-c', dest='column_multiplier', required=False, type=int, default=1, help="number of times to multiply the columns in the seed file.")
     parser.add_argument('-s', dest='source_csv', required=False, default=os.path.join(DATAFILES, 'seed.csv'), help="path to the source file, default is seed.csv")
     parser.add_argument('-o', dest='output_data_csv', required=False, default=os.path.join(DATAFILES, 'output.csv'), help="path to the file that will contain the newly generated csv data.")
-    parser.add_argument('-m', dest='output_metadata_csv', required=False, default=os.path.join(DATAFILES, 'output_meta_data.csv'), help="path to the file that will contain the newly generated csv metadata.")
     args = parser.parse_args()
 
-    conf = {'row_multiplier': args.row_multiplier,
-            'column_multiplier': args.column_multiplier,
-            'source_csv': args.source_csv,
-            'output_data_csv': args.output_data_csv}
+    if args.source_csv == str(os.path.join(DATAFILES, 'seed.csv')):
+        conf = {'row_multiplier': args.row_multiplier,
+                'column_multiplier': args.column_multiplier,
+                'source_csv': args.source_csv,
+                'output_data_csv': args.output_data_csv}
+        csv_file_path = generate_stretched_csv_file(conf)
+    else:
+        csv_file_path = args.source_csv
 
-    csv_file_path = generate_stretched_csv_file(conf)
     start_pipeline(csv_file_path)

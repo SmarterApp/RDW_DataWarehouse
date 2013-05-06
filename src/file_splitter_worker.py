bf6250b06267f00524448fb856feba67c2d8b96d
Lili Chen <lichen@wgen.net>
2013-05-06 16:23:19 -0400

    update comments

--- src/file_splitter_worker.py
+++ src/file_splitter_worker.py
@@ -10,10 +10,10 @@ def main():
 
     # verify input parameters
     try:
-        if int(parm['parts']) < 0:
+        if parm['parts'] < 0:
             print("The value for parts should be a positive integer")
             exit()
-        if int(parm['row_limit']) < 0:
+        if parm['row_limit'] < 0:
             print("The value for number of rows should be a positive integer")
             exit()
         if not is_valid_file(parm['input_file']):
@@ -27,22 +27,27 @@ def main():
     # execute the file_splitter task
     split_file_result = task.apply_async([parm], queue='Q_files_to_be_split')
     print("Status", split_file_result.status)
+    print("Result", split_file_result.get())
 
 
 def get_parameters():
+    '''
+    Returns parameters from command line as a dictionary
+    '''
+
+    # default values for the arguments are defined in FILE_SPLITTER_CONF
     parser = argparse.ArgumentParser(description='File Splitter Worker')
-    parser.add_argument("-p", "--parts", type=int, default=FILE_SPLITTER_CONF['parts'], help="number of parts to be splitted")
+    parser.add_argument("-p", "--parts", type=int, default=FILE_SPLITTER_CONF['parts'], help="number of parts to be split")
     parser.add_argument("-r", "--row_limit", type=int, default=FILE_SPLITTER_CONF['row_limit'], help="number of rows in each sub file")
     parser.add_argument("-i", "--input_file", required=True, help="input file")
     parser.add_argument("-o", "--output_path", default=FILE_SPLITTER_CONF['output_path'], help="output directory")
-    parser.add_argument("-k", "--keep_headers", default=FILE_SPLITTER_CONF['keep_headers'], help="keep headers in split files or not")
     args = parser.parse_args()
     conf = {
             'parts': args.parts,
             'row_limit': args.row_limit,
             'input_file': args.input_file,
             'output_path': args.output_path,
-            'keep_headers': args.keep_headers
+            'keep_headers': FILE_SPLITTER_CONF['keep_headers']
             }
     print(conf)
     return conf

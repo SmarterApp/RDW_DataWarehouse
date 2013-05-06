fb38c0c46ba789f529c39b7e43151134f789e9db
Doris Ip <dip@wgen.net>
2013-05-06 17:17:42 -0400

    Support for celery server in dev mode

--- pdfmaker/pdfmaker/pdf.py
+++ pdfmaker/pdfmaker/pdf.py
@@ -5,10 +5,33 @@ Created on May 3, 2013
 '''
 from celery import Celery
 from pdfmaker import celeryconfig
+import os
 
 
 celery = Celery('pdf')
 celery.config_from_object(celeryconfig)
+
+
+def start():
+    '''
+    Start celery.  Intended for dev mode
+    '''
+    here = os.path.abspath(os.path.dirname(__file__))
+    current_dir = os.getcwd()
+    try:
+        os.chdir(here)
+        args = ['celery', 'worker', '--app', 'pdfmaker.pdf']
+        celery.start(args)
+    finally:
+        os.chdir(current_dir)
+
+
+def stop():
+    '''
+    Stop Celery
+    '''
+    celery.close()
+
 
 if __name__ == '__main__':
     celery.start()

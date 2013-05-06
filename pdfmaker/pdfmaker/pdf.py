75c2b27c8f9bd5b1f6e660549b2c9fa27907da27
Doris Ip <dip@wgen.net>
2013-05-06 12:11:44 -0400

    remove extra celery initialization

--- pdfmaker/pdfmaker/pdf.py
+++ pdfmaker/pdfmaker/pdf.py
@@ -4,6 +4,8 @@ Created on May 3, 2013
 @author: dip
 '''
 from celery import Celery
+
+
 celery = Celery('pdfmaker', backend='amqp', broker='amqp://guest@localhost//', include=['tasks'])
 
 if __name__ == '__main__':
--- pdfmaker/pdfmaker/tasks.py
+++ pdfmaker/pdfmaker/tasks.py
@@ -3,11 +3,10 @@ Created on May 2, 2013
 
 @author: tosako
 '''
-from celery import Celery
 import subprocess
-celery = Celery('pdfmaker', backend='amqp', broker='amqp://guest@localhost//', include=['tasks'])
+from pdfmaker.pdf import celery
+
 
 @celery.task
 def generate_pdf(cookie, url, outputfile):
-    return subprocess.call(['wkhtmltopdf', '--grayscale', '--page-size', 'Letter', '--enable-javascript', '--javascript-delay', '10000', '--cookie', 'edware', cookie, url, outputfile]);
-    
+    return subprocess.call(['wkhtmltopdf', '--grayscale', '--page-size', 'Letter', '--enable-javascript', '--javascript-delay', '10000', '--cookie', 'edware', cookie, url, outputfile])

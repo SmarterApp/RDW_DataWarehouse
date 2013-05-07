200ffa87a9edf0ad3746c788e18438ef279a5bb2
agrebnevawgen <agrebneva@gmail.com>
2013-05-07 09:44:43 -0400

    configurable options

--- pdfmaker/pdfmaker/tasks.py
+++ pdfmaker/pdfmaker/tasks.py
@@ -6,7 +6,10 @@ Created on May 2, 2013
 import subprocess
 from pdfmaker.pdf import celery
 
+pdf_procs = ['wkhtmltopdf']
+pdf_defaults = ['--enable-javascript', '--page-size', 'Letter', '--javascript-delay', '3000']
+
 
 @celery.task(name='tasks.generate_pdf')
-def generate_pdf(cookie, url, outputfile):
-    return subprocess.call(['wkhtmltopdf', '--grayscale', '--page-size', 'Letter', '--enable-javascript', '--javascript-delay', '10000', '--cookie', 'edware', cookie, url, outputfile])
+def generate_pdf(cookie, url, outputfile, options=pdf_defaults):
+    return subprocess.call(pdf_procs + options + ['--cookie', 'edware', cookie, url, outputfile])

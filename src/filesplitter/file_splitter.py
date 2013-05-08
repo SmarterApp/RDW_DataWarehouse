c013336d0dc22515d1e98160b2aaa583fedafe8f
Iqbal Gill <igill@igill-mac.wgenhq.net>
2013-05-08 11:51:44 -0400

    Refactored file splitter, created helper functions

--- src/filesplitter/file_splitter.py
+++ src/filesplitter/file_splitter.py
@@ -5,6 +5,30 @@ import math
 import argparse
 import time
 
+def create_output_destination(file_name):
+	#create output template from supplied input file path
+    base =  os.path.splitext(os.path.basename(file_name))[0]
+    output_name_template = base+'_part_'
+    
+    #create output directory
+    timestamp = int(time.time())
+    output_dir = os.path.join(output_path,base,str(timestamp))
+    if not os.path.exists(output_dir): os.makedirs(output_dir)
+    
+    return output_name_template,output_dir
+    
+def run_command(cmd_string)
+    p = subprocess.Popen(cmd_string, stdout=subprocess.PIPE,shell=True)
+    (output,err) = p.communicate()
+    return output,err
+    
+def get_list_split_files(output_name_template,output_dir):
+    output_list=[]
+    abs_path = os.path.abspath(output_dir)
+    for files in os.listdir(output_dir):
+    	if output_name_template in files: output_list.append(os.path.join(abs_path,files))
+    return output_list
+
 def split_file(file_name, delimiter=',', row_limit=10000, parts=0, output_path='.'):
     isValid = os.path.exists(file_name) and os.path.isfile(file_name)
     if isValid is False:
@@ -19,35 +43,22 @@ def split_file(file_name, delimiter=',', row_limit=10000, parts=0, output_path='
     
     #create copy of csv without the header
     remove_header_cmd = "sed '1d' %s > noheaders.csv" % file_name
-    p = subprocess.Popen(remove_header_cmd, stdout=subprocess.PIPE,shell=True)
-    (output,err) = p.communicate()
-
+    run_command(remove_header_cmd)
     
     #if going by parts, get total rows and define row limit
     if parts > 0:
         word_count_cmd = "wc noheaders.csv"
-        p = subprocess.Popen(word_count_cmd, stdout=subprocess.PIPE,shell=True)
-        (output,err) = p.communicate()
-
-        #print("Output is ",output.decode('utf-8'))
-        #print("Err is ",err)
+        run_command(word_count_cmd)
 
         totalrows = int(output.split()[0])
         row_limit = math.ceil(totalrows / parts) # round up for row limit
     
-    #create output template from supplied input file path
-    base =  os.path.splitext(os.path.basename(file_name))[0]
-    output_name_template = base+'_part_'
-    
-    #create output directory
-    timestamp = int(time.time())
-    output_dir = os.path.join(output_path,base,str(timestamp))
-    if not os.path.exists(output_dir): os.makedirs(output_dir)
+    #set up output location
+    output_name_template,output_dir = create_output_destination(file_name)
     
     #call unix split command
     split_command = 'split -a1 -l %d noheaders.csv %s' % (row_limit, os.path.join(output_dir,output_name_template))
-    p = subprocess.Popen(split_command, stdout=subprocess.PIPE,shell=True)
-    (output,err) = p.communicate()
+    run_command(split_command)
     
     #clean up
     os.remove('noheaders.csv')
@@ -57,11 +68,7 @@ def split_file(file_name, delimiter=',', row_limit=10000, parts=0, output_path='
     current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
     current_out_writer.writerow(header)
     
-    output_list=[]
-    abs_path = os.path.abspath(output_dir)
-    for files in os.listdir(output_dir):
-    	if output_name_template in files: output_list.append(os.path.join(abs_path,files))
-    return output_list
+    return get_list_split_files(output_name_template,output_dir)
 
         
 if __name__ == "__main__":

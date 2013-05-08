6c5e70f7f2b0eef344a9da094b41f81551d0e1c9
Doris Ip <dip@wgen.net>
2013-05-08 10:11:03 -0400

    refactor and add UT

--- smarter/smarter/security/context.py
+++ smarter/smarter/security/context.py
@@ -15,11 +15,6 @@ def select_with_context(columns=None, whereclause=None, from_obj=[], **kwargs):
                        'STUDENT': append_student_context,
                        'PARENT': append_parent_context}
 
-    # Maps to function that queries database for context
-    cacheable_context_mapping = {'TEACHER': get_teacher_cacheable_context,
-                                 'STUDENT': get_student_cacheable_context,
-                                 'PARENT': get_parent_cacheable_context}
-
     with SmarterDBConnection() as connector:
 
         # get role and context
@@ -32,19 +27,13 @@ def select_with_context(columns=None, whereclause=None, from_obj=[], **kwargs):
 
         user_mapping = connector.get_table('user_mapping')
         guid_query = select([user_mapping.c.staff_guid],
-                            from_obj=[user_mapping])
+                            from_obj=[user_mapping], limit=1)
         guid_query = guid_query.where(user_mapping.c.user_id == user_id)
         result = connector.get_result(guid_query)
 
         guid = None
         if result:
             guid = result[0]['staff_guid']
-
-        # Query db to get the context
-        context = None
-        role_context_method = cacheable_context_mapping.get(role, None)
-        if role_context_method:
-            context = role_context_method(connector, guid)
 
         query = Select(columns, whereclause=whereclause, from_obj=from_obj, **kwargs)
 
@@ -54,12 +43,12 @@ def select_with_context(columns=None, whereclause=None, from_obj=[], **kwargs):
         # TODO: we probably need some kind of generic context in case there is a role that isn't mapped
         if context_method:
             # apply context security
-            query = context_method(connector, query, context)
+            query = context_method(connector, query, guid)
 
     return query
 
 
-def get_teacher_cacheable_context(connector, guid):
+def get_teacher_context(connector, guid):
     context = []
     if guid is not None:
         dim_staff = connector.get_table('dim_staff')
@@ -72,30 +61,39 @@ def get_teacher_cacheable_context(connector, guid):
     return context
 
 
-def get_student_cacheable_context(connector, guid):
+def get_student_context(connector, guid):
     context = []
     if guid is not None:
-        context = [guid]
+        dim_student = connector.get_table('dim_student')
+        context_query = select([dim_student.c.student_guid],
+                               from_obj=[dim_student], limit=1)
+        context_query = context_query.where(dim_student.c.student_guid == guid)
+        results = connector.get_result(context_query)
+        if results:
+            context.append(results[0]['student_guid'])
     return context
 
 
-def get_parent_cacheable_context(connector, guid):
+def get_parent_context(connector, guid):
     # TODO
     context = ['3efe8485-9c16-4381-ab78-692353104cce', '34b99412-fd5b-48f0-8ce8-f8ca3788634a&sl=1367959810']
     return context
 
 
-def append_teacher_context(connector, query, context):
+def append_teacher_context(connector, query, guid):
     fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
+    context = get_teacher_context(connector, guid)
     return query.where(fact_asmt_outcome.c.section_guid.in_(context))
 
 
-def append_student_context(connector, query, context):
+def append_student_context(connector, query, guid):
     fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
+    context = get_student_context(connector, guid)
     return query.where(fact_asmt_outcome.c.student_guid.in_(context))
 
 
-def append_parent_context(connector, query, context):
+def append_parent_context(connector, query, guid):
     # TODO
     fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
+    context = get_parent_context(connector, guid)
     return query.where(fact_asmt_outcome.c.student_guid.in_(context))
--- smarter/smarter/tests/security/test_context.py
+++ smarter/smarter/tests/security/test_context.py
@@ -8,8 +8,8 @@ from smarter.tests.utils.unittest_with_smarter_sqlite import Unittest_with_smart
 from pyramid import testing
 from pyramid.testing import DummyRequest
 from smarter.database.connector import SmarterDBConnection
-from smarter.security.context import get_teacher_cacheable_context,\
-    get_student_cacheable_context, append_teacher_context,\
+from smarter.security.context import get_teacher_context,\
+    get_student_context, append_teacher_context,\
     append_student_context
 from sqlalchemy.sql.expression import Select
 
@@ -29,45 +29,39 @@ class TestContext(Unittest_with_smarter_sqlite):
         # TODO
         pass
 
-    def test_get_teacher_cacheable_context(self):
+    def test_get_teacher_context(self):
         guid = "272"
         with SmarterDBConnection() as connector:
-            context = get_teacher_cacheable_context(connector, guid)
+            context = get_teacher_context(connector, guid)
             self.assertListEqual(context, ['345'])
 
-    def test_get_student_cacheable_context(self):
-        guid = "123"
+    def test_get_student_context(self):
+        guid = "a016a4c1-5aca-4146-a85b-ed1172a01a4d"
         with SmarterDBConnection() as connector:
-            context = get_student_cacheable_context(connector, guid)
+            context = get_student_context(connector, guid)
             self.assertListEqual(context, [guid])
 
     def test_append_teacher_context(self):
         with SmarterDBConnection() as connector:
-            context = ['345']
+            guid = '963'
             fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
             query = Select([fact_asmt_outcome.c.section_guid],
                            from_obj=([fact_asmt_outcome]))
-            # Query without the where cause
-            all_results = connector.get_result(query)
-            query = append_teacher_context(connector, query, context)
+            query = append_teacher_context(connector, query, guid)
 
             results = connector.get_result(query)
-            self.assertTrue(len(all_results) > len(results))
             for result in results:
-                self.assertEqual(result['section_guid'], '345')
+                self.assertEqual(result['section_guid'], '974')
 
     def test_append_student_context(self):
         with SmarterDBConnection() as connector:
-            context = ['61ec47de-e8b5-4e78-9beb-677c44dd9b50']
+            guid = '61ec47de-e8b5-4e78-9beb-677c44dd9b50'
             fact_asmt_outcome = connector.get_table('fact_asmt_outcome')
             query = Select([fact_asmt_outcome.c.student_guid],
                            from_obj=([fact_asmt_outcome]))
-            # Query without the where cause
-            all_results = connector.get_result(query)
-            query = append_student_context(connector, query, context)
+            query = append_student_context(connector, query, guid)
 
             results = connector.get_result(query)
-            self.assertTrue(len(all_results) > len(results))
             for result in results:
                 self.assertEqual(result['student_guid'], '61ec47de-e8b5-4e78-9beb-677c44dd9b50')
 

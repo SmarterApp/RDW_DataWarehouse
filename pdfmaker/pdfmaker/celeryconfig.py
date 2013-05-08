359e44190f4b15df2084234ec4e4da94fb51eae2
Daniel Wu <dawu@wgen.net>
2013-05-08 17:23:14 -0400

    move celery configration from celeryconfig.py to celeryconfig.ini

--- pdfmaker/pdfmaker/celeryconfig.py
+++ pdfmaker/pdfmaker/celeryconfig.py
@@ -3,13 +3,30 @@ Created on May 3, 2013
 
 @author: dip
 '''
-# Broker setting
-BROKER_URL = 'amqp://guest@localhost//'
+import configparser
+import os 
 
-# List of modules to import when celery starts
-CELERY_INCLUDE = ("tasks",)
-
-# Using rabbitmq for task state and results
-CELERY_RESULT_BACKEND = 'amqp'
-
-CELERYD_CONCURRENCY = '8'
+class CeleryConfig:
+    def __init__(self):
+        # load configuration file
+        self.config = configparser.RawConfigParser()
+        resources_dir = os.path.dirname(os.path.dirname(__file__))
+        self.config.read(os.path.join(resources_dir , 'celeryconfig.ini'))
+        self.read("Celery Config")
+        
+    def read(self, section):
+        self.dict = {}
+        options = self.config.options(section)
+        for option in options:
+            try:
+                self.dict[option.upper()] = self.config.get(section, option)
+            except:
+                print("exception on %s!" % option)
+                self.dict[option] = None
+        
+    def __getattr__(self, name):
+        if name in self.dict:
+            return self.dict[name]
+        else:
+            raise AttributeError()
+    

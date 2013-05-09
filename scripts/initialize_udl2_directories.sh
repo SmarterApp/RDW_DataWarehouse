7c67358abad63e522798f18be2f095d875acf66d
Eugene Jen <ejen@wgen.net>
2013-05-09 17:56:41 -0400

    fix landing zone creation for permissions in previous version

--- scripts/initialize_udl2_directories.sh
+++ scripts/initialize_udl2_directories.sh
@@ -3,17 +3,17 @@
 echo "make option directory"
 sudo mkdir -p /opt/wgen/edware-udl/etc
 # we need to fix permission later not to own by root by udl app user
-chmod 755 /opt/wgen
-chmod 755 /opt/wgen/edware-udl
-chmod 777 /opt/wgen/edware-udl/etc
+sudo chmod 755 /opt/wgen
+sudo chmod 755 /opt/wgen/edware-udl
+sudo chmod 777 /opt/wgen/edware-udl/etc
 
 echo "make log directory"
 sudo mkdir -p /var/log/wgen/edware-udl/logs
 # we need to fix permission later not to own by root by udl app user
-chmod 755 /var/log/
-chmod 755 /var/log/wgen
-chmod 755 /var/log/wgen/edware-udl/
-chmod 777 /var/log/wgen/edware-udl/logs
+sudo chmod 755 /var/log/
+sudo chmod 755 /var/log/wgen
+sudo chmod 755 /var/log/wgen/edware-udl/
+sudo chmod 777 /var/log/wgen/edware-udl/logs
 
 echo "make zones directory"
 sudo mkdir -p /opt/wgen/edware-udl/zones/
@@ -21,7 +21,7 @@ sudo mkdir -p /opt/wgen/edware-udl/zones/landing
 sudo mkdir -p /opt/wgen/edware-udl/zones/work
 sudo mkdir -p /opt/wgen/edware-udl/zones/history
 # we need to fix permission later not to own by root by udl app user
-chmod 755 /opt/wgen/edware-udl/zones
-chmod 777 /opt/wgen/edware-udl/zones/landing
-chmod 777 /opt/wgen/edware-udl/zones/work
-chmod 777 /opt/wgen/edware-udl/zones/history
+sudo chmod 777 /opt/wgen/edware-udl/zones
+sudo chmod 777 /opt/wgen/edware-udl/zones/landing
+sudo chmod 777 /opt/wgen/edware-udl/zones/work
+sudo chmod 777 /opt/wgen/edware-udl/zones/history

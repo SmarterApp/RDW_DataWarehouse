8c4893e747e960690cd31b89cbcc5b117d040e2f
Daniel Wu <dawu@wgen.net>
2013-05-10 15:14:12 -0400

    US15300: change comments from double quote to single quote

--- smarter/services/celeryconfig.py
+++ smarter/services/celeryconfig.py
@@ -12,11 +12,12 @@ def setup_celery(celery_config):
     celery.config_from_object(celery_config)
 
 def load_config(settings, prefix="celery"):
-    """ Loads celery configuration from setting dict.
-        Any value whose corresponding key starts with prefix and followed by a period 
-        is considered as celery configuration.
-        Configuration key will be stored in uppercase as celery's convention.
-    """
+    '''
+    Loads celery configuration from setting dict.
+    Any value whose corresponding key starts with prefix and followed by a period 
+    is considered as celery configuration.
+    Configuration key will be stored in uppercase as celery's convention.
+    '''
     celery_config = {}
     prefix = prefix + "."
     # get config values

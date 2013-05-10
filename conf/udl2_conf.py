40f9cdb92f437856ca79d5d4e9e9277af034947a
Eugene Jen <ejen@wgen.net>
2013-05-10 18:52:43 -0400

    update conf with database

--- conf/udl2_conf.py
+++ conf/udl2_conf.py
@@ -81,9 +81,17 @@ udl2_conf = {
         'error':'/var/log/wgen/edware-udl/logs/udl2.error.log',  # for error message and exceptions, 
     },
     'postgresql' : { # PostgresQL for UDL2 processing. This is not the target database.
-        'db_host':'',
-        'db_port':'',
-        'db_db':'',
+        'db_host':'localhost',
+        'db_port':'5432',
+        'db_database':'udl2',
+        'db_schema':'udl2',
+        'db_user':'udl2',
+        'db_pass':'udl2abc1234',
+    },
+    'udl2_db': {
+        'csv_schema':'udl2',
+        'fdw_server':'',
+        'staging_schema':'udl2',
     }
 }
 

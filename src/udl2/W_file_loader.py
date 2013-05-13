986cadd938a026661b74e5ae4b97cf14e05f6785
Andrew Brien <abrien@wgen.net>
2013-05-13 16:16:38 -0400

    Implementing file clean up in clean up worker

--- src/udl2/W_file_loader.py
+++ src/udl2/W_file_loader.py
@@ -12,12 +12,15 @@ logger = get_task_logger(__name__)
 
 @celery.task(name="udl2.W_file_loader.task")
 def task(msg):
-    csv_file_path = msg['input_file']
+    csv_file_path = msg['file_to_load']
     header_file_path = msg['header_file']
     start_seq = msg['row_start']
+    landing_zone_file = msg['landing_zone_file']
+    work_zone = msg['work_zone']
+    history = msg['history']
     logger.info(task.name)
     logger.info('Loading file %s...' % csv_file_path)
-    conf = generate_conf_for_loading(csv_file_path, header_file_path, start_seq)
+    conf = generate_conf_for_loading(csv_file_path, header_file_path, start_seq, landing_zone_file, work_zone, history)
     load_file(conf)
    
 #    if udl2_stages[task.name]['next'] is not None:
@@ -26,13 +29,11 @@ def task(msg):
 #        task_instance.apply_async(next_msg,
 #                                  udl2_queues[task.name]['queue'],
 #                                  udl2_stages[task.name]['routing_key'])
-    udl2.W_final_cleanup.task.apply_async([csv_file_path + ' passed after ' + task.name],
-                                           queue='Q_final_cleanup',
-                                           routing_key='udl2')
+    udl2.W_final_cleanup.task.apply_async([conf], queue='Q_final_cleanup', routing_key='udl2')
     return msg
 
 
-def generate_conf_for_loading(csv_file_path, header_file_path, start_seq):
+def generate_conf_for_loading(csv_file_path, header_file_path, start_seq, landing_zone_file, work_zone, history):
     csv_file_name_and_ext = os.path.splitext(os.path.basename(csv_file_path))
     csv_file_name = csv_file_name_and_ext[0]
     csv_table = csv_file_name
@@ -50,7 +51,10 @@ def generate_conf_for_loading(csv_file_path, header_file_path, start_seq):
             'fdw_server': 'udl_import',
             'staging_schema': 'public',
             'staging_table': 'tmp',
-            'apply_rules': False
+            'apply_rules': False,
+            'landing_zone_file': landing_zone_file,
+            'work_zone': work_zone,
+            'history': history
     }
     return conf
 

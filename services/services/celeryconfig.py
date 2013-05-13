e02703226afdf2a536cea01f5b10c43bf65e649b
Daniel Wu <dawu@wgen.net>
2013-05-13 13:48:43 -0400

    US15300: created services project for celery related code; implemented pdf generating task;

--- services/services/celeryconfig.py
+++ services/services/celeryconfig.py
@@ -7,17 +7,30 @@ from celery.app.base import Celery
 
 celery = Celery()
 
+# default timeout 10 seconds
+TIMEOUT = 10
+
 
 def setup_celery(celery_config):
     celery.config_from_object(celery_config)
 
-def load_config(settings, prefix="celery"):
+
+def load_timeout(setting):
+    '''
+    Loads timeout for pdf generating process
+    '''
+    key = 'pdf.generate.timeout'
+    return setting[key] if key in setting else TIMEOUT
+
+
+def load_celeryconfig(settings, prefix):
     '''
     Loads celery configuration from setting dict.
-    Any value whose corresponding key starts with prefix and followed by a period 
+    Any value whose corresponding key starts with prefix and followed by a period
     is considered as celery configuration.
     Configuration key will be stored in uppercase as celery's convention.
     '''
+    # load celery config
     celery_config = {}
     prefix = prefix + "."
     # get config values
@@ -25,4 +38,12 @@ def load_config(settings, prefix="celery"):
         if key.startswith(prefix):
             celery_key = key[len(prefix):].upper()
             celery_config[celery_key] = settings[key]
+    return celery_config
+
+
+def load_config(settings, prefix="celery"):
+    # load pdf generation timeout
+    TIMEOUT = load_timeout(settings)
+    # load celery config
+    celery_config = load_celeryconfig(settings, prefix)
     return celery_config

2e31b44580db2a77b31e26b697f2a80389b71125
Eugene Jen <ejen@wgen.net>
2013-05-14 13:01:26 -0400

    remove unused code

--- scripts/initialize_udl2_database.py
+++ scripts/initialize_udl2_database.py
@@ -5,14 +5,5 @@ import sys
 
 
 if __name__ == '__main__':
-    print("initialzie udl2 database\n")
-    db_host = input("please type in host name for postgres server\n")
-    db_superuser = input("please type in postgres superuser name you want to connect\n")
-    print("now run createuser -W -s -e -E -d udl2 -P -h %s -U %s -W \n" % (db_host, db_superuser))
-    subprocess.call("createuser -W -s -e -E -d udl2 -P -h %s -U %s -W " % (db_host, db_superuser), shell=True)
-    print("now create udl2 db for udl2 system")
-    print("now run createdb -e -E utf-8 -O udl2 -h %s -U %s -W udl2" % (db_host, db_superuser))
-    subprocess.call("createdb -e -E utf-8 -O udl2 -h %s -U %s -W udl2" % (db_host, db_superuser) , shell=True)
-    print("now run udl2 specific database initialization")
     cleaned_argv = [ arg for arg in sys.argv[1:] if arg != '']
     subprocess.call("python -m udl2.database %s " % " ".join(cleaned_argv), shell=True)

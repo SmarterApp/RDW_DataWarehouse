432372ded247f1e38b77c319c5240568fb8a19e4
Lili Chen <lichen@wgen.net>
2013-05-14 10:55:43 -0400

    update functional test for file laoder

--- functional_tests/fTest_file_loader.py
+++ functional_tests/fTest_file_loader.py
@@ -20,14 +20,14 @@ class FileSplitterFTest(unittest.TestCase):
             'header_file': HEADER_FILE,
             'db_host': 'localhost',
             'db_port': '5432',
-            'db_user': 'postgres',
-            'db_name': 'fdw_test',
-            'db_password': '3423346',
-            'csv_schema': 'public',
+            'db_user': 'udl2',
+            'db_name': 'udl2',
+            'db_password': 'udl2abc1234',
+            'csv_schema': 'udl2',
             'fdw_server': 'udl_import',
-            'staging_schema': 'public',
-            'staging_table': 'edware_test_for_file_loader',
-            'apply_rules': False,
+            'staging_schema': 'udl2',
+            'staging_table': 'STG_SBAC_ASMT_OUTCOME',
+            'apply_rules': False
         }
         # connect to db
         conn, _engine = connect_db(self.conf)
@@ -36,6 +36,7 @@ class FileSplitterFTest(unittest.TestCase):
     def test_row_number(self):
         # load data
         self.conf['start_seq'] = 10
+        self.conf['batch_id'] = generate_non_exsisting_batch_id(self.conf, self.conn)
         load_file(self.conf)
 
         # verify
@@ -46,6 +47,7 @@ class FileSplitterFTest(unittest.TestCase):
     def test_compare_data(self):
         # load data
         self.conf['start_seq'] = 24
+        self.conf['batch_id'] = generate_non_exsisting_batch_id(self.conf, self.conn)
         load_file(self.conf)
 
         # get the result of db
@@ -60,15 +62,16 @@ class FileSplitterFTest(unittest.TestCase):
                 # verify each of the value
                 for i in range(len(row_in_csv)):
                     value_in_csv = row_in_csv[i]
-                    value_in_table = row_in_table[i]
-                    self.assertEqual(value_in_csv, value_in_table)
+                    value_in_table = row_in_table[i + 3]
+                    if value_in_csv and value_in_table:
+                        self.assertEqual(value_in_csv, value_in_table)
                     # verify the src_column_name
-                    self.assertEqual(row_in_table['src_row_number'], row_number + self.conf['start_seq'])
+                    self.assertEqual(row_in_table['src_file_rec_num'], row_number + self.conf['start_seq'])
                 row_number += 1
 
     def tearDown(self):
         # truncate staging table or delete all rows just inserted.
-        query = 'TRUNCATE TABLE {schema_name}.{table_name}'.format(schema_name=self.conf['staging_schema'], table_name=self.conf['staging_table'])
+        query = 'DELETE FROM \"{schema_name}\".\"{table_name}\" WHERE batch_id={batch_id}'.format(schema_name=self.conf['staging_schema'], table_name=self.conf['staging_table'], batch_id=self.conf['batch_id'])
         trans = self.conn.begin()
         try:
             self.conn.execute(query)
@@ -77,6 +80,23 @@ class FileSplitterFTest(unittest.TestCase):
             print('Exception -- ', e)
             trans.rollback()
         self.conn.close()
+        print("Tear Down successful for batch", self.conf['batch_id'])
+
+
+def generate_non_exsisting_batch_id(conf, conn):
+    query = "SELECT DISTINCT batch_id FROM \"{schema_name}\".\"{table_name}\"".format(schema_name=conf['staging_schema'], table_name=conf['staging_table'])
+    trans = conn.begin()
+    try:
+        result = conn.execute(query)
+        trans.commit()
+    except Exception as e:
+        print('Exception -- ', e)
+        trans.rollback()
+    exsisting_batch_ids = [row[0] for row in result]
+    if len(exsisting_batch_ids) == 0:
+        return 1
+    else:
+        return max(exsisting_batch_ids) + 10
 
 
 def get_row_number_in_csv(csv_file):
@@ -89,7 +109,7 @@ def get_row_number_in_csv(csv_file):
 
 
 def get_row_number_in_table(conf, conn):
-    query = 'SELECT COUNT(*) FROM {schema_name}.{table_name}'.format(schema_name=conf['staging_schema'], table_name=conf['staging_table'])
+    query = 'SELECT COUNT(*) FROM \"{schema_name}\".\"{table_name}\" WHERE batch_id={batch_id}'.format(schema_name=conf['staging_schema'], table_name=conf['staging_table'], batch_id=conf['batch_id'])
     trans = conn.begin()
     try:
         result = conn.execute(query)
@@ -104,7 +124,7 @@ def get_row_number_in_table(conf, conn):
 
 
 def get_rows_in_table(conf, conn):
-    query = 'SELECT * FROM {schema_name}.{table_name}'.format(schema_name=conf['staging_schema'], table_name=conf['staging_table'])
+    query = 'SELECT * FROM \"{schema_name}\".\"{table_name}\" WHERE batch_id={batch_id}'.format(schema_name=conf['staging_schema'], table_name=conf['staging_table'], batch_id=conf['batch_id'])
     print(query)
     trans = conn.begin()
     try:

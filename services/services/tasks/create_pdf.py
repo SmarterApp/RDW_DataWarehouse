54a6bb9cc66132ec6f800ff6da63df23f54abe42
Doris Ip <dip@wgen.net>
2013-05-14 12:02:37 -0400

    US15300 Add UT for create_pdf

--- services/services/tasks/create_pdf.py
+++ services/services/tasks/create_pdf.py
@@ -9,26 +9,34 @@ import logging
 import subprocess
 from services.celeryconfig import celery
 from services.celeryconfig import TIMEOUT
+import platform
 
 pdf_procs = ['wkhtmltopdf']
 pdf_defaults = ['--enable-javascript', '--page-size', 'Letter', '--javascript-delay', '5000']
 
+OK = 0
+FAIL = 1
+
 
 @celery.task
-def generate_pdf(cookie, url, outputfile, options=pdf_defaults, timeout=TIMEOUT):
+def generate_pdf(cookie, url, outputfile, options=pdf_defaults, timeout=TIMEOUT, cookie_name='edware'):
     '''
     Generates pdf from given url. Returns exist status code from shell command.
     We set up timeout in order to terminate pdf generating process, for wkhtmltopdf 0.10.0 doesn't exit
     properly upon successfully completion (see wkhtmltopdf ISSUE 141). TIMEOUT can be removed if that bug is fixed in future.
     '''
     try:
-        return subprocess.call(pdf_procs + options + ['--cookie', 'edware', cookie, url, outputfile], timeout=timeout)
+        # Only for windows environment, set shell to true
+        shell = False
+        if platform.system() == 'Windows':
+            shell = True
+
+        return subprocess.call(pdf_procs + options + ['--cookie', cookie_name, cookie, url, outputfile], timeout=timeout, shell=shell)
     except subprocess.TimeoutExpired:
-        (SUCCESS, FAILURE) = (0, 1)
         # check output file, return 0 if file is created successfully
         isSucceed = os.path.exists(outputfile) and os.path.getsize(outputfile)
-        return SUCCESS if isSucceed else FAILURE
+        return OK if isSucceed else FAIL
     except:
         log = logging.getLogger(__name__)
         log.error("Generate PDF error: %s", sys.exc_info())
-        return False
+        return FAIL

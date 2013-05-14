6aadcd4bceb76c3a659b7ed49f57fb94c30d0183
Andrew Brien <abrien@wgen.net>
2013-05-14 15:16:15 -0400

    Cleaning up test_final_cleanup

--- unit_tests/test_final_cleanup.py
+++ unit_tests/test_final_cleanup.py
@@ -1,10 +1,12 @@
 __author__ = 'abrien'
 
 import unittest
-import final_cleanup.final_cleanup
+import final_cleanup.final_cleanup as final_cleanup
 
-class TestFinalCleanup(unittest.TestCase:
+class TestFinalCleanup(unittest.TestCase):
 
     def test_extract_file_name(self):
         test_path = '/fake/file/path/fake_file.csv'
-        file_name = final_cleanup.extract_file_name(test_path)
+        actual_result = final_cleanup.extract_file_name(test_path)
+        expected_result = 'fake_file'
+        self.assertEquals(actual_result, expected_result)

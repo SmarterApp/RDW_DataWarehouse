78df044c96f1eaeea60d0c28ec5ade7acf3e7877
Eugene Jen <ejen@wgen.net>
2013-05-14 13:02:06 -0400

    remove initialized_udl2_database.py

--- scripts/initialize_udl2_database.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-from udl2.defaults import UDL2_DEFAULT_CONFIG_PATH_FILE
-import sys
-
-
-if __name__ == '__main__':
-    cleaned_argv = [ arg for arg in sys.argv[1:] if arg != '']
-    subprocess.call("python -m udl2.database %s " % " ".join(cleaned_argv), shell=True)

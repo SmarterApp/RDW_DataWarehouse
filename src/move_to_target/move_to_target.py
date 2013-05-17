8108bcfce7ed1f840e025b21cf79c118ea27a015
Lili Chen <lichen@wgen.net>
2013-05-17 13:26:12 -0400

    add function to generate query for copying data from integration to star schema

--- src/move_to_target/move_to_target.py
+++ src/move_to_target/move_to_target.py
@@ -6,6 +6,11 @@ DBDRIVER = "postgresql+pypostgresql"
 
 
 def explode_data_to_target(conf):
+    """
+    This function will NOT be used in Celery Worker. It is used from main in this script
+    This function starts to explode dim and fact table to target (star schema)
+    The equivalent component is the W_move_to_target.py which is used in UDL pipeline framework
+    """
     # connect to db
     conn, _engine = connect_db(conf)
 
@@ -26,7 +31,26 @@ def explode_data_to_one_table(conf, target_table, column_mapping):
     '''
     Will use parameters passed in to create query with sqlalchemy
     '''
-    print("In explode_data_to_one_table, %s, %s" % (str(conf), str(target_table)))
+    print("In explode_data_to_one_table****, %s, %s" % (str(conf), str(target_table)))
+    # create db connection
+
+    # create insertion query
+    query = create_insert_query(conf['batch_id'], conf['source_schema'], conf['source_table'], conf['target_schema'], target_table, column_mapping)
+
+    # execute the query
+    print(query)
+
+
+def create_insert_query(batch_id, source_schema, source_table, target_schema, target_table, column_mapping):
+    print("Is here ***************")
+    insert_sql = ["INSERT INTO \"{target_schema}\".\"{target_table}\"(",
+             ",".join(column_mapping.keys()),
+             ") SELECT ",
+             ",".join(column_mapping.values()),
+             " FROM \"{source_schema}\".\"{source_table}\" WHERE batch_id={batch_id}",
+            ]
+    insert_sql = "".join(insert_sql).format(target_schema=target_schema, target_table=target_table, source_schema=source_schema, source_table=source_table, batch_id=batch_id)
+    return insert_sql
 
 
 if __name__ == "__main__":

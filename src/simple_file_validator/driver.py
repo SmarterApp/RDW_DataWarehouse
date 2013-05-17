41b2227dbdbe4eda5ab6f41da7d66607b25054b9
Andrew Brien <abrien@wgen.net>
2013-05-17 14:46:51 -0400

    Adding basic functionality to sfv

--- src/simple_file_validator/driver.py
+++ src/simple_file_validator/driver.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
+
 __author__ = 'abrien'
+
+
+
+from simple_file_validator.csv_validator import CsvValidator
+
+
+def main():
+    validator = CsvValidator()
+
+    dir_path = '.'
+    file_name = 'test_csv.csv'
+    batch_sid = 1
+
+    error_codes = validator.execute(dir_path, file_name, batch_sid)
+
+    for error_code in error_codes:
+        print(error_code)
+
+
+
+if __name__ == "__main__":
+    main()

468ec662f6dfebf91ba04a213dfb7df7db17ff36
Andrew Brien <abrien@wgen.net>
2013-05-20 13:43:34 -0400

    Updating import statement (sfv) and adding parameter parsing to sfv driver

--- src/sfv/driver.py
+++ src/sfv/driver.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+import argparse
+import os
+
 __author__ = 'abrien'
 
-
-
-from simple_file_validator.csv_validator import CsvValidator
+from sfv.csv_validator import CsvValidator
 
 
 def main():
-    validator = CsvValidator()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest='file_path', required=True, type=str, help="Path to the file to be validated.")
 
-    dir_path = '.'
-    file_name = 'test_csv.csv'
+    args = parser.parse_args()
+    full_file_path = args.file_path
+    dir_path = os.path.dirname(full_file_path)
+    file_name = os.path.basename(full_file_path)
     batch_sid = 1
 
+    validator = CsvValidator()
     error_codes = validator.execute(dir_path, file_name, batch_sid)
 
     for error_code in error_codes:

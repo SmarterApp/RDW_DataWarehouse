f5c7ff170e4d7c87a3d62c42f3a6f3f9ca7dca7a
Doris Ip <dip@wgen.net>
2013-05-20 15:14:13 -0400

    US15623 Add UT

---
+++ edauth/edauth/tests/security/test_utils.py
@@ -0,0 +1,32 @@
+'''
+Created on May 20, 2013
+
+@author: dip
+'''
+import unittest
+from pyramid.testing import DummyRequest
+from pyramid import testing
+from edauth.security.utils import get_session_cookie
+
+
+class TestUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.__request = DummyRequest()
+        # Must set hook_zca to false to work with uniittest_with_sqlite
+        self.__config = testing.setUp(request=self.__request, hook_zca=False)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_get_session_cookie(self):
+        self.__request.registry['auth.policy.cookie_name'] = 'dummy'
+        self.__request.cookies = {'dummy': 'abc'}
+        (cookie_name, cookie_value) = get_session_cookie()
+        self.assertEqual(cookie_name, 'dummy')
+        self.assertEqual(cookie_value, 'abc')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

6170b109f0b6f6f1c538873e68d955f111792517
Andrew Brien <abrien@wgen.net>
2013-05-20 17:04:12 -0400

    Adding epydoc comments to json_validator.py

--- src/sfv/json_validator.py
+++ src/sfv/json_validator.py
@@ -3,6 +3,9 @@ import os
 from sfv import error_codes
 
 class JsonValidator():
+    """
+    Invoke a suite of validations for json files.
+    """
 
     def __init__(self):
         self.validators = [IsValidJsonFile(),
@@ -10,6 +13,18 @@ class JsonValidator():
 
 
     def execute(self, dir_path, file_name, batch_sid):
+        """
+        Run all validation tests and return a list of error codes for all failures, or
+        errorcodes.STATUS_OK if all tests pass
+
+        @param dir_path: path of the file
+        @type dir_path: string
+        @param file_name: name of the file
+        @type file_name: string
+        @param batch_sid: batch id of the file
+        @type batch_sid: integer
+        @return: tuple of the form: (status_code, dir_path, file_name, batch_sid)
+        """
         error_list = []
         for validator in self.validators:
             result = validator.execute(dir_path, file_name, batch_sid)
@@ -21,8 +36,20 @@ class JsonValidator():
 
 
 class IsValidJsonFile(object):
+    '''Make sure the file contains a parsable json string'''
 
     def execute(self, dir_path, file_name, batch_sid):
+        '''
+        Run json.load() on the given file, if it is invalid json, the exception will be caught and the proper code returned
+
+        @param dir_path: path of the file
+        @type dir_path: string
+        @param file_name: name of the file
+        @type file_name: string
+        @param batch_sid: batch id of the file
+        @type batch_sid: integer
+        @return: tuple of the form: (status_code, dir_path, file_name, batch_sid)
+        '''
         complete_path = os.path.join(dir_path, file_name)
         with open(complete_path) as f:
             try:
@@ -33,9 +60,11 @@ class IsValidJsonFile(object):
 
 
 class HasExpectedFormat(object):
-
+    '''Make sure the JSON file is formatted to our standards '''
     def __init__(self):
 
+        # mapping is a dictionary with keys = fields and values = paths to that field within the json structure
+        # the paths will consist of a list of strings, each one a component of the path to the given field
         self.mapping = {'asmt_guid': ['identification', 'guid'],
                'asmt_type': ['identification', 'type'],
                'asmt_period': ['identification', 'period'],
@@ -73,6 +102,19 @@ class HasExpectedFormat(object):
 
 
     def execute(self, dir_path, file_name, batch_sid):
+        '''
+        Iterate through all the elements of mapping, and check that we can reach all expected fields using
+        the provided paths.
+
+        @param dir_path: path of the file
+        @type dir_path: string
+        @param file_name: name of the file
+        @type file_name: string
+        @param batch_sid: batch id of the file
+        @type batch_sid: integer
+        @return: tuple of the form: (status_code, dir_path, file_name, batch_sid, field) or (status_code, dir_path, file_name, batch_sid)
+        '''
+
         complete_path = os.path.join(dir_path, file_name)
         with open(complete_path) as f:
             json_object = json.load(f)
@@ -85,6 +127,17 @@ class HasExpectedFormat(object):
 
 
     def does_json_path_exist(self, json_object, path):
+        '''
+        Given a json_object [a dictionary] and a path [a list of keys through the dictionary], ensure we can follow the path
+        all the way through the json_object
+
+        @param json_object: A dictionary that represents some json object
+        @type json_object: dict
+        @param path: A list of components that form a path through the json_object
+        @type path: list of str
+        @return: whether or not the given path exists within json_object
+        @rtype: bool
+        '''
         current_position = json_object
         for component in path:
             if component in current_position.keys():

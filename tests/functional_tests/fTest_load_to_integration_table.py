49db1356f90c67a22d65d570785004a7ace4e717
Eugene Jen <ejen@wgen.net>
2013-05-24 13:27:40 -0400

    initial checkin for skeletons of fTest_load_to_integration_table to run functional tests of loading data from integration table into integration tables

---
+++ tests/functional_tests/fTest_load_to_integration_table.py
@@ -0,0 +1,22 @@
+'''
+Created on May 24, 2013
+
+@author: ejen
+'''
+import unittest
+
+
+class FuncTestLoadToIntegrationTable(unittest.TestCase):
+
+    def setUp(self, ):
+        pass
+
+    def tearDown(self, ):
+        pass
+
+    def load_sbac_csv(self, ):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

cdfe4cf9f8838d1ab9ff7384790fe1d7c933d424
Seth Wimberly <swimberly@wgen.net>
2013-05-24 15:09:29 -0400

    Changes to json loader to use new message

--- src/udl2/W_file_content_validator.py
+++ src/udl2/W_file_content_validator.py
@@ -24,12 +24,13 @@ logger = get_task_logger(__name__)
 
 @celery.task(name='udl2.W_file_content_validator.task')
 def task(incoming_msg):
+    logger.info(task.name)
     job_control = incoming_msg[JOB_CONTROL]
     stg_table = incoming_msg[STG_TABLE]
     lzw = incoming_msg[LANDING_ZONE_WORK_DIR]
     json_file = incoming_msg[JSON_FILE]
 
-    # TODO call file validator
+    # TODO Validate file
 
     outgoing_msg = generate_load_json_msg(lzw, json_file, job_control)
     return outgoing_msg
--- src/udl2/W_load_json_to_integration.py
+++ src/udl2/W_load_json_to_integration.py
@@ -23,6 +23,7 @@ from fileloader.json_loader import load_json
 from udl2_util.udl_mappings import get_json_to_asmt_tbl_mappings
 
 FILE_TO_LOAD = 'file_to_load'
+JOB_CONTROL = 'job_control'
 
 INT_TABLE = 'integration_table'
 INT_SCHEMA = 'integration_schema'
@@ -41,27 +42,38 @@ logger = get_task_logger(__name__)
 @celery.task(name="udl2.W_load_json_to_integration.task")
 def task(msg):
     logger.info(task.name)
-    logger.info('Loading json file %s...' % msg[FILE_TO_LOAD])
-    conf = generate_conf_for_loading(msg[FILE_TO_LOAD], msg[BATCH_ID])
+    logger.info('Loading json file %s...' % msg[JSON_FILE])
+    job_control = msg[JOB_CONTROL]
+    batch_id = job_control[1]
+    conf = generate_conf_for_loading(msg[FILE_TO_LOAD], batch_id)
     load_json(conf)
 
-    return msg
+    outgoing_msg = generate_stg_to_int_msg(conf[INT_TABLE], job_control)
+    return outgoing_msg
 
 
 def generate_conf_for_loading(file_to_load, batch_id):
     conf = {
-            JSON_FILE: file_to_load,
-            MAPPINGS: get_json_to_asmt_tbl_mappings(),
-            DB_HOST: udl2_conf['postgresql']['db_host'],
-            DB_PORT: udl2_conf['postgresql']['db_port'],
-            DB_USER: udl2_conf['postgresql']['db_user'],
-            DB_NAME: udl2_conf['postgresql']['db_database'],
-            DB_PASSWORD: udl2_conf['postgresql']['db_pass'],
-            INT_SCHEMA: udl2_conf['udl2_db']['staging_schema'],
-            INT_TABLE: 'INT_SBAC_ASMT',
-            BATCH_ID: batch_id
-            }
+        JSON_FILE: file_to_load,
+        MAPPINGS: get_json_to_asmt_tbl_mappings(),
+        DB_HOST: udl2_conf['postgresql']['db_host'],
+        DB_PORT: udl2_conf['postgresql']['db_port'],
+        DB_USER: udl2_conf['postgresql']['db_user'],
+        DB_NAME: udl2_conf['postgresql']['db_database'],
+        DB_PASSWORD: udl2_conf['postgresql']['db_pass'],
+        INT_SCHEMA: udl2_conf['udl2_db']['staging_schema'],
+        INT_TABLE: 'INT_SBAC_ASMT',
+        BATCH_ID: batch_id
+    }
     return conf
+
+
+def generate_stg_to_int_msg(int_table, job_control):
+    msg = {
+        INT_TABLE: int_table,
+        JOB_CONTROL: job_control
+    }
+    return msg
 
 
 @celery.task(name="udl2.W_load_json_to_integration.error_handler")

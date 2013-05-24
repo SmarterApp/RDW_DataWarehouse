2e5a304c7d02dfe41ce510195a078b9e86a2599b
Takashi Osako <tosako@wgen.net>
2013-05-23 21:22:16 -0400

    US15540: pep8
    Former-commit-id: f4df082bf783fe60bed14876f06272f072a7d6f6

--- smarter/smarter/tests/test_services.py
+++ smarter/smarter/tests/test_services.py
@@ -34,7 +34,7 @@ class TestServices(Unittest_with_smarter_sqlite):
         # Must set hook_zca to false to work with uniittest_with_sqlite
         reg = Registry()
         self.__temp_dir = tempfile.mkdtemp()
-        reg.settings={}
+        reg.settings = {}
         reg.settings['pdf.report_base_dir'] = self.__temp_dir
         self.__config = testing.setUp(registry=reg, request=self.__request, hook_zca=False)
         with SmarterDBConnection() as connection:

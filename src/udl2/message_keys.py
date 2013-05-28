501439252863a17cc48094239e7c0ce84893a26c
Andrew Brien <abrien@wgen.net>
2013-05-28 17:24:51 -0400

    Updating message keys for new messages

--- src/udl2/message_keys.py
+++ src/udl2/message_keys.py
@@ -2,12 +2,12 @@ __author__ = 'abrien'
 
 # LZ to file_arrived
 INPUT_FILE_PATH = 'input_file_path'
-JOB_CONTROL_TABLE_CONF = 'job_control_table_conf'
+JOB_CONTROL = 'job_control'
 LANDING_ZONE_WORK_DIR = 'landing_zone_work_dir'
+BATCH_ID = 'batch_id'
 
 # file_arrived to file_expander
 FILE_TO_EXPAND = 'file_to_expand'
-JOB_CONTROL = 'job_control'
 
 # file_expander to file_validator
 JSON_FILENAME = 'json_filename'
@@ -16,8 +16,15 @@ CSV_FILENAME = 'csv_filename'
 # file_validator to file_splitter
 PARTS = 'parts'
 
-# json_to_integration
+# file_splitter to csv_to_staging
 FILE_TO_LOAD = 'file_to_load'
+HEADERS = 'headers'
+APPLY_RULES = 'apply_rules'
+FDW_CONF = 'fdw_conf'
+STAGING_CONF = 'staging_conf'
+
+# json_to_integration
+
 
 # report errors
 EMAIL = 'email_address'

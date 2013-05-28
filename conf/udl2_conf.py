13ec2401b4447f25ef6db620f022071a4417985f
Eugene Jen <ejen@wgen.net>
2013-05-28 10:28:03 -0400

    change driver to postgresql due to sqlalchmey has changed driver name scheme

--- conf/udl2_conf.py
+++ conf/udl2_conf.py
@@ -104,7 +104,7 @@ udl2_conf = {
         'db_schema':'udl2',
         'db_user':'udl2',
         'db_pass':'udl2abc1234',
-        'db_driver':'postgres',
+        'db_driver':'postgresql',
     },
     'udl2_db': {
         'csv_schema':'udl2',  # this is the same as postgresql schema
@@ -118,7 +118,7 @@ udl2_conf = {
         'db_schema':'udl2',
         'db_user':'udl2',
         'db_pass':'udl2abc1234',
-        'db_driver':'postgres',
+        'db_driver':'postgresql',
     },
     'target_db': {
         'db_schema': 'edware',
@@ -128,7 +128,7 @@ udl2_conf = {
         'db_database':'edware',
         'db_user':'edware',
         'db_pass':'password',
-        'db_driver':'postgres'
+        'db_driver':'postgresql'
     }
 }
 

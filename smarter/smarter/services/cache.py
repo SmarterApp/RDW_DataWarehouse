781c140359eb1e2a8a17051d8baad6a4777c5cf5
Takashi Osako <tosako@wgen.net>
2013-06-03 16:20:56 -0400

    US15545: CR
    Former-commit-id: a28f4e88af5df7b9a79ded45f6977333f1cd575e

--- smarter/smarter/services/cache.py
+++ smarter/smarter/services/cache.py
@@ -6,7 +6,6 @@ Created on May 30, 2013
 from pyramid.view import view_config
 from beaker.cache import cache_managers
 from pyramid.httpexceptions import HTTPOk, HTTPNotFound
-from edauth.security.session_backend import get_session_backend
 import logging
 
 logger = logging.getLogger(__name__)
@@ -17,13 +16,13 @@ def cache_flush(request):
     '''
     service call for flush cache
     '''
-    region_name = request.matchdict['cache_name']
-    if region_name == 'session':
+    cache_name = request.matchdict['cache_name']
+    if cache_name == 'session':
         cache_flush_session()
-    elif region_name == 'all':
+    elif cache_name == 'all':
         cache_flush_session()
         cache_flush_data()
-    elif region_name == 'data':
+    elif cache_name == 'data':
         cache_flush_data()
     else:
         return HTTPNotFound()
@@ -37,20 +36,18 @@ def cache_flush_session():
     logger.info("flush cache for session")
     for namespace_name in cache_managers.values():
         '''
-        flush all cache exception session
+        flush session cache
         '''
         if 'edware_session' == namespace_name.namespace_name[:14]:
             namespace_name.clear()
             break
-    return True
 
 
 def cache_flush_data():
     logger.info("flush cache for data")
     for namespace_name in cache_managers.values():
         '''
-        flush all cache exception session
+        flush all cache except session
         '''
         if 'edware_session' != namespace_name.namespace_name[:14]:
             namespace_name.clear()
-    return True

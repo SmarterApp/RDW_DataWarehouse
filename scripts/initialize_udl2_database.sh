bef9bbf767e6a4b40b84a0f9c465d7820bc4319f
Eugene Jen <ejen@wgen.net>
2013-06-03 16:43:53 -0400

    add py-postgresql due to ed_schema uses it to initialize databases

--- scripts/initialize_udl2_database.sh
+++ scripts/initialize_udl2_database.sh
@@ -4,3 +4,7 @@
 # otherwise, it must be python3.3
 
 python3.3 -m udl2.database $1 $2 --action setup
+
+# initialize edware star schema
+
+python3.3 -m edschema.ed_metadata -s edware -d edware --host=localhost:5432 -u edware -p edware2013

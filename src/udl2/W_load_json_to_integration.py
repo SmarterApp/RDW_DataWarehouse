8100e1365f0fc5bfa674407f2d201363647f673a
Eugene Jen <ejen@wgen.net>
2013-06-04 14:52:57 -0400

    fix all database use configurations database

--- src/udl2/W_load_json_to_integration.py
+++ src/udl2/W_load_json_to_integration.py
@@ -52,11 +52,11 @@ def generate_conf_for_loading(json_file, jc):
     conf = {
         mk.FILE_TO_LOAD: json_file,
         mk.MAPPINGS: get_json_to_asmt_tbl_mappings(),
-        mk.TARGET_DB_HOST: udl2_conf['postgresql']['db_host'],
-        mk.TARGET_DB_PORT: udl2_conf['postgresql']['db_port'],
-        mk.TARGET_DB_USER: udl2_conf['postgresql']['db_user'],
-        mk.TARGET_DB_NAME: udl2_conf['postgresql']['db_database'],
-        mk.TARGET_DB_PASSWORD: udl2_conf['postgresql']['db_pass'],
+        mk.TARGET_DB_HOST: udl2_conf['udl2_db']['db_host'],
+        mk.TARGET_DB_PORT: udl2_conf['udl2_db']['db_port'],
+        mk.TARGET_DB_USER: udl2_conf['udl2_db']['db_user'],
+        mk.TARGET_DB_NAME: udl2_conf['udl2_db']['db_database'],
+        mk.TARGET_DB_PASSWORD: udl2_conf['udl2_db']['db_pass'],
         mk.TARGET_DB_SCHEMA: udl2_conf['udl2_db']['integration_schema'],
         mk.TARGET_DB_TABLE: 'INT_SBAC_ASMT',
         BATCH_ID: jc[1]

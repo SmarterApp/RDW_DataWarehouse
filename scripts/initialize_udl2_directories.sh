694ec7c6d9142bf9b1857052156ce7127dfbbd6e
Eugene Jen <ejen@wgen.net>
2013-06-05 14:26:13 -0400

    update initialize_udl2_directories.sh with cleanup old directories

--- scripts/initialize_udl2_directories.sh
+++ scripts/initialize_udl2_directories.sh
@@ -7,6 +7,9 @@ sudo chmod 755 /opt/wgen
 sudo chmod 755 /opt/wgen/edware-udl
 sudo chmod 777 /opt/wgen/edware-udl/etc
 
+echo "rebuild log directory"
+sudo rm -fr /var/log/wgen/edware-udl/logs
+
 echo "make log directory"
 sudo mkdir -p /var/log/wgen/edware-udl/logs
 # we need to fix permission later not to own by root by udl app user
@@ -14,6 +17,9 @@ sudo chmod 755 /var/log/
 sudo chmod 755 /var/log/wgen
 sudo chmod 755 /var/log/wgen/edware-udl/
 sudo chmod 777 /var/log/wgen/edware-udl/logs
+
+echo "rebuild zones directory"
+sudo rm -fr /opt/wgen/edware-udl/zones
 
 echo "make zones directory"
 sudo mkdir -p /opt/wgen/edware-udl/zones/

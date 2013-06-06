80c9c31f03d99a05a994443ff611bff689119c24
Eugene Jen <ejen@wgen.net>
2013-06-06 11:44:27 -0400

    fix debug code

--- scripts/initialize_udl2_system.sh
+++ scripts/initialize_udl2_system.sh
@@ -11,7 +11,7 @@ echo "create udl2 users to run udl2 celery and own the zone area for work"
 sudo -u root -s "useradd -d /opt/wgen/edware-udl/udl2 -r -s /bin/sh udl2"
 
 # copy current udl2 installation's code ssh
-echo PWD
+echo `pwd`
 echo "copy udl2 installations' ssh keys"
 sudo -u root -s "cp ../git/id_rsa* /opt/wgen/edware-udl/udl2/.ssh"
 sudo -u root -s "chmod 600 /opt/wgen/edware-udl/udl2/.ssh/id_rsa.pub"

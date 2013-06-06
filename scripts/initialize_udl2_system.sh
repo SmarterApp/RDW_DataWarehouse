181e9e3b3269d35f8bf358ae864df35731cdb089
Eugene Jen <ejen@edwappsrv3.poc.dum.edwdc.net>
2013-06-06 13:15:21 -0400

    update uninstll scripts update initialized_udl2_directories.sh update initialized_udl2_system.sh

--- scripts/initialize_udl2_system.sh
+++ scripts/initialize_udl2_system.sh
@@ -9,11 +9,13 @@ sudo -u root -s "chmod 777 /opt/wgen/edware-udl/udl2/.ssh"
 # create udl2 system users for
 echo "create udl2 users to run udl2 celery and own the zone area for work"
 sudo -u root -s "useradd -d /opt/wgen/edware-udl/udl2 -r -s /bin/sh udl2"
+sudo -u root -s "passwd udl2"
 sudo -u root -s "chown udl2.udl2 /opt/wgen/edware-udl/udl2"
 sudo -u root -s "chown udl2.udl2 /opt/wgen/edware-udl/udl2/.ssh"
 
 # copy current udl2 installation's code ssh
 echo `pwd`
+PWD = `pwd`
 echo "copy udl2 installations' ssh keys"
 sudo -u root -s "cp `pwd`/../git/id_rsa /opt/wgen/edware-udl/udl2/.ssh"
 sudo -u root -s "cp `pwd`/../git/id_rsa.pub /opt/wgen/edware-udl/udl2/.ssh"
@@ -27,29 +29,30 @@ sudo -u root -s "chmod 755 /opt/wgen/edware-udl/udl2/.ssh"
 echo "install virtualenv-3.3 for celery, due to python3 rpm install path under /opt/python3/bin, we need to specifiy the path"
 cd /opt/wgen/edware-udl/udl2
 echo "/opt/python3/bin/virtualenv-3.3 --distribute python3.3"
-sudo -u udl2 -s "/opt/python3/bin/virtualenv-3.3 --distribute python3.3"
+su udl2 -c "/opt/python3/bin/virtualenv-3.3 --distribute python3.3"
 
 # install virtualenv-2.7
 echo "install virtual-2.7 for flower"
 cd /opt/wgen/edware-udl/udl2
 echo "virtualenv-2.7 --distribute python2.7"
-sudo -u udl2 -s "virtualenv-2.7 --distribute python2.7"
+su udl2 -c "/usr/bin/virtualenv-2.7 --distribute python2.7"
 
 # pull code from git
-echo "pull code from git"
-cd /opt/wgen/edware-udl/udl2
-sudo -u udl2 -s "git clone ssh://git@github.wgenhq.net/Ed-Ware-SBAC/edware-udl-2.0"
+#echo "pull code from git"
+#cd /opt/wgen/edware-udl/udl2
+#su udl2 -c "git clone ssh://git@github.wgenhq.net/Ed-Ware-SBAC/edware-udl-2.0"
+
+cd $PWD
+
+# initialze directory
+echo "initialze directories"
+su udl2 -c "./initialize_udl2_directories.sh"
 
 #install all python dependency for udl2 code to python 3.3
 echo "install all python dependency code for udl2"
-cd /opt/wgen/edware-udl/udl2/edware-udl-2.0
-source /opt/wgen/edware-udl/udl2/python3.3/bin/activate
-sudo -u udl2 -s "python3.3 setup.py install"
-cd /opt/wgen/edware/udl2/edware-udl-2.0
-/opt/wgen/edware-udl/udl2/python3.3/bin/python3.3 ./setup.py install
+cd  $PWD
+su udl2 -c "source /opt/wgen/edware-udl/udl2/python3.3/bin/activate; /opt/wgen/edware-udl/udl2/python3.3/bin/python3.3 setup.py install"
+
 #install all python dependency for flower to monitor celery
 echo "install all python dependency code for flower to minitor celery"
-source /opt/wgen/edware-udl/udl2/python2.7/bin/activate
-sudo -u udl2 -s "pip-2.7 install flower"
-cd /opt/wgen/edware/udl2/edware-udl-2.0
-/opt/wgen/edware-udl/udl2/python2.7/bin/pip-2.7 ./setup.py install
+su udl2 -c "source /opt/wgen/edware-udl/udl2/python2.7/bin/activate; /opt/wgen/edware-udl/udl2/python2.7/bin/pip-2.7 install flower"
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo -u root -s "rm -fr /opt/wgen/edware-udl/*"
-sudo -u root -s "/usr/sbin/userdel udl2"
+echo "sudo -u root -s \"rm -fr /opt/wgen/edware-udl/*\""
+sudo -u root -s rm -fr /opt/wgen/edware-udl/*
+echo "sudo -u root -s \"/usr/sbin/userdel udl2\""
+sudo -u root -s /usr/sbin/userdel udl2

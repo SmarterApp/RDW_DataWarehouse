436bdc6578e24bb0aba2bdd426e8b340798cd284
Lili Chen <lichen@wgen.net>
2013-06-11 17:39:03 -0400

    update poc code for validation rules

--- src/rule_maker/rules/validate1.py
+++ src/rule_maker/rules/validate1.py
@@ -39,7 +39,7 @@ BOTH = 'both'  # if ALL rows are bad, then one entry in ERR table
 ISNOTNULL = 'not_null'  # error if value is null
 DATE = 'date'  # parameter=date-format, check that the given column (a VARCHAR) corresponds to a date with the given format
 MINDATE = 'min_date'  # parameter=date-value, check that the given column (a VARCHAR) has a date value not less than the given date-value
-UNIQUE = 'unique'  # error if column value is not unique across all rows --- perhaps this can assume 'ALL'
+ISUNIQUE = 'unique'  # error if column value is not unique across all rows --- perhaps this can assume 'ALL'
 REGEX = 'regex'  # validate the column against the given regex
 ALPHA = {REGEX: '[A-Za-z]'}
 GUID = {REGEX: '[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}'}
@@ -56,7 +56,7 @@ __validation_rules_definition = {BYCOLUMN: {'guid_asmt': [ISNOTNULL],  # for EAC
                                             'guid_assessment': [ISNOTNULL, BOTH],  # combination of ALL then EACH
                                             'date_assessed': [ISNOTNULL, {DATE:'YYYY-MM-DD'}],  # check EACH date not null, and has the given format
                                             'dob_student': [ISNOTNULL, AFTER_2001, ALL],  # record 1 error if ALL dob_student are NULL or if all dob_student are less than Jan 1st 2001
-                                            'guid_staff': [[ISNOTNULL, ALL], [UNIQUE, EACH]],
+                                            'guid_staff': [[ISNOTNULL, ALL], [ISUNIQUE, EACH]],
                                             'name_student_first': ALPHA,
                                             'guid_inst_hier': GUID
 
@@ -75,11 +75,11 @@ __validation_rules_definition = {BYCOLUMN: {'guid_asmt': [ISNOTNULL],  # for EAC
 
 
 # dictionary to map notation and corresponding sql
-__validation_notation_sql_dict = {ISNOTNULL: {ALL: NULL_ALL_SQL, EACH: NULL_SQL, BOTH: 'TBD'},
+__validation_notation_sql_dict = {ISNOTNULL: {ALL: NULL_ALL_SQL, EACH: NULL_SQL, BOTH: '\nGOING TO GENERATE SQL FOR ISNOTNULL -- BOTH\n'},
                                   DATE: DATE_FORMAT_SQL,
-                                  # AFTER_2001: 'TBD',
-                                  UNIQUE: UNIQUE_SQL,
-                                  # ALPHA: 'TBD',
+                                  MINDATE: '\nGOING TO GENERATE SQL FOR MINDATE RULE\n',
+                                  ISUNIQUE: UNIQUE_SQL,
+                                  REGEX: '\nGOING TO GENERATE SQL FOR REGEX RULE\n',
                                   # GUID: 'TBD'
                                }
 
@@ -88,58 +88,85 @@ __validation_rules_error_code = {}
 
 
 def by_column_func(column_name, rules_for_column, **para):
+    # print(column_name, rules_for_column)
     para['column'] = column_name
+    scope = EACH
     sql_stat = []
-    # check the items in rules_for_column
+    # check the type of items in rules_for_column
     if isinstance(rules_for_column, list):
-        # individual notation defined, e.g. [ISNOTNULL, ALL]
-        if not all(isinstance(n, list) for n in rules_for_column):
-            sql_stat = get_sql_for_notations(rules_for_column, **para)
-        # list of notations defined. e.g. [[ISNOTNULL, ALL], [UNIQUE, EACH]]
-        else:
-            for one_rule_set in rules_for_column:
-                sql_stat.extend(get_sql_for_notations(one_rule_set, **para))
+        for one_rule_set in rules_for_column:
+            if one_rule_set in [ALL, EACH, BOTH]:
+                continue
+            # pattern 1 -- list of notations defined. e.g. [[ISNOTNULL, ALL], [ISUNIQUE, EACH]]
+            if isinstance(one_rule_set, list):
+                sql_stat.extend(by_column_func(column_name, one_rule_set, **para))
+                # print("recursive once,", one_rule_set, column_name, len(sql_stat))
+
+            # pattern 2 -- dictionary, e.g. {action: parameter}
+            elif isinstance(one_rule_set, dict):
+                for action, parameters in one_rule_set.items():
+                    para = construct_parameters(action, parameters, para)
+                    sql_stat.extend(get_sql_for_notation(action, scope, ** para))
+
+            # pattern 3 -- individual action, but not ALL, EACH or BOTH
+            elif one_rule_set not in [ALL, EACH, BOTH]:
+                # individual notation defined, e.g. [ISNOTNULL, ALL]
+                if ALL in rules_for_column:
+                    scope = ALL
+                elif BOTH in rules_for_column:
+                    scope = BOTH
+                sql_stat = get_sql_for_notation(one_rule_set, scope, ** para)
+            else:
+                print("error", column_name, one_rule_set)
+                raise Exception
     else:
         # single notation
-        pass
+        sql_stat = by_column_func(column_name, [rules_for_column], **para)
     return sql_stat
 
 
 def by_rule_func(rule_content, **parm):
-    pass
+    '''
+    Main function to process by_rule validation notations
+    #TODO: TO BE WRITTEN
+    '''
+    return []
 
 
-def get_sql_for_notations(rules_for_column, **parm):
-    scope = EACH
-    if ALL in rules_for_column:
-        scope = ALL
-    elif BOTH in rules_for_column:
-        scope = BOTH
+def construct_parameters(action, parameters, para):
+    # TODO: make it for all cases
+    if action == DATE:
+        para['date_format'] = parameters
+    return para
+
+
+def get_sql_for_notation(notation, scope, **parm):
+    # print('get_sql_for_notation', notation)
+    sql_stat_template = ''
+
     # get rest of notations
-    notations = [rule for rule in rules_for_column if rule is not scope]
     sql_stat = []
-    for notation in notations:
-        parm['error_code'] = get_error_code_for_notation(notation, scope)
-        if notation in list(__validation_notation_sql_dict.keys()):
-            if notation == ISNOTNULL:
-                sql_stat_template = __validation_notation_sql_dict[ISNOTNULL][scope]
-            elif notation == UNIQUE:
-                sql_stat_template = __validation_notation_sql_dict[UNIQUE]
-        elif isinstance(notation, dict):
-            if DATE in list(notation.keys()):
-                sql_stat_template = __validation_notation_sql_dict[DATE]
-                parm['date_format'] = notation[DATE]
-        # replace value in template
-        sql_statement = sql_stat_template.format(**parm)
+    parm['error_code'] = get_error_code_for_notation(notation, scope)
+    if notation in list(__validation_notation_sql_dict.keys()):
+        if notation == ISNOTNULL:
+            sql_stat_template = __validation_notation_sql_dict[ISNOTNULL][scope]
+        else:
+            sql_stat_template = __validation_notation_sql_dict[notation]
+
+    # replace value in template
+    sql_statement = sql_stat_template.format(**parm)
+    if len(sql_statement) > 0:
+        # print("One rule is generated for %s" % notation)
         sql_stat.append(sql_statement)
     return sql_stat
 
 
 def get_error_code_for_notation(notation, scope):
-    # TODO: TBD
+    # TODO: need to write code to get error code
     return random.randint(1, 1000)
 
 
+# This is the dictionary to map validation rule category and corresponding function
 __validation_rules_func = {BYCOLUMN: by_column_func,
                            BYRULE: by_rule_func}
 
@@ -153,16 +180,13 @@ def generate_validation_proc(para):
         func = __validation_rules_func[rule_type]
         # BYRULE
         if isinstance(rule_content, list):
-            temp = func(rule_content, **para)
-            if temp is not None:
-                sql_stat.extend(temp)
+            sql_stat.extend(func(rule_content, **para))
         # BYCOLUMN
         elif isinstance(rule_content, dict):
             for column_name, rules_for_column in rule_content.items():
-                temp = func(column_name, rules_for_column, **para)
-                if temp is not None:
-                    sql_stat.extend(temp)
+                sql_stat.extend(func(column_name, rules_for_column, **para))
         else:
+            # can be replaced by raising exception
             print("error")
     return sql_stat
 
@@ -174,5 +198,6 @@ if __name__ == '__main__':
             'table': 'INT_SBAC_ASMT_OUTCOME'
     }
     validation_proc = generate_validation_proc(para)
+    print("Generated %i number of rules" % len(validation_proc))
     for proc in validation_proc:
         sys.stdout.write(proc)

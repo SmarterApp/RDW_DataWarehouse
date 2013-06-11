30b914cc819211064d16bf845e10191de5cb6559
Doris Ip <dip@wgen.net>
2013-06-11 15:12:43 -0400

    Fix UT issue in edapi
    Former-commit-id: 31a69f0f0895db5abb13ee15b3c52a450fbb21d0

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -87,10 +87,6 @@ function run_unit_tests {
     echo "Running unit tests"
    
     cd "$WORKSPACE/$1"
-    # Tech debt in edapi logging test - it must go inside one more dir
-    if [ ${1:=""} == "edapi" ]; then
-       cd "$1"
-    fi
      
     nosetests --with-xunit --xunit-file=$WORKSPACE/nosetests.xml --cov-report xml
 

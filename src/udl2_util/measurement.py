e381b8089fc17f285f9a3969c0b400db64a5c553
Eugene Jen <ejen@wgen.net>
2013-06-11 09:45:06 -0400

    add unit for measurements

--- src/udl2_util/measurement.py
+++ src/udl2_util/measurement.py
@@ -18,7 +18,7 @@ def measure_cpu_time(fn):
         start_cpu_time = time.clock()
         fn(*args, **kwargs)
         end_cpu_time = time.clock()
-        print("cpu time {time} for executing {function}".format(time=end_cpu_time - start_cpu_time,
+        print("cpu time {time} seconds for executing {function}".format(time=end_cpu_time - start_cpu_time,
                                                                 function=fn.__name__))
     return _wrapped
 
@@ -29,6 +29,6 @@ def measure_elasped_time(fn):
         start_time = time.time()
         fn(*args, **kwargs)
         end_time = time.time()
-        print("elapsed time {time} for executing {function}".format(time=end_time - start_time,
+        print("elapsed time {time} seconds for executing {function}".format(time=end_time - start_time,
                                                                     function=fn.__name__))
     return _wrapped

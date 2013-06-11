899cee2e8db25e5ee146cca6c9e23cfb966b82bd
Eugene Jen <ejen@wgen.net>
2013-06-11 14:58:54 -0400

    make comment clearer

--- src/udl2_util/measurement.py
+++ src/udl2_util/measurement.py
@@ -88,7 +88,8 @@ def show_amount_of_data_affected(fn, quiet=udl2_conf['quiet_mode']):
     '''
     a decorator to print normalized message for amount of data to be moved
     @param fn: function that are to be executed
-    @type fn: a funtion in python that return amount of data be moved
+    @type fn: a funtion in python that return amount of data be moved, it returns a dictionary with following fields {amount: number of data,
+            unit: measure unit, module: where the code module is, function: what function is used}
     '''
     def _wrapped(*args, **kwargs):
         if quite:

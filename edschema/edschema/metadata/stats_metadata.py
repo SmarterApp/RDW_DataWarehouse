c1529f2cf1b822b0b07ff1a0c03fca84f72f3e04
Doris Ip <dip@wgen.net>
2013-06-25 15:09:58 -0400

    Fix UT
    Former-commit-id: 74a3a6bb0640fe03b5d502cf9ccf9fc554f7a021

--- edschema/edschema/metadata/stats_metadata.py
+++ edschema/edschema/metadata/stats_metadata.py
@@ -10,14 +10,14 @@ from sqlalchemy.types import String, DateTime, BigInteger
 def generate_stats_metadata(schema_name=None, bind=None):
     metadata = MetaData(schema=schema_name, bind=bind)
     udl_stats = Table('udl_stats', metadata,
-                        Column('state_code', String(2), nullable=False),
-                        Column('tenant', String(32), nullable=True),
-                        Column('load_start', DateTime, nullable=False),
-                        Column('load_end', DateTime, nullable=True),
-                        Column('load_status', String(32), nullable=False),
-                        Column('batch_guid', String(50), nullable=True),
-                        Column('record_loaded_count', BigInteger, nullable=False),
-                        Column('last_pdf_task_requested', DateTime, nullable=True),
-                        Column('last_pre_cached', DateTime, nullable=True)
-                        )
+                      Column('state_code', String(2), nullable=False),
+                      Column('tenant', String(32), nullable=True),
+                      Column('load_start', DateTime, nullable=False),
+                      Column('load_end', DateTime, nullable=True),
+                      Column('load_status', String(32), nullable=False),
+                      Column('batch_guid', String(50), nullable=True),
+                      Column('record_loaded_count', BigInteger, nullable=False),
+                      Column('last_pdf_task_requested', DateTime, nullable=True),
+                      Column('last_pre_cached', DateTime, nullable=True)
+                      )
     return metadata

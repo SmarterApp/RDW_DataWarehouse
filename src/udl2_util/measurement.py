710d7c3ec00716ce1298698a410d5f740dc37227
Seth Wimberly <swimberly@wgen.net>
2013-06-27 17:53:10 -0400

    Updates for story to add demographics columns

--- src/udl2_util/measurement.py
+++ src/udl2_util/measurement.py
@@ -27,14 +27,14 @@ from udl2_conf import udl2_conf
 
 def remove_celery_system_frame_objects(frames):
     '''
-    a function that removes frame objects that is part of celery system. So we keep only UDL2 related calling 
+    a function that removes frame objects that is part of celery system. So we keep only UDL2 related calling
     @param frames: a list of frames names
     @type frames: list
     '''
     udl2_frames = [f for f in frames if '/celery/' not in f and '<frozen ' not in f and '/billiard/' not in f and '/kombu/' not in f and 'measurement.py:_wrapped' not in f and
-                   '/celery:<module>' not in f] 
+                   '/celery:<module>' not in f]
     return udl2_frames
-    
+
 
 def measure_cpu_time(fn, quiet=udl2_conf['quiet_mode']):
     '''
@@ -49,14 +49,14 @@ def measure_cpu_time(fn, quiet=udl2_conf['quiet_mode']):
             start_cpu_time = time.clock()
             result = fn(*args, **kwargs)
             end_cpu_time = time.clock()
-            current_frame = inspect.currentframe()   
+            current_frame = inspect.currentframe()
             outer_frames = inspect.getouterframes(current_frame, 1)
             frames = [fn.__name__] + [of[1] + ':' + of[3] for of in outer_frames]
             frames = remove_celery_system_frame_objects(frames)
             print("MEASURE-- cpu time {time:.10f} seconds for executing {module:s}.{function:s}".format(time=(end_cpu_time - start_cpu_time),
                                                                                               module=fn.__module__,
                                                                                               function=fn.__name__,))
-            return result   
+            return result
     return _wrapped
 
 
@@ -73,7 +73,7 @@ def measure_elapsed_time(fn, quiet=udl2_conf['quiet_mode']):
             start_time = time.time()
             result = fn(*args, **kwargs)
             end_time = time.time()
-            current_frame = inspect.currentframe()   
+            current_frame = inspect.currentframe()
             outer_frames = inspect.getouterframes(current_frame, 1)
             frames = [fn.__name__] + [of[1] + ':' + of[3] for of in outer_frames]
             frames = remove_celery_system_frame_objects(frames)
@@ -99,20 +99,20 @@ def measure_cpu_plus_elasped_time(fn, quiet=udl2_conf['quiet_mode']):
             result = fn(*args, **kwargs)
             end_clock = time.clock()
             end_time = time.time()
-            current_frame = inspect.currentframe()   
+            current_frame = inspect.currentframe()
             outer_frames = inspect.getouterframes(current_frame, 1)
             frames = [fn.__name__] + [of[1] + ':' + of[3] for of in outer_frames]
             frames = remove_celery_system_frame_objects(frames)
-            print(("MEASURE-- " +
-                  " call stack: {frame}, " +
-                  " cpu time: {ctime:.10f} seconds, elapsed time: {etime:.10f} seconds " +
-                  " for executing {module:s}.{function:s}, args: {args},kwargs: {kwargs}").format(frame=" <== ".join(frames),
-                                                                                                 etime=(end_time - start_time),
-                                                                                                 module=fn.__module__,
-                                                                                                 ctime=(end_clock - start_clock),
-                                                                                                 function=fn.__name__,
-                                                                                                 args=args,
-                                                                                                 kwargs=kwargs))
+#             print(("MEASURE-- " +
+#                   #" call stack: {frame}, " +
+#                   " cpu time: {ctime:.10f} seconds, elapsed time: {etime:.10f} seconds " +
+#                   " for executing {module:s}.{function:s}, args: {args},kwargs: {kwargs}").format(frame=" <== ".join(frames),
+#                                                                                                  etime=(end_time - start_time),
+#                                                                                                  module=fn.__module__,
+#                                                                                                  ctime=(end_clock - start_clock),
+#                                                                                                  function=fn.__name__,
+#                                                                                                  args=args,
+#                                                                                                  kwargs=kwargs))
             return result
     return _wrapped
 
@@ -129,18 +129,18 @@ def show_amount_of_data_affected(fn, quiet=udl2_conf['quiet_mode']):
             return fn(*args, **kwargs)
         else:
             result = fn(*args, **kwargs)
-            current_frame = inspect.currentframe()   
+            current_frame = inspect.currentframe()
             outer_frames = inspect.getouterframes(current_frame, 1)
             frames = [fn.__name__] + [of[1] + ':' + of[3] for of in outer_frames]
             frames = remove_celery_system_frame_objects(frames)
-            print(("MEASURE--  call stack: {frame}, " +
-                  "{amount:s} {unit:s} are {action:s} by {module:s}.{function:s}").format(frame=" <== ".join(frames),
-                                                                                          amount=str(result['amount']),
-                                                                                          unit=result['unit'],
-                                                                                          action=result['action'],
-                                                                                          module=result['module'],
-                                                                                          function=result['function']))
-            return result
+#             print(("MEASURE--  call stack: {frame}, " +
+#                   "{amount:s} {unit:s} are {action:s} by {module:s}.{function:s}").format(frame=" <== ".join(frames),
+#                                                                                           amount=str(result['amount']),
+#                                                                                           unit=result['unit'],
+#                                                                                           action=result['action'],
+#                                                                                           module=result['module'],
+#                                                                                           function=result['function']))
+#             return result
     return _wrapped
 
 

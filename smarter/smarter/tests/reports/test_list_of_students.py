6f1b91c9ef1d04bcfe9d62f6916ab3e27537e796
agrebnevawgen <agrebneva@gmail.com>
2013-06-28 17:17:02 -0400

    fixing build
    Former-commit-id: 0093c6ab8e33f3290d76447a15ac86cfebe09bb5

--- smarter/smarter/tests/reports/test_list_of_students.py
+++ smarter/smarter/tests/reports/test_list_of_students.py
@@ -21,7 +21,7 @@ class TestLOS(Unittest_with_smarter_sqlite):
     def setUp(self):
         cache_opts = {
             'cache.type': 'memory',
-            'cache.regions': 'public.data.short'
+            'cache.regions': 'public.shortlived'
         }
 
         CacheManager(**parse_cache_config_options(cache_opts))

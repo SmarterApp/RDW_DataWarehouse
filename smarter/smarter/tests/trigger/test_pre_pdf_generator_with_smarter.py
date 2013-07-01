ac9d1338ac36841c096885ddf46234f473000cd5
Nidhi Paroha <nparoha@wgen.net>
2013-07-01 13:02:34 -0400

    Updating the build_guid for one student in fact_asmt_outcome csv and updated unit tests
    Former-commit-id: 2f3445d563f1f595f2896e7ad8a60c1e6849cf24

--- smarter/smarter/tests/trigger/test_pre_pdf_generator_with_smarter.py
+++ smarter/smarter/tests/trigger/test_pre_pdf_generator_with_smarter.py
@@ -25,8 +25,10 @@ class TestPrePdfGenerator(Unittest_with_smarter_sqlite):
         results = prepare_pre_pdf(self.tenant, 'NY', '820568d0-ddaa-11e2-a63d-68a86d3c2f82')
         self.assertEqual(58, len(results))
         results = prepare_pre_pdf(self.tenant, 'NY', '90901b70-ddaa-11e2-a95d-68a86d3c2f82')
-        self.assertEqual(120, len(results))
-
+        self.assertEqual(118, len(results))
+        results = prepare_pre_pdf(self.tenant, 'NY', 'd2d02660-ddd7-11e2-a28f-0800200c9a66')
+        self.assertEqual(2, len(results))
+        
     def test_prepare_pre_pdf_with_future_date(self):
         results = prepare_pre_pdf(self.tenant, 'NY', 'd1d7d814-ddb1-11e2-b3dd-68a86d3c2f82')
         self.assertEqual(0, len(results))

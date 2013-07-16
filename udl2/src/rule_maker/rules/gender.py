f8f47564401a87460b3f93077f6147197c72854c
Karishma Velingkar <kvelingkar@wgen.net>
2013-07-16 12:59:24 -0400

    pep8 fix
    Former-commit-id: 667d1662d33d550794bb37be04e06f0536b870bd

--- udl2/src/rule_maker/rules/gender.py
+++ udl2/src/rule_maker/rules/gender.py
@@ -10,7 +10,4 @@ __prepare = __default_prep
 __canon = {'M': ['M', 'B'],
            'F': ['F', 'G'],
            'NS': ['NS', 'NOT_SPECIFIED', 'NOT SPECIFIED']
-           }  
-
-
-  
+           }

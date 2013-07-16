6f72eee3de4102709dab139cec742dc9f36afed6
Karishma Velingkar <kvelingkar@wgen.net>
2013-07-16 17:05:22 -0400

    pep8 fix
    Former-commit-id: 95bd53a26337d915a16ba7c1e67192ed643e43f3

--- udl2/tests/integration_tests/iTest_load_csv_to_SBAC_star.py
+++ udl2/tests/integration_tests/iTest_load_csv_to_SBAC_star.py
@@ -9,8 +9,9 @@ import imp
 import uuid
 from udl2 import message_keys as mk
 
+
 class ITestLoadCsvToSBACStar(unittest.TestCase):
-    
+
     def setUp(self):
         try:
             config_path = dict(os.environ)['UDL2_CONF']
@@ -19,10 +20,10 @@ class ITestLoadCsvToSBACStar(unittest.TestCase):
         udl2_conf = imp.load_source('udl2_conf', config_path)
         from udl2_conf import udl2_conf
         self.conf = udl2_conf
-    
+
     def tearDown(self):
         pass
-    
+
     def test_load_csv_to_sbac_star(self):
         if os.path.isfile("{datafiles}/REALDATA_ASMT_ValidData.csv".format(datafiles=self.conf['zones']['datafiles'])):
             print("driver.py -c {datafiles}/REALDATA_ASMT_ValidData.csv -j {datafiles}/METADATA_ASMT_ValidData.json".format(datafiles=self.conf['zones']['datafiles']))
@@ -72,4 +73,4 @@ class ITestLoadCsvToSBACStar(unittest.TestCase):
             self.assertTrue(False)
 
 if __name__ == "__main__":
-	unittest.main()    
+    unittest.main()

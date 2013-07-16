ea512380206ec3b5b1966f8812457dceae6bf8af
Doris Ip <dip@wgen.net>
2013-07-16 16:56:06 -0400

    Fix typo in jenkins script
    Former-commit-id: c804dd152ec9c17191de56d2b3cd556151ce9786

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -224,7 +224,7 @@ function create_sym_link_for_apache {
     mkdir -p ${APACHE_DIR}
     /bin/ln -sf ${VIRTUALENV_DIR}/lib/python3.3/site-packages ${APACHE_DIR}/pythonpath
     /bin/ln -sf ${WORKSPACE}/config/${INI_FILE_FOR_ENV} ${SMARTER_INI}
-    /bin/ln -sf ${WORKSPACE}/config/comparing_populations_precache_filters.json ${PRECACHE_JSON}
+    /bin/ln -sf ${WORKSPACE}/config/comparing_populations_precache_filters.json ${PRECACHE_FILTER_JSON}
     /bin/ln -sf ${WORKSPACE}/smarter/smarter.wsgi ${APACHE_DIR}/pyramid_conf
     /bin/ln -sf ${VIRTUALENV_DIR} ${APACHE_DIR}/venv
 

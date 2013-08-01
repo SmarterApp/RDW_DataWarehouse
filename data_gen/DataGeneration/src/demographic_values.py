b4776ab0f1a50e28e59fbd38f40fcba995d76d8f
Seth Wimberly <swimberly@wgen.net>
2013-08-01 16:14:34 -0400

    Removing commented code.
    Former-commit-id: d6eb8535489179e424b5d3cb4d85158d5ddd8be2

--- data_gen/DataGeneration/src/demographic_values.py
+++ data_gen/DataGeneration/src/demographic_values.py
@@ -15,33 +15,6 @@ def get_demo_values(total, possible_value_list, percentage_list):
     return results
 
 
-# def get_single_demo_value(total, possible_value, percentage):
-#     results = []
-#     res_dict = {}
-#     if total > 0:
-#         if len(possible_value) == len(percentage) and len(possible_value) > 0:
-#             for i in range(len(possible_value)):
-#                 value = possible_value[i]
-#                 perc = percentage[i]
-#                 count = round(total * perc / 100)
-#                 results.extend([value] * count)
-#                 res_dict[value] = count
-#             # adjust the results
-#             print("Distance % i" % (total - len(results)))
-#             if len(results) < total:
-#                 # find the smallest one
-#                 val, idx = min((val, idx) for (idx, val) in enumerate(percentage) if int(val) > 0)
-#                 print("Going to add value '%s'" % possible_value[idx])
-#                 results.extend([possible_value[idx]] * abs(total - len(results)))
-#             elif len(results) > total:
-#                 # find the biggest one
-#                 val, idx = max((val, idx) for (idx, val) in enumerate(percentage) if int(val) > 0)
-#                 print("Going to remove value '%s'" % possible_value[idx])
-#                 for _j in range(abs(total - len(results))):
-#                     results.remove(possible_value[idx])
-#     return results
-
-
 def get_single_demo_value(total, possible_value, percentage):
     results = {}
 

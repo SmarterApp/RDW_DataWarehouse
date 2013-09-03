807ab05d6577ca940766ed2a536343787d446569
Doris Ip <dip@wgen.net>
2013-09-03 16:13:10 -0400

    Fix path for FT docs
    Former-commit-id: 011d61fea3c14ffdea914617ebd3fe6a4d3a7d49

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -219,7 +219,7 @@ function run_functional_tests {
        nosetests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
     else
        nosetests --exclude-dir=e2e_tests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
-       generate_docs functional_tests
+       generate_docs edware_test/edware_test/functional_tests
     fi
 
     echo "Finish running functional tests"

94a0bdda2c1841e61d372270d9e6e32946480987
Bhumi Patel <bpatel@amplify.com>
2013-09-16 23:08:23 -0400

    Banchmarking functional test
    Former-commit-id: 40ed3b51b48e52b7c96a0b9bf8dec689bcdda0a7

--- udl2/tests/functional_tests/fTest_banchmarking_test.py
+++ udl2/tests/functional_tests/fTest_banchmarking_test.py
@@ -8,9 +8,12 @@ from sqlalchemy.engine import create_engine
 import imp
 import subprocess
 import os
+import time
 
-METADATA_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/henshin_out/Banchmarking_json_file.json'
-FACT_OUTCOME_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/Banchmarking_test_data.csv'
+
+METADATA_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/Banchmarking_json_file.json'
+FACT_OUTCOME_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/BANCHMARK_DATA.csv'
+#FACT_OUTCOME_FILE_PATTERN = '/Users/bpatel/Documents/henshin_out/REALDATA_ASMT_ID_20b0b242-59c9-4b07-aed2-7524f43ef9cb.csv'
 UDL2_DEFAULT_CONFIG_PATH_FILE = '/opt/wgen/edware-udl/etc/udl2_conf.py'
 
 
@@ -22,7 +25,11 @@ class ValidateTableData(unittest.TestCase):
         self.host = 'localhost'
         self.port = '5432'
         self.database = 'udl2'
-    
+
+    def empty_batch_table(self, db_connection):
+        output = db_connection.execute('DELETE FROM udl2."UDL_BATCH";')
+        print ("Sucessfully delete all data from Batch Table")
+
     def run_udl_pipeline(self):
         try:
             config_path = dict(os.environ)['UDL2_CONF']
@@ -31,32 +38,36 @@ class ValidateTableData(unittest.TestCase):
         udl2_conf = imp.load_source('udl2_conf', config_path)
         from udl2_conf import udl2_conf
         self.conf = udl2_conf
-        #files = subprocess.Popen("ls {json_file}".format(json_file=METADATA_FILE_PATTERN), stdout=subprocess.PIPE, shell=True)
-        #out, err = files.communicate()
-        run_udl1 = 'mv python ../../scripts/driver.py -c{csv_file}'.format(csv_file=FACT_OUTCOME_FILE_PATTERN)
-        run_udl2 = 'mv python ../../scripts/driver.py -c{json_file}'.format(json_file=METADATA_FILE_PATTERN)
-        subprocess.call(run_udl1)
-        subprocess.call(run_udl2)
-        
+
+        command = "python ../../scripts/driver.py -c {csv_file} -j {json_file}".format(csv_file=FACT_OUTCOME_FILE_PATTERN, json_file=METADATA_FILE_PATTERN)
+        print(command)
+        subprocess.call(command, shell=True)
+
     def connect_db(self):
         db_string = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{database}'.format(user=self.user, passwd=self.passwd, host=self.host, port=self.port, database=self.database)
         engine = create_engine(db_string)
         db_connection = engine.connect()
         return db_connection, engine
 
-    def excecute_query(self, db_connection):
-        try:
-            result = db_connection.execute('SELECT record_sid FROM udl2."STG_SBAC_ASMT_OUTCOME";').fetchall()
-            print (result)
-        except:
-            print("error")
+    def get_data_from_batch_table(self, db_connection):
+        time.sleep(40)
+        result = db_connection.execute('SELECT * FROM udl2."UDL_BATCH";').fetchall()
+        print (len(result))
+        number_of_row = len(result)
+        if number_of_row < 20:
+            time.sleep(30)
+            print(number_of_row)
+        self.assertEqual(number_of_row, 20)
 
-    def testnew(self):
-        
+        output = db_connection.execute('SELECT udl_phase FROM udl2."UDL_BATCH";').fetchall()
+        tuple_str = ('UDL_COMPLETE',)
+        self.assertIn(tuple_str, output)
+
+    def test_batchmarking_data(self):
         db_conn, engine = self.connect_db()
-        self.excecute_query(db_conn)
+        self.empty_batch_table(db_conn)
         self.run_udl_pipeline()
-        
+        self.get_data_from_batch_table(db_conn)
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

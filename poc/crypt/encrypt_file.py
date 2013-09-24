fa908c9592f6d9438bd384937c6ddbc2eed78e11
Sadish Ravi <sravi@amplify.com>
2013-09-24 14:49:41 -0400

    poc script for encryption
    Former-commit-id: 41919fc02b7b06410b748fe9e8b1614f4d49c508

--- poc/crypt/encrypt_file.py
+++ poc/crypt/encrypt_file.py
@@ -1,12 +1,27 @@
 import os
 import gnupg
+import argparse
+import time
+
 from pprint import pprint
+
 gpg = gnupg.GPG(gnupghome='/Users/Shared/Amplify/wgen_dev/gpghome')
  
-#open('data/my-unencrypted.txt', 'w').write('EdWare Secret Data')
-with open('data/my-unencrypted.txt', 'rb') as f:
-    status = gpg.encrypt_file(f,'edware_dataprovider_1@amplify.com', output='data/my-encrypted.txt.gpg')
- 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--source", dest="source_file")
+parser.add_argument("--dest", dest="dest_file")
+parser.add_argument("--sendto", dest="sendto")
+args = parser.parse_args()
+
+start=time.time()
+with open(args.source_file, 'rb') as f:
+    status = gpg.encrypt_file(f,args.sendto, output=args.dest_file)
+end=time.time()
+
 print ('ok: ', status.ok)
 print ('status: ', status.status)
 print ('stderr: ', status.stderr)
+print ('source file size (B): ', os.path.getsize(args.source_file))
+print ('time taken (ms): ', round((end-start)*1000))
+print ('encrypted file size (B): ', os.path.getsize(args.dest_file))

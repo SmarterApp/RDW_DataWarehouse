3f5a3a2f75f342a53c8afd4c511be1bcc8efa9ad
Sadish Ravi <sravi@amplify.com>
2013-09-24 15:26:25 -0400

    gnupg poc Decrypt script
    Former-commit-id: 186d1e2f271953e0a9ef615ba317052317400f23

--- poc/crypt/decrypt_file.py
+++ poc/crypt/decrypt_file.py
@@ -1,11 +1,27 @@
 import os
 import gnupg
+import argparse
+import time
+
 from pprint import pprint
+
 gpg = gnupg.GPG(gnupghome='/Users/Shared/Amplify/wgen_dev/gpghome')
  
-with open('data/my-encrypted.txt.gpg', 'rb') as f:
-    status = gpg.decrypt_file(f, passphrase='edware udl2', output='data/my-decrypted.txt')
- 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--source", dest="source_file")
+parser.add_argument("--dest", dest="dest_file")
+parser.add_argument("--sendto", dest="sendto")
+args = parser.parse_args()
+
+start=time.time()
+with open(args.source_file, 'rb') as f:
+    status = gpg.decrypt_file(f,passphrase='edware udl2', output=args.dest_file)
+end=time.time()
+
 print ('ok: ', status.ok)
 print ('status: ', status.status)
 print ('stderr: ', status.stderr)
+print ('source file size (B): ', os.path.getsize(args.source_file))
+print ('time taken (ms): ', round((end-start)*1000))
+print ('encrypted file size (B): ', os.path.getsize(args.dest_file))

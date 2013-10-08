b713ca771e6006de59dd72167fc54f85a09b3cc7
Sadish Ravi <sravi@amplify.com>
2013-10-08 19:11:47 -0400

    changing distutils to setup tools to support "dash" in python-gnupg
    http://stackoverflow.com/questions/9810603/adding-install-requires-to-se
    tup-py-when-making-a-python-package
    
    http://stackoverflow.com/questions/16127080/setup-py-expected-parenthesi
    zed-list-when-required-package-name-contains-a-dash
    
    Former-commit-id: 61a823c9ccaa9bf96fa128bc02056c7d3f9cc42c

--- udl2/setup.py
+++ udl2/setup.py
@@ -3,15 +3,16 @@ Created on May 2, 2013
 
 @author: ejen
 '''
-from distutils.core import setup
+from setuptools import setup
 import subprocess
 
 subprocess.call('cd ./scripts/; ./install_udl_requirements.sh', shell=True)
 
-requires = ['celery(>=3.0.19)',
+install_requires = ['celery >= 3.0.19',
             'Sphinx',
             'docutils',
-            'repoze.sphinx.autointerface']
+            'repoze.sphinx.autointerface',
+            'python-gnupg']
 
 scripts = ['scripts/initialize_udl2_database.sh',
            'scripts/initialize_udl2_system.sh',
@@ -32,7 +33,7 @@ setup(name='udl2',
       description="Edware's Universal Data Loader",
       author="Amplify Insight Edware Team",
       author_email="edwaredev@wgen.net",
-      packages=['edschema', 'edschema.metadata', 'edschema.tests', 'fileloader', 'filesplitter', 'final_cleanup', 'move_to_integration',
+      packages=['edschema', 'edschema.metadata', 'edschema.tests', 'fileloader','filedecrypter', 'filesplitter', 'final_cleanup', 'move_to_integration',
                 'move_to_target', 'sfv', 'udl2', 'udl2_util', 'udl2_tests', 'fileexpander',
                 'rule_maker', 'rule_maker.makers', 'rule_maker.rules', 'preetl', 'scripts', 'benchmarking'],
       package_dir={'edschema': '../edschema/edschema',
@@ -50,11 +51,12 @@ setup(name='udl2',
                    'scripts': 'scripts',
                    'benchmarking': 'benchmarking',
                    'fileexpander': 'src/fileexpander',
+                   'filedecrypter': 'src/filedecrypter',
                    },
       package_data={'udl2': ['datafiles/*.csv']},
       url='https://github.wgenhq.net/Ed-Ware-SBAC/edware-udl-2.0/',
       scripts=scripts,
-      requires=requires,
+      install_requires=install_requires,
       data_files=[('/var/log/wgen/edware-udl/logs', ['logs/udl2.audit.log', 'logs/udl2.error.log']),
                   ('/opt/wgen/edware-udl/etc', ['conf/udl2_conf.ini', 'conf/udl2_conf.cfg', 'conf/udl2_conf.py']),
                   ('/opt/wgen/edware-udl/zones/datafiles/', ['src/udl2/datafiles/seed.csv']),

67579aeb691a420561a19eb35890da253e41b22d
Sadish Ravi <sravi@amplify.com>
2013-10-09 12:58:26 -0400

    Tests for filexpander
    Former-commit-id: d9417ca259e0bc7f28110fd4e5be171bc3f5d46c

--- udl2/src/fileexpander/file_expander.py
+++ udl2/src/fileexpander/file_expander.py
@@ -10,33 +10,33 @@ import tarfile
 logger = logging.getLogger(__name__)
 
 
-def __is_file_exists(file_to_expand):
+def _is_file_exists(file_to_expand):
     '''
     check if file exists and readable
     '''
     return os.path.isfile(file_to_expand) and os.access(file_to_expand, os.R_OK)
 
 
-def __is_tar_file(file_to_expand):
+def _is_tar_file(file_to_expand):
     '''
     check for valid tar file
     '''
     return tarfile.is_tarfile(file_to_expand)
 
 
-def __is_valid__tar_file(file_to_expand):
+def _is_valid__tar_file(file_to_expand):
     '''
     Basic file validation checks before expanding
     '''
     valid = False
-    if __is_file_exists(file_to_expand):
+    if _is_file_exists(file_to_expand):
         valid = True
         print("File exists and is readable -- %s " % file_to_expand)
     else:
         print("File missing or un-readable -- %s " % file_to_expand)
 
     if valid:
-        if __is_tar_file(file_to_expand):
+        if _is_tar_file(file_to_expand):
             print("Tar file format recongnized -- %s " % file_to_expand)
         else:
             valid = False
@@ -44,29 +44,32 @@ def __is_valid__tar_file(file_to_expand):
     return valid
 
 
-def __extract_tar_file_contents(file_to_expand, expanded_dir):
+def _extract_tar_file_contents(file_to_expand, expanded_dir):
     '''
     extract file contents to the destination directory
     '''
+    tar_file_contents = []
     tar = tarfile.open(file_to_expand, "r:gz")
     for tarinfo in tar:
+        tar_file_contents.append(expanded_dir + tarinfo.name)
         print(tarinfo.name, tarinfo.size, " bytes in size, is a regular file: ", tarinfo.isreg())
     # TODO: how to deal with file's which are archived with absolute paths
     tar.extractall(expanded_dir)
     tar.close()
+    return tar_file_contents
 
 
 def expand_file(file_to_expand, expanded_dir):
     '''
     Expand the file after needed validations
     '''
-    if not __is_valid__tar_file(file_to_expand):
+    if not _is_valid__tar_file(file_to_expand):
         raise Exception('Invalid source file -- %s' % file_to_expand)
 
     # the contents of the tar file will be validated as part of simple file validator
-    __extract_tar_file_contents(file_to_expand, expanded_dir)
+    tar_file_contents = _extract_tar_file_contents(file_to_expand, expanded_dir)
 
-    return expanded_dir
+    return tar_file_contents
 
 
 if __name__ == "__main__":

af10d446431dcaabf585e4c226f03e6aac07c5f0
Seth Wimberly <swimberly@wgen.net>
2013-10-10 11:20:15 -0400

    updates to tenant creation script
    Former-commit-id: 74c3ddffc63f4a43193affc6745427beaac1df0d

--- udl2/scripts/add_tenant.sh
+++ udl2/scripts/add_tenant.sh
@@ -22,32 +22,34 @@ function create_tenant_directories {
 
 function remove_tenant_directories {
     # landing directories
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/landing/arrivals/$1
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/landing/work/$1
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/landing/history/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/landing/arrivals/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/landing/work/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/landing/history/$1
 
     #pickup directories
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/work/$1
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/departures/$1
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/history/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/work/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/departures/$1
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/history/$1
 
     # landing-work subdirectores
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/arrived
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/expanded
-    sudo -u -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/subfiles
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/arrived
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/expanded
+    sudo -u root -s rm -rf /opt/wgen/edware-udl/zones/pickup/landing/work/$1/subfiles
 }
 
 function get_opts {
-    if ( ! getopts "t:hcr" opt); then
-        echo "Usage: `basename $0` options (-t tenant_name) -h for help"
-        echo $E_OPTERROR;
+    if ( ! getopts ":t:hcr" opt); then
+        echo "Usage: `basename $0` options (-t tenant_name) (-c) (-r) -h for help"
+        exit $E_OPTERROR;
     fi
 
     MODE='CREATE'
     while getopts "t:hcr" opt; do
         case $opt in
             t)
-                TENANT=$OPTARG
+                # we may want to validate this value
+                TENANT=$OPTARG 
+                echo "Tenent:" $TENANT
                 ;;
             c)
                 echo "create mode"

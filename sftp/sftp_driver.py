5f9b5a9da0c0f74c98e97215f0a35361fe49aae0
Sadish Ravi <sravi@amplify.com>
2013-10-19 00:30:34 -0400

    sftp zone test cases and refactoring
    Former-commit-id: 36606e25301f6f35e7744f5d216d15ae9d7f1762

--- sftp/sftp_driver.py
+++ sftp/sftp_driver.py
@@ -6,10 +6,21 @@ Module to initialize sftp zones and creating groups
 
 __author__ = 'sravi'
 
-from src.initialize_sftp_zone import initialize
+import argparse
+from src.configure_sftp_zone import initialize, cleanup
+from src.sftp_config import sftp_conf
 
 if __name__ == "__main__":
     """
     Driver script to build and maintain sftp machine
+    This script needs to be run as root user
     """
-    initialize()
+    parser = argparse.ArgumentParser(description='Process sftp driver args')
+    parser.add_argument('--init', dest='driver_init_action', action='store_true')
+    parser.add_argument('--cleanup', dest='driver_init_action', action='store_false')
+    parser.set_defaults(driver_init_action=True)
+    args = parser.parse_args()
+    if args.driver_init_action:
+        initialize(sftp_conf)
+    else:
+        cleanup(sftp_conf)

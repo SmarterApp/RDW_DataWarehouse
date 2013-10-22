b3c82b6b6c3a56238938ef75e4e7d1f0aee66103
Seth Wimberly <swimberly@wgen.net>
2013-10-22 17:15:33 -0400

    updating tenant names and unnecessary remove that broke tests
    Former-commit-id: 4be19f015b4a3086ea66697ff11314be35ad3cf3

--- sftp/tests/test_initialize_sftp_tenant.py
+++ sftp/tests/test_initialize_sftp_tenant.py
@@ -14,44 +14,41 @@ class TestInitializeTenant(unittest.TestCase):
         self.sftp_conf = {
             'sftp_home': '/',
             'sftp_base_dir': 'tmp',
-            'sftp_arrivals_dir': 'test_arrivals',
-            'sftp_departures_dir': 'test_departures',
+            'sftp_arrivals_dir': 'test_arrivals_1',
+            'sftp_departures_dir': 'test_departures_1',
             'groups': ['sftparrivals', 'tenantadmin'],
             'group_directories': {
                 'sftparrivals': 'arrivals',
                 'tenantadmin': 'departures'
             }
         }
-        # make sure directories are empty
-        shutil.rmtree('/tmp/test_arrivals')
-        shutil.rmtree('/tmp/test_departures')
 
         # Create directories
-        os.mkdir('/tmp/test_arrivals')
-        os.mkdir('/tmp/test_departures')
+        os.mkdir('/tmp/test_arrivals_1')
+        os.mkdir('/tmp/test_departures_1')
 
     def tearDown(self):
-        shutil.rmtree('/tmp/test_arrivals')
-        shutil.rmtree('/tmp/test_departures')
+        shutil.rmtree('/tmp/test_arrivals_1')
+        shutil.rmtree('/tmp/test_departures_1')
 
     def test_create_tenant_path_string_arrivals(self):
         tenant = 'test_tenant123'
-        expected = '/tmp/test_arrivals/test_tenant123'
+        expected = '/tmp/test_arrivals_1/test_tenant123'
         result = initialize_sftp_tenant.create_tenant_path_string(tenant, self.sftp_conf, True)
 
         self.assertEqual(expected, result)
 
     def test_create_tenant_path_string_departures(self):
         tenant = 'test_tenant123'
-        expected = '/tmp/test_departures/test_tenant123'
+        expected = '/tmp/test_departures_1/test_tenant123'
         result = initialize_sftp_tenant.create_tenant_path_string(tenant, self.sftp_conf, False)
 
         self.assertEqual(expected, result)
 
     def test_create_tenant(self):
         tenant = 'test_tenant123'
-        expected_path1 = '/tmp/test_arrivals/test_tenant123'
-        expected_path2 = '/tmp/test_departures/test_tenant123'
+        expected_path1 = '/tmp/test_arrivals_1/test_tenant123'
+        expected_path2 = '/tmp/test_departures_1/test_tenant123'
         self.assertFalse(os.path.exists(expected_path1), 'expected path should not exist')
         self.assertFalse(os.path.exists(expected_path2), 'expected path should not exist')
 
@@ -61,8 +58,8 @@ class TestInitializeTenant(unittest.TestCase):
 
     def test_remove_tenant(self):
         tenant = 'test_tenant123'
-        expected_path1 = '/tmp/test_arrivals/test_tenant123'
-        expected_path2 = '/tmp/test_departures/test_tenant123'
+        expected_path1 = '/tmp/test_arrivals_1/test_tenant123'
+        expected_path2 = '/tmp/test_departures_1/test_tenant123'
         initialize_sftp_tenant.create_tenant(tenant, self.sftp_conf)
         self.assertTrue(os.path.exists(expected_path1))
         self.assertTrue(os.path.exists(expected_path2))

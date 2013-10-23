26ef4b68a3aef3d27a9986973472cbb4191acc96
Seth Wimberly <swimberly@wgen.net>
2013-10-23 16:39:18 -0400

    Updating driver script to not create by default
    Former-commit-id: 31a377db9aae4069d2d0ae0a747c31284a569003

--- sftp/sftp_driver.py
+++ sftp/sftp_driver.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser(description='Process sftp driver args')
     parser.add_argument('--init', dest='driver_init_action', action='store_true')
-    parser.add_argument('--cleanup', dest='driver_init_action', action='store_false')
+    parser.add_argument('--cleanup', dest='driver_cleanup_action', action='store_true')
     parser.add_argument('-a', '--add_user', action='store_true', help='create the given username')
     parser.add_argument('-s', '--add_tenant', action='store_true', help='create the given tenant name')
     parser.add_argument('-u', '--username', help='The username to add')
@@ -29,12 +29,14 @@ if __name__ == "__main__":
     parser.add_argument('-r', '--role-name', help='The role that the user should have')
     parser.add_argument('--remove-user', action='store_true', help='Delete the user defined by the -u option')
     parser.add_argument('--remove-tenant', action='store_true', help='Remove the tenant specified by the -t option')
-    parser.set_defaults(driver_init_action=True)
     args = parser.parse_args()
 
     if args.driver_init_action:
         sftp_zone_init(sftp_conf)
         sftp_groups_init(sftp_conf)
+    elif args.driver_cleanup_action:
+        sftp_groups_cleanup(sftp_conf)
+        sftp_zone_cleanup(sftp_conf)
     elif args.add_tenant:
         if args.tenant_name is None:
             parser.error('Tenant name is required to add a new tenant')
@@ -42,7 +44,7 @@ if __name__ == "__main__":
     elif args.add_user:
         if args.username is None or args.tenant_name is None or args.role_name is None:
             parser.error('Username (-u), tenant name (-t) and role name (-r) are required to add a new user (-a)')
-        create_sftp_user(args.tenant_name, args.username, args.role, sftp_conf)
+        create_sftp_user(args.tenant_name, args.username, args.role_name, sftp_conf)
     elif args.remove_user:
         if args.username is None:
             parser.error('Must specify a username (-u) to delete a user')
@@ -52,5 +54,4 @@ if __name__ == "__main__":
             parser.error("Must specify a tenant name (-t) to delete a tenant")
         remove_tenant(args.tenant_name, sftp_conf)
     else:
-        sftp_groups_cleanup(sftp_conf)
-        sftp_zone_cleanup(sftp_conf)
+        parser.error('Please specify a valid argument')

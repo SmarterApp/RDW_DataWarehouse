2ace1d1a0c5d31fb4b1f795e4379395a17a84725
Bhumi Patel <bpatel@amplify.com>
2013-10-28 14:12:06 -0400

    Rsynch functional test
    Former-commit-id: 159590478fca9f76b499cfadb8e2500eab72676d

---
+++ sftp/tests/ftest_UDL_with_rsynch.py
@@ -0,0 +1,44 @@
+'''
+Created on Oct 25, 2013
+
+@author: bpatel
+'''
+import unittest
+import pysftp
+import sys
+import os
+import argparse
+
+
+class rsynch_validation(unittest.TestCase):
+
+    def setUp(self):
+        self.test_rsynch_conf = {
+                                 'remote_server': 'edwappsrv5.poc.dum.edwdc.net',
+                                 'file': '/Users/Shared/Amplify/edware/sftp/tests/data/test_source_file_tar_gzipped.tar.gz.gpg'
+                                 }
+
+    def testdropfile_to_remote(self):
+
+        local_file = self.test_rsynch_conf['file']
+        srv = pysftp.Connection(host=self.test_rsynch_conf['remote_server'], username="test_user",
+                            password="test_user123")
+
+        # Make no file is on remote server(app server 5)
+        data = srv.listdir('/filedrop')
+        for i in data:
+                print(i)
+
+        # To upload the file
+        srv.put(local_file, 'filedrop/' + os.path.basename(local_file))
+
+        #Assert file is loded to remote server (appserver5)
+        data = srv.listdir('/filedrop')
+        for i in data:
+            self.assertEqual(i, 'test_source_file_tar_gzipped.tar.gz.gpg')
+            print(i)
+
+        # Closes the connection
+        srv.close()
+if __name__ == '__main__':
+    unittest.main()

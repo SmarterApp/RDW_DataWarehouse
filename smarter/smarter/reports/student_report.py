beb7e3e581d26fc26c3e90d377cd5c215487de0e
Doris Ip <dip@wgen.net>
2013-10-29 17:38:00 -0400

    US19993 Remove unused report
    Former-commit-id: 50f8eb899d68d0ecff4d96d709e03f00f7fc8459

--- smarter/smarter/reports/student_report.py
+++ smarter/smarter/reports/student_report.py
@@ -208,33 +208,3 @@ def get_student_report(params):
 
         result['context'] = context
     return result
-
-
-@report_config(name='student_assessments_report',
-               params={
-                   "studentGuid": {
-                       "type": "string",
-                       "required": True,
-                       "pattern": "^[a-zA-Z0-9\-]{0,50}$"}
-               })
-def get_student_assessment(params):
-    '''
-    Given a student id, return student assessment
-    '''
-    student_guid = params['studentGuid']
-
-    with SmarterDBConnection() as connection:
-        dim_asmt = connection.get_table('dim_asmt')
-        fact_asmt_outcome = connection.get_table('fact_asmt_outcome')
-
-        query = select([dim_asmt.c.asmt_guid,
-                        dim_asmt.c.asmt_subject,
-                        dim_asmt.c.asmt_type,
-                        dim_asmt.c.asmt_period,
-                        dim_asmt.c.asmt_version,
-                        fact_asmt_outcome.c.asmt_grade],
-                       from_obj=[fact_asmt_outcome.join(dim_asmt, fact_asmt_outcome.c.asmt_rec_id == dim_asmt.c.asmt_rec_id)])
-        query = query.where(fact_asmt_outcome.c.student_guid == student_guid)
-        query = query.order_by(dim_asmt.c.asmt_subject)
-        result = connection.get_result(query)
-        return result

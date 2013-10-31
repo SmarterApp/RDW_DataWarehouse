357baabf1fcec81eb77434482b22af38502de7a5
Eugene Jen <ejen@wgen.net>
2013-10-31 18:26:41 -0400

    add unit tests for extraction queries
    Former-commit-id: 63b9b2c25af8fc3a6a2a777091a0b52991a0de9e

--- smarter/smarter/reports/extraction.py
+++ smarter/smarter/reports/extraction.py
@@ -10,6 +10,7 @@ from edapi.logging import audit_event
 from smarter.reports.helpers.constants import Constants, AssessmentType
 from smarter.security.context import select_with_context
 
+
 def get_extract_query(params):
     """
     """
@@ -17,21 +18,22 @@ def get_extract_query(params):
     asmt_subject = params.get(Constants.ASMT_SUBJECT, None)
     asmt_year = params.get(Constants.ASMT_YEAR, None)
     limit = params.get(Constants.LIMIT, None)
-    most_recent = params.get(Constants.MOST_RECENT, true)
+    most_recent = params.get(Constants.MOST_RECENT, True)
 
     with EdCoreDBConnection() as connector:
         # get handle to tables
         fact_asmt_outcome = connector.get_table(Constants.FACT_ASMT_OUTCOME)
-        # if no subject, and asmt_subject, a query should be 0 because nothing should be found. should check more
-        # before really execute those useless query
+        # if no subject, and asmt_subject, a query should be 0 because nothing
+        # should be found. should check more before really execute those
+        # useless query
         if asmt_type is None or asmt_subject is None:
-            query = select_with_context([fact_asmt_outcome.c.asmnt_outcome_rec_id.label('asmnt_outcome_rec_id')],
-                                        from_obj=[fact_asmt_outcome])
+            query = Select([fact_asmt_outcome.c.asmnt_outcome_rec_id.label('asmnt_outcome_rec_id')],
+                           from_obj=[fact_asmt_outcome])
             query = query.where(fact_asmt_outcome.c.asmt_year == 0)
             query = query.limit(0)
         else:
-            query = select_with_context([fact_asmt_outcome.c.asmnt_outcome_rec_id.label('asmnt_outcome_rec_id')],
-                                        from_obj=[fact_asmt_outcome])
+            query = Select([fact_asmt_outcome.c.asmnt_outcome_rec_id.label('asmnt_outcome_rec_id')],
+                           from_obj=[fact_asmt_outcome])
 
             query = query.where(fact_asmt_outcome.c.asmt_type == asmt_type)
             query = query.where(and_(fact_asmt_outcome.c.asmt_subject == asmt_subject))
@@ -47,7 +49,7 @@ def get_check_ela_interim_existence_query(asmt_year):
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.COMPREHENSIVE_INTERIM,
                              Constants.ASMT_SUBJECT: Constants.ELA,
-                             Constatns.LIMIT: 1})
+                             Constants.LIMIT: 1})
 
 
 def get_check_ela_summative_existence_query(asmt_year):
@@ -55,7 +57,7 @@ def get_check_ela_summative_existence_query(asmt_year):
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.SUMMATIVE,
                              Constants.ASMT_SUBJECT: Constants.ELA,
-                             Constatns.LIMIT: 1})
+                             Constants.LIMIT: 1})
 
 
 def get_check_math_interim_existence_query(asmt_year):
@@ -63,7 +65,7 @@ def get_check_math_interim_existence_query(asmt_year):
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.COMPREHENSIVE_INTERIM,
                              Constants.ASMT_SUBJECT: Constants.MATH,
-                             Constatns.LIMIT: 1})
+                             Constants.LIMIT: 1})
 
 
 def get_check_math_summative_existence_query(asmt_year):
@@ -71,7 +73,7 @@ def get_check_math_summative_existence_query(asmt_year):
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.SUMMATIVE,
                              Constants.ASMT_SUBJECT: Constants.MATH,
-                             Constatns.LIMIT: 1})
+                             Constants.LIMIT: 1})
 
 
 def get_ela_interim_query(asmt_year):
@@ -85,21 +87,18 @@ def get_ela_summative_query(asmt_year):
     """
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.SUMMATIVE,
-                             Constants.ASMT_SUBJECT: Constants.ELA,
-                             Constatns.LIMIT: 1})
+                             Constants.ASMT_SUBJECT: Constants.ELA})
 
 
 def get_math_interim_query(asmt_year):
     """
     """
     return get_extract_query({Constants.ASMT_TYPE: AssessmentType.COMPREHENSIVE_INTERIM,
-                                          Constants.ASMT_SUBJECT: Constants.MATH,
-                                          Constatns.LIMIT: 1})
+                             Constants.ASMT_SUBJECT: Constants.MATH})
 
 
 def get_math_summative_query(asmt_year):
     """
     """
-    return get_check_data_existence_query({Constants.ASMT_TYPE: AssessmentType.SUMMATIVE,
-                                          Constants.ASMT_SUBJECT: Constants.MATH,
-                                          Constatns.LIMIT: 1})
+    return get_extract_query({Constants.ASMT_TYPE: AssessmentType.SUMMATIVE,
+                             Constants.ASMT_SUBJECT: Constants.MATH})
---
+++ smarter/smarter/tests/reports/test_extraction.py
@@ -0,0 +1,117 @@
+'''
+Created on Jan 24, 2013
+
+@author: tosako
+'''
+import unittest
+from smarter.reports.extraction import get_extract_query,\
+    get_check_ela_interim_existence_query,\
+    get_check_math_interim_existence_query,\
+    get_check_ela_summative_existence_query,\
+    get_check_math_summative_existence_query,\
+    get_ela_interim_query,\
+    get_math_interim_query,\
+    get_ela_summative_query,\
+    get_math_summative_query
+
+from edapi.exceptions import NotFoundException
+from pyramid.testing import DummyRequest
+from pyramid import testing
+from edauth.security.session import Session
+from smarter.security.roles.teacher import Teacher  # @UnusedImport
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+from edcore.tests.utils.unittest_with_edcore_sqlite import \
+    Unittest_with_edcore_sqlite,\
+    UnittestEdcoreDBConnection, get_unittest_tenant_name
+
+
+class TestExtraction(Unittest_with_edcore_sqlite):
+
+    def setUp(self):
+        cache_opts = {
+            'cache.type': 'memory',
+            'cache.regions':
+            'public.data,public.filtered_data,public.shortlived'
+        }
+
+        CacheManager(**parse_cache_config_options(cache_opts))
+
+        # Set up user context
+        self.__request = DummyRequest()
+        # Must set hook_zca to false to work with unittest_with_sqlite
+        self.__config = testing.setUp(request=self.__request, hook_zca=False)
+        # Set up context security
+        with UnittestEdcoreDBConnection() as connection:
+            # Insert into user_mapping table
+            user_mapping = connection.get_table('user_mapping')
+            connection.execute(user_mapping.insert(),
+                               user_id='272', guid='272')
+        dummy_session = Session()
+        dummy_session.set_roles(['TEACHER'])
+        dummy_session.set_uid('272')
+        dummy_session.set_tenant(get_unittest_tenant_name())
+        self.__config.testing_securitypolicy(dummy_session)
+
+    def tearDown(self):
+        # reset the registry
+        testing.tearDown()
+
+        # delete user_mapping entries
+        with UnittestEdcoreDBConnection() as connection:
+            user_mapping = connection.get_table('user_mapping')
+            connection.execute(user_mapping.delete())
+
+    def test_get_check_ela_interim_existence_query(self):
+        query = get_check_ela_interim_existence_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 1)
+
+    def test_get_check_math_interim_existence_query(self):
+        query = get_check_math_interim_existence_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 1)
+
+    def test_get_check_ela_summative_existence_query(self):
+        query = get_check_ela_summative_existence_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 1)
+
+    def test_get_check_math_summative_existence_query(self):
+        query = get_check_math_summative_existence_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 1)
+
+    def test_get_ela_interim_query(self):
+        query = get_ela_interim_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 86)
+
+    def test_get_math_interim_query(self):
+        query = get_math_interim_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 94)
+
+    def test_get_ela_summative_query(self):
+        query = get_ela_summative_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 449)
+
+    def test_get_math_summative_query(self):
+        query = get_math_summative_query(2015)
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 480)
+
+    def test_extract_query(self):
+        query = get_extract_query({})
+        with UnittestEdcoreDBConnection() as connection:
+            result = connection.execute(query).fetchall()
+            self.assertEqual(len(result), 0)

ae5305855ea178ca456ffce1243ddc146f718e0e
Eugene Jen <ejen@wgen.net>
2013-10-31 12:05:39 -0400

    add initial edextract setup
    Former-commit-id: 4907a28549f284ecbad8f41a7d2ed500550984a8

---
+++ edextract/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'SQLAlchemy',
+    "celery",
+    "anyjson",
+    "amqp"
+]
+
+tests_require = requires
+
+docs_extras = [
+    'Sphinx',
+    'docutils',
+    'repoze.sphinx.autointerface']
+
+setup(name='edextract',
+      version='0.1',
+      description='Generic Request Extraction for Edware Reporting',
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application", ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid celery',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      install_requires=requires,
+      tests_require=tests_require,
+      extras_require={
+          'docs': docs_extras, },
+      entry_points="""\
+      """,
+      )

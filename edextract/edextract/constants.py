6decac3f6e4ac65c72cab400b0bab341df4c2f63
Doris Ip <dip@wgen.net>
2013-10-31 17:33:49 -0400

    US20338 skeleton for init stats db for edextract
    Former-commit-id: a06898203b66875b7e5093211593e6400fee8817

---
+++ edextract/edextract/constants.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 31, 2013
+
+@author: dip
+'''
+
+
+class Constants():
+    '''
+    Constants related to extracts
+    '''
+    EXTRACT_END = 'extract_end'
+    EXTRACT_START = 'extract_start'
+    EXTRACT_STATS = 'extract_stats'
+    EXTRACT_PARAMS = 'extract_params'
+    EXTRACT_STATUS = 'extract_status'
+    STATE_CODE = 'state_code'
+    TASK_ID = 'task_id'
+    TENANT = 'tenant'
+    USER_GUID = 'user_guid'

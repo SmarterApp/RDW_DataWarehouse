387f52038aa4d9a1e6b76187ebb6d2810f8595e4
Sadish Ravi <sravi@amplify.com>
2013-10-31 11:50:59 -0400

    post_etl base strcuture
    Former-commit-id: 2951c8dbd1c35c3eec7aa1042f1c5638f8e79b78

--- udl2/src/post_etl/post_etl.py
+++ udl2/src/post_etl/post_etl.py
@@ -1,3 +1,27 @@
+import os
+import argparse
+import logging
+
 __author__ = 'sravi'
 
+logger = logging.getLogger(__name__)
 
+
+def cleanup_work_zone(batch_guid):
+    return True
+
+if __name__ == "__main__":
+    """
+    Entry point to post_etl to run as stand alone script
+    """
+    parser = argparse.ArgumentParser(description='Process post etl args')
+    parser.add_argument('-g', '--batch_guid', dest='batch_guid',
+                        help='Batch GUID of the process work zone which needs cleanup')
+
+    args = parser.parse_args()
+    if args.batch_guid:
+        print('Cleanup work zone for process with GUID: ' + args.batch_guid)
+        if cleanup_work_zone(args.batch_guid):
+            print('Cleanup complete successfully')
+        else:
+            print('Cleanup failed')
--- udl2/tests/unit_tests/test_post_etl.py
+++ udl2/tests/unit_tests/test_post_etl.py
@@ -1,9 +1,26 @@
 __author__ = 'sravi'
 
 import unittest
+from uuid import uuid4
+from post_etl import post_etl
 
 
 class TestPostEtl(unittest.TestCase):
 
-    def test_verify_work_zone_cleanedup(self):
+    @classmethod
+    def setUpClass(self):
         pass
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_verify_work_zone_cleanedup(self):
+        fake_guid_batch = str(uuid4())
+        self.assertTrue(post_etl.cleanup_work_zone(fake_guid_batch))

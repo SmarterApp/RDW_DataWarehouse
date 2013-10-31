2e39d6f2732882000dc1c9308ceb3612ea82132e
Doris Ip <dip@wgen.net>
2013-10-31 11:56:12 -0400

    US20338 Add edcore
    Former-commit-id: 2b7e5942b75e47243bb4adb48a55cf1d47d1667d

---
+++ edcore/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = [
+    'pyramid == 1.4']
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'nose >= 1.2.1',
+    'coverage',
+    'virtualenv']  # for scaffolding tests
+
+
+docs_extras = [
+    'Sphinx',
+    'docutils',
+    'repoze.sphinx.autointerface']
+
+setup(name='edcore',
+      version='0.1',
+      description='Generic Reporting Platform',
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application", ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=tests_require,
+      test_suite="nose.collector",
+      install_requires=install_requires,
+      extras_require={
+          'docs': docs_extras, },
+      entry_points="""\
+      """,
+      )

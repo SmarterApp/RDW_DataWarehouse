f1914a6a6dd29412b9f55295d51f3d825dbddb93
Doris Ip <dip@wgen.net>
2013-11-05 17:28:33 -0500

    US20338 Add section for extract to read ini values that are not celery related
    Former-commit-id: 4495356ae277c975a4d2c30c8ea6e228d515fb7d

--- edextract/edextract/celery.py
+++ edextract/edextract/celery.py
@@ -7,14 +7,10 @@ from edworker.celery import setup_celery as setup, configure_celeryd,\
     get_config_file
 from edextract.status.status import setup_db_connection
 
-# default timeout 20 seconds
-TIMEOUT = 20
-# default number of pdf generation retries
+# default number of extract retries
 MAX_RETRIES = 1
-# minimum file size of pdf generated
-MINIMUM_FILE_SIZE = 80000
-# delay in retry. Default to 5 seconds
-RETRY_DELAY = 5
+# delay in retry. Default to 60 seconds
+RETRY_DELAY = 60
 
 PREFIX = 'extract.celery'
 
@@ -28,6 +24,20 @@ def setup_celery(settings, prefix=PREFIX):
     :param prefix: prefix in configurations used for configuring celery
     '''
     setup(celery, settings, prefix)
+    setup_global_settings(settings)
+
+
+def setup_global_settings(settings):
+    '''
+    Setup configuration settings for extract
+
+    :param settings:  dict of configurations
+    '''
+    global MAX_RETRIES
+    global RETRY_DELAY
+    MAX_RETRIES = int(settings.get('extract.retries_allowed', MAX_RETRIES))
+    RETRY_DELAY = int(settings.get('extract.retry_delay', RETRY_DELAY))
+
 
 # Create an instance of celery, check if it's for prod celeryd mode and configure it for prod mode if so
 celery = configure_celeryd(PREFIX, prefix=PREFIX)
@@ -35,3 +45,4 @@ prod_config = get_config_file()
 if prod_config:
     # We should only need to setup db connection in prod mode
     setup_db_connection(prod_config)
+    setup_global_settings(prod_config)

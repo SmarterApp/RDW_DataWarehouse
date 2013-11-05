f4ad13a653ff68535ce51ec4086c44f0c9f4540d
Doris Ip <dip@wgen.net>
2013-11-05 14:33:20 -0500

    US20338 - Define status for extract monitoring table
    Former-commit-id: 1fbae32b58569d3e186fa75a13a8816c4792024d

--- edextract/edextract/extract_status.py
+++ edextract/edextract/extract_status.py
@@ -9,20 +9,43 @@ from edcore.database import initialize_db
 from edcore.database.edcore_connector import EdCoreDBConnection
 
 
+class ExtractStatus():
+    QUEUED = 'QUEUED'               # Extract is queued in broker
+    STARTED = 'STARTED'             # Celery Worker has dequeued the task
+    EXTRACTING = 'EXTRACTING'       # Extracting is in progress
+    EXTRACTED = 'EXTRACTED'         # File has been extracted in work zone
+    COPYING = 'COPYING'             # File is being copied to pick up zone
+    COPIED = 'COPIED'               # File has been copied to pick up zone
+    COMPLETED = 'COMPLETED'         # Extract process is completed
+
+
 def insert_extract_stats(values):
     '''
     Insert into extract status table
 
     :params dict values:  dictionary of values to insert
+
+    ex. values = {Constants.TENANT, "tenantName", Constants.STATE_CODE, "EX"}
     '''
     with StatsDBConnection() as connector:
         extract_stats = connector.get_table(Constants.EXTRACT_STATS)
-        stmt = extract_stats.insert(values={extract_stats.c.state_code: values.get(Constants.STATE_CODE),
-                                            extract_stats.c.tenant: values.get(Constants.TENANT),
-                                            extract_stats.c.user_guid: values.get(Constants.USER_GUID),
-                                            extract_stats.c.extract_status: values.get(Constants.EXTRACT_STATUS),
-                                            extract_stats.c.extract_params: values.get(Constants.EXTRACT_PARAMS),
-                                            extract_stats.c.task_id: values.get(Constants.TASK_ID)})
+        stmt = extract_stats.insert(values=values)
+        connector.execute(stmt)
+
+
+def update_extract_stats(task_id, values):
+    '''
+    Update extract status table
+
+    :params dict values:  dictionary of values to update
+
+    ex. values = {Constants.STATE_CODE, "EX"}
+    '''
+    with StatsDBConnection() as connector:
+        extract_stats = connector.get_table(Constants.EXTRACT_STATS)
+        stmt = extract_stats.update().\
+            where(extract_stats.c.task_id == task_id).\
+            values(values)
         connector.execute(stmt)
 
 

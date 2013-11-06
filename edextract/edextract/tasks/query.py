1080811065a8177f0b6241db978faeac2da4ec9f
Eugene Jen <ejen@wgen.net>
2013-11-06 10:33:45 -0500

    add proper log info and associate with right batch id
    Former-commit-id: 31115e925848325c041304a3223b307708a0f43a

--- edextract/edextract/tasks/query.py
+++ edextract/edextract/tasks/query.py
@@ -40,11 +40,11 @@ def handle_request(cookie=None, task_queries=None):
     :param task_queries: queries from caller
     '''
     current_task_id = handle_request.request.id
-    celery_check_result = is_available.delay(cookie=cookie, check_query=task_queries[0])
-
+    celery_check_result = is_available.delay(cookie=cookie, check_query=task_queries[0], batch_id=current_task_id)
+    log.info('extract request with id ' + current_task_id)
     if celery_check_result.get():
         output_uri = '/tmp/extract_' + current_task_id + '.csv'
-        celery_extract_result = generate_csv.delay(cookie=cookie, extract_query=task_queries[1], output_uri=output_uri)
+        celery_extract_result = generate_csv.delay(cookie=cookie, extract_query=task_queries[1], output_uri=output_uri, batch_id=current_task_id)
         return True
     else:
         return False
@@ -53,13 +53,15 @@ def handle_request(cookie=None, task_queries=None):
 @celery.task(name="tasks.extract.is_available",
              max_retries=MAX_RETRIES,
              default_retry_delay=RETRY_DELAY)
-def is_available(cookie=None, check_query=None):
+def is_available(cookie=None, check_query=None, batch_id=None):
     '''
     celery entry point to execute data availability check query.
     it checks availiablity of data, then replies to smarter service point.
     :param cookie: cookie for caller, for context security checkings
     :param check_queries: data availablitly query
+    :param batch_id: the batch_id for tracking
     '''
+    log.info('extract check query for task ' + batch_id)
     with EdCoreDBConnection() as connection:
         result = connection.execute(check_query).fetchone()
     if len(result) >= 1:
@@ -71,15 +73,16 @@ def is_available(cookie=None, check_query=None):
 @celery.task(name="tasks.extract.generate_csv",
              max_retries=MAX_RETRIES,
              default_retry_delay=RETRY_DELAY)
-def generate_csv(cookie=None, extract_query=None, output_uri=None):
+def generate_csv(cookie=None, extract_query=None, output_uri=None, batch_id=None):
     '''
     celery entry point to execute data extraction query.
     it execute extraction query and dump data into csv file that specified in output_uri
     :param cookie: cookie for caller, for context security checkings
     :param extract_queries: extraction query to dump data
     :param output_uri: output file uri
+    :param batch_id: batch_id for tracking
     '''
-    print('execute tasks.extract.generate_csv')
+    log.info('execute tasks.extract.generate_csv for task + ' + batch_id)
 
     with EdCoreDBConnection() as connection:
         counter = 0

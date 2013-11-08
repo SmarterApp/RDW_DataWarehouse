7f7554f471a61129eb567135ee567827a2556dcf
Eugene Jen <ejen@wgen.net>
2013-11-08 17:07:07 -0500

    initial commit of csv streaming encryptor
    Former-commit-id: 3c0df0beed339d09b3fc594e771d0d817d16a86e

---
+++ edextract/edextract/utils/csv_stream_encryptor.py
@@ -0,0 +1,35 @@
+'''
+Created on Nov 8, 2013
+
+@author: ejen
+'''
+
+from edextract.utils.file_encryptor import FileEncryptor, FileEncryptorException
+import csv
+import threading
+
+
+class CsvStreamEncryptor(object):
+
+    def __init__(self):
+        self.__file_encryptor
+        self.__
+        pass
+
+    def __del__(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def write(self, row):
+        pass
+
+    def flush(self):
+        pass
+
+    def close(self):
+        pass

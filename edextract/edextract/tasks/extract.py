c2d30cb2eb5cccb819a29724691c39a52a8f0d5d
Eugene Jen <ejen@wgen.net>
2013-11-08 14:06:01 -0500

    collapse loop and combine with context
    Former-commit-id: e846ad6a219d23ccd3b9ae95ae33f96759080caa

--- edextract/edextract/tasks/extract.py
+++ edextract/edextract/tasks/extract.py
@@ -43,24 +43,20 @@ def generate(session, query, request_id, task_id, file_name):
         return False
 
     # TODO: add try/catch, update extract status
-    with EdCoreDBConnection(tenant) as connection:
-        results = connection.get_streaming_result(query)
-        #print(results)
+    with EdCoreDBConnection(tenant) as connection, open(output_uri, 'w') as csvfile:
+        results = connection.get_streaming_result(query)  # this result is a generator
+        csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
         header = []
         # TODO: why is this here?
-        # TODO: collapse into one loop
-        with open(output_uri, 'w') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
-            for result in results:
-                #print(result)
-                # remove teacher names from results
-                result = multi_delete(result, ['teacher_first_name', 'teacher_middle_name', 'teacher_last_name'])
-                if len(header) is 0:
-                    header = list(result.keys())
-                    csvwriter.writerow(header)
-                row = list(result.values())
-                csvwriter.writerow(row)
-            csvfile.close()
+        for result in results:
+            # remove teacher names from results
+            result = multi_delete(result, ['teacher_first_name', 'teacher_middle_name', 'teacher_last_name'])
+            if len(header) is 0:
+                header = list(result.keys())
+                csvwriter.writerow(header)
+            row = list(result.values())
+            csvwriter.writerow(row)
+        csvfile.close()
         update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.EXTRACTED, Constants.EXTRACT_END: datetime.now()})
         # TODO: what does the return values do?
         return True

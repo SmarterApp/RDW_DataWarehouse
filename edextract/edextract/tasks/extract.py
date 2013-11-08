a16e7290b6026417efe532012cdf270648f56e15
Eugene Jen <ejen@wgen.net>
2013-11-08 15:44:05 -0500

    add exception handling to log error and update status tables
    Former-commit-id: f63c4c313923f8c0b8032a5d8a0f67e3b5ac2430

--- edextract/edextract/tasks/extract.py
+++ edextract/edextract/tasks/extract.py
@@ -35,25 +35,29 @@ def generate(tenant, query, request_id, task_id, file_name):
     start_time = datetime.now()
     output_uri = '/tmp/' + file_name + str(start_time.strftime("%m-%d-%Y_%H-%M-%S")) + '.csv'
     log.info('execute tasks.extract.generate_csv for task ' + task_id)
-    update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.EXTRACTING, Constants.EXTRACT_START: start_time, Constants.CELERY_TASK_ID: generate.request.id})
-    if tenant is None:
-        return False
+    try:
+        update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.EXTRACTING, Constants.EXTRACT_START: start_time, Constants.CELERY_TASK_ID: generate.request.id})
+        if tenant is None:
+            return False
 
-    # TODO: add try/catch, update extract status
-    with EdCoreDBConnection(tenant) as connection, open(output_uri, 'w') as csvfile:
-        results = connection.get_streaming_result(query)  # this result is a generator
-        csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
-        header = []
-        # TODO: why is this here?
-        for result in results:
-            # remove teacher names from results
-            result = multi_delete(result, ['teacher_first_name', 'teacher_middle_name', 'teacher_last_name'])
-            if len(header) is 0:
-                header = list(result.keys())
-                csvwriter.writerow(header)
-            row = list(result.values())
-            csvwriter.writerow(row)
-        csvfile.close()
-        update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.EXTRACTED, Constants.EXTRACT_END: datetime.now()})
-        # TODO: what does the return values do?
-        return True
+        with EdCoreDBConnection(tenant) as connection, open(output_uri, 'w') as csvfile:
+            results = connection.get_streaming_result(query)  # this result is a generator
+            csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
+            header = []
+            # TODO: why is this here?
+            for result in results:
+                # remove teacher names from results
+                result = multi_delete(result, ['teacher_first_name', 'teacher_middle_name', 'teacher_last_name'])
+                if len(header) is 0:
+                    header = list(result.keys())
+                    csvwriter.writerow(header)
+                row = list(result.values())
+                csvwriter.writerow(row)
+            csvfile.close()
+            update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.EXTRACTED, Constants.EXTRACT_END: datetime.now()})
+            # TODO: what does the return values do?
+            return True
+    except Exception as e:
+        log.error(e)
+        update_extract_stats(task_id, {Constants.EXTRACT_STATUS: ExtractStatus.FAILED, Constants.EXTRACT_END: datetime.now()})
+        return False

2a076360a9c5bdf005709ef56e69d260c5267033
Doris Ip <dip@wgen.net>
2013-11-12 11:09:33 -0500

    add UTs for extracts
    Former-commit-id: c0ac065f35fc5576f75e4cdeb53a522c70e3704b

--- edextract/edextract/tests/tasks/test_extract.py
+++ edextract/edextract/tests/tasks/test_extract.py
@@ -4,18 +4,46 @@ Created on Nov 7, 2013
 @author: dip
 '''
 import unittest
+from edextract.tasks.extract import archive
+import tempfile
+import os
+import shutil
+from zipfile import ZipFile
 
 
-class Test(unittest.TestCase):
+class TestExtractTask(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.__files = ['a.txt', 'b.txt', 'c.txt']
+        self.__tmp_dir = tempfile.mkdtemp('file_archiver_test')
+        self.__tmp_zip = os.path.join(tempfile.mkdtemp('zip'), 'test.zip')
+        for file in self.__files:
+            with open(os.path.join(self.__tmp_dir, file), "w") as f:
+                f.write('hello ' + file)
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.__tmp_dir)
+        shutil.rmtree(os.path.dirname(self.__tmp_zip))
 
     def test_generate(self):
         pass
+
+    def test_archive(self):
+        archive(self.__tmp_zip, self.__tmp_dir)
+        zipfile = ZipFile(self.__tmp_zip, "r")
+        namelist = zipfile.namelist()
+        self.assertEqual(3, len(namelist))
+        self.assertIn('a.txt', namelist)
+        self.assertIn('b.txt', namelist)
+        self.assertIn('c.txt', namelist)
+
+        file_a = zipfile.read('a.txt')
+        self.assertEqual(b'hello a.txt', file_a)
+        file_b = zipfile.read('b.txt')
+        self.assertEqual(b'hello b.txt', file_b)
+        file_c = zipfile.read('c.txt')
+        self.assertEqual(b'hello c.txt', file_c)
+        zipfile.close()
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
---
+++ edextract/edextract/tests/utils/test_file_utils.py
@@ -0,0 +1,31 @@
+'''
+Created on Nov 12, 2013
+
+@author: dip
+'''
+import unittest
+import os
+import shutil
+from edextract.utils.file_utils import prepare_path
+import tempfile
+
+
+class Test_FileUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.__temp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.__temp_dir, ignore_errors=True)
+
+    def test_prepare_path(self):
+        file_name = os.path.join(self.__temp_dir, 'a', 'b', 'c', 'test.csv.gpg')
+        # make sure directory does not exist first.
+        shutil.rmtree(os.path.dirname(file_name), ignore_errors=True)
+        prepare_path(file_name)
+        self.assertTrue(os.access(os.path.dirname(file_name), os.R_OK))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

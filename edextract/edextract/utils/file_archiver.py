aa20ac3aa158b35c90adae59358a22e18dd8f585
Eugene Jen <ejen@wgen.net>
2013-11-12 18:16:06 -0500

    add document for zip_archiver
    Former-commit-id: 3fb33f6ff7ac60a19cdb6a35ed45eb84da12de43

--- edextract/edextract/utils/file_archiver.py
+++ edextract/edextract/utils/file_archiver.py
@@ -11,7 +11,9 @@ def archive_files(zip_filename, directory):
     '''
     zip up all files under given directory.
     archive name is flat directory and no recursive.
-    no compression apply because all gpg files shoud be compressed already.
+    no compression apply because all gpg files should be compressed already.
+    :param zip_filename: the zip file that contains all files under directory
+    :param directory: the directory that contain all files that are zipped, no recursive traversal in this funciton
     '''
     with ZipFile(zip_filename, "w") as zipfile:
         files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]

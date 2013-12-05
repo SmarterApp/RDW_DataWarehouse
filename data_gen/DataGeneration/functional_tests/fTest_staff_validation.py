03e731b8fa8dd6f1c270b2651126d8ec19ac7502
Seth Wimberly <swimberly@wgen.net>
2013-12-05 10:20:42 -0500

    updating functional test to use generate_data module instead of relative path
    Former-commit-id: 466b619c3d20c2b111f66de67752563afa64762d

--- data_gen/DataGeneration/functional_tests/fTest_staff_validation.py
+++ data_gen/DataGeneration/functional_tests/fTest_staff_validation.py
@@ -15,27 +15,24 @@ class ValidateStaffColumn(unittest.TestCase):
 
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
-        #os.mkdir('/tmp/datageneration_files', mode=0o777)
 
     def tearDown(self):
         shutil.rmtree(self.temp_dir, ignore_errors=True)
-        #shutil.rmtree('/tmp/datageneration_files')
 
     def test_generate_staff(self):
 
-        command = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output={dirname} -s'
+        command = 'python -m DataGeneration.src.generate_data --config DataGeneration.src.configs.dg_types_SDS --output {dirname} -s'
         command = command.format(dirname=self.temp_dir)
         subprocess.call(command, shell=True)
         time.sleep(3)
         self.assertTrue(os.path.exists(os.path.join(self.temp_dir, 'dim_staff.csv')))
 
     def test_not_generate_staff(self):
-        cmd = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output={dirname}'
+        cmd = 'python -m DataGeneration.src.generate_data --config DataGeneration.src.configs.dg_types_SDS --output {dirname}'
         cmd = cmd.format(dirname=self.temp_dir)
         subprocess.call(cmd, shell=True)
         time.sleep(3)
         self.assertFalse(os.path.exists(os.path.join(self.temp_dir, 'dim_staff.csv')))
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

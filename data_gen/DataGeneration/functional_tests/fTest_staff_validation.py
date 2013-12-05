a7c1b0e17e5c640fdad377e0891cb4b67e737342
Seth Wimberly <swimberly@wgen.net>
2013-12-05 09:33:45 -0500

    update to use builtin temp file module
    Former-commit-id: 028659cba1989669e6f8ad2823c80598794c193d

--- data_gen/DataGeneration/functional_tests/fTest_staff_validation.py
+++ data_gen/DataGeneration/functional_tests/fTest_staff_validation.py
@@ -8,29 +8,34 @@ import os
 import subprocess
 import time
 import shutil
+import tempfile
 
 
 class ValidateStaffColumn(unittest.TestCase):
 
-        def setUp(self):
-            os.mkdir('/tmp/datageneration_files', mode=0o777)
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        #os.mkdir('/tmp/datageneration_files', mode=0o777)
 
-        def tearDown(self):
-            shutil.rmtree('/tmp/datageneration_files')
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir, ignore_errors=True)
+        #shutil.rmtree('/tmp/datageneration_files')
 
-        def test_generate_staff(self):
+    def test_generate_staff(self):
 
-            command = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output=/tmp/datageneration_files -s'
-            subprocess.call(command, shell=True)
-            time.sleep(3)
-            self.assertTrue(os.path.exists('/tmp/datageneration_files/dim_staff.csv'))
+        command = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output={dirname} -s'
+        command = command.format(dirname=self.temp_dir)
+        subprocess.call(command, shell=True)
+        time.sleep(3)
+        self.assertTrue(os.path.exists(os.path.join(self.temp_dir, 'dim_staff.csv')))
 
-        def test_not_generate_staff(self):
-            cmd = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output=/tmp/datageneration_files'
-            subprocess.call(cmd, shell=True)
-            time.sleep(3)
-            self.assertFalse(os.path.exists('/tmp/datageneration_files/dim_staff.csv'))
+    def test_not_generate_staff(self):
+        cmd = 'python ../../DataGeneration/src/generate_data.py --config configs.dg_types_SDS --output={dirname}'
+        cmd = cmd.format(dirname=self.temp_dir)
+        subprocess.call(cmd, shell=True)
+        time.sleep(3)
+        self.assertFalse(os.path.exists(os.path.join(self.temp_dir, 'dim_staff.csv')))
 
-        if __name__ == "__main__":
-            #import sys;sys.argv = ['', 'Test.testName']
-            unittest.main()
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

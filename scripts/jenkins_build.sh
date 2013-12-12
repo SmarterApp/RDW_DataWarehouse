86ae0c0537391d4b506ff656b2f738846f5f10d1
Sadish Ravi <sravi@amplify.com>
2013-12-12 11:52:02 -0500

    fixing the script
    Former-commit-id: 551f30b811eb75b5f16bb614995852d4f2163f13

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -319,8 +319,8 @@ function build_rpm {
     rpmbuild -bb $1.spec
 
     ENV_NAME=$(echo ${1}_env_name | tr '[:lower:]' '[:upper:]')
-    scp /var/lib/jenkins/rpmbuild/RPMS/x86_64/$1${ENV_NAME}-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm pynest@${PYNEST_SERVER}:/opt/wgen/rpms
-    ssh pynest@${PYNEST_SERVER} "ln -sf /opt/wgen/rpms/$1${ENV_NAME}-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm /opt/wgen/rpms/$1-latest.rpm"
+    scp /var/lib/jenkins/rpmbuild/RPMS/x86_64/$1${!ENV_NAME}-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm pynest@${PYNEST_SERVER}:/opt/wgen/rpms
+    ssh pynest@${PYNEST_SERVER} "ln -sf /opt/wgen/rpms/$1${!ENV_NAME}-${RPM_VERSION}-${BUILD_NUMBER}.el6.x86_64.rpm /opt/wgen/rpms/$1-latest.rpm"
 
     echo "Upload to pulp"
     pulp-admin content upload --dir /var/lib/jenkins/rpmbuild/RPMS/x86_64 --repoid edware-el6-x86_64-upstream --nosig -v

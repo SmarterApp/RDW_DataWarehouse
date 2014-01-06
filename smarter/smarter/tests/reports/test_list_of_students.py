e946c3497fd86ecf334399d10c6951b68bdefe0e
Daniel Wu <dawu@wgen.net>
2014-01-06 08:55:54 -0500

    US17709: fixed UT
    Former-commit-id: ec6658c6775971f792d5a91acd02c95381edd582

--- smarter/smarter/tests/reports/test_list_of_students.py
+++ smarter/smarter/tests/reports/test_list_of_students.py
@@ -99,7 +99,7 @@ class TestLOS(Unittest_with_edcore_sqlite):
         results = get_list_of_students_report(testParam)
 
         self.assertTrue('asmt_administration' in results, "asmt_administration is missing")
-        self.assertEquals(len(results['asmt_administration']), 129, "should have 129 different test")
+        self.assertEquals(len(results['asmt_administration']), 4, "should have 4 different test")
 
     def test_ELA_only(self):
         testParam = {}

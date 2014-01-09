541c4385620f8ddea79602a93cb17357c85a13ef
Bhumi Patel <bpatel@amplify.com>
2014-01-09 14:33:53 -0500

    e2e functional test update
    Former-commit-id: 0240d950bef083b8081aa89e9d91ecd92979a1a8

--- udl2/tests/e2e_tests/fTest_benchmarking_test.py
+++ udl2/tests/e2e_tests/fTest_benchmarking_test.py
@@ -10,71 +10,67 @@ import subprocess
 import os
 import time
 import shutil
+from udl2.udl2_connector import UDL2DBConnection
+from sqlalchemy.sql import select, delete
 from udl2.celery import udl2_conf
 
-#METADATA_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/Benchmarking_json_file.json'
-#FACT_OUTCOME_FILE_PATTERN = '/opt/wgen/edware-udl/zones/datafiles/BENCHMARK_DATA.csv'
-UDL2_DEFAULT_CONFIG_PATH_FILE = '/opt/wgen/edware/conf/udl2_conf.py'
+UDL2_DEFAULT_CONFIG_PATH_FILE = '/opt/edware/conf/udl2_conf.py'
 ARCHIVED_FILE = '/opt/edware/zones/datafiles/test_source_file_tar_gzipped.tar.gz.gpg'
 TENANT_DIR = '/opt/edware/zones/landing/arrivals/ftest_test_tenant/'
 
 
 class ValidateTableData(unittest.TestCase):
-
     def setUp(self):
-        self.user = 'edware'
-        self.passwd = 'edware2013'
-        self.host = 'localhost'
-        self.port = '5432'
-        self.database = 'udl2'
-        self.archived_file = ARCHIVED_FILE
         self.tenant_dir = TENANT_DIR
+        self.archived_file = ARCHIVED_FILE
 
     def tearDown(self):
-        if os.path.exists(self.tenant_dir):
-            shutil.rmtree(self.tenant_dir)
+        shutil.rmtree(self.tenant_dir)
 
-    def empty_batch_table(self, db_connection):
-        output = db_connection.execute('DELETE FROM udl2."UDL_BATCH";')
-        print("Sucessfully delete all data from Batch Table")
+    def empty_batch_table(self, connector):
+        batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
+        result = connector.execute(batch_table.delete())
+        query = select([batch_table])
+        result1 = connector.execute(query).fetchall()
+        number_of_row = len(result1)
+        print(number_of_row)
+        self.assertEqual(number_of_row, 0)
 
     def run_udl_pipeline(self):
-
         self.conf = udl2_conf
-
         arch_file = self.copy_file_to_tmp()
         command = "python ../../scripts/driver.py -a {}".format(arch_file)
         print(command)
         subprocess.call(command, shell=True)
 
-    def connect_db(self):
-        db_string = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{database}'.format(user=self.user, passwd=self.passwd, host=self.host, port=self.port, database=self.database)
-        engine = create_engine(db_string)
-        db_connection = engine.connect()
-        return db_connection, engine
-
-    def get_data_from_batch_table(self, db_connection):
-        time.sleep(40)
-        result = db_connection.execute('SELECT * FROM udl2."UDL_BATCH";').fetchall()
-        print(len(result))
+    def connect_verify_db(self, connector):
+        time.sleep(20)
+        batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
+        query = select([batch_table])
+        result = connector.execute(query).fetchall()
         number_of_row = len(result)
         if number_of_row < 22:
             time.sleep(30)
             print(number_of_row)
         self.assertEqual(number_of_row, 23)
 
-        output = db_connection.execute('SELECT udl_phase FROM udl2."UDL_BATCH";').fetchall()
-        tuple_str = ('UDL_COMPLETE',)
-        self.assertIn(tuple_str, output)
+        output = select([batch_table.c.udl_phase_step_status]).where(batch_table.c.udl_phase == 'UDL_COMPLETE')
+        output_data = connector.execute(output).fetchall()
+        tuple_str = [('SUCCESS',)]
+        self.assertEqual(tuple_str, output_data)
 
     def test_benchmarking_data(self):
-        db_conn, engine = self.connect_db()
-        self.empty_batch_table(db_conn)
+        self.connector = UDL2DBConnection()
+        self.empty_batch_table(self.connector)
         self.run_udl_pipeline()
-        self.get_data_from_batch_table(db_conn)
+        self.connect_verify_db(self.connector)
 
     def copy_file_to_tmp(self):
-        os.makedirs(self.tenant_dir)
+        if os.path.exists(self.tenant_dir):
+                print("tenant dir already exists")
+        else:
+                os.makedirs(self.tenant_dir)
+
         return shutil.copy2(self.archived_file, self.tenant_dir)
 
 if __name__ == "__main__":

cdcf1528bae78218c65ed19d031951273b8d1ec2
Sadish Ravi <sadish.r@gmail.com>
2014-01-27 14:01:29 -0500

    starting with edload. just skeleton similar to edextract
    Former-commit-id: d588d6a7a2605fef9cdf1208c593b25dc327a1f4

---
+++ edload/edload/celery.py
@@ -0,0 +1,29 @@
+__author__ = 'sravi'
+
+from edworker.celery import setup_celery as setup, configure_celeryd,\
+    get_config_file
+
+PREFIX = 'edload.celery'
+
+
+def setup_celery(settings, prefix=PREFIX):
+    '''
+    Setup celery based on parameters defined in setting (ini file).
+    This calls by client application when dictionary of settings is given
+
+    :param settings:  dict of configurations
+    :param prefix: prefix in configurations used for configuring celery
+    '''
+    setup(celery, settings, prefix)
+    #setup_settings(settings)
+
+
+# Create an instance of celery, check if it's for prod celeryd mode and configure it for prod mode if so
+celery, conf = configure_celeryd(PREFIX, prefix=PREFIX)
+prod_config = get_config_file()
+if prod_config:
+    # We should only need to setup db connection in prod mode
+    #setup_db_connection(conf)
+    #setup_settings(conf)
+    #run_cron_cleanup(conf)
+    pass

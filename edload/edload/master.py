8d94aba6e9cc7cbadf74ce63cb23ec482d9ab34b
Sadish Ravi <sadish.r@gmail.com>
2014-01-28 10:50:59 -0500

    sarting with standalone celery master. will integrate with edworker later
    Former-commit-id: a4e2e327bba064cf99928ba4313d8ac1277baa20

--- edload/edload/master.py
+++ edload/edload/master.py
@@ -1,9 +1,10 @@
 __author__ = 'sravi'
 
 import logging
-import datetime
+from celery.decorators import periodic_task
 from celery.schedules import crontab
-from celery.task import periodic_task
+from celery import Celery
+from datetime import timedelta
 from edload.settings.config import Config, get_setting
 
 MASTER_SCHEDULER_HOUR = get_setting(Config.MASTER_SCHEDULER_HOUR)
@@ -11,7 +12,30 @@ MASTER_SCHEDULER_MIN = get_setting(Config.MASTER_SCHEDULER_MIN)
 
 log = logging.getLogger('edload')
 
-@periodic_task(run_every=crontab(minute="*/" + MASTER_SCHEDULER_MIN))
-def start_data_refresh():
-    log.info("Hi from Master Scheduler At: " + str(datetime.datetime.now()))
+# hack till integrarion with edworker
+celery = Celery('master', broker='amqp://guest@localhost//', backend='amqp')
+celery.conf.CELERY_TASK_SERIALIZER = 'json'
+celery.conf.CELERYBEAT_SCHEDULE = {
+    'add-every-30-seconds': {
+        'task': 'edload.master.start_edware_data_refresh',
+        'schedule': timedelta(seconds=10),
+        'args': (16, 16)
+    },
+}
+celery.conf.CELERY_TIMEZONE = 'US/Eastern'
+# hack till integrarion with edworker
 
+@celery.task
+def start_edware_data_refresh():
+    print('Starting Scheduled edware data refresh task')
+
+
+
+@periodic_task(run_every=timedelta(seconds=2))
+def myPeriodicTask():
+    print('periodic_task')
+
+
+@periodic_task(run_every=crontab())
+def myCronTask():
+    print('cron_task')

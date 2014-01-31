354d92cd35236bbf3577f42c1923bc77a448ec1c
Sadish Ravi <sadish.r@gmail.com>
2014-01-31 16:55:27 -0500

    some unit tests for queries.py
    Former-commit-id: 460f44cf80ec8536b43bff3f13fdac5cef844d72

--- edmigrate/edmigrate/utils/queries.py
+++ edmigrate/edmigrate/utils/queries.py
@@ -15,15 +15,26 @@ def get_host_name_from_node_conn_info(conn_info):
     conn_info is of format: host=dbpgdw0.qa.dum.edwdc.net user=repmgr dbname=edware
     return parsed host_name from this string
     '''
-    return (conn_info.split()[0]).split('=')[1]
+    splits = conn_info.split()
+    if len(splits) < 1:
+        return None
+    host_splits = splits[0].split('=')
+    if len(host_splits) < 2:
+        return None
+    return host_splits[1]
 
 
 def is_sync_satus_acceptable(status, tolerence):
     '''
     the slave lag in bytes should be less then tolerence allowed
     '''
-    lag_in_bytes = int(status.split()[0])
-    return True if lag_in_bytes < int(tolerence) else False
+    splits = status.split()
+    if len(splits) < 1:
+        return False
+    if not splits[0].isdigit():
+        return False
+    lag_in_bytes = int(splits[0])
+    return True if lag_in_bytes <= int(tolerence) else False
 
 
 def get_slave_nodes_info_dict(tenant, slave_host_names):
@@ -39,9 +50,9 @@ def get_slave_nodes_info_dict(tenant, slave_host_names):
                        from_obj=[repl_nodes_table])
         repl_nodes_rows = connector.get_result(query)
         for repl_node in repl_nodes_rows:
-            host_name = get_host_name_from_node_conn_info(repl_node['conninfo'])
+            host_name = get_host_name_from_node_conn_info(repl_node[Constants.REPL_NODE_CONN_INFO])
             if host_name in slave_host_names:
-                slave_node_info[repl_node['id']] = host_name
+                slave_node_info[repl_node[Constants.REPL_NODE_ID]] = host_name
     return slave_node_info
 
 
@@ -53,13 +64,13 @@ def get_slave_nodes_status(tenant, slave_nodes_info):
     node_status = {}
     with RepMgrDBConnection(tenant) as connector:
         metadata = connector.get_metadata(Constants.REPL_MGR_SCHEMA)
-        repl_Status_table = Table(Constants.REPL_STATUS_TABLE, metadata)
-        query = select([repl_Status_table.c.primary_node, repl_Status_table.c.standby_node, repl_Status_table.c.replication_lag],
-                       from_obj=[repl_Status_table])
+        repl_status_table = Table(Constants.REPL_STATUS_TABLE, metadata)
+        query = select([repl_status_table.c.primary_node, repl_status_table.c.standby_node, repl_status_table.c.replication_lag],
+                       from_obj=[repl_status_table])
         repl_status_rows = connector.get_result(query)
         for repl_status_row in repl_status_rows:
-            if repl_status_row['standby_node'] in slave_nodes_info.keys():
-                node_status[repl_status_row['standby_node']] = repl_status_row['replication_lag']
+            if repl_status_row[Constants.REPL_STANDBY_NODE] in slave_nodes_info.keys():
+                node_status[repl_status_row[Constants.REPL_STANDBY_NODE]] = repl_status_row[Constants.REPL_STATUS_LAG]
     return node_status
 
 

4822cd8ba14231a3cff15fe0dc878260b04f50e9
Eugene Jen <ejen@wgen.net>
2014-02-06 10:58:00 -0500

    initial test for pull all jenkins conf into a shell script. this is for testing correctness. the goal is to integrate this into edware/scripts/jenkins_build.sh
    Former-commit-id: 5db45611e37abac577bac0a0b7740a67de53d514

---
+++ udl2/scripts/jenkins_build.sh
@@ -0,0 +1,37 @@
+#!/bin/env bash
+
+set -e # Exit on errors
+
+
+function build_unittest {
+	PATH=$PATH:/usr/pgsql-9.2/bin/
+	export PATH
+	source /opt/wgen/edware-udl/udl2/python3.3/bin/activate
+	cd config
+	python setup.py install --force
+	python generate_ini.py -i udl2_conf.yaml -e development -o udl2_conf.ini
+
+	cd $WORKSPACE/udl2
+	python setup_developer.py install --force
+
+
+	#cd $WORKSPACE/config
+	#python setup.py install --force
+	/opt/wgen/edware-udl/udl2/python3.3/bin/stop_celery.sh
+	#cd $WORKSPACE/udl2/scripts
+	$WORKSPACE/udl2/scripts/teardown_udl2_database.sh
+	$WORKSPACE/udl2/scripts/initialize_udl2_database.sh
+	#cd ..
+	cd $WORKSPACE/udl2
+	nosetests --with-cov --cov=src/ --cov-report xml tests/unit_tests/test*.py
+}
+
+function main {
+	build_unittest
+}
+
+main $@
+
+# Completely successful
+
+exit 0

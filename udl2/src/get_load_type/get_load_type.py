e02fb794f8b0248326e39ee0739671f57caa3cc8
Thomas Shewchuk <tshewchuk@wgen.net>
2014-02-07 19:00:53 -0500

    US17765: Some code clean up.
    Former-commit-id: af0c08597829a6b250eb0456654d6fb445560fe5

--- udl2/src/get_load_type/get_load_type.py
+++ udl2/src/get_load_type/get_load_type.py
@@ -11,47 +11,53 @@ load_types = udl2_conf['load_type'].values()
 
 
 def _get_json_file_in_dir(json_file_dir):
+    '''
+    Get the name of the json file which resides in the directory
+    @param json_file_dir: A directory that houses the json file
+    @type string
+    @return: JSON file name
+    @rtype: string
+    '''
     for file_name in os.listdir(json_file_dir):
         if file_name.split('.')[-1] == 'json':
             return file_name 
     return None
 
 
-def _get_content_type_from_json(json_file_dir):
+def _get_load_type_from_json(json_file_dir):
     '''
-    Determine the content type from the json file in the directory
-
-    @param json_file_dir: A dictionary that houses the json file
-    @type json_object: dict
-    @return: content type
-    @rtype: string
+    Determine the udl load type from the json file in the directory
+    @param json_file_dir: A directory that houses the json file
+    @type string
+    @return: UDL load type, JSON file name (for error reporting)
+    @rtype: string, string
     '''
     json_file_name = _get_json_file_in_dir(json_file_dir)
     json_file_path = os.path.join(json_file_dir, json_file_name)
-    content = None
+    load_type = None
     with open(json_file_path) as json_file:
         try:
             json_object = json.load(json_file)
+            load_type_key = udl2_conf['load_type_key']
             for key in json_object:
-                if key.lower() == udl2_conf['load_type_key']:
-                    content = json_object.get(key).lower()
+                if key.lower() == load_type_key:
+                    load_type = json_object.get(key).lower()
                     break
-            if content not in load_types:
-                logger.error('Invalid content specified in json file %s' % json_file_name)
+            if load_type not in load_types:
+                logger.error('Invalid load type specified in json file %s' % json_file_name)
         except ValueError:
             logger.error('Malformed json file %s' % json_file_name)
-    return content, json_file_name
+    return load_type, json_file_name
 
 
 def get_load_type(json_file_dir):
     """
     Get the load type for this UDL job from the json file
-
-    @param json_file_path: the full pathname of the json file
+    @param json_file_dir: A directory that houses the json file
     @return: UDL job load type
     @rtype: string
     """
-    load_type, json_file_name = _get_content_type_from_json(json_file_dir)
+    load_type, json_file_name = _get_load_type_from_json(json_file_dir)
     if load_type not in load_types:
         raise ValueError('No valid load type specified in json file -- %s' % json_file_name)
     return load_type
--- udl2/tests/unit_tests/test_get_load_type.py
+++ udl2/tests/unit_tests/test_get_load_type.py
@@ -2,10 +2,9 @@ __author__ = 'tshewchuk'
 
 from udl2.defaults import UDL2_DEFAULT_CONFIG_PATH_FILE
 from udl2_util.config_reader import read_ini_file
-from shutil import copy
-import unittest
 import os
-from os import remove
+import shutil
+import unittest
 
 from get_load_type import get_load_type
 
@@ -19,25 +18,30 @@ class TestGetLoadType(unittest.TestCase):
             config_path = UDL2_DEFAULT_CONFIG_PATH_FILE
         udl2_conf = read_ini_file(config_path)
         self.data_dir = udl2_conf['zones']['datafiles'] + '/'
-        self.test_expanded_dir = '/tmp/'
+        self.test_expanded_dir = '/tmp/test_get_load_type/'
+        if not os.path.exists(self.test_expanded_dir):
+            os.makedirs(self.test_expanded_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.test_expanded_dir)
 
     def test_get_load_type_from_valid_json(self):
-        copy(self.data_dir + 'test_valid_content_type.json', self.test_expanded_dir)
+        shutil.copy(self.data_dir + 'test_valid_content_type.json', self.test_expanded_dir)
         load_type = get_load_type.get_load_type(self.test_expanded_dir)
         self.assertEqual('studentregistration', load_type)
-        remove(self.test_expanded_dir + 'test_valid_content_type.json')
+        os.remove(self.test_expanded_dir + 'test_valid_content_type.json')
 
     def test_get_load_type_from_invalid_content_json(self):
-        copy(self.data_dir + 'test_invalid_content_type.json', self.test_expanded_dir)
+        shutil.copy(self.data_dir + 'test_invalid_content_type.json', self.test_expanded_dir)
         self.assertRaises(ValueError, get_load_type.get_load_type, self.test_expanded_dir)
-        remove(self.test_expanded_dir + 'test_invalid_content_type.json')
+        os.remove(self.test_expanded_dir + 'test_invalid_content_type.json')
 
     def test_get_load_type_from_missing_content_json(self):
-        copy(self.data_dir + 'test_missing_content_type.json', self.test_expanded_dir)
+        shutil.copy(self.data_dir + 'test_missing_content_type.json', self.test_expanded_dir)
         self.assertRaises(ValueError, get_load_type.get_load_type, self.test_expanded_dir)
-        remove(self.test_expanded_dir + 'test_missing_content_type.json')
+        os.remove(self.test_expanded_dir + 'test_missing_content_type.json')
 
     def test_get_load_type_from_malformed_json(self):
-        copy(self.data_dir + 'test_malformed.json', self.test_expanded_dir)
+        shutil.copy(self.data_dir + 'test_malformed.json', self.test_expanded_dir)
         self.assertRaises(ValueError, get_load_type.get_load_type, self.test_expanded_dir)
-        remove(self.test_expanded_dir + 'test_malformed.json')
+        os.remove(self.test_expanded_dir + 'test_malformed.json')

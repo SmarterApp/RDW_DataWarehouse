f15120184b73f42c6c7b89c8024823997c7663be
Doris Ip <dip@wgen.net>
2014-02-11 13:14:33 -0500

    CR use dummy names in UT
    Former-commit-id: c781fbaae949a7349d3ae976a047e24c2591e944

--- edmigrate/edmigrate/tests/tasks/test_nodes.py
+++ edmigrate/edmigrate/tests/tasks/test_nodes.py
@@ -25,13 +25,13 @@ class TesNodes(Unittest_with_repmgr_sqlite):
     def test_slave_node_host_names_for_group(self):
         node = collections.namedtuple('node', 'host group')
         test_registered_slaves = []
-        test_registered_slaves.append(node(host='testslave0.qa.dum.edwdc.net', group='A'))
-        test_registered_slaves.append(node(host='testslave1.qa.dum.edwdc.net', group='B'))
+        test_registered_slaves.append(node(host='testslave0.dummy.net', group='A'))
+        test_registered_slaves.append(node(host='testslave1.dummy.net', group='B'))
         self.assertTrue(len(nodes.get_slave_node_host_names_for_group('A', test_registered_slaves)) == 1)
 
     def test_regiser_slave_node(self):
-        nodes.register_slave_node('testslave0.qa.dum.edwdc.net', 'A')
-        nodes.register_slave_node('testslave1.qa.dum.edwdc.net', 'B')
+        nodes.register_slave_node('testslave0.dummy.net', 'A')
+        nodes.register_slave_node('testslave1.dummy.net', 'B')
         self.assertTrue(len(nodes.get_registered_slave_nodes()) == 2)
 
 

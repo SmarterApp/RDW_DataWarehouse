352574a12a7f030d8f8f681eb1c58de38f708078
Doris Ip <dip@wgen.net>
2014-02-12 10:33:24 -0500

    Fix typo in env var
    Former-commit-id: 1591e94c264739a241a911c6f1806a5064fac12e

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -301,8 +301,8 @@ function import_data_from_csv {
     
     # This is a workaround as pgpool is read-only, replace pgpool server with db master
     echo "${WORKSPACE}/config/data_copy.ini"
-    echo "${WORKSPACE}/config/${INI_FIlE_FOR_ENV}"
-    cp "${WORKSPACE}/config/${INI_FIlE_FOR_ENV}" "${WORKSPACE}/config/data_copy.ini"
+    echo "${WORKSPACE}/config/${INI_FILE_FOR_ENV}"
+    cp "${WORKSPACE}/config/${INI_FILE_FOR_ENV}" "${WORKSPACE}/config/data_copy.ini"
     sed -i.bak "s/edwdbsrv4.poc.dum.edwdc.net:9999/edwdbsrv1.poc.dum.edwdc.net:5432/" ${WORKSPACE}/config/data_copy.ini
     
     python import_data.py --config ${WORKSPACE}/config/data_copy.ini --resource ${WORKSPACE}/edschema/database/tests/resources

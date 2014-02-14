126e90b981092ed290174f71271b00a7acf26263
Thomas Shewchuk <tshewchuk@wgen.net>
2014-02-14 02:45:20 -0500

    US17761: Created student registration staging to integration mapping.
    Former-commit-id: dfb6eed5896f7904590658fd5fff4025752cdd42

--- edudl2/edudl2/tests/unit_tests/test_load_to_integration_table.py
+++ edudl2/edudl2/tests/unit_tests/test_load_to_integration_table.py
@@ -9,13 +9,14 @@ from edudl2.move_to_integration.move_to_integration import create_migration_quer
 
 
 class TestLoadToIntegrationTable(unittest.TestCase):
-    def test_create_migration_query(self,):
+
+    def test_create_migration_query(self):
         # get unit test column mapping
         self.maxDiff = None
         expected_query_result = """
         INSERT INTO "test_udl2"."test_int_sbac_asmt_outcome"
-            (guid_batch, substr_test, number_test)
-        SELECT A.guid_batch, SUBSTR(A.substr_test, 1, 10), TO_NUMBER(A.number_test, '99999')
+            (guid_batch, substr_test, number_test, bool_test)
+        SELECT A.guid_batch, SUBSTR(A.substr_test, 1, 10), TO_NUMBER(A.number_test, '99999'), CAST(A.bool_test as bool)
             FROM "test_udl2"."test_stg_sbac_asmt_outcome" AS A LEFT JOIN
             "test_udl2"."test_err_list" AS B ON (A.record_sid = B.record_sid )
              WHERE B.record_sid IS NULL AND A.guid_batch = '00000000-0000-0000-0000-000000000000'
@@ -27,8 +28,8 @@ class TestLoadToIntegrationTable(unittest.TestCase):
         error_schema = 'test_udl2'
         error_table = 'test_err_list'
         guid_batch = '00000000-0000-0000-0000-000000000000'
-        target_columns = ['guid_batch', 'substr_test', 'number_test']
-        source_columns_with_tran_rule = ['A.guid_batch', 'SUBSTR(A.substr_test, 1, 10)', 'TO_NUMBER(A.number_test, \'99999\')']
+        target_columns = ['guid_batch', 'substr_test', 'number_test', 'bool_test']
+        source_columns_with_tran_rule = ['A.guid_batch', 'SUBSTR(A.substr_test, 1, 10)', 'TO_NUMBER(A.number_test, \'99999\')', 'CAST(A.bool_test as bool)']
         actual_query_result = create_migration_query(source_schema, source_table, target_schema, target_table,
                                                      error_schema, error_table, guid_batch, target_columns, source_columns_with_tran_rule)
         self.assertEqual(re.sub('\s+', ' ', expected_query_result.replace('\n', ' ').replace('\t', ' ')),

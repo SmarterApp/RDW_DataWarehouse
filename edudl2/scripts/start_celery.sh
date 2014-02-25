1cc37d5de2d37da154a8413fadac847a091cdc24
Eugene Jen <ejen@wgen.net>
2014-02-25 15:18:06 -0500

    add celeryctl purge into start_celery.sh
    Former-commit-id: b1632b226f5be6b4b7599176667f720b63638c52

--- edudl2/scripts/start_celery.sh
+++ edudl2/scripts/start_celery.sh
@@ -4,6 +4,10 @@
 
 # we pass --config_file path_and_name_of config file, so it counts as 2 arguments
 
+# remove outstanding failed celery tasks
+
+celeryctl purge
+
 if [ $# == 2 ]; then
     start_celery.py $1 $2;
 else

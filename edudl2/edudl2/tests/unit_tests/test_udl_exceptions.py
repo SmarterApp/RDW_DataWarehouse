9ea6a8f4fe69637a0ed099e5de660f45ddcd0ab1
Eugene Jen <ejen@wgen.net>
2014-02-27 20:03:02 -0500

    add one more unit test to test raise exceptions
    Former-commit-id: 03be5d2ca59cf576cae6169886c4ba3d41bf1240

--- edudl2/edudl2/tests/unit_tests/test_udl_exceptions.py
+++ edudl2/edudl2/tests/unit_tests/test_udl_exceptions.py
@@ -12,7 +12,13 @@ class TestUdlExceptions(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testDeleteRecordNotFound(self):
+    def raise_exception(self):
+        raise DeleteRecordNotFound('test_student_guid', 'test_asmt_guid', '2014-12-31', 'Record not found')
+
+    def test_raise_DeleteRecordNotFound(self):
+        self.assertRaises(DeleteRecordNotFound, self.raise_exception)
+
+    def test_DeleteRecordNotFound(self):
         exception = DeleteRecordNotFound('test_student_guid', 'test_asmt_guid', '2014-12-31', 'Record not found')
         self.assertEqual(str(exception),
                          "DeleteRecordNotFound for student_guid: test_student_guid, asmt_guid: test_asmt_guid, "

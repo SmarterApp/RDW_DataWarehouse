2f6f521b042280d933c0c85a682e3a983172e10f
Takashi Osako <tosako@amplify.com>
2014-03-05 18:51:40 -0500

    fix pep8
    Former-commit-id: 453d913e17fb9ea7794ce24340f3b102810ac610

--- edmigrate/edmigrate/utils/migrate_cleanup.py
+++ edmigrate/edmigrate/utils/migrate_cleanup.py
@@ -8,8 +8,8 @@ def cleanup_tables(connector, schema_name, batch_guid, table_name_prefix, tables
     """
     clean up migration tables
     """
-    cleanup_all_tables(connector=connector, schema_name=schema_name, column_name='batch_guid', 
-        value=batch_guid, batch_delete=True, table_name_prefix=table_name_prefix, tables=tables)
+    cleanup_all_tables(connector=connector, schema_name=schema_name, column_name='batch_guid',
+                       value=batch_guid, batch_delete=True, table_name_prefix=table_name_prefix, tables=tables)
 
 
 def cleanup_batch(batch_guid, tenant, tables):

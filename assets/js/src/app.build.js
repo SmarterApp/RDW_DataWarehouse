42db9e5288865c958d4fce6d237b774724bd2962
Daniel Wu <dawu@wgen.net>
2014-03-06 12:31:05 -0500

    Tech Debt: requirejs best practice: make modules naming convention consist
    Former-commit-id: e89beb0b63b149b0b077326daab964371de621e5

--- assets/js/src/app.build.js
+++ assets/js/src/app.build.js
@@ -6,15 +6,15 @@
   optimize: 'uglify',
   modules: [
     {
-      name : "modules/Edware.comparingPopulationsReport"
+      name : "modules/EDWARE.comparingPopulationsReport"
     },
     {
-      name : "modules/Edware.individualStudentReport"
+      name : "modules/EDWARE.individualStudentReport"
     },
     {
-      name : "modules/Edware.studentListReport"
+      name : "modules/EDWARE.studentListReport"
     },
     {
-      name: "modules/Edware.stateMap"
+      name: "modules/EDWARE.stateMap"
     }]
 })

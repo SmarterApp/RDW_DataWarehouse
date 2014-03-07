edb34338de481926966d4f66e1d324d70ea36166
ablum <ablum@wgen.net>
2014-03-07 14:41:40 -0500

    Modifying the end chain to not wait for the chain to finish
    Former-commit-id: dffc27d3fcc3b9bd4051ab7c080563605754fbf5

--- edudl2/edudl2/udl2/W_determine_end_chain.py
+++ edudl2/edudl2/udl2/W_determine_end_chain.py
@@ -9,6 +9,15 @@ from celery.canvas import chain
 logger = get_task_logger(__name__)
 
 
+def _get_loop_dir_task(msg):
+    loop_tasks = []
+    if mk.LOOP_PIPELINE in msg and msg[mk.LOOP_PIPELINE] is True:
+        import edudl2.udl2.W_get_udl_file as W_get_udl_file
+        return [W_get_udl_file.get_next_file.s()]
+
+    return loop_tasks
+
+
 @celery.task(name='udl2.W_determine_end_chain.task', base=Udl2BaseTask)
 def task(msg):
     logger.info(task.name)
@@ -24,8 +33,6 @@ def task(msg):
                       "studentregistration": [W_post_etl.task.s(), W_all_done.task.s(),
                                               W_job_status_notification.task.s()]}
 
-    result = chain(target_tasks[load_type] + post_etl_tasks[load_type]).delay()
+    loop_dir_tasks = _get_loop_dir_task(msg)
 
-    outgoing_msg = result.get()
-
-    return outgoing_msg
+    chain(target_tasks[load_type] + post_etl_tasks[load_type] + loop_dir_tasks).delay()

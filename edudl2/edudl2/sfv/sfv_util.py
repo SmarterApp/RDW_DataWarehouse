eb0c3af7e65112df7ac4b8889c5ac3a3874b7d31
dip <dip@wgen.net>
2014-03-07 10:01:58 -0500

    Tech Debt: sfv was creating db engine
    Former-commit-id: 9de826d8479d7b33128c237cce272da67a3cce51

--- edudl2/edudl2/sfv/sfv_util.py
+++ edudl2/edudl2/sfv/sfv_util.py
@@ -6,7 +6,7 @@ Utility methods for CSV and JSON validators
 
 from sqlalchemy.sql import select
 from edudl2.udl2.celery import udl2_conf
-from edudl2.udl2_util.database_util import connect_db, get_sqlalch_table_object
+from edudl2.udl2.udl2_connector import UDL2DBConnection
 
 
 def get_source_column_values_from_ref_column_mapping(source_table, load_type):
@@ -15,15 +15,7 @@ def get_source_column_values_from_ref_column_mapping(source_table, load_type):
     :param: source_table: source_table values to query for
     :return: A set containing all the columns expected in a source csv file
     """
-    (conn, engine) = connect_db(udl2_conf['udl2_db']['db_driver'],
-                                udl2_conf['udl2_db']['db_user'],
-                                udl2_conf['udl2_db']['db_pass'],
-                                udl2_conf['udl2_db']['db_host'],
-                                udl2_conf['udl2_db']['db_port'],
-                                udl2_conf['udl2_db']['db_database'])
-
-    table_meta = get_sqlalch_table_object(engine, udl2_conf['udl2_db']['reference_schema'],
-                                          udl2_conf['udl2_db']['ref_tables'][load_type])
-
-    select_object = select([table_meta]).where(table_meta.c.source_table == source_table)
-    return [row['source_column'] for row in conn.execute(select_object)]
+    with UDL2DBConnection() as conn:
+        table_meta = conn.get_table(udl2_conf['udl2_db']['ref_tables'][load_type])
+        select_object = select([table_meta]).where(table_meta.c.source_table == source_table)
+        return [row['source_column'] for row in conn.execute(select_object)]

7097064cabc49fd3c85d12f68e435986c7083423
Nidhi Paroha <nparoha@wgen.net>
2014-03-10 23:35:49 -0400

    Removing all changes to skip integration_tests for EdUdl2_build
    Former-commit-id: 3893c3343114cfffbd71844de2219fa420c3dc00

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -89,12 +89,7 @@ function run_unit_tests {
    
     cd "$WORKSPACE/$1"
 
-	if [ $MAIN_PKG == "edudl2" ]; then
-	    nosetests --exclude-dir=integration_tests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml --cov-report xml
-	else
-	    nosetests --with-xunit --xunit-file=$WORKSPACE/nosetests.xml --cov-report xml
-	fi
-
+	nosetests --with-xunit --xunit-file=$WORKSPACE/nosetests.xml --cov-report xml
     if [ -f coverage.xml ]; then
        # move coverage results
        mv coverage.xml $WORKSPACE/coverage.xml

68369df5c9e30bbae76ef6100c52de27c7872ea4
Eugene Jen <ejen@wgen.net>
2014-03-12 12:11:19 -0400

    fix skip test for the moment until connection issues in func test is figured out
    Former-commit-id: 905c9db0fc7285f65c4abd375e9e8cfe928b863d

--- edudl2/edudl2/tests/functional_tests/test_udl_exceptions.py
+++ edudl2/edudl2/tests/functional_tests/test_udl_exceptions.py
@@ -24,7 +24,7 @@ DETAIL:  Key (asmnt_outcome_rec_id)=(11339) already exists.
         self.udl_phase_step = "Handle Deletion"
         self.working_schema = "edware"
 
-    @unittest.skip("")
+    @skip("under development")
     def test_exception_insert_err_list(self):
         exc = UDLDataIntegrityError(self.guid_batch, self.insert_error_message, self.schema_table,
                                     self.error_source, self.udl_phase_step, self.working_schema)

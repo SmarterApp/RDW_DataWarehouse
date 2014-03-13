7d88c316b55ac90213ba050a715ddefc7b4001e0
Takashi Osako <tosako@amplify.com>
2014-03-13 15:40:18 -0400

    US22211: added reply message back to conductor
    Former-commit-id: a0d276778820273897c4d88f3735398c03fc773f

---
+++ edmigrate/edmigrate/utils/reply_to_conductor.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 13, 2014
+
+@author: tosako
+'''
+
+
+def register_slave(node_id):
+    pass
+
+def acknowledgement_pgpool_disconnected(node_id):
+    pass
+
+def acknowledgement_pgpool_connected(node_id):
+    pass
+
+def acknowledgement_master_disconnected(node_id):
+    pass
+
+def acknowledgement_master_connected(node_id):
+    pass

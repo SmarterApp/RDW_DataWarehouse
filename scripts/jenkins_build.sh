be5b294af6888990df7bd8eec065aaec3888abea
Sabbir Muhit <smuhit@amplify.com>
2014-03-13 17:15:20 -0400

    Attempt to make smarter functional tests faster on jenkins...
    Former-commit-id: 2d89ec82b78d7328869c0fbadbdc7042c85c3b1e

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -177,6 +177,7 @@ function setup_functional_test_dependencies {
     python setup.py develop
 
     pip install pep8
+    pip install nose-xunitmp
     echo "Finish functional test dependencies setup"
 }
 
@@ -224,7 +225,7 @@ function run_functional_tests {
        cd e2e_tests
        nosetests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
     else
-       nosetests --exclude-dir=e2e_tests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
+       nosetests --exclude-dir=e2e_tests -v --with-xunitmp --xunitmp-file=$WORKSPACE/nosetests.xml --processes=6 --process-timeout=1000
        generate_docs edware_test/edware_test/functional_tests
     fi
 

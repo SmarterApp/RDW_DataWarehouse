9e396232c6ed677817359e0ad47a1d7c1c2ddbcf
Takashi Osako <tosako@amplify.com>
2014-03-13 16:08:09 -0400

    US22211: refactor function
    Former-commit-id: 5fcec6222e43eb09c9eadb461f5d49e110adebc9

--- edmigrate/edmigrate/utils/reply_to_conductor.py
+++ edmigrate/edmigrate/utils/reply_to_conductor.py
@@ -5,17 +5,17 @@ Created on Mar 13, 2014
 '''
 
 
-def register_slave(node_id):
+def register_slave(node_id, url, exchange_name='edmigrate', queue_name='edmigrate_conductor', routing_key='edmigrate.conductor'):
     pass
 
-def acknowledgement_pgpool_disconnected(node_id):
+def acknowledgement_pgpool_disconnected(node_id, url, exchange_name='edmigrate', queue_name='edmigrate_conductor', routing_key='edmigrate.conductor'):
     pass
 
-def acknowledgement_pgpool_connected(node_id):
+def acknowledgement_pgpool_connected(node_id, url, exchange_name='edmigrate', queue_name='edmigrate_conductor', routing_key='edmigrate.conductor'):
     pass
 
-def acknowledgement_master_disconnected(node_id):
+def acknowledgement_master_disconnected(node_id, url, exchange_name='edmigrate', queue_name='edmigrate_conductor', routing_key='edmigrate.conductor'):
     pass
 
-def acknowledgement_master_connected(node_id):
+def acknowledgement_master_connected(node_id, url, exchange_name='edmigrate', queue_name='edmigrate_conductor', routing_key='edmigrate.conductor'):
     pass

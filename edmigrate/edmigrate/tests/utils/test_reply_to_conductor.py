12ea58a1b55f18848c6613bf6917628f184be19a
Takashi Osako <tosako@amplify.com>
2014-03-16 11:19:02 -0400

    US22211: UT
    Former-commit-id: c269fefe9f9fca3712062c915491ce36bd0175cf

---
+++ edmigrate/edmigrate/tests/utils/test_reply_to_conductor.py
@@ -0,0 +1,69 @@
+'''
+Created on Mar 16, 2014
+
+@author: tosako
+'''
+import unittest
+from kombu import Connection
+from kombu.entity import Exchange, Queue
+from edmigrate.utils.reply_to_conductor import register_slave,\
+    acknowledgement_pgpool_disconnected, acknowledgement_pgpool_connected,\
+    acknowledgement_master_disconnected, acknowledgement_master_connected
+from kombu.tests.mocks import Transport
+from edmigrate.utils.constants import Constants
+import json
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.__conn = Connection(transport=Transport)
+        self.__conn.connect()
+        self.__exchange = Exchange('test_exchange')
+        self.__routing_key = 'test.routing'
+        self.__queue = Queue('test_queue', self.__exchange, routing_key=self.__routing_key)
+
+    def test_register_slave(self):
+        message, exchange, routing_key = register_slave(111, self.__conn, self.__exchange, self.__routing_key)
+        body = json.loads(message['body'])
+        self.assertEqual(Constants.ACK_COMMAND_FIND_SLAVE, body[Constants.MESSAGE_ACK_COMMAND])
+        self.assertEqual(111, body[Constants.MESSAGE_NODE_ID])
+        self.assertEqual(self.__exchange.name, exchange)
+        self.assertEqual(self.__routing_key, routing_key)
+        
+    def test_acknowledgement_pgpool_disconnected(self):
+        message, exchange, routing_key = acknowledgement_pgpool_disconnected(111, self.__conn, self.__exchange, self.__routing_key)
+        body = json.loads(message['body'])
+        self.assertEqual(Constants.ACK_COMMAND_DISCONNECT_PGPOOL, body[Constants.MESSAGE_ACK_COMMAND])
+        self.assertEqual(111, body[Constants.MESSAGE_NODE_ID])
+        self.assertEqual(self.__exchange.name, exchange)
+        self.assertEqual(self.__routing_key, routing_key)
+
+    def test_acknowledgement_pgpool_connected(self):
+        message, exchange, routing_key = acknowledgement_pgpool_connected(111, self.__conn, self.__exchange, self.__routing_key)
+        body = json.loads(message['body'])
+        self.assertEqual(Constants.ACK_COMMAND_CONNECT_PGPOOL, body[Constants.MESSAGE_ACK_COMMAND])
+        self.assertEqual(111, body[Constants.MESSAGE_NODE_ID])
+        self.assertEqual(self.__exchange.name, exchange)
+        self.assertEqual(self.__routing_key, routing_key)
+    
+    def test_acknowledgement_master_disconnected(self):
+        message, exchange, routing_key = acknowledgement_master_disconnected(111, self.__conn, self.__exchange, self.__routing_key)
+        body = json.loads(message['body'])
+        self.assertEqual(Constants.ACK_COMMAND_DISCONNECT_MASTER, body[Constants.MESSAGE_ACK_COMMAND])
+        self.assertEqual(111, body[Constants.MESSAGE_NODE_ID])
+        self.assertEqual(self.__exchange.name, exchange)
+        self.assertEqual(self.__routing_key, routing_key)
+    
+    def test_acknowledgement_master_connected(self):
+        message, exchange, routing_key = acknowledgement_master_connected(111, self.__conn, self.__exchange, self.__routing_key)
+        body = json.loads(message['body'])
+        self.assertEqual(Constants.ACK_COMMAND_CONNECT_MASTER, body[Constants.MESSAGE_ACK_COMMAND])
+        self.assertEqual(111, body[Constants.MESSAGE_NODE_ID])
+        self.assertEqual(self.__exchange.name, exchange)
+        self.assertEqual(self.__routing_key, routing_key)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
--- edmigrate/edmigrate/utils/reply_to_conductor.py
+++ edmigrate/edmigrate/utils/reply_to_conductor.py
@@ -3,39 +3,36 @@ Created on Mar 13, 2014
 
 @author: tosako
 '''
-from kombu.connection import Connection
 from edmigrate.utils.constants import Constants
-from kombu.entity import Exchange
+from kombu import Producer
 
 
-def register_slave(node_id, url, exchange, routing_key):
-    __send_message_to_conductor(node_id, Constants.ACK_COMMAND_FIND_SLAVE, url, exchange, routing_key)
+def register_slave(node_id, connection, exchange, routing_key):
+    return __send_message_to_conductor(node_id, Constants.ACK_COMMAND_FIND_SLAVE, connection, exchange, routing_key)
 
 
-def acknowledgement_pgpool_disconnected(node_id, url, exchange, routing_key):
-    __send_message_to_conductor(node_id, Constants.ACK_COMMAND_DISCONNECT_PGPOOL, url, exchange, routing_key)
+def acknowledgement_pgpool_disconnected(node_id, connection, exchange, routing_key):
+    return __send_message_to_conductor(node_id, Constants.ACK_COMMAND_DISCONNECT_PGPOOL, connection, exchange, routing_key)
 
 
-def acknowledgement_pgpool_connected(node_id, url, exchange, routing_key):
-    __send_message_to_conductor(node_id, Constants.ACK_COMMAND_CONNECT_PGPOOL, url, exchange, routing_key)
+def acknowledgement_pgpool_connected(node_id, connection, exchange, routing_key):
+    return __send_message_to_conductor(node_id, Constants.ACK_COMMAND_CONNECT_PGPOOL, connection, exchange, routing_key)
 
 
-def acknowledgement_master_disconnected(node_id, url, exchange, routing_key):
-    __send_message_to_conductor(node_id, Constants.ACK_COMMAND_DISCONNECT_MASTER, url, exchange, routing_key)
+def acknowledgement_master_disconnected(node_id, connection, exchange, routing_key):
+    return __send_message_to_conductor(node_id, Constants.ACK_COMMAND_DISCONNECT_MASTER, connection, exchange, routing_key)
 
 
-def acknowledgement_master_connected(node_id, url, exchange, routing_key):
-    __send_message_to_conductor(node_id, Constants.ACK_COMMAND_CONNECT_MASTER, url, exchange, routing_key)
+def acknowledgement_master_connected(node_id, connection, exchange, routing_key):
+    return __send_message_to_conductor(node_id, Constants.ACK_COMMAND_CONNECT_MASTER, connection, exchange, routing_key)
 
 
-def __send_message_to_conductor(node_id, command, url, exchange, routing_key):
+def __send_message_to_conductor(node_id, command, connection, exchange, routing_key):
+    '''
+    return should be None unless you are Unit Testing with Connection which uses Mock Transport
+    '''
     message = {}
     message[Constants.MESSAGE_NODE_ID] = node_id
     message[Constants.MESSAGE_ACK_COMMAND] = command
-    with Connection(url) as conn:
-        producer = conn.Producer(serializer='json')
-        producer.publish(message, exchange=exchange, routing_key=routing_key)
-
-if __name__ == '__main__':
-    exchange = Exchange('edmigrate_conductor', type='direct')
-    register_slave(123, 'amqp://guest:guest@localhost:5672', exchange, 'edmigrate.conductor')
+    producer = Producer(connection, serializer='json')
+    return producer.publish(message, exchange=exchange, routing_key=routing_key)

2f70ab2f113b4b4a28643109daef43e7decc25ec
Takashi Osako <tosako@amplify.com>
2014-03-17 16:14:54 -0400

    US22211: refactor
    Former-commit-id: 980b565d5787bd060d1329b3dffcae9330cd823e

---
+++ edmigrate/edmigrate/utils/utils.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 17, 2014
+
+@author: tosako
+'''
+
+
+def get_broker_url(settings):
+    url = "memory://"
+    celery_always_eager = settings.get('migrate.celery.celery_always_eager', False)
+    if not celery_always_eager:
+        url = settings.get('migrate.celery.BROKER_URL', url)
+    return url

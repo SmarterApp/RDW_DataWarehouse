112c681475de0dc7fe4979037832741f5efe6c90
Takashi Osako <tosako@amplify.com>
2014-03-19 15:17:07 -0400

    US22211: added init script
    Former-commit-id: dea85c68b8f608d5a616cb4eddccc7e83d0e3ab4

--- edmigrate/edmigrate/main.py
+++ edmigrate/edmigrate/main.py
@@ -21,9 +21,17 @@ from edmigrate.edmigrate_celery import setup_celery
 import logging
 import logging.config
 from edmigrate.utils.consumer import ConsumerThread
+import sys
+import signal
 
 
 logger = logging.getLogger('edmigrate')
+pidfile = None
+
+
+def signal_handler(signal, frame):
+    os.unlink(pidfile)
+    os._exit(0)
 
 
 def get_ini_file():
@@ -70,11 +78,47 @@ def main(file=None, tenant='cat', run_migrate_only=False):
         controller.join()
 
 
+def create_daemon(_pidfile):
+    global pidfile
+    pidfile = _pidfile
+    if os.path.isfile(pidfile):
+        print('pid file[' + pidfile + '] still exist.  please check your system.')
+        os._exit(1)
+    if not os.path.isdir(os.path.dirname(pidfile)):
+        os.mkdir(os.path.dirname(pidfile))
+    pid = os.fork()
+    if pid == 0:
+        os.setsid()
+        with open(pidfile, 'w') as f:
+            f.write(str(os.getpid()))
+        os.chdir('/')
+        os.umask(0)
+    else:  # parent goes bye bye
+        os._exit(0)
+
+    si = os.open('/dev/null', os.O_RDONLY)
+    so = os.open('/dev/null', os.O_RDWR)
+    se = os.open('/dev/null', os.O_RDWR)
+    os.dup2(si, sys.stdin.fileno())
+    os.dup2(so, sys.stdout.fileno())
+    os.dup2(se, sys.stderr.fileno())
+    os.close(si)
+    os.close(so)
+    os.close(se)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGHUP, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+
 if __name__ == '__main__':
     # Entry point for testing migration
     # python main.py -t dog --migrateOnly
     parser = ArgumentParser(description='EdMigrate entry point')
     parser.add_argument('--migrateOnly', action='store_true', dest='migrate_only', default=False, help="migrate only mode")
     parser.add_argument('-t', dest='tenant', default='cat', help="tenant name")
+    parser.add_argument('-p', dest='pidfile', default='/opt/edware/run/edmigrate.pid', help="pid file for daemon")
+    parser.add_argument('-d', dest='daemon', action='store_true', default=False, help="daemon")
     args = parser.parse_args()
+    if args.daemon:
+        create_daemon(args.pidfile)
     main(tenant=args.tenant, run_migrate_only=args.migrate_only)

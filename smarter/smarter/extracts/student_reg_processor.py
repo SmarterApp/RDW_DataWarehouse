6c26f8579c8dd2aa00b915e0d9821cf6de343891
ablum <ablum@wgen.net>
2014-03-21 13:37:18 -0400

    US23240 Code review fixes
    Former-commit-id: 30f157ca83c582a37b53aa9147836a65687d8d49

--- smarter/smarter/extracts/student_reg_processor.py
+++ smarter/smarter/extracts/student_reg_processor.py
@@ -34,7 +34,7 @@ def process_async_extraction_request(params):
     state_code = params[EndpointConstants.STATECODE][0]
     request_id, user, tenant = processor.get_extract_request_user_info(state_code)
 
-    extract_params = {TaskConstants.STATE_CODE: params[EndpointConstants.STATECODE][0],
+    extract_params = {TaskConstants.STATE_CODE: state_code,
                       TaskConstants.ACADEMIC_YEAR: params[EndpointConstants.ACADEMIC_YEAR][0],
                       TaskConstants.REPORT_TYPE: ReportType.STATISTICS}
 
@@ -48,27 +48,30 @@ def process_async_extraction_request(params):
 
     response['tasks'] = [task_response]
 
+    encrypted_file_path = processor.get_archive_file_path(user.get_uid(), tenant, request_id)
+    response['fileName'] = os.path.basename(encrypted_file_path)
+
+    data_directory_to_archive = processor.get_extract_work_zone_path(tenant, request_id)
+
     public_key_id = processor.get_encryption_public_key_identifier(tenant)
-    archive_file_name = processor.get_archive_file_path(user.get_uid(), tenant, request_id)
-    response['fileName'] = os.path.basename(archive_file_name)
-    directory_to_archive = processor.get_extract_work_zone_path(tenant, request_id)
     gatekeeper_id = processor.get_gatekeeper(tenant)
     pickup_zone_info = processor.get_pickup_zone_info(tenant)
 
-    start_extract.apply_async(args=[tenant, request_id, public_key_id, archive_file_name, directory_to_archive, gatekeeper_id, pickup_zone_info, task_info], queue=queue)
+    start_extract.apply_async(args=[tenant, request_id, public_key_id, encrypted_file_path, data_directory_to_archive, gatekeeper_id, pickup_zone_info, task_info], queue=queue)
     return response
 
 
 def _create_task_info(request_id, user, tenant, extract_params):
     task_info = {TaskConstants.TASK_TASK_ID: create_new_entry(user, request_id, extract_params),
-                 TaskConstants.TASK_FILE_NAME: _get_extract_file_path(extract_params, tenant, request_id)}
+                 TaskConstants.TASK_FILE_NAME: _get_extract_file_path(request_id, tenant, extract_params)}
     task_info.update(extract_params)
 
     return task_info
 
 
-def _get_extract_file_path(params, tenant, request_id):
-    file_name = '{stateCode}_{academicYear}_{currentTime}_STATS.csv'.format(stateCode=params.get(TaskConstants.STATE_CODE),
-                                                                            academicYear=params.get(TaskConstants.ACADEMIC_YEAR),
-                                                                            currentTime=str(datetime.now().strftime("%m-%d-%Y_%H-%M-%S")))
+def _get_extract_file_path(request_id, tenant, params):
+    file_name = '{stateCode}_{academicYear}_{reportType}_{currentTime}.csv'.format(stateCode=params.get(TaskConstants.STATE_CODE),
+                                                                                   academicYear=params.get(TaskConstants.ACADEMIC_YEAR),
+                                                                                   reportType=params.get(TaskConstants.REPORT_TYPE),
+                                                                                   currentTime=str(datetime.now().strftime("%m-%d-%Y_%H-%M-%S")))
     return os.path.join(processor.get_extract_work_zone_path(tenant, request_id), file_name)

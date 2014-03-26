5f02c096940f4edf69019462275897d90faa4b2d
npandey <npandey@amplify.com>
2014-03-26 16:31:36 -0400

    Fixing pep8 issues
    Former-commit-id: 0b52987dab142b149b28d83948dfba00063f520b

--- edextract/edextract/tests/data_extract_generation/test_statistics_data_generator.py
+++ edextract/edextract/tests/data_extract_generation/test_statistics_data_generator.py
@@ -29,4 +29,3 @@ class TestStatisticsDataGenerator(unittest.TestCase):
         self.assertEquals(result[4], 10)
         self.assertEquals(result[5], 11.11)
         self.assertEquals(result[6], 0)
-

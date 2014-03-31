7d9f956cb56a19927c534f6615218d660b202f84
ablum <ablum@wgen.net>
2014-03-31 10:07:00 -0400

    US19502 Enabling program statistics
    Former-commit-id: 27dfc37c9f95a4d698ee48be876299de3e47ab29

--- edextract/edextract/data_extract_generation/student_reg_report_generator.py
+++ edextract/edextract/data_extract_generation/student_reg_report_generator.py
@@ -117,8 +117,8 @@ def _get_sr_stat_tenant_data_for_academic_year(db_rows, academic_year):
 
     total_tracker = TotalTracker()
     trackers = [total_tracker, MaleTracker(), FemaleTracker(), HispanicLatinoTracker(), AmericanIndianTracker(),
-                AsianTracker(), AfricanAmericanTracker(), PacificIslanderTracker(), WhiteTracker(), MultiRaceTracker()]
-                #IDEAIndicatorTracker(), LEPStatusTracker(), Sec504StatusTracker(), EconDisadvStatusTracker(), MigrantStatusTracker()]
+                AsianTracker(), AfricanAmericanTracker(), PacificIslanderTracker(), WhiteTracker(), MultiRaceTracker(),
+                IDEAIndicatorTracker(), LEPStatusTracker(), Sec504StatusTracker(), EconDisadvStatusTracker(), MigrantStatusTracker()]
 
     data_processors = [StateDataProcessor(trackers), DistrictDataProcessor(trackers), SchoolDataProcessor(trackers)]
 

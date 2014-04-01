427d5b553bb8bbb819cf005e35b2b12e6511550e
Daniel Wu <dawu@wgen.net>
2014-04-01 11:17:48 -0400

    US21484: refactored FT test_file_loader.py to get rid of query string
    Former-commit-id: 421c1f80f11b9654b4632d3df0bbc08c9b7616e4

--- edudl2/edudl2/tests/functional_tests/test_file_loader.py
+++ edudl2/edudl2/tests/functional_tests/test_file_loader.py
@@ -2,19 +2,21 @@ import unittest
 import subprocess
 import csv
 import os
+import time
+import uuid
+from datetime import datetime
+from sqlalchemy import select, delete
 from edudl2.fileloader.file_loader import load_file
 from edudl2.udl2.defaults import UDL2_DEFAULT_CONFIG_PATH_FILE
-import uuid
 from edudl2.udl2 import message_keys as mk
-from edudl2.database.udl2_connector import initialize_db_target, initialize_db_udl
+from edudl2.database.udl2_connector import initialize_db_target, \
+    initialize_db_udl, get_udl_connection
 from edudl2.udl2_util.config_reader import read_ini_file
-from datetime import datetime
-import time
-from edudl2.udl2_util.database_util import connect_db
 
 
-STG_SBAC_ASMT_OUTCOME_COLUMNS = 'record_sid,op,guid_batch,src_file_rec_num,guid_asmt,guid_asmt_location,name_asmt_location,grade_asmt,name_state,code_state,guid_district,name_district,guid_school,name_school,type_school,guid_student,external_student_id,name_student_first,name_student_middle,name_student_last,address_student_line1,address_student_line2,address_student_city,address_student_zip,gender_student,email_student,dob_student,grade_enrolled,dmg_eth_hsp,dmg_eth_ami,dmg_eth_asn,dmg_eth_blk,dmg_eth_pcf,dmg_eth_wht,dmg_prg_iep,dmg_prg_lep,dmg_prg_504,dmg_prg_tt1,date_assessed,score_asmt,score_asmt_min,score_asmt_max,score_perf_level,score_claim_1,score_claim_1_min,score_claim_1_max,asmt_claim_1_perf_lvl,score_claim_2,score_claim_2_min,score_claim_2_max,asmt_claim_2_perf_lvl,score_claim_3,score_claim_3_min,score_claim_3_max,asmt_claim_3_perf_lvl,score_claim_4,score_claim_4_min,score_claim_4_max,asmt_claim_4_perf_lvl,asmt_type,asmt_subject,asmt_year,acc_asl_video_embed,acc_asl_human_nonembed,acc_braile_embed,acc_closed_captioning_embed,acc_text_to_speech_embed,acc_abacus_nonembed,acc_alternate_response_options_nonembed,acc_calculator_nonembed,acc_multiplication_table_nonembed,acc_print_on_demand_nonembed,acc_read_aloud_nonembed,acc_scribe_nonembed,acc_speech_to_text_nonembed,acc_streamline_mode'
-STG_SBAC_STU_REG_COLUMNS = 'record_sid,guid_batch,src_file_rec_num,name_state,code_state,guid_district,name_district,guid_school,name_school,guid_student,external_ssid_student,name_student_first,name_student_middle,name_student_last,gender_student,dob_student,grade_enrolled,dmg_eth_hsp,dmg_eth_ami,dmg_eth_asn,dmg_eth_blk,dmg_eth_pcf,dmg_eth_wht,dmg_prg_iep,dmg_prg_lep,dmg_prg_504,dmg_sts_ecd,dmg_sts_mig,dmg_multi_race,code_confirm,code_language,eng_prof_lvl,us_school_entry_date,lep_entry_date,lep_exit_date,t3_program_type,prim_disability_type'
+STG_SBAC_ASMT_OUTCOME_COLUMNS = ['record_sid', 'op', 'guid_batch', 'src_file_rec_num', 'guid_asmt', 'guid_asmt_location', 'name_asmt_location', 'grade_asmt', 'name_state', 'code_state', 'guid_district', 'name_district', 'guid_school', 'name_school', 'type_school', 'guid_student', 'external_student_id', 'name_student_first', 'name_student_middle', 'name_student_last', 'address_student_line1', 'address_student_line2', 'address_student_city', 'address_student_zip', 'gender_student', 'email_student', 'dob_student', 'grade_enrolled', 'dmg_eth_hsp', 'dmg_eth_ami', 'dmg_eth_asn', 'dmg_eth_blk', 'dmg_eth_pcf', 'dmg_eth_wht', 'dmg_prg_iep', 'dmg_prg_lep', 'dmg_prg_504', 'dmg_prg_tt1', 'date_assessed', 'score_asmt', 'score_asmt_min', 'score_asmt_max', 'score_perf_level', 'score_claim_1', 'score_claim_1_min', 'score_claim_1_max', 'asmt_claim_1_perf_lvl', 'score_claim_2', 'score_claim_2_min', 'score_claim_2_max', 'asmt_claim_2_perf_lvl', 'score_claim_3', 'score_claim_3_min', 'score_claim_3_max', 'asmt_claim_3_perf_lvl', 'score_claim_4', 'score_claim_4_min', 'score_claim_4_max', 'asmt_claim_4_perf_lvl', 'asmt_type', 'asmt_subject', 'asmt_year', 'acc_asl_video_embed', 'acc_asl_human_nonembed', 'acc_braile_embed', 'acc_closed_captioning_embed', 'acc_text_to_speech_embed', 'acc_abacus_nonembed', 'acc_alternate_response_options_nonembed', 'acc_calculator_nonembed', 'acc_multiplication_table_nonembed', 'acc_print_on_demand_nonembed', 'acc_read_aloud_nonembed', 'acc_scribe_nonembed', 'acc_speech_to_text_nonembed', 'acc_streamline_mode']
+
+STG_SBAC_STU_REG_COLUMNS = ['record_sid', 'guid_batch', 'src_file_rec_num', 'name_state', 'code_state', 'guid_district', 'name_district', 'guid_school', 'name_school', 'guid_student', 'external_ssid_student', 'name_student_first', 'name_student_middle', 'name_student_last', 'gender_student', 'dob_student', 'grade_enrolled', 'dmg_eth_hsp', 'dmg_eth_ami', 'dmg_eth_asn', 'dmg_eth_blk', 'dmg_eth_pcf', 'dmg_eth_wht', 'dmg_prg_iep', 'dmg_prg_lep', 'dmg_prg_504', 'dmg_sts_ecd', 'dmg_sts_mig', 'dmg_multi_race', 'code_confirm', 'code_language', 'eng_prof_lvl', 'us_school_entry_date', 'lep_entry_date', 'lep_exit_date', 't3_program_type', 'prim_disability_type']
 
 
 class FileLoaderFTest(unittest.TestCase):
@@ -56,44 +58,42 @@ class FileLoaderFTest(unittest.TestCase):
             mk.APPLY_RULES: False
         }
         # connect to db
-        conn, _engine = connect_db(self.conf[mk.SOURCE_DB_DRIVER], self.conf[mk.TARGET_DB_USER], self.conf[mk.TARGET_DB_PASSWORD],
-                                   self.conf[mk.TARGET_DB_HOST], self.conf[mk.TARGET_DB_PORT], self.conf[mk.TARGET_DB_NAME])
-        self.conn = conn
         initialize_db_target(self.udl2_conf)
         initialize_db_udl(self.udl2_conf)
+        self._conn = get_udl_connection()
 
     def test_assessment_row_number(self):
         # load data
         self.load_config('assessment')
         self.conf[mk.ROW_START] = 10
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         load_file(self.conf)
 
         # verify
         row_total_in_csv = get_row_number_in_csv(self.conf[mk.FILE_TO_LOAD])
-        row_total_in_db = get_row_number_in_table(self.conf, self.conn)
+        row_total_in_db = self.get_row_number_in_table()
         self.assertEqual(row_total_in_csv, row_total_in_db)
 
     def test_stu_reg_row_number(self):
         self.load_config('studentregistration')
         self.conf[mk.ROW_START] = 10
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         load_file(self.conf)
 
         row_total_in_csv = get_row_number_in_csv(self.conf[mk.FILE_TO_LOAD])
-        row_total_in_db = get_row_number_in_table(self.conf, self.conn)
+        row_total_in_db = self.get_row_number_in_table()
         self.assertEqual(row_total_in_csv, row_total_in_db)
 
     def test_assessment_compare_data(self):
         # load data
         self.load_config('assessment')
         self.conf[mk.ROW_START] = 24
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         load_file(self.conf)
         # wait for a while to avoid timing issue.
         time.sleep(20)
         # get the result of db
-        records_in_db = get_rows_in_table(self.conf, self.conn, STG_SBAC_ASMT_OUTCOME_COLUMNS)
+        records_in_db = self.get_rows_in_table(STG_SBAC_ASMT_OUTCOME_COLUMNS)
 
         # read the csv file
         self.verify_regular_table_content(records_in_db)
@@ -102,11 +102,11 @@ class FileLoaderFTest(unittest.TestCase):
         # load data
         self.load_config('studentregistration')
         self.conf[mk.ROW_START] = 24
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         load_file(self.conf)
 
         # get the result of db
-        records_in_db = get_rows_in_table(self.conf, self.conn, STG_SBAC_STU_REG_COLUMNS)
+        records_in_db = self.get_rows_in_table(STG_SBAC_STU_REG_COLUMNS)
 
         # read the csv file
         self.verify_table_content(records_in_db)
@@ -114,7 +114,7 @@ class FileLoaderFTest(unittest.TestCase):
     def test_assessment_transformations_occur_during_load(self):
         self.load_config('assessment')
         self.conf[mk.ROW_START] = 124
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         self.conf[mk.FILE_TO_LOAD] = self.assessment_csv_file2
         self.conf[mk.APPLY_RULES] = True
         load_file(self.conf)
@@ -125,7 +125,7 @@ class FileLoaderFTest(unittest.TestCase):
     def test_stu_reg_transformations_occur_during_load(self):
         self.load_config('studentregistration')
         self.conf[mk.ROW_START] = 124
-        self.conf[mk.GUID_BATCH] = generate_non_exsisting_guid_batch(self.conf, self.conn)
+        self.conf[mk.GUID_BATCH] = self.generate_non_exsisting_guid_batch()
         self.conf[mk.FILE_TO_LOAD] = self.stu_reg_csv_file2
         self.conf[mk.APPLY_RULES] = True
         load_file(self.conf)
@@ -134,15 +134,15 @@ class FileLoaderFTest(unittest.TestCase):
         self.compare_csv_table_data(self.stu_reg_csv_file2_clean, 'StudentIdentifier')
 
     def tearDown(self):
-        # truncate staging table or delete all rows just inserted.
-        query = 'DELETE FROM \"{schema_name}\".\"{table_name}\" WHERE guid_batch=\'{guid_batch}\''.format(schema_name=self.conf[mk.TARGET_DB_SCHEMA], table_name=self.conf[mk.TARGET_DB_TABLE], guid_batch=self.conf['guid_batch'])
-        trans = self.conn.begin()
+        table_name = self.conf[mk.TARGET_DB_TABLE]
+        table = self._conn.get_table(table_name)
+        guid_batch = self.conf['guid_batch']
         try:
-            self.conn.execute(query)
-            trans.commit()
+            delete(table).where(table.c.guid_batch == guid_batch)
         except Exception as e:
-            trans.rollback()
-        self.conn.close()
+            print('Exception -- ', e)
+        else:
+            print("Tear Down successful for batch", self.conf['guid_batch'])
 
     def load_config(self, type):
         if type == 'assessment':
@@ -161,8 +161,7 @@ class FileLoaderFTest(unittest.TestCase):
     def verify_regular_table_content(self, records_in_db):
         with open(self.conf[mk.FILE_TO_LOAD], newline='') as file:
             reader = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONE)
-            row_number = 0
-            for row_in_csv in reader:
+            for row_number, row_in_csv in enumerate(reader):
                 row_in_table = records_in_db[row_number]
                 # verify the src_file_rec_num and guid_batch
                 self.assertEqual(row_in_table['src_file_rec_num'], row_number + self.conf[mk.ROW_START])
@@ -172,13 +171,11 @@ class FileLoaderFTest(unittest.TestCase):
                 for value_in_csv, value_in_table in zip(row_in_csv, row_in_table):
                     if value_in_csv and value_in_table and type(value_in_table) != datetime:
                         self.assertEqual(value_in_csv.lower(), value_in_table.lower())
-                row_number += 1
 
     def verify_table_content(self, records_in_db):
         with open(self.conf[mk.FILE_TO_LOAD], newline='') as file:
             reader = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONE)
-            row_number = 0
-            for row_in_csv in reader:
+            for row_number, row_in_csv in enumerate(reader):
                 row_in_table = records_in_db[row_number]
                 # verify the src_file_rec_num and guid_batch
                 self.assertEqual(row_in_table['src_file_rec_num'], row_number + self.conf[mk.ROW_START])
@@ -188,12 +185,12 @@ class FileLoaderFTest(unittest.TestCase):
                 for value_in_csv, value_in_table in zip(row_in_csv, row_in_table):
                     if value_in_csv and value_in_table and type(value_in_table) != datetime:
                         self.assertEqual(value_in_csv.lower(), value_in_table.lower())
-                row_number += 1
 
     def compare_csv_table_data(self, csv_file, key_column):
-        sel_query = 'SELECT * FROM "{schema}"."{table}" WHERE guid_batch=\'{batch}\''.format(
-            schema=self.conf[mk.TARGET_DB_SCHEMA], table=self.conf[mk.TARGET_DB_TABLE], batch=self.conf['guid_batch'])
-        results = self.conn.execute(sel_query)
+        table = self._conn.get_table(self.conf[mk.TARGET_DB_TABLE])
+        guid_batch = self.conf['guid_batch']
+        query = select([table]).where(table.c.guid_batch == guid_batch)
+        results = self._conn.execute(query)
         result_list = results.fetchall()
         expected_rows = get_clean_rows_from_file(csv_file)
         # sort rows
@@ -214,6 +211,25 @@ class FileLoaderFTest(unittest.TestCase):
                                      change_empty_vals_to_none(expect_row[results.keys()[ci]]),
                                      'Values are not the same for column %s' % results.keys()[ci])
 
+    def generate_non_exsisting_guid_batch(self):
+        return str(uuid.uuid4())
+
+    def get_row_number_in_table(self):
+        table = self._conn.get_table(self.conf[mk.TARGET_DB_TABLE])
+        guid_batch = self.conf['guid_batch']
+        query = select([table]).where(table.c.guid_batch == guid_batch)
+        result = self._conn.execute(query)
+        return result.rowcount
+
+    def get_rows_in_table(self, columns):
+        table = self._conn.get_table(self.conf[mk.TARGET_DB_TABLE])
+        guid_batch = self.conf['guid_batch']
+        select_columns = [table.c[column] for column in columns]
+        query = select(select_columns).where(table.c.guid_batch == guid_batch)\
+                                      .order_by(table.c.src_file_rec_num)
+        result = self._conn.execute(query)
+        return result.fetchall()
+
 
 def get_clean_rows_from_file(filename):
     filerows = []
@@ -225,53 +241,12 @@ def get_clean_rows_from_file(filename):
     return filerows
 
 
-def generate_non_exsisting_guid_batch(conf, conn):
-    query = "SELECT DISTINCT guid_batch FROM \"{schema_name}\".\"{table_name}\"".format(schema_name=conf[mk.TARGET_DB_SCHEMA], table_name=conf[mk.TARGET_DB_TABLE])
-    trans = conn.begin()
-    try:
-        result = conn.execute(query)
-        trans.commit()
-    except Exception as e:
-        trans.rollback()
-    exsisting_guid_batchs = [row[0] for row in result]
-    if len(exsisting_guid_batchs) == 0:
-        return uuid.uuid4()
-    else:
-        #return max(exsisting_guid_batchs) + 10
-        return uuid.uuid4()
-
-
 def get_row_number_in_csv(csv_file):
     cmd_str = 'wc ' + csv_file
     prog = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, shell=True)
     (output, err) = prog.communicate()
     rows_in_csv = int(output.split()[0])
     return rows_in_csv
-
-
-def get_row_number_in_table(conf, conn):
-    query = 'SELECT COUNT(*) FROM \"{schema_name}\".\"{table_name}\" WHERE guid_batch=\'{guid_batch}\''.format(schema_name=conf[mk.TARGET_DB_SCHEMA], table_name=conf[mk.TARGET_DB_TABLE], guid_batch=conf['guid_batch'])
-    trans = conn.begin()
-    try:
-        result = conn.execute(query)
-        trans.commit()
-    except Exception as e:
-        trans.rollback()
-    for row in result:
-        row_total = row[0]
-        break
-    return int(row_total)
-
-
-def get_rows_in_table(conf, conn, columns):
-    query = 'SELECT {columns} FROM \"{schema_name}\".\"{table_name}\" WHERE guid_batch=\'{guid_batch}\' ORDER BY src_file_rec_num ASC'.format(schema_name=conf[mk.TARGET_DB_SCHEMA], table_name=conf[mk.TARGET_DB_TABLE], guid_batch=conf['guid_batch'], columns=columns)
-    trans = conn.begin()
-    try:
-        result = conn.execute(query)
-        trans.commit()
-    except Exception as e:
-        trans.rollback()
-    return list(result)
 
 
 def change_empty_vals_to_none(val):

ae1a16ba99dde6ae165c349c4d6c4a3a267a283d
ablum <ablum@wgen.net>
2014-04-01 17:42:12 -0400

    US22829 Adding UT for row data processor
    Former-commit-id: 074d6f31a2287944662d993e5b0ce87511219113

--- edextract/edextract/tests/student_reg_extract_processors/test_row_data_processor.py
+++ edextract/edextract/tests/student_reg_extract_processors/test_row_data_processor.py
@@ -1,3 +1,5 @@
+from mock import MagicMock, call
+
 __author__ = 'tshewchuk'
 
 """
@@ -6,15 +8,21 @@ This module contains unit tests for the functions in the row_data_processor modu
 
 import unittest
 
-from edextract.trackers.total_tracker import TotalTracker
-from edextract.student_reg_extract_processors.ed_org_data_processor import EdOrgNameKey
 from edextract.student_reg_extract_processors.row_data_processor import process_row_data
-from edextract.student_reg_extract_processors.state_data_processor import StateDataProcessor
-from edextract.student_reg_extract_processors.district_data_processor import DistrictDataProcessor
-from edextract.student_reg_extract_processors.school_data_processor import SchoolDataProcessor
 
 
 class TestRowDataProcessor(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_processor1 = MagicMock()
+        self.mock_processor1.process_yearly_data = MagicMock(return_value=None)
+        self.mock_processor1.process_matched_ids_data = MagicMock(return_value=None)
+
+        self.mock_processor2 = MagicMock()
+        self.mock_processor2.process_yearly_data = MagicMock(return_value=None)
+        self.mock_processor2.process_matched_ids_data = MagicMock(return_value=None)
+
+        self.data = [{'first': 'first'}, {'second': 'second'}]
 
     def test_process_row_data(self):
 
@@ -29,8 +37,22 @@ class TestRowDataProcessor(unittest.TestCase):
                 if expected_id != self.dp_id or expected_data != data:
                     raise IndexError()
 
+            def process_matched_ids_data(self, data):
+                    expected_id, expected_data = self.expected_call_stack.pop()
+                    if expected_id != self.dp_id or expected_data != data:
+                        raise IndexError()
+
         expected_calls_stack = [(2, 2), (1, 2), (2, 1), (1, 1)]  # reversed order, tuples of id and expected value
         dp1 = _DataProcessor(1, expected_calls_stack)
         dp2 = _DataProcessor(2, expected_calls_stack)
 
-        process_row_data([1, 2], [dp1, dp2])
+        process_row_data([1, 2], [], [dp1, dp2])
+
+    def test_process_matched_row_data(self):
+        process_row_data(self.data, self.data, [self.mock_processor1, self.mock_processor2])
+
+        self.assertEquals([call({'first': 'first'}), call({'second': 'second'})], self.mock_processor1.process_yearly_data.call_args_list)
+        self.assertEquals([call({'first': 'first'}), call({'second': 'second'})], self.mock_processor1.process_matched_ids_data.call_args_list)
+
+        self.assertEquals([call({'first': 'first'}), call({'second': 'second'})], self.mock_processor2.process_yearly_data.call_args_list)
+        self.assertEquals([call({'first': 'first'}), call({'second': 'second'})], self.mock_processor2.process_matched_ids_data.call_args_list)

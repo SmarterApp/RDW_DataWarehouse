629d88a6fbcd52b14606410d789a5b3036616f44
Eugene Jen <ejen@wgen.net>
2014-03-31 21:34:57 -0400

    forgot to commit ipables code
    Former-commit-id: 3e0cb8e8dbddede8698209454672df488229c763

---
+++ edmigrate/edmigrate/tests/utils/test_iptables.py
@@ -0,0 +1,100 @@
+import unittest
+from edmigrate.utils.iptables import Iptables
+from edmigrate.exceptions import IptablesCommandError
+from unittest.mock import patch, MagicMock
+from edmigrate.utils.constants import Constants
+import socket
+from mocket.mocket import MocketSocket
+import mocket
+import subprocess
+
+
+class IptableTest(unittest.TestCase):
+
+    def setUp(self):
+        self.iptables = Iptables()
+
+    def tearDown(self):
+        Iptables.cleanup()
+
+    @patch('subprocess.check_output')
+    def test_block_pgsql_input(self, MockSubprocess):
+        MockSubprocess.return_value = lambda: None
+        self.iptables.block_pgsql_INPUT()
+        MockSubprocess.assert_called_once_with([Constants.IPTABLES_SUDO, Constants.IPTABLES_COMMAND,
+                                                Constants.IPTABLES_TABLE, Constants.IPTABLES_FILTER,
+                                                Constants.IPTABLES_INSERT, Constants.IPTABLES_INPUT_CHAIN,
+                                                Constants.IPTABLES_JUMP, Constants.IPTABLES_TARGET],
+                                               universal_newlines=True)
+        #
+
+    @patch('subprocess.check_output')
+    def test_block_pgsql_output(self, MockSubprocess):
+        MockSubprocess.return_value = lambda: None
+        self.iptables.block_pgsql_OUTPUT()
+        MockSubprocess.assert_called_once_with([Constants.IPTABLES_SUDO, Constants.IPTABLES_COMMAND,
+                                                Constants.IPTABLES_TABLE, Constants.IPTABLES_FILTER,
+                                                Constants.IPTABLES_INSERT, Constants.IPTABLES_OUTPUT_CHAIN,
+                                                Constants.IPTABLES_JUMP, Constants.IPTABLES_TARGET],
+                                               universal_newlines=True)
+
+    @patch('subprocess.check_output')
+    def test_unblock_pgsql_input(self, MockSubprocess):
+        MockSubprocess.return_value = lambda: None
+        self.iptables.unblock_pgsql_INPUT()
+        MockSubprocess.assert_called_once_with([Constants.IPTABLES_SUDO, Constants.IPTABLES_COMMAND,
+                                                Constants.IPTABLES_TABLE, Constants.IPTABLES_FILTER,
+                                                Constants.IPTABLES_DELETE, Constants.IPTABLES_INPUT_CHAIN,
+                                                Constants.IPTABLES_JUMP, Constants.IPTABLES_TARGET],
+                                               universal_newlines=True)
+
+    @patch('subprocess.check_output')
+    def test_unblock_pgsql_output(self, MockSubprocess):
+        MockSubprocess.return_value = lambda: None
+        self.iptables.unblock_pgsql_OUTPUT()
+        MockSubprocess.assert_called_once_with([Constants.IPTABLES_SUDO, Constants.IPTABLES_COMMAND,
+                                                Constants.IPTABLES_TABLE, Constants.IPTABLES_FILTER,
+                                                Constants.IPTABLES_DELETE, Constants.IPTABLES_OUTPUT_CHAIN,
+                                                Constants.IPTABLES_JUMP, Constants.IPTABLES_TARGET],
+                                               universal_newlines=True)
+
+    @patch.object(mocket.mocket.MocketSocket, 'close')
+    @patch('socket.create_connection')
+    def test_check_block_input_non_blocked(self, MockSocket, MockMethod):
+        MockSocket.side_effect = None
+        MockSocket.return_value = mocket.mocket.MocketSocket(socket.AF_INET, socket.SOCK_STREAM)
+        MockMethod.return_value = lambda: None
+        block_status = self.iptables.check_block_input('localhost')
+        self.assertTrue(block_status)
+
+    @patch.object(mocket.mocket.MocketSocket, 'close')
+    @patch('socket.create_connection')
+    def test_check_block_output_non_blocked(self, MockSocket, MockMethod):
+        MockSocket.side_effect = None
+        MockSocket.return_value = mocket.mocket.MocketSocket(socket.AF_INET, socket.SOCK_STREAM)
+        MockMethod.return_value = lambda: None
+        block_status = self.iptables.check_block_output('localhost')
+        self.assertTrue(block_status)
+
+    @patch.object(mocket.mocket.MocketSocket, 'close')
+    @patch('socket.create_connection')
+    def test_check_block_input_blocked(self, MockSocket, MockMethod):
+        MockSocket.side_effect = ConnectionRefusedError()
+        MockSocket.return_value = mocket.mocket.MocketSocket(socket.AF_INET, socket.SOCK_STREAM)
+        MockMethod.return_value = lambda: None
+        block_status = self.iptables.check_block_input('localhost')
+        self.assertFalse(block_status)
+
+    @patch.object(mocket.mocket.MocketSocket, 'close')
+    @patch('socket.create_connection')
+    def test_check_block_output_blocked(self, MockSocket, MockMethod):
+        MockSocket.side_effect = ConnectionRefusedError()
+        MockSocket.return_value = mocket.mocket.MocketSocket(socket.AF_INET, socket.SOCK_STREAM)
+        MockMethod.return_value = lambda: None
+        block_status = self.iptables.check_block_output('localhost')
+        self.assertFalse(block_status)
+
+    @patch('subprocess.check_output')
+    def test_subprocess_exception(self, MockSubprocess):
+        MockSubprocess.side_effect = subprocess.CalledProcessError(-1, 'iptables')
+        self.assertRaises(IptablesCommandError, self.iptables._modify_rule, Constants.IPTABLES_INSERT, Constants.IPTABLES_INPUT_CHAIN)
---
+++ edmigrate/edmigrate/utils/iptables.py
@@ -0,0 +1,58 @@
+import subprocess
+import socket
+from edmigrate.utils.utils import Singleton
+from edmigrate.utils.constants import Constants
+from edmigrate.exceptions import IptablesCommandError
+
+
+class Iptables(metaclass=Singleton):
+
+    def __init__(self, input_port=5432, output_port=5432, target=Constants.IPTABLES_TARGET):
+        self._input_port = input_port
+        self._output_port = output_port
+        self._localhost = '127.0.0.1'
+        self._timeout = 1
+        self._target = target
+
+    @classmethod
+    def cleanup(cls):
+        Iptables._instances = {}
+
+    def _modify_rule(self, mode, chain):
+        try:
+            subprocess.check_output([Constants.IPTABLES_SUDO, Constants.IPTABLES_COMMAND,
+                                     Constants.IPTABLES_TABLE, Constants.IPTABLES_FILTER,
+                                     mode, chain,
+                                     Constants.IPTABLES_JUMP, self._target], universal_newlines=True)
+        except subprocess.CalledProcessError:
+            raise IptablesCommandError
+
+    def block_pgsql_INPUT(self):
+        self._modify_rule(Constants.IPTABLES_INSERT, Constants.IPTABLES_INPUT_CHAIN)
+
+    def block_pgsql_OUTPUT(self):
+        self._modify_rule(Constants.IPTABLES_INSERT, Constants.IPTABLES_OUTPUT_CHAIN)
+
+    def unblock_pgsql_INPUT(self):
+        self._modify_rule(Constants.IPTABLES_DELETE, Constants.IPTABLES_INPUT_CHAIN)
+
+    def unblock_pgsql_OUTPUT(self):
+        self._modify_rule(Constants.IPTABLES_DELETE, Constants.IPTABLES_OUTPUT_CHAIN)
+
+    def _check_block(self, host, port):
+        connected = True
+        s = None
+        try:
+            s = socket.create_connection((host, port), 1)
+        except ConnectionRefusedError:
+            connected = False
+        finally:
+            if s is not None:
+                s.close()
+        return connected
+
+    def check_block_output(self, host):
+        return self._check_block(host, self._output_port)
+
+    def check_block_input(self, host):
+        return self._check_block(host, self._input_port)

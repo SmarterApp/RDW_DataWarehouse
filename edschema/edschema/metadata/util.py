12e213a4705da7c0deb29433326122c1d5575eff
Sadish Ravi <sadish.r@gmail.com>
2014-04-05 13:33:10 -0400

    meta columns and util methods to get meta and matcher columns
    Former-commit-id: 31891f58838dc10951210c847653ef214f697976

--- edschema/edschema/metadata/util.py
+++ edschema/edschema/metadata/util.py
@@ -6,13 +6,14 @@ Created on Mar 21, 2014
 
 #TODO: good place for it
 NATURAL_KEY_ATTR = 'natural_key'
+META_COLUMN = 'MetaColumn'
 
 
 def get_natural_key(table):
     '''
     Find the natural keys for the table by looking at all columns in the table with info attribute 'natural_key'
     '''
-    natural_key = [column for column in table.columns if 'natural_key' in column.info.keys()]
+    natural_key = [column for column in table.columns if NATURAL_KEY_ATTR in column.info.keys()]
     return natural_key if len(natural_key) > 0 else None
 
 
@@ -30,3 +31,31 @@ def get_foreign_key_reference_columns(table):
     '''
     columns = [column for column in table.columns if column.foreign_keys != set()]
     return columns if len(columns) > 0 else None
+
+
+def get_meta_columns(table):
+    '''
+    Find columns for the table that are of type MetaColumn
+    '''
+    columns = [column for column in table.columns if column.__class__.__name__ == META_COLUMN]
+    return columns if len(columns) > 0 else None
+
+
+def get_primary_key_columns(table):
+    '''
+    Find primary key columns for the table
+    '''
+    columns = [column for column in table.primary_key]
+    return columns if len(columns) > 0 else None
+
+
+def get_matcher_key_columns(table):
+    '''
+    Find columns to match records in the table to detect duplicates
+    return All_columns - (meta_columns + pk_columns)
+    '''
+    meta = get_meta_columns(table)
+    meta = meta if meta is not None else []
+    pk = get_primary_key_columns(table)
+    pk = pk if pk is not None else []
+    return list(set(table.columns) - set(meta + pk))
--- edschema/edschema/tests/test_util.py
+++ edschema/edschema/tests/test_util.py
@@ -7,7 +7,8 @@ import unittest
 from sqlalchemy.schema import MetaData
 from sqlalchemy import Table, Column, Index
 from sqlalchemy import String, BigInteger, ForeignKey
-from edschema.metadata.util import get_natural_key, get_natural_key_columns, get_foreign_key_reference_columns
+from edschema.metadata.util import *
+from edschema.metadata.ed_metadata import MetaColumn
 
 
 class TestMetadataUtil(unittest.TestCase):
@@ -15,7 +16,7 @@ class TestMetadataUtil(unittest.TestCase):
         self.__metadata = MetaData(schema='test_me', bind=False)
         test_table = Table('dim_inst_hier', self.__metadata,
                            Column('inst_hier_rec_id', BigInteger, primary_key=True),
-                           Column('batch_guid', String(50), nullable=True),
+                           MetaColumn('batch_guid', String(50), nullable=True),
                            Column('state_name', String(32), nullable=False, info={'natural_key': True}),
                            Column('state_code', String(2), nullable=False),
                            Column('district_guid', String(50), nullable=False, info={'natural_key': True}),
@@ -23,12 +24,17 @@ class TestMetadataUtil(unittest.TestCase):
                            Column('school_guid', String(50), nullable=False, info={'natural_key': True}),
                            Column('school_name', String(256), nullable=False),
                            Column('school_category', String(20), nullable=False),
+                           MetaColumn('from_date', String(8), nullable=False),
+                           MetaColumn('to_date', String(8), nullable=True),
+                           MetaColumn('rec_status', String(1), nullable=False)
                            )
         Index('dim_inst_hier_idx', test_table.c.inst_hier_rec_id, unique=True)
         Index('dim_inst_hier_codex', test_table.c.state_code, test_table.c.district_guid,
               test_table.c.school_guid, unique=False)
         self.__test_table = test_table
         self.__nkcol = [test_table.c.state_name, test_table.c.district_guid, test_table.c.school_guid]
+        self.__mkcol = [test_table.c.batch_guid, test_table.c.from_date, test_table.c.to_date, test_table.c.rec_status]
+        self.__pkcol = [test_table.c.inst_hier_rec_id]
 
     def test_get_natural_key(self):
         '''
@@ -99,3 +105,40 @@ class TestMetadataUtil(unittest.TestCase):
         self.assertTrue(len(f_cols) == 2)
         self.assertTrue(f_cols[0].name in ('enroll_inst_hier_rec_id', 'asmt_inst_hier_rec_id'))
         self.assertTrue(f_cols[1].name in ('enroll_inst_hier_rec_id', 'asmt_inst_hier_rec_id'))
+
+    def test_get_meta_columns(self):
+        '''
+        test getting meta key columns
+        '''
+        self.assertEquals(self.__mkcol, get_meta_columns(self.__test_table))
+
+    def test_get_meta_columns_when_none_defined(self):
+        '''
+        test getting meta key columns if none defined
+        '''
+        test_table_none = Table('dim_student', self.__metadata,
+                                Column('student_rec_id', BigInteger, primary_key=True),
+                                Column('batch_guid', String(50), nullable=True))
+        self.assertTrue(get_meta_columns(test_table_none) is None)
+
+    def test_get_primary_key_columns(self):
+        '''
+        test getting primary key columns
+        '''
+        self.assertEquals(self.__pkcol, get_primary_key_columns(self.__test_table))
+
+    def test_get_primary_key_columns_when_none_defined(self):
+        '''
+        test getting primary key columns if none defined
+        '''
+        test_table_none = Table('dim_student', self.__metadata,
+                                Column('student_rec_id', BigInteger),
+                                Column('batch_guid', String(50), nullable=True))
+        self.assertTrue(get_primary_key_columns(test_table_none) is None)
+
+    def test_get_matcher_key_columns(self):
+        '''
+        test getting primary key columns
+        '''
+        expected_columns = set(self.__test_table.columns) - set(self.__pkcol + self.__mkcol)
+        self.assertEquals(expected_columns, set(get_matcher_key_columns(self.__test_table)))

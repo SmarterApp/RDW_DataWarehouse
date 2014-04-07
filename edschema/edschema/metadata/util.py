171e9331784c87a4ca1c96ddde986df5def4de06
Sadish Ravi <sadish.r@gmail.com>
2014-04-07 10:22:07 -0400

    util method and tests to get matcher key column names for intelligent inserts
    Former-commit-id: a71f3f5d6b78d3f29aae60b9844454e3a9a48754

--- edschema/edschema/metadata/util.py
+++ edschema/edschema/metadata/util.py
@@ -12,6 +12,8 @@ META_COLUMN = 'MetaColumn'
 def get_natural_key(table):
     '''
     Find the natural keys for the table by looking at all columns in the table with info attribute 'natural_key'
+
+    :param table: SQLAlchemy table object
     '''
     natural_key = [column for column in table.columns if NATURAL_KEY_ATTR in column.info.keys()]
     return natural_key if len(natural_key) > 0 else None
@@ -20,6 +22,8 @@ def get_natural_key(table):
 def get_natural_key_columns(table):
     '''
     Find columns for the table marked with a info attribute 'natural_key'
+
+    :param table: SQLAlchemy table object
     '''
     natural_key = get_natural_key(table)
     return None if natural_key is None else [c.name for c in natural_key]
@@ -28,6 +32,8 @@ def get_natural_key_columns(table):
 def get_foreign_key_reference_columns(table):
     '''
     Find columns for the table that reference other tables
+
+    :param table: SQLAlchemy table object
     '''
     columns = [column for column in table.columns if column.foreign_keys != set()]
     return columns if len(columns) > 0 else None
@@ -36,6 +42,8 @@ def get_foreign_key_reference_columns(table):
 def get_meta_columns(table):
     '''
     Find columns for the table that are of type MetaColumn
+
+    :param table: SQLAlchemy table object
     '''
     columns = [column for column in table.columns if column.__class__.__name__ == META_COLUMN]
     return columns if len(columns) > 0 else None
@@ -44,6 +52,8 @@ def get_meta_columns(table):
 def get_primary_key_columns(table):
     '''
     Find primary key columns for the table
+
+    :param table: SQLAlchemy table object
     '''
     columns = [column for column in table.primary_key]
     return columns if len(columns) > 0 else None
@@ -52,10 +62,23 @@ def get_primary_key_columns(table):
 def get_matcher_key_columns(table):
     '''
     Find columns to match records in the table to detect duplicates
+
     return All_columns - (meta_columns + pk_columns)
+
+    :param table: SQLAlchemy table object
     '''
     meta = get_meta_columns(table)
     meta = meta if meta is not None else []
     pk = get_primary_key_columns(table)
     pk = pk if pk is not None else []
     return list(set(table.columns) - set(meta + pk))
+
+
+def get_matcher_key_column_names(table):
+    '''
+    Returns matcher key columns names as a list
+
+    :param table: SQLAlchemy table object
+    '''
+    matcher_columns = get_matcher_key_columns(table)
+    return None if matcher_columns is None else [c.name for c in matcher_columns]

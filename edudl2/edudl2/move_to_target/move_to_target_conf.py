34f1b7944be6a72c955b6a147bcdd5d07d3f5137
Nik Estep <nestep@amplify.com>
2014-04-08 10:16:16 -0400

    Some cleanup of move_to_target_conf
    Former-commit-id: e3d49215763de2eecc0fbf75b646d830f9de48d3

--- edudl2/edudl2/move_to_target/move_to_target_conf.py
+++ edudl2/edudl2/move_to_target/move_to_target_conf.py
@@ -16,24 +16,9 @@ def get_move_to_target_conf():
             'guid_column_name': 'asmt_guid',
             'guid_column_in_source': 'guid_asmt'
         },
-        'update_inst_hier_rec_id_fk': {
-            'rec_id_map': ('inst_hier_rec_id', 'inst_hier_rec_id'),
-            'table_map': ('dim_inst_hier', 'fact_asmt_outcome'),
-            'guid_column_map': {
-                'state_code': 'state_code',
-                'district_guid': 'district_guid',
-                'school_guid': 'school_guid'
-            },
-        },
         'section_rec_id_info': {
             'rec_id': 'section_rec_id',
             'value': '1'
-        },
-        'update_student_rec_id_fk': {
-            'rec_id_map': ('student_rec_id', 'student_rec_id'),
-            'table_map': ('dim_student', 'fact_asmt_outcome'),
-            'guid_column_map': {
-                'student_guid': 'student_guid'},
         },
         'handle_deletions': {
             'prod_table': 'fact_asmt_outcome',

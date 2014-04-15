feca2186599c730ffea9e09dcef3bf76826ac087
Bhumi Patel <bpatel@amplify.com>
2014-04-15 16:10:20 -0400

    New test for delete/update with fact_asmt_primary
    Former-commit-id: 39a34b85fb36c22379373749ec16c99ca6702ee3

--- edudl2/edudl2/tests/e2e_tests/test_benchmarking_test.py
+++ edudl2/edudl2/tests/e2e_tests/test_benchmarking_test.py
@@ -20,23 +20,24 @@ class ValidateTableData(unittest.TestCase):
         data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
         self.tenant_dir = '/opt/edware/zones/landing/arrivals/test_tenant/test_user/filedrop/'
         self.archived_file = os.path.join(data_dir, 'test_source_file_tar_gzipped.tar.gz.gpg')
-        self.connector = get_udl_connection()
+        #self.connector = get_udl_connection()
         self.guid_batch_id = str(uuid4())
 
     def tearDown(self):
         if os.path.exists(self.tenant_dir):
             shutil.rmtree(self.tenant_dir)
-        self.connector.close_connection()
+        #self.connector.close_connection()
         drop_target_schema(self.guid_batch_id)
 
-    def empty_batch_table(self, connector):
-        batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
-        result = connector.execute(batch_table.delete())
-        query = select([batch_table])
-        result1 = connector.execute(query).fetchall()
-        number_of_row = len(result1)
-        print(number_of_row)
-        self.assertEqual(number_of_row, 0)
+    def empty_batch_table(self):
+        with get_udl_connection() as connector:
+            batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
+            result = connector.execute(batch_table.delete())
+            query = select([batch_table])
+            result1 = connector.execute(query).fetchall()
+            number_of_row = len(result1)
+            print(number_of_row)
+            self.assertEqual(number_of_row, 0)
 
     def run_udl_pipeline(self):
         self.conf = udl2_conf
@@ -46,35 +47,36 @@ class ValidateTableData(unittest.TestCase):
         command = "python {driver_path} -a {file_name} -g {guid}".format(driver_path=driver_path, file_name=arch_file, guid=self.guid_batch_id)
         print(command)
         subprocess.call(command, shell=True)
-        self.check_job_completion(self.connector)
+        self.check_job_completion()
 
-    def check_job_completion(self, connector, max_wait=30):
-        batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
-        query = select([batch_table.c.udl_phase], batch_table.c.udl_phase == 'UDL_COMPLETE')
-        timer = 0
-        result = connector.execute(query).fetchall()
-        while timer < max_wait and result == []:
-            sleep(0.25)
-            timer += 0.25
+    def check_job_completion(self, max_wait=30):
+        with get_udl_connection() as connector:
+            batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
+            query = select([batch_table.c.udl_phase], batch_table.c.udl_phase == 'UDL_COMPLETE')
+            timer = 0
             result = connector.execute(query).fetchall()
-        print('Waited for', timer, 'second(s) for job to complete.')
+            while timer < max_wait and result == []:
+                sleep(0.25)
+                timer += 0.25
+                result = connector.execute(query).fetchall()
+            print('Waited for', timer, 'second(s) for job to complete.')
 
-    def connect_verify_db(self, connector):
-        batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
-        query = select([batch_table])
-        result = connector.execute(query).fetchall()
-        number_of_row = len(result)
-        self.assertEqual(number_of_row, 33)
-
-        output = select([batch_table.c.udl_phase_step_status]).where(batch_table.c.udl_phase == 'UDL_COMPLETE')
-        output_data = connector.execute(output).fetchall()
-        tuple_str = [('SUCCESS',)]
-        self.assertEqual(tuple_str, output_data)
+    def connect_verify_db(self):
+        with get_udl_connection() as connector:
+            batch_table = connector.get_table(udl2_conf['udl2_db']['batch_table'])
+            query = select([batch_table])
+            result = connector.execute(query).fetchall()
+            number_of_row = len(result)
+            self.assertEqual(number_of_row, 33)
+            output = select([batch_table.c.udl_phase_step_status]).where(batch_table.c.udl_phase == 'UDL_COMPLETE')
+            output_data = connector.execute(output).fetchall()
+            tuple_str = [('SUCCESS',)]
+            self.assertEqual(tuple_str, output_data)
 
     def test_benchmarking_data(self):
-        self.empty_batch_table(self.connector)
+        self.empty_batch_table()
         self.run_udl_pipeline()
-        self.connect_verify_db(self.connector)
+        self.connect_verify_db()
 
     def copy_file_to_tmp(self):
         if os.path.exists(self.tenant_dir):

46ee3944da9fab0cc5b73f2c8171bfe7c6898aa0
Thomas Shewchuk <tshewchuk@wgen.net>
2014-04-16 10:58:05 -0400

    US24258: Fourth (and  finali final) crack at creating assessment query to return distinct results per student/assmt_type/assmt_subj.
    Former-commit-id: 6f362c53ccfd0a29ae539e4630547ff784d8fbd1

--- smarter/smarter/extracts/student_reg_completion.py
+++ smarter/smarter/extracts/student_reg_completion.py
@@ -43,8 +43,7 @@ def get_assessment_query(academic_year, state_code):
                                       student_reg.c.dmg_prg_lep, student_reg.c.dmg_prg_504, student_reg.c.dmg_sts_ecd,
                                       student_reg.c.dmg_sts_mig, student_reg.c.dmg_multi_race, student_reg.c.academic_year,
                                       assmt_query.c.asmt_subject, assmt_query.c.asmt_type],
-                                     from_obj=[student_reg.join(assmt_query, and_(student_reg.c.academic_year == assmt_query.c.asmt_year,
-                                                                                  student_reg.c.student_guid == assmt_query.c.student_guid))])\
+                                     from_obj=[student_reg.join(assmt_query, student_reg.c.student_guid == assmt_query.c.student_guid)])\
             .where(student_reg.c.academic_year == academic_year)
 
     return academic_year_query

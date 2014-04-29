26a15f44afb72afb94dcf937eb41f4f3f633040e
Sadish Ravi <sadish.r@gmail.com>
2014-04-29 11:43:21 -0400

    removing unwanted parms from filesync methods
    Former-commit-id: f8a2987d046cce56a0787110f025b3b10982d8be

--- edsftp/edsftp/scripts/watcher.py
+++ edsftp/edsftp/scripts/watcher.py
@@ -35,38 +35,41 @@ def set_interval(interval):
 
 class FileSync:
 
-    def __init__(self):
+    def __init__(self, source_dir, dest_dir, pattern):
         globals()[self.__class__.__name__] = self
-        self.file_stats = {}
+        self._file_stats = {}
+        self._source_dir = source_dir
+        self._dest_dir = dest_dir
+        self._pattern = pattern
 
     def __call__(self):
         return self
-
-    def clear_file_stats(self):
-        self.file_stats.clear()
-
-    def find_all_files(self, directory, extension):
-        for root, dirs, files in os.walk(directory):
-            for filename in fnmatch.filter(files, extension):
-                file_path = os.path.join(root, filename)
-                self.file_stats[file_path] = FileSync.get_file_stat(file_path)
 
     @staticmethod
     def get_file_stat(filename):
         return os.stat(filename).st_size
 
+    def clear_file_stats(self):
+        self._file_stats.clear()
+
+    def find_all_files(self):
+        for root, dirs, files in os.walk(self._source_dir):
+            for filename in fnmatch.filter(files, self._pattern):
+                file_path = os.path.join(root, filename)
+                self._file_stats[file_path] = FileSync.get_file_stat(file_path)
+
     def get_file_stats(self):
-        return {filename: FileSync.get_file_stat(filename) for filename in self.file_stats.keys()}
+        return {filename: FileSync.get_file_stat(filename) for filename in self._file_stats.keys()}
 
     @set_interval(interval=FILE_STAT_WATCH_INTERVAL_IN_SECONDS)
     def watch_and_filter_files_by_stats_changes(self):
         file_stats_latest = self.get_file_stats()
-        for file, size in self.file_stats.copy().items():
+        for file, size in self._file_stats.copy().items():
             if file_stats_latest[file] != size:
                 print('Removing file due to size changes during monitoring: ', file)
-                del self.file_stats[file]
+                del self._file_stats[file]
 
-    def filter_files_by_stat(self):
+    def watch_files(self):
         # monitor the files for change in stats
         stop = self.watch_and_filter_files_by_stats_changes()
         # monitor for a duration
@@ -74,31 +77,30 @@ class FileSync:
         # stop the timer
         stop.set()
 
-    @staticmethod
-    def move_files(files, source_base_dir, dest_base_dir):
-        for file in files:
-            file_relative_path = os.path.relpath(file, source_base_dir)
-            destination_file_path = os.path.join(dest_base_dir, file_relative_path)
+    def move_files(self):
+        files_to_move = self._file_stats.keys()
+        for file in files_to_move:
+            destination_file_path = os.path.join(self._dest_dir, os.path.relpath(file, self._source_dir))
             destination_file_directory = os.path.split(destination_file_path)[0]
             if not os.path.exists(destination_file_directory):
                 os.makedirs(destination_file_directory)
             print('Moving file ', file, 'to ', destination_file_path)
             shutil.move(file, destination_file_path)
-        return len(files)
+        return len(files_to_move)
 
-    def find_and_move_files(self, source_directory, dest_directory, extension):
+    def find_and_move_files(self):
         self.clear_file_stats()
-        self.find_all_files(source_directory, extension)
-        self.filter_files_by_stat()
-        files_moved = FileSync.move_files(self.file_stats.keys(), source_directory, dest_directory)
+        self.find_all_files()
+        self.watch_files()
+        files_moved = self.move_files()
         return files_moved
 
 
 def sftp_file_sync():
-    finder = FileSync()
+    finder = FileSync(SOURCE_DIR, DEST_DIR, PATTERN)
     while True:
         print('Searching for new files')
-        files_moved = finder.find_and_move_files(SOURCE_DIR, DEST_DIR, PATTERN)
+        files_moved = finder.find_and_move_files()
         print('Files Moved: ' + str(files_moved))
         time.sleep(FILE_SYSTEM_SCAN_DELAY_IN_SECONDS)
 

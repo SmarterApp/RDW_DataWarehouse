09ca208709d3056cd9c2e168d497d461b22feeeb
Eugene Jen <ejen@wgen.net>
2014-05-01 15:55:05 -0400

    add pgpool_failover.sh into repo
    Former-commit-id: 97433e51e08edd5a374fa44fa3f0e0b1857b9ecf

---
+++ scripts/pgpool_failover.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+node_id=$1
+host_name=$2
+port=$3
+pcp_user=postgres
+pcp_pass=postgres
+
+COMMAND_TEST="nc -w 1 $host_name $port"
+COMMAND_ATTACH="/usr/bin/pcp_attach_node 100 localhost 9898 postgres postgres $node_id"
+
+wait_for_host_to_recover() {
+    eval $COMMAND_TEST
+    while [ $? -ne 0 ]; do
+       eval $COMMAND_TEST
+    done
+    eval $COMMAND_ATTACH
+}
+
+wait_for_host_to_recover &
+~                          

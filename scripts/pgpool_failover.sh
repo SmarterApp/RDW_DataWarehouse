deb2797ab358ffadb1bebef5cc92c9bcc673e2d0
Eugene Jen <ejen@wgen.net>
2014-05-12 12:49:33 -0400

    1. add steps to detect pgpool server existences. so stop the failover process if pgpool is not running in order not locking up port for pgpool for its restart 2. add steps to clean up /var/log/pgpool-II/pgpool_status file, So edmigrate's blocking status will not be persistent when it is restarted. And this will trigger proper failover execution again. 3. Add more variables to make the script easier to be configured in production without worrying breaking code
    Former-commit-id: 3de8a35c7b0fc69912100b6c1238db49e497dd95

--- scripts/pgpool_failover.sh
+++ scripts/pgpool_failover.sh
@@ -3,15 +3,29 @@
 node_id=$1
 host_name=$2
 port=$3
-pcp_user=postgres
-pcp_pass=postgres
+pcp_user=postgres  # replace this with your pcp.conf user name if necessary
+pcp_pass=postgres  # replace this with your pcp.conf password if necessary
+pcp_host=localhost
+pcp_port=9898     # replace this with your pcp.conf pgpool.conf pcp port if necessary
+attach_timeout=100
+pgpool_status_file=/var/log/pgpool-II/pgpool_status
 
+# command to check pgpool parent is running or not
+COMMAND_PS="ps -C pgpool|wc -l"
+# command to check failover host name
 COMMAND_TEST="nc -w 1 $host_name $port"
-COMMAND_ATTACH="/usr/bin/pcp_attach_node 100 localhost 9898 postgres postgres $node_id"
+# command to reattach node to pgpool. change port number and user name, password
+COMMAND_ATTACH="/usr/bin/pcp_attach_node $attach_timeout localhost $pcp_port $pcp_user $pcp_pass $node_id"
 
 wait_for_host_to_recover() {
     eval $COMMAND_TEST
     while [ $? -ne 0 ]; do
+       PARENT=`eval $COMMAND_PS`
+       if [ $PARENT -eq "1" ]; then
+
+          rm $pgpool_status_file
+          exit
+       fi
        eval $COMMAND_TEST
     done
     eval $COMMAND_ATTACH

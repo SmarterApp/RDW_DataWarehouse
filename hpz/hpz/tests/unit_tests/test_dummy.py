3d733c3ed1904b1dba8064bb75c9c4a513187996
npandey <npandey@amplify.com>
2014-05-16 11:53:21 -0400

    US24683: Adding a dummy UT
    Former-commit-id: d1b7802df22e682b1cdc2cb48eee2416e183bdf1

---
+++ hpz/hpz/tests/unit_tests/test_dummy.py
@@ -0,0 +1,10 @@
+import unittest
+
+__author__ = 'npandey'
+
+
+#Dummy test for testing Jenkins Setup. Test should be removed during development
+class DummyTest(unittest.TestCase):
+
+    def test_nothing(self):
+        self.assertTrue(True)

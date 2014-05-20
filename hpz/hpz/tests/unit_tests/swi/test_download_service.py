6f9e6009cf788e839ac3520a8d2fe2dbf8855637
npandey <npandey@amplify.com>
2014-05-20 15:02:12 -0400

    US24661: Adding UT
    Former-commit-id: 735cdfa432092ee4f8b11f0e6854bb20e80ecd6b

---
+++ hpz/hpz/tests/unit_tests/swi/test_download_service.py
@@ -0,0 +1,41 @@
+import unittest
+from pyramid.testing import DummyRequest
+from pyramid import testing
+from hpz.swi.download_service import download_file
+from pyramid.registry import Registry
+import json
+from unittest.mock import patch
+
+__author__ = 'npandey'
+
+
+class RegistrationTest(unittest.TestCase):
+
+    def setUp(self):
+        self.__request = DummyRequest()
+        reg = Registry()
+        reg.settings = {}
+        self.__config = testing.setUp(registry=reg, request=self.__request, hook_zca=False)
+        #self.__config.add_route('download', '/{reg_id}')
+
+    def tearDown(self):
+        self.__request = None
+
+    @patch('hpz.frs.registration_service.FileRegistry.get_file_path')
+    def test_download_file(self, path_patch):
+        dummy_file_path = 'tmp/filename.zip'
+        path_patch.return_value = dummy_file_path
+
+        self.__request.method = 'GET'
+        self.__request.matchdict['reg_id'] = '1234'
+
+        response = download_file(None, self.__request)
+
+        self.assertEqual(response.status_code, 200)
+
+        headers = response.headers
+
+        self.assertEqual(len(headers), 3)
+        self.assertEqual(headers['X-Sendfile'], dummy_file_path)
+        self.assertEqual(headers['Content-Type'], '')
+        self.assertEqual(headers['Content-Disposition'], 'attachment; filename=filename.zip')

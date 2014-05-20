51e383a74ab1f39a09a6ef3e86265a0a1e4d2abf
ablum <ablum@wgen.net>
2014-05-20 13:52:52 -0400

    US24661 Adding method to update the database
    Former-commit-id: c3074f7aab4f206070805ae51c4b57dbbc0733bd

---
+++ hpz/hpz/database/constants.py
@@ -0,0 +1,8 @@
+__author__ = 'ablum'
+
+
+class DatabaseConstants:
+    HPZ_TABLE = 'file_registry'
+    FILE_PATH = 'file_path'
+    CREATION_DATE = 'creation_date'
+    UUID = 'uuid'
--- hpz/hpz/database/file_registry.py
+++ hpz/hpz/database/file_registry.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from uuid import uuid4
+from hpz.database.constants import DatabaseConstants
 from hpz.database.hpz_connector import get_hpz_connection
 
 __author__ = 'okrook'
@@ -9,14 +11,19 @@ class FileRegistry:
     @staticmethod
     def register_request():
         registration_id = uuid4()
-        registration_info = {'uuid': str(registration_id)}
+        registration_info = {DatabaseConstants.UUID: str(registration_id)}
 
         with get_hpz_connection() as conn:
-            file_reg_table = conn.get_table(table_name='file_registry')
+            file_reg_table = conn.get_table(table_name=DatabaseConstants.HPZ_TABLE)
             conn.execute(file_reg_table.insert().values(registration_info))
 
         return registration_id
 
     @staticmethod
     def register_file(registration_id, file_path):
-        pass
+        registration_info = {DatabaseConstants.FILE_PATH: file_path,
+                             DatabaseConstants.CREATION_DATE: datetime.date()}
+
+        with get_hpz_connection() as conn:
+            file_reg_table = conn.get_table(table_name=DatabaseConstants.HPZ_TABLE)
+            conn.execute(file_reg_table.update().where(file_reg_table.c.uuid == registration_id).values(registration_info))

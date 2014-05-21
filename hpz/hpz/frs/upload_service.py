2a7cafd805789a30a2204ae4733f486857af4f56
Thomas Shewchuk <tshewchuk@wgen.net>
2014-05-21 12:53:55 -0400

    US24661: Changed upload service to use file object copy.
    Former-commit-id: 2fa53be72aa4ffd19385e66bb2c563e2b71632d8

--- hpz/hpz/frs/upload_service.py
+++ hpz/hpz/frs/upload_service.py
@@ -5,9 +5,12 @@ This module describes the file upload endpoint for HPZ.
 __author__ = 'ablum,'
 __author__ = 'tshewchuk'
 
+import os
+import shutil
+
 from pyramid.response import Response
 from pyramid.view import view_config
-import os
+
 from hpz.database.file_registry import FileRegistry
 
 
@@ -18,21 +21,12 @@ def file_upload_service(context, request):
     file_name = request.headers['Filename']
     base_upload_path = request.registry.settings['hpz.frs.upload_base_path']
 
-    base_directory = os.path.join(base_upload_path, registration_id)
-    if not os.path.exists(base_directory):
-        os.makedirs(base_directory)
+    file_pathname = os.path.join(base_upload_path, registration_id + '__' + file_name)
 
-    file_pathname = os.path.join(base_upload_path, registration_id, file_name)
+    input_file = request.POST['file'].file
 
-    for item, f in request.POST.items():
-        with open(file_pathname, mode='wb',) as new_file:
-            input_file = f.file
-            input_file.seek(0)
-            while True:
-                data = input_file.read(2 << 16)
-                if not data:
-                    break
-                new_file.write(data)
+    with open(file_pathname, mode='wb') as output_file:
+        shutil.copyfileobj(input_file, output_file)
 
     FileRegistry.file_upload_request(registration_id, file_pathname)
 

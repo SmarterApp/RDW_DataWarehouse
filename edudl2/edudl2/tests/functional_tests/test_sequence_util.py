13c3756e8913fa691dc2d109255f2ea1a97188b1
Sadish Ravi <sadish.r@gmail.com>
2014-05-24 14:00:31 -0400

    fixing unit tests for sequence util
    Former-commit-id: d8c60628b66a7107b32e023fb7e507bb052aae04

--- edudl2/edudl2/tests/functional_tests/test_sequence_util.py
+++ edudl2/edudl2/tests/functional_tests/test_sequence_util.py
@@ -1,7 +1,7 @@
 import unittest
 import uuid
 from sqlalchemy import Sequence
-from sqlalchemy.schema import DropSequence
+from sqlalchemy.schema import DropSequence, CreateSequence
 from edudl2.udl2_util.sequence_util import UDLSequence, get_global_sequence
 from edudl2.database.udl2_connector import get_prod_connection, initialize_db_prod
 from edudl2.tests.functional_tests.util import UDLTestHelper
@@ -12,6 +12,9 @@ class TestSequenceUtil(UDLTestHelper):
     def setUp(self):
         self.test_tenant = "cat"
         self.sequence_guid = str(uuid.uuid4())
+        with get_prod_connection(self.test_tenant) as conn:
+            seq = Sequence(name=self.sequence_guid, metadata=conn.get_metadata(), start=20000)
+            conn.execute(CreateSequence(seq))
 
     @classmethod
     def setUpClass(cls):
@@ -19,27 +22,18 @@ class TestSequenceUtil(UDLTestHelper):
         initialize_db_prod(cls.udl2_conf)
 
     def tearDown(self,):
-        #drop temporary sequence
-        try:
-            with get_prod_connection(self.test_tenant) as conn:
-                seq = Sequence(name=self.sequence_guid, metadata=conn.get_metadata())
-                conn.execute(DropSequence(seq))
-        except:
-            pass
-
-    def test_next_id(self):
-        seq = UDLSequence(tenant_name=self.test_tenant, seq_name=self.sequence_guid)
-        for i in range(0, 2000):
-            self.assertEqual(seq.next(), 20000 + i, "Should return next sequence id")
+        with get_prod_connection(self.test_tenant) as conn:
+            seq = Sequence(name=self.sequence_guid, metadata=conn.get_metadata())
+            conn.execute(DropSequence(seq))
 
     def test_fetch_next_batch(self):
         seq = UDLSequence(tenant_name=self.test_tenant, seq_name=self.sequence_guid)
-        seq.fetch_next_batch()
-        self.assertEqual(seq.current, 20000, "next batch should start from 20100")
-        self.assertEqual(seq.max_value, 20099, "next batch should end with 20200")
-        seq.fetch_next_batch()
+        seq.fetch_next_batch(batch_size=100)
+        self.assertEqual(seq.current, 20000, "next batch should start from 20000")
+        seq.fetch_next_batch(batch_size=200)
         self.assertEqual(seq.current, 20100, "next batch should start from 20100")
-        self.assertEqual(seq.max_value, 20199, "next batch should end with 20200")
+        seq.fetch_next_batch(batch_size=300)
+        self.assertEqual(seq.current, 20300, "next batch should start from 20300")
 
     def test_sequence_existence(self):
         seq = UDLSequence(tenant_name=self.test_tenant, seq_name=self.sequence_guid)

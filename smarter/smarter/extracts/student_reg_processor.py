59e5701ac7e22430bd24ef71bceb8bf170a0b19b
Thomas Shewchuk <tshewchuk@wgen.net>
2014-05-25 06:47:01 -0400

    US24734: HPZ reg code repositioned for future file upload.
    Former-commit-id: 6e2774233031df219449b997e5b85c22d413f5ba

--- smarter/smarter/extracts/student_reg_processor.py
+++ smarter/smarter/extracts/student_reg_processor.py
@@ -67,11 +67,11 @@ def process_async_extraction_request(params):
     gatekeeper_id = processor.get_gatekeeper(tenant)
     pickup_zone_info = processor.get_pickup_zone_info(tenant)
 
-    start_extract.apply_async(args=[tenant, request_id, public_key_id, encrypted_file_path, data_directory_to_archive, gatekeeper_id, pickup_zone_info, [task_info]], queue=queue)
-
     # Register extract file with HPZ.
-    registration_id, download_url = register_file(user.get_uid())  # TODO: Implement storage and later use of registration_id.
+    registration_id, download_url = register_file(user.get_uid())  # TODO: Pass registration_id to start_extract for file upload to HPZ.
     response['download_url'] = download_url
+
+    start_extract.apply_async(args=[tenant, request_id, public_key_id, encrypted_file_path, data_directory_to_archive, gatekeeper_id, pickup_zone_info, [task_info]], queue=queue)
 
     return response
 

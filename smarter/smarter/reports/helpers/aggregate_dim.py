ef795e126ec0dc772fe0623047dfa82a8fa9109c
Takashi Osako <tosako@amplify.com>
2014-05-27 17:43:09 -0400

    US24782: make get_aggregate_dim cacheable
    Former-commit-id: 001004ea8b3d9878c8906a0c64c9dddcd6d6139e

--- smarter/smarter/reports/helpers/aggregate_dim.py
+++ smarter/smarter/reports/helpers/aggregate_dim.py
@@ -8,10 +8,32 @@ from smarter.reports.helpers.constants import Constants
 from edcore.database.edcore_connector import EdCoreDBConnection
 from edextract.student_reg_extract_processors.assessment_constants import AssessmentType
 from sqlalchemy.sql.expression import distinct
+from edapi.cache import cache_region
 from copy import deepcopy
 
 
-def get_aggregate_dim(subjects={}, stateCode=None, districtGuid=None, schoolGuid=None, asmtType=AssessmentType.SUMMATIVE, asmtYear=None, tenant=None):
+def get_aggregate_dim(stateCode=None, districtGuid=None, schoolGuid=None, asmtType=AssessmentType.SUMMATIVE, asmtYear=None, tenant=None, subjects={}):
+    records = {}
+    for subject_key in subjects.keys():
+        subject = subjects[subject_key]
+        rows = _get_aggregate_dim(stateCode, districtGuid, schoolGuid, asmtType, asmtYear, tenant, subject_key, subject)
+        for key in rows.keys():
+            record = records.get(key)
+            if record is None:
+                records[key] = deepcopy(rows[key])
+            else:
+                record[Constants.RESULTS][subject_key] = deepcopy(rows[key][Constants.RESULTS][subject_key])
+    sorted_records = sorted(list(records.values()), key=lambda k: k['name'])
+    for subject_key in subjects.keys():
+        for sorted_record in sorted_records:
+            results = sorted_record[Constants.RESULTS]
+            if results.get(subject_key) is None:
+                results[subject_key] = {Constants.ASMT_SUBJECT: subjects[subject_key], Constants.TOTAL: 0, Constants.HASINTERIM: False}
+    return {Constants.RECORDS: sorted_records}
+
+
+@cache_region('public.shortlived')
+def _get_aggregate_dim(stateCode=None, districtGuid=None, schoolGuid=None, asmtType=AssessmentType.SUMMATIVE, asmtYear=None, tenant=None, subject_key=None, subject=None):
     '''
     Query for institution or grades that have asmts for the year provided
     :param string stateCode
@@ -24,43 +46,37 @@ def get_aggregate_dim(subjects={}, stateCode=None, districtGuid=None, schoolGuid
     :returns: set of records with district guids
     '''
     rows = {}
-    for key in subjects.keys():
-        subject = subjects[key]
-        with EdCoreDBConnection(tenant=tenant, state_code=stateCode) as connector:
-            # query custom metadata by state code
-            dim_inst_hier = connector.get_table(Constants.DIM_INST_HIER)
-            fact_asmt_outcome = connector.get_table(Constants.FACT_ASMT_OUTCOME_VW)
-            s = exists(['*'], from_obj=[dim_inst_hier]).where(and_(fact_asmt_outcome.c.asmt_year == asmtYear, fact_asmt_outcome.c.state_code == stateCode, fact_asmt_outcome.c.rec_status == 'C',
-                                                                   fact_asmt_outcome.c.asmt_type == asmtType, fact_asmt_outcome.c.inst_hier_rec_id == dim_inst_hier.c.inst_hier_rec_id,
-                                                                   fact_asmt_outcome.c.asmt_subject == subject))
-            if districtGuid is None and schoolGuid is None:
-                query = get_select_for_state_view(dim_inst_hier, stateCode).where(s)
-            elif districtGuid is not None and schoolGuid is not None:
-                query = get_select_for_school_view(fact_asmt_outcome, stateCode, districtGuid, schoolGuid, asmtYear, asmtType, subject)
-            else:
-                query = get_select_for_district_view(dim_inst_hier, stateCode, districtGuid).where(s)
-            results = connector.get_result(query)
-            for result in results:
-                params = {Constants.ID: result.get(Constants.ID), Constants.STATECODE: stateCode}
-                if districtGuid is not None:
-                    params[Constants.DISTRICTGUID] = districtGuid
-                if schoolGuid is not None:
-                    params[Constants.SCHOOLGUID] = schoolGuid
-                data = {Constants.ID: result.get(Constants.ID),
-                        Constants.ROWID: result.get(Constants.ID),
-                        Constants.NAME: result.get(Constants.NAME),
-                        Constants.PARAMS: params} if rows.get(result.get(Constants.ID)) is None else rows.get(result.get(Constants.ID))
-                results = data.get(Constants.RESULTS, {})
-                results[key] = {Constants.ASMT_SUBJECT: subject, Constants.TOTAL: -1, Constants.HASINTERIM: True}
-                data[Constants.RESULTS] = results
-                rows[data[Constants.ID]] = data
-    records = sorted(list(rows.values()), key=lambda k: k['name'])
-    for record in records:
-        results = record[Constants.RESULTS]
-        for key in subjects.keys():
-            if results.get(key) is None:
-                results[key] = {Constants.ASMT_SUBJECT: subject, Constants.TOTAL: 0, Constants.HASINTERIM: False}
-    return {Constants.RECORDS: records}
+    # for key in subjects.keys():
+    #    subject = subjects[key]
+    with EdCoreDBConnection(tenant=tenant, state_code=stateCode) as connector:
+        # query custom metadata by state code
+        dim_inst_hier = connector.get_table(Constants.DIM_INST_HIER)
+        fact_asmt_outcome = connector.get_table(Constants.FACT_ASMT_OUTCOME_VW)
+        s = exists(['*'], from_obj=[dim_inst_hier]).where(and_(fact_asmt_outcome.c.asmt_year == asmtYear, fact_asmt_outcome.c.state_code == stateCode, fact_asmt_outcome.c.rec_status == 'C',
+                                                               fact_asmt_outcome.c.asmt_type == asmtType, fact_asmt_outcome.c.inst_hier_rec_id == dim_inst_hier.c.inst_hier_rec_id,
+                                                               fact_asmt_outcome.c.asmt_subject == subject))
+        if districtGuid is None and schoolGuid is None:
+            query = get_select_for_state_view(dim_inst_hier, stateCode).where(s)
+        elif districtGuid is not None and schoolGuid is not None:
+            query = get_select_for_school_view(fact_asmt_outcome, stateCode, districtGuid, schoolGuid, asmtYear, asmtType, subject)
+        else:
+            query = get_select_for_district_view(dim_inst_hier, stateCode, districtGuid).where(s)
+        results = connector.get_result(query)
+        for result in results:
+            params = {Constants.ID: result.get(Constants.ID), Constants.STATECODE: stateCode}
+            if districtGuid is not None:
+                params[Constants.DISTRICTGUID] = districtGuid
+            if schoolGuid is not None:
+                params[Constants.SCHOOLGUID] = schoolGuid
+            data = {Constants.ID: result.get(Constants.ID),
+                    Constants.ROWID: result.get(Constants.ID),
+                    Constants.NAME: result.get(Constants.NAME),
+                    Constants.PARAMS: params,
+                    Constants.RESULTS: {}
+                    }
+            data[Constants.RESULTS][subject_key] = {Constants.ASMT_SUBJECT: subject, Constants.TOTAL:-1, Constants.HASINTERIM: True}
+            rows[data[Constants.ID]] = data
+    return rows
 
 
 def get_select_for_state_view(dim_inst_hier, state_code):

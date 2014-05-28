9ca30f322f378fd053ad5cde2e31f9313b8e9cf5
Bhumi Patel <bpatel@amplify.com>
2014-05-28 14:04:47 -0400

    update in tar script
    Former-commit-id: ac3bfe1f5c937ec94ea6e42dcd15c6b3cb38a324

--- edudl2/scripts/qa/tar_script.sh
+++ edudl2/scripts/qa/tar_script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd <dir path to files to be tar and gpg>
+cd /Users/bpatel/Documents/sds_19_data_extracts/gman_05-27-2014_15-43-16/
 mkdir tar_dir
 cp *.json tar_dir
 cp *.csv tar_dir
@@ -28,7 +28,27 @@ do
 		fi
 	done
 done
+for filename in `ls *.json`
+do
+    echo ${filename#*SUMMATIVE_}
+    substr=${filename#*SUMMATIVE_}
+    final_substr=${substr%.*}
+    echo $final_substr
+    for csv_file in `ls *.csv`
+    do
+        if [[ $csv_file =~ $final_substr ]];then echo $csv_file;
+            csv=${csv_file%.*}
+            echo $csv
+            tar -cvzf $csv.tar.gz --disable-copyfile $csv_file $filename
+            echo sbac udl2 | gpg --armor --local-user ca_user@ca.com --recipient sbac_data_provider@sbac.com --encrypt --passphrase-fd 0 --sign $csv.tar.gz
+            mv $csv.tar.gz.asc $csv.tar.gz.gpg
+        fi
+    done
+done
+mkdir gz_dir
+mkdir gpg_dir
+cp *.gz gz_dir
+cp *.gpg gpg_dir
 
 
-	
-	
+

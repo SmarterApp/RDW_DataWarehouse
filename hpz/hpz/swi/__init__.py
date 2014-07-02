944ae29991e09edd69cf477bfe24565d283956a4
Takashi Osako <tosako@amplify.com>
2014-07-02 15:22:54 -0400

    US26160: graceful 404 page
    Former-commit-id: 0b32e830fc72c676c9b4b46500d12fa0bc75982f

--- hpz/hpz/swi/__init__.py
+++ hpz/hpz/swi/__init__.py
@@ -1,3 +1,8 @@
+from pyramid.view import notfound_view_config, view_config
+from pyramid.httpexceptions import HTTPNotFound, HTTPMovedPermanently
+from pyramid.response import Response
+import os
+from pyramid.security import NO_PERMISSION_REQUIRED
 __author__ = 'okrook'
 
 
@@ -8,3 +13,7 @@ def includeme(config):
 
     # Add File Download end point
     config.add_route('download', '/download/{reg_id}')
+
+    here = os.path.abspath(os.path.dirname(__file__))
+    assets_dir = os.path.abspath(os.path.join(os.path.join(here, '..', '..'), 'assets'))
+    config.add_static_view('assets/images', os.path.join(assets_dir, 'images'))

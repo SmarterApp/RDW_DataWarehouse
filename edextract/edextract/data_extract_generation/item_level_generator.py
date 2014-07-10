a5cd6162e3378a2397f5e75e4334e503baaa8ab8
Takashi Osako <tosako@amplify.com>
2014-07-10 00:21:46 -0400

    create multiple csv file by size
    Former-commit-id: 270a46116580f0bc202f8743d9bb2a3912dfe5c4

--- edextract/edextract/data_extract_generation/item_level_generator.py
+++ edextract/edextract/data_extract_generation/item_level_generator.py
@@ -12,12 +12,15 @@ from edcore.database.edcore_connector import EdCoreDBConnection
 from edextract.status.constants import Constants
 from edextract.status.status import ExtractStatus, insert_extract_stats
 from edextract.tasks.constants import Constants as TaskConstants, QueryType
-from edextract.utils.csv_writer import write_csv
 
 logger = logging.getLogger(__name__)
 
 ITEM_KEY_POS = 0
-
+# Write the header to the file
+# TODO: Should not be hard coded
+CSV_HEADER = ['key', 'student_guid', 'segmentId', 'position', 'clientId', 'operational', 'isSelected',
+              'format', 'score', 'scoreStatus', 'adminDate', 'numberVisits', 'strand', 'contentLevel',
+              'pageNumber', 'pageVisits', 'pageTime', 'dropped']
 
 def generate_items_csv(tenant, output_file, task_info, extract_args):
     """
@@ -33,35 +36,12 @@ def generate_items_csv(tenant, output_file, task_info, extract_args):
     items_root_dir = extract_args[TaskConstants.ROOT_DIRECTORY]
     item_ids = extract_args[TaskConstants.ITEM_IDS]
     threshold_size = extract_args.get(TaskConstants.THRESHOLD_SIZE, -1)
-    checking_ids = item_ids is not None
 
     with EdCoreDBConnection(tenant=tenant) as connection:
         # Get results (streamed, it is important to avoid memory exhaustion)
         results = connection.get_streaming_result(query)
 
-        # Write the header to the file
-        # TODO: Should not be hard coded
-        if not os.path.exists(output_file):
-            write_csv(output_file,
-                      ['key', 'student_guid', 'segmentId', 'position', 'clientId', 'operational', 'isSelected',
-                       'format', 'score', 'scoreStatus', 'adminDate', 'numberVisits', 'strand', 'contentLevel',
-                       'pageNumber', 'pageVisits', 'pageTime', 'dropped'],
-                      [])
-
-        # Open the file to stream it out
-        with open(output_file, 'a') as out_file:
-            # Read each result for item-level data
-            for result in results:
-                # Build path to file
-                path = _get_path_to_item_csv(items_root_dir, result)
-
-                # Write this file to output file if we are not checking for specific item IDs or if this file contains
-                # at least one of the requested item IDs
-                if os.path.exists(path):
-                    with open(path, 'r') as in_file:
-                        if not checking_ids or _check_file_for_items(in_file, item_ids):
-                            in_file.seek(0)
-                            out_file.write(in_file.read())
+        _append_csv_files(items_root_dir, item_ids, results, output_file, CSV_HEADER, threshold_size)
         # Done
         insert_extract_stats(task_info, {Constants.STATUS: ExtractStatus.EXTRACTED})
 
@@ -79,3 +59,44 @@ def _check_file_for_items(file_descriptor, item_ids):
         if row[ITEM_KEY_POS] in item_ids:
             return True
     return False
+
+
+def _append_csv_files(items_root_dir, item_ids, results, output_file, csv_header, threshold_size):
+
+    def open_outfile():
+        out_file = open(output_file, 'w')
+        csvwriter = csv.writer(out_file, quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(csv_header)
+        return out_file
+
+    out_file = open_outfile()
+    file_number = 0
+    for result in results:
+        # Build path to file
+        path = _get_path_to_item_csv(items_root_dir, result)
+        # Write this file to output file if we are not checking for specific item IDs or if this file contains
+        # at least one of the requested item IDs
+        if os.path.exists(path):
+            in_file = open(path, 'r')
+            if not item_ids is not None or _check_file_for_items(in_file, item_ids):
+                if threshold_size > 0:
+                    # we want to limit file size for output file.
+                    in_file_size = os.fstat(in_file.fileno()).st_size
+                    if in_file_size + out_file.tell() > threshold_size:
+                        # close current out_file and write in new file
+                        out_file.close()
+                        _rename_to_partial_filename(output_file, file_number)
+                        file_number += 1
+                        out_file = open_outfile()
+                in_file.seek(0)
+                out_file.write(in_file.read())
+            in_file.close()
+    out_file.close()
+    if file_number is not 0:
+        _rename_to_partial_filename(path, file_number)
+
+
+def _rename_to_partial_filename(src, file_number):
+    dir_name, base_name = os.path.split(src)
+    new_name = "%03d%s" % (file_number, base_name)
+    os.rename(src, os.path.join(dir_name, new_name))

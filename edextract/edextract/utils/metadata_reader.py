93b0c3d61f0744f2c578a1523dc12b74403a88d7
Takashi Osako <tosako@amplify.com>
2014-07-22 14:19:44 -0400

    update UT for metadata
    Former-commit-id: ca09eb22b92008239d6449f25d70f20e724c1f16

--- edextract/edextract/utils/metadata_reader.py
+++ edextract/edextract/utils/metadata_reader.py
@@ -10,6 +10,7 @@ class MetadataReader():
 
     def __init__(self, metadata_filename='.metadata', delimiter=':'):
         self.__metadata = {}
+        self.__metadata_tracker = set()
         self.__metadata_filename = metadata_filename
         self.__delimiter = delimiter
 
@@ -22,11 +23,14 @@ class MetadataReader():
 
     def _load_metadata(self, filepath):
         metadata_file = os.path.join(os.path.dirname(filepath), self.__metadata_filename)
-        if os.path.exists(metadata_file):
+        if os.path.exists(metadata_file) and metadata_file not in self.__metadata_tracker:
+            self.__metadata_tracker.add(metadata_file)
             with open(metadata_file, 'r') as f:
                 directory = os.path.dirname(metadata_file)
                 for l in f:
                     metadata = l.strip().split(self.__delimiter)
                     self.__metadata[os.path.join(directory, metadata[1])] = int(metadata[2])
+            if self.__metadata.get(filepath) is None:
+                self.__metadata[filepath] = os.stat(filepath).st_size
         else:
             self.__metadata[filepath] = os.stat(filepath).st_size

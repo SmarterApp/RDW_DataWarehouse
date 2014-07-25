3c966091b9740387b8a67e7e4fdb0576735c268a
Anna Grebneva <agrebneva@gmail.com>
2014-07-25 11:18:24 -0400

    score batcher poc
    Former-commit-id: 0d525518505bb6418831e6d9595eeeeffb55b0ae

---
+++ smarter_score_batcher/setup-developer.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+import shutil
+from distutils.core import run_setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+dependencies = [
+    'edapi',
+    'edschema',
+    'edauth',
+    'edcore',
+    'edextract',
+    'config',
+    'smarter_common']
+
+
+for dependency in dependencies:
+    pkg_path = os.path.abspath(here + "/../" + dependency + "/")
+    os.chdir(pkg_path)
+    run_setup("setup.py")
+    os.chdir(here)
+run_setup("setup.py")

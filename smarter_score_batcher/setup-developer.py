9861dfbd9072005e54c641df6d6fd28420fbb2ab
Takashi Osako <tosako@amplify.com>
2014-07-28 17:45:16 -0400

    added Aptana config file
    Former-commit-id: 3370514c0dcf59e5940556136a231882cfd8fae7

--- smarter_score_batcher/setup-developer.py
+++ smarter_score_batcher/setup-developer.py
@@ -8,11 +8,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 dependencies = [
     'edapi',
-    'edschema',
-    'edauth',
-    'edcore',
-    'edextract',
-    'config',
     'smarter_common']
 
 

7331243e320dac05b48d9c36cb5d2c303293b95f
Daniel Wu <dawu@wgen.net>
2014-07-30 16:49:45 -0400

    return 403 if client doesn't have authentication
    Former-commit-id: a27281bdb4a8d05c0dded67d6b0c80b0dd432837

--- smarter_score_batcher/smarter_score_batcher/__init__.py
+++ smarter_score_batcher/smarter_score_batcher/__init__.py
@@ -13,13 +13,15 @@ def main(global_config, **settings):
     config = Configurator(settings=settings, root_factory=RootFactory)
     # include edauth. Calls includeme
     config.include(edauth)
+    # Pass edauth the roles/permission mapping that is defined in smarter
+    edauth.set_roles(RootFactory.__acl__)
     # include add routes from edapi. Calls includeme
     config.include(edapi)
-    # Pass edauth the roles/permission mapping that is defined in smarter
-    # edauth.set_roles(RootFactory.__acl__)
 
     config.add_route('xml', '/services/xml')
     config.scan()
+    # Set default permission on all views
+    config.set_default_permission('view')
 
     logger.info("Smarter tsb started")
     return config.make_wsgi_app()
--- smarter_score_batcher/smarter_score_batcher/services/xml.py
+++ smarter_score_batcher/smarter_score_batcher/services/xml.py
@@ -4,10 +4,12 @@ Created on Jul 15, 2014
 @author: agrebneva
 '''
 import logging
-from pyramid.view import view_config
+from pyramid.view import view_config, forbidden_view_config
 from pyramid.response import Response
 from smarter_score_batcher.processors import process_tdsreport
 from edapi.decorators import validate_params
+from pyramid.httpexceptions import HTTPForbidden
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,12 +18,12 @@ XML_PARAMS = {
     "additionalProperties": False,
     "properties": {
         "content": "string",
-        "required": True,
+        "required": True
     }
 }
 
 
-@view_config(route_name='xml', request_method='POST', content_type="application/json")
+@view_config(route_name='xml', request_method='POST', content_type="application/json", renderer='json')
 @validate_params(schema=XML_PARAMS)
 def xml_catcher(context, request):
     """ XML cacther service expects XML post and will delegate processing based on the root element.
@@ -32,4 +34,9 @@ def xml_catcher(context, request):
         logger.error(str(e))
         raise
     else:
-        return Response('OK')
+        return Response()
+
+
+@forbidden_view_config()
+def forbidden(request):
+    return HTTPForbidden(body="", content_type='application/json')

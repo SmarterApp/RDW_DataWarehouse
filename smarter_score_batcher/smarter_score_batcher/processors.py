ec8ba703cbe63363645d83505d8389076895c110
sshrestha <sshrestha@amplify.com>
2014-08-03 20:53:25 -0400

    more changes as per code review
    Former-commit-id: 0f87ac09ddd5e61e6c1b9d4995bff5e6c37c4c20

--- smarter_score_batcher/smarter_score_batcher/processors.py
+++ smarter_score_batcher/smarter_score_batcher/processors.py
@@ -19,15 +19,51 @@ ROOT_DIR = '/tmp'
 
 class Meta:
     def __init__(self, valid_meta, student_id, state_name, district_id, academic_year, asmt_type, subject, grade, effective_date):
-        self.student_id = student_id
-        self.state_name = state_name
-        self.district_id = district_id
-        self.academic_year = academic_year
-        self.asmt_type = asmt_type
-        self.grade = grade
-        self.subject = subject
-        self.effective_date = effective_date
-        self.valid_meta = valid_meta
+        self.__student_id = student_id
+        self.__state_name = state_name
+        self.__district_id = district_id
+        self.__academic_year = academic_year
+        self.__asmt_type = asmt_type
+        self.__grade = grade
+        self.__subject = subject
+        self.__effective_date = effective_date
+        self.__valid_meta = valid_meta
+        
+    @property
+    def student_id(self):
+        return self.__student_id
+        
+    @property
+    def state_name(self):
+        return self.__state_name
+        
+    @property
+    def district_id(self):
+        return self.__district_id
+        
+    @property
+    def academic_year(self):
+        return self.__academic_year
+        
+    @property
+    def asmt_type(self):
+        return self.__asmt_type
+        
+    @property
+    def grade(self):
+        return self.__grade
+        
+    @property
+    def subject(self):
+        return self.__subject
+        
+    @property
+    def effective_date(self):
+        return self.__effective_date
+        
+    @property
+    def valid_meta(self):
+        return self.__valid_meta
 
 
 def process_xml(raw_xml_string):
@@ -35,7 +71,7 @@ def process_xml(raw_xml_string):
     '''
     meta_names = extract_meta_names(raw_xml_string)
     if not meta_names.valid_meta:
-        raise EdApiHTTPPreconditionFailed("content cannot be empty")
+        raise EdApiHTTPPreconditionFailed("Invalid XML")
     file_path = create_path(ROOT_DIR, meta_names)
     args = (file_path, raw_xml_string)
     settings = get_current_registry().settings
@@ -55,41 +91,39 @@ def extract_meta_names(raw_xml_string):
     '''Validates and extracts meta from the XML.
     Returns: True if meta for file path is valid. And the parts for folder creation.
     '''
-    root = ET.fromstring(raw_xml_string)
-    state_name, is_state_name_valid = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeRelationship/[@name='StateName']" )
-    student_id, is_student_id_valid = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeAttribute/[@name='SSID']" )
-    district_id, is_district_id_valid = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeRelationship/[@name='DistrictID']" )
-    academic_year, is_academic_year_valid = extract_meta_without_fallback_helper(root, "academicYear")
-    asmt_type, is_asmt_type_valid = extract_meta_without_fallback_helper(root, "assessmentType")
-    subject, is_subject_valid = extract_meta_without_fallback_helper(root, "subject")
-    grade, is_grade_valid = extract_meta_without_fallback_helper(root, "grade")
-    effective_date = 'NA'  # root.find("./test").get('effectiveDate', DEFAULT_VALUE)
-    validMeta = is_state_name_valid and is_student_id_valid and is_district_id_valid and is_academic_year_valid and is_asmt_type_valid and is_subject_valid and is_grade_valid
-    return Meta(validMeta, student_id, state_name, district_id, academic_year, asmt_type, subject, grade, effective_date)
+    try:
+        root = ET.fromstring(raw_xml_string)
+        state_name = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeRelationship/[@name='StateName']", "value", "context" )
+        student_id = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeAttribute/[@name='SSID']", "value", "context" )
+        district_id = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeRelationship/[@name='DistrictID']", "value", "context" )
+        academic_year = extract_meta_without_fallback_helper(root, "./Test", "academicYear")
+        asmt_type = extract_meta_without_fallback_helper(root, "./Test", "assessmentType")
+        subject = extract_meta_without_fallback_helper(root, "./Test", "subject")
+        grade = extract_meta_without_fallback_helper(root, "./Test", "grade")
+        effective_date = 'NA'  # root.find("./test").get('effectiveDate', DEFAULT_VALUE)
+        validMeta = state_name and student_id and district_id and academic_year and asmt_type and subject and grade
+        return Meta(validMeta, student_id, state_name, district_id, academic_year, asmt_type, subject, grade, effective_date)
+    except ET.ParseError:
+        raise EdApiHTTPPreconditionFailed("Invalid XML")
+        
 
-
-def extract_meta_with_fallback_helper(root, element_xpath):
-    valid_meta = True
+def extract_meta_with_fallback_helper(root, element_xpath, attribute_to_get, attribute_to_compare):
     if (root.find(element_xpath)) is not None:
         try:
-            element = [e.get('value', DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib['context'] == ATTRIBUTE_CONTEXT_VALUE_FINAL][0]
+            element = [e.get(attribute_to_get, DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == ATTRIBUTE_CONTEXT_VALUE_FINAL][0]
         except IndexError:
             try:
-                element = [e.get('value', DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib['context'] == ATTRIBUTE_CONTEXT_VALUE_INITIAL][0]
+                element = [e.get(attribute_to_get, DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == ATTRIBUTE_CONTEXT_VALUE_INITIAL][0]
             except IndexError:
                 element = DEFAULT_VALUE
     else:
-        element = None
-        valid_meta = False
-    return (element, valid_meta)
+        element = False
+    return element
 
 
-def extract_meta_without_fallback_helper(root, element_xpath):
-    valid_meta = True
-    if (root.find("./Test").get(element_xpath)) is not None:
-        element = root.find("./Test").get(element_xpath, DEFAULT_VALUE)
-    else:
-        element = None
-        valid_meta = False
-    return (element, valid_meta)
-
+def extract_meta_without_fallback_helper(root, element_xpath, attribute_to_get):
+    element = False
+    if (root.find(element_xpath)) is not None:
+        if(root.find(element_xpath).get(attribute_to_get)) is not None:
+            element = root.find(element_xpath).get(attribute_to_get, DEFAULT_VALUE)
+    return element

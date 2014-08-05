903189a6f61562ceb4a963010d85aa331b86bd66
Takashi Osako <tosako@amplify.com>
2014-08-05 10:33:31 -0400

    clean up code
    Former-commit-id: 586fd9dd3f5c3b5b01437c91b5284470829f7d86

--- smarter_score_batcher/smarter_score_batcher/services/xml.py
+++ smarter_score_batcher/smarter_score_batcher/services/xml.py
@@ -7,29 +7,17 @@ import logging
 from pyramid.view import view_config
 from pyramid.response import Response
 from pyramid.httpexceptions import HTTPServiceUnavailable
-from edapi.decorators import validate_params
 from smarter_score_batcher.processors import process_xml
-from smarter_score_batcher.constants import Constants
 from edapi.httpexceptions import EdApiHTTPPreconditionFailed
 
 
 logger = logging.getLogger("smarter_score_batcher")
 
 
-XML_PARAMS = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {
-        Constants.CONTENT: "string",
-        "required": True
-    }
-}
-
-
 @view_config(route_name='xml', request_method='POST', content_type="application/xml", renderer='json')
-# @validate_params(schema=XML_PARAMS)
 def xml_catcher(request):
-    """ XML cacther service expects XML post and will delegate processing based on the root element.
+    """
+    XML cacther service expects XML post and will delegate processing based on the root element.
     """
     try:
         xml_body = request.body

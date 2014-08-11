9cd8c649ab80b49479f3c151aa6911a9a5a83fc8
Takashi Osako <tosako@amplify.com>
2014-08-11 14:35:57 -0400

    US26641: refactor
    Former-commit-id: 9f1ab4dfb77ec2744bd621b6c05d07126ec8125a

--- smarter_score_batcher/smarter_score_batcher/tasks/remote_file_writer.py
+++ smarter_score_batcher/smarter_score_batcher/tasks/remote_file_writer.py
@@ -5,17 +5,10 @@ Created on Jul 28, 2014
 '''
 
 
-import os
 from smarter_score_batcher.celery import celery
+from smarter_score_batcher.utils.file_utils import file_writer
 
 
 @celery.task(name="tasks.extract.separator")
 def remote_write(path, data, mode=0o700):
-    # create directory
-    os.makedirs(os.path.dirname(path), mode=0o700, exist_ok=True)
-    with open(path, 'wb') as f:
-        f.write(str.encode(data) if type(data) is str else data)
-        written = True
-    if os.path.exists(path):
-        os.chmod(path, mode)
-    return written if written else False
+    return file_writer(path, data, mode=mode)

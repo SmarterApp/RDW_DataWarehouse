3618e92df8d9d2f7e7fc739db99b9e328e30be95
Takashi Osako <tosako@amplify.com>
2014-08-11 14:54:59 -0400

    US26641: skelton
    Former-commit-id: e91d2add64edca14cc7df4ce0d9ebd1b29347e15

--- smarter_score_batcher/smarter_score_batcher/tasks/remote_file_writer.py
+++ smarter_score_batcher/smarter_score_batcher/tasks/remote_file_writer.py
@@ -9,6 +9,11 @@ from smarter_score_batcher.celery import celery
 from smarter_score_batcher.utils.file_utils import file_writer
 
 
-@celery.task(name="tasks.extract.separator")
+@celery.task(name="tasks.tsb.remote_writer")
 def remote_write(path, data, mode=0o700):
     return file_writer(path, data, mode=mode)
+
+
+@celery.task(name='task.tsb.update_metadata')
+def update_metadata(file_path):
+    pass

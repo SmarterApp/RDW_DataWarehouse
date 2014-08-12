67ca6c4918e324b22a32d7058c5b2dd47599fa63
dip <dip@wgen.net>
2014-08-12 17:16:35 -0400

    clean up
    Former-commit-id: 77a5e88d514828112c4a63935f89820783cac333

--- smarter_score_batcher/smarter_score_batcher/services/csv.py
+++ smarter_score_batcher/smarter_score_batcher/services/csv.py
@@ -15,9 +15,9 @@ def create_csv(raw_xml_string):
     settings = get_current_registry().settings
     root_dir_csv = settings.get("smarter_score_batcher.base_dir.csv")
     root_dir_xml = settings.get("smarter_score_batcher.base_dir.xml")
+    # TODO:  We need the async queue
+    queue_name = settings.get('smarter_score_batcher.sync_queue')
     xml_file_path = create_path(root_dir_xml, meta_names, generate_path_to_raw_xml)
     csv_file_path = create_path(root_dir_csv, meta_names, generate_path_to_item_csv)
-    queue_name = settings.get('smarter_score_batcher.sync_queue')
-    args = (csv_file_path, xml_file_path)
-    celery_response = remote_csv_generator.apply_async(args=args, queue=queue_name)
+    celery_response = remote_csv_generator.apply_async(args=(csv_file_path, xml_file_path), queue=queue_name)     # @UndefinedVariable
     return celery_response
--- smarter_score_batcher/smarter_score_batcher/utils/file_utils.py
+++ smarter_score_batcher/smarter_score_batcher/utils/file_utils.py
@@ -18,12 +18,12 @@ def file_writer(path, data, mode=0o700):
     return written if written else False
 
 
-def csv_file_writer(csv_file_path, matrix_to_feed_csv, mode=0o700):
+def csv_file_writer(csv_file_path, data, mode=0o700):
     # create directory
     os.makedirs(os.path.dirname(csv_file_path), mode=mode, exist_ok=True)
     with open(csv_file_path, 'w', newline='') as csv_out:
         csv_writer = csv.writer(csv_out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerows(matrix_to_feed_csv)
+        csv_writer.writerows(data)
         csv_out.close()
         written = True
     if os.path.exists(csv_file_path):

ab80d1aba287d6e11d97fa3e4693a767eed0b490
sshrestha <sshrestha@amplify.com>
2014-08-13 15:41:54 -0400

    UT for item_level_utils.py
    Former-commit-id: de8ab84fffd46d9656b92b0a17be3ec03fd3b058

---
+++ smarter_score_batcher/smarter_score_batcher/tests/utils/test_item_level_utils.py
@@ -0,0 +1,37 @@
+import unittest
+from smarter_score_batcher.utils import item_level_utils
+from pyramid import testing
+
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+
+class TestItemLevelUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_get_all_item_level_for_tsb_csv(self):
+        xml_string = '''<TestXML>
+        <ElementOne key="">
+        <ElementTwo context="FINAL" name="dummyValue" value="DummyState" />
+        </ElementOne><Opportunity>
+        <Item position="test" segmentId="segmentId_value"
+        bankKey="test" key="key_value" operational="test" isSelected="test" format="test"
+        score="test" scoreStatus="test" adminDate="test" numberVisits="test"
+        mimeType="test" strand="test" contentLevel="test" pageNumber="test" pageVisits="test"
+        pageTime="test" dropped="test">
+        </Item>
+        </Opportunity></TestXML>'''
+        root = ET.fromstring(xml_string)
+        row = item_level_utils.get_item_level_data(root)
+        self.assertEqual('key_value', row[0][0])
+        self.assertEqual('segmentId_value', row[0][2])
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
--- smarter_score_batcher/smarter_score_batcher/utils/item_level_utils.py
+++ smarter_score_batcher/smarter_score_batcher/utils/item_level_utils.py
@@ -17,8 +17,9 @@ def get_item_level_data(root):
     '''
     student_guid = extract_meta_with_fallback_helper(root, "./Examinee/ExamineeAttribute/[@name='StudentIdentifier']", "value", "context")
     matrix = []
-    list_of_elements = ItemLevelCsvColumns.get_item_level_csv_keys(ItemLevelCsvColumns)
-    attribute_name_keys = ['key', 'segmentId', 'position', 'clientId', 'operational', 'isSelected', 'format', 'score', 'scoreStatus', 'adminDate', 'numberVisits', 'strand', 'contentLevel', 'pageNumber', 'pageVisits', 'pageTime', 'dropped']
+    list_of_elements = get_all_elements(root, './Opportunity/Item')
+    attribute_name_keys = ItemLevelCsvColumns.get_item_level_csv_keys(ItemLevelCsvColumns)
+    #attribute_name_keys = ['key', 'segmentId', 'position', 'clientId', 'operational', 'isSelected', 'format', 'score', 'scoreStatus', 'adminDate', 'numberVisits', 'strand', 'contentLevel', 'pageNumber', 'pageVisits', 'pageTime', 'dropped']
     for element_item in list_of_elements:
         row = [element_item.get(key) for key in attribute_name_keys]
         row.insert(1, student_guid)

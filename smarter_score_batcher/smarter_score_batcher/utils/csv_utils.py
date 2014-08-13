47b599b24f17c6dc6044b20f5098e0da1bde2b2c
Takashi Osako <tosako@amplify.com>
2014-08-13 11:16:19 -0400

    US26641: CR
    Former-commit-id: d0d4d645e4e829f7adceadd0a23eda10f282a021

--- smarter_score_batcher/smarter_score_batcher/utils/csv_utils.py
+++ smarter_score_batcher/smarter_score_batcher/utils/csv_utils.py
@@ -53,6 +53,11 @@ def process_assessment_data(root):
     # TODO: write to db in next story
 
 
+def process_item_level_data(root, csv_file_path):
+    matrix_to_feed_csv = get_item_level_data(root)
+    return csv_file_writer(csv_file_path, matrix_to_feed_csv)
+
+
 def generate_csv_from_xml(csv_file_path, xml_file_path):
     '''
     Creates a csv in the given csv file path by reading from the xml file path
@@ -62,13 +67,15 @@ def generate_csv_from_xml(csv_file_path, xml_file_path):
         tree = ET.parse(xml_file_path)
         root = tree.getroot()
         process_assessment_data(root)
-        # TODO:  Takashi fix this to be processed in its own function
-        matrix_to_feed_csv = get_item_level_data(root)
-        written = csv_file_writer(csv_file_path, matrix_to_feed_csv)
+        written = process_item_level_data(root, csv_file_path)
         if written:
             metadata_generator_bottom_up(csv_file_path, generateMetadata=True)
     except ET.ParseError as e:
+        '''
+        this should not be happened because we already validate against xsd
+        '''
         logger.error('csv file[' + csv_file_path + '] is failed to generate: ' + str(e))
+        logger.error('this error may be caused because you have an old xsd?')
     except Exception as e:
         if written:
             logger.error('metadata for csv file[' + csv_file_path + '] is failed to updated: ' + str(e))

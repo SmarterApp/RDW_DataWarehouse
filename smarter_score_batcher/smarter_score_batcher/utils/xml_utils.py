5565a20f94d07beef50c29d95428561756cae92c
Takashi Osako <tosako@amplify.com>
2014-08-12 21:29:05 -0400

    US26641: refactored
    Former-commit-id: e30ce1f48cc58723a45b3d17f224ebe0e21b49a5

--- smarter_score_batcher/smarter_score_batcher/utils/xml_utils.py
+++ smarter_score_batcher/smarter_score_batcher/utils/xml_utils.py
@@ -1,11 +1,8 @@
 import logging
+from smarter_score_batcher.utils.constants import Constants
 
 
 logger = logging.getLogger("smarter_score_batcher")
-
-DEFAULT_VALUE = 'NA'
-ATTRIBUTE_CONTEXT_VALUE_FINAL = 'FINAL'
-ATTRIBUTE_CONTEXT_VALUE_INITIAL = 'INITIAL'
 
 
 def get_all_elements(root, xpath_of_element):
@@ -23,12 +20,12 @@ def extract_meta_with_fallback_helper(root, element_xpath, attribute_to_get, att
     element = None
     if (root.find(element_xpath)) is not None:
         try:
-            element = [e.get(attribute_to_get, DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == ATTRIBUTE_CONTEXT_VALUE_FINAL][0]
+            element = [e.get(attribute_to_get, Constants.DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == Constants.ATTRIBUTE_CONTEXT_VALUE_FINAL][0]
         except IndexError:
             try:
-                element = [e.get(attribute_to_get, DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == ATTRIBUTE_CONTEXT_VALUE_INITIAL][0]
+                element = [e.get(attribute_to_get, Constants.DEFAULT_VALUE) for e in root.findall(element_xpath) if e.attrib[attribute_to_compare] == Constants.ATTRIBUTE_CONTEXT_VALUE_INITIAL][0]
             except IndexError:
-                element = DEFAULT_VALUE
+                element = Constants.DEFAULT_VALUE
     return element
 
 
@@ -36,5 +33,5 @@ def extract_meta_without_fallback_helper(root, element_xpath, attribute_to_get):
     element = None
     if (root.find(element_xpath)) is not None:
         if(root.find(element_xpath).get(attribute_to_get)) is not None:
-            element = root.find(element_xpath).get(attribute_to_get, DEFAULT_VALUE)
+            element = root.find(element_xpath).get(attribute_to_get, Constants.DEFAULT_VALUE)
     return element

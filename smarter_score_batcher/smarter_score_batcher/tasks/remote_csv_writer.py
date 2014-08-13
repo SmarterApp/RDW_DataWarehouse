27505d642131cbfc7059da6312cab60697a8b41d
Takashi Osako <tosako@amplify.com>
2014-08-12 20:42:39 -0400

    US26641: added log for csv fail
    Former-commit-id: 2cff65606299a06eb5521c2e8bec6eefb0a99228

--- smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
+++ smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
@@ -2,6 +2,7 @@ import logging
 from smarter_score_batcher.celery import celery
 from smarter_score_batcher.utils.file_utils import csv_file_writer
 from smarter_score_batcher.utils.csv_utils import get_all_elements_for_tsb_csv
+import os
 
 try:
     import xml.etree.cElementTree as ET
@@ -13,10 +14,16 @@ logger = logging.getLogger("smarter_score_batcher")
 
 @celery.task(name="tasks.tsb.remote_csv_writer")
 def remote_csv_generator(csv_file_path, xml_file_path):
+    written = False
     try:
         tree = ET.parse(xml_file_path)
         root = tree.getroot()
+        matrix_to_feed_csv = get_all_elements_for_tsb_csv(root, './Opportunity/Item')
+        written = csv_file_writer(csv_file_path, matrix_to_feed_csv)
     except ET.ParseError as e:
-        logger.error(str(e))
-    matrix_to_feed_csv = get_all_elements_for_tsb_csv(root, './Opportunity/Item')
-    return csv_file_writer(csv_file_path, matrix_to_feed_csv)
+        logger.error('csv file['+csv_file_path+'] is failed to generate: ' + str(e))
+    except Exception as e:
+        logger.error('csv file['+csv_file_path+'] is failed to generate: ' + str(e))
+        if os.path.exists(csv_file_path):
+            os.remove(csv_file_path)
+    return written

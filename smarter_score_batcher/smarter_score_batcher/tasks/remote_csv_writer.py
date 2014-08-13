c3c4f469869c5c5715ec98622f4cb7f92253cd56
Takashi Osako <tosako@amplify.com>
2014-08-12 20:43:40 -0400

    US26641: pep8
    Former-commit-id: 983d1a4131cdb560d2dc058a5063f1e1dc73d534

--- smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
+++ smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
@@ -21,9 +21,9 @@ def remote_csv_generator(csv_file_path, xml_file_path):
         matrix_to_feed_csv = get_all_elements_for_tsb_csv(root, './Opportunity/Item')
         written = csv_file_writer(csv_file_path, matrix_to_feed_csv)
     except ET.ParseError as e:
-        logger.error('csv file['+csv_file_path+'] is failed to generate: ' + str(e))
+        logger.error('csv file[' + csv_file_path + '] is failed to generate: ' + str(e))
     except Exception as e:
-        logger.error('csv file['+csv_file_path+'] is failed to generate: ' + str(e))
+        logger.error('csv file[' + csv_file_path + '] is failed to generate: ' + str(e))
         if os.path.exists(csv_file_path):
             os.remove(csv_file_path)
     return written

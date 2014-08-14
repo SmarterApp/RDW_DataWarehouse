56820c9739d4ef8e2016639d7b610ca2edd65932
Takashi Osako <tosako@amplify.com>
2014-08-14 13:59:02 -0400

    fix jenkins
    Former-commit-id: b9f8d279569e3588b180175c2201f44b53c495b2

--- smarter_score_batcher/smarter_score_batcher/utils/metadata_generator.py
+++ smarter_score_batcher/smarter_score_batcher/utils/metadata_generator.py
@@ -8,6 +8,7 @@ import fcntl
 import logging
 from smarter_score_batcher.exceptions import MetadataDirNotExistException
 from smarter_score_batcher.utils.constants import Constants
+import argparse
 
 
 logger = logging.getLogger("smarter_score_batcher")
@@ -307,3 +308,23 @@ class FileMetadata():
         @property
         def type(self):
             return Constants.FILE
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Metadata generator')
+    parser.add_argument('-p', '--path', help='directory/file path to read', required=True)
+    parser.add_argument('-r', '--recursive', help='generate metadata recursively', action='store_true', default=True)
+    parser.add_argument('-m', '--metadata', help='metadata filename', default='.metadata')
+    parser.add_argument('-f', '--force', help='force generate metadata if exists', action='store_true', default=False)
+    parser.add_argument('-u', '--up', help='update metadat from bottom to up', action='store_true', default=False)
+    args = parser.parse_args()
+    __path = args.path
+    __recursive = args.recursive
+    __metadata = args.metadata
+    __force = args.force
+    __up = args.up
+
+    if __up:
+        metadata_generator_top_down(__path, metadata_filename=__metadata, recursive=__recursive, force=__force)
+    else:
+        metadata_generator_bottom_up(__path, metadata_filename=__metadata, recursive=__recursive)

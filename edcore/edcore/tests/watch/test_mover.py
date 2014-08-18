bbe8f384988b4a3a63d4fe18214241cebfdab736
Takashi Osako <tosako@amplify.com>
2014-08-18 01:58:59 -0400

    UT
    Former-commit-id: ff47b5106cca16c871a18c1b76023867e576d51d

--- edcore/edcore/tests/watch/test_mover.py
+++ edcore/edcore/tests/watch/test_mover.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch, call
 __author__ = 'sravi'
 
 import unittest
@@ -6,6 +7,7 @@ import os
 import tempfile
 from edcore.watch.mover import FileMover
 from edcore.tests.watch.common_test_utils import write_something_to_a_blank_file, create_checksum_file
+from edcore.watch.constants import MoverConstants as MoverConst, WatcherConstants as WatcherConst
 
 
 class TestUtil(unittest.TestCase):
@@ -43,3 +45,31 @@ class TestUtil(unittest.TestCase):
         files_to_move = []
         files_moved = self.file_mover.move_files(files_to_move=files_to_move)
         self.assertEqual(files_moved, 0)
+
+    @patch('edcore.watch.mover.FileMover.move_files_local')
+    def test_move_files_with_local(self, mock_move_files_local):
+        files_to_move = ['hello']
+        self.file_mover.move_files(files_to_move=files_to_move)
+        self.assertEqual(1, mock_move_files_local.call_count)
+        self.assertEqual(mock_move_files_local.call_args, call(files_to_move))
+
+    @patch('edcore.watch.mover.FileMover.move_files_by_sftp')
+    def test_move_files_with_sftp(self, mock_move_files_by_sftp):
+        self.file_mover.conf[MoverConst.FILE_MOEV_TYPE] = 'sftp'
+        files_to_move = ['hello']
+        self.file_mover.move_files(files_to_move=files_to_move)
+        self.assertEqual(1, mock_move_files_by_sftp.call_count)
+        self.assertEqual(mock_move_files_by_sftp.call_args, call(files_to_move))
+
+    @patch('edcore.watch.mover.FileUtil.get_file_tenant_and_user_name')
+    def test_move_files_local(self, mock_get_file_tenant_and_user_name):
+        file1 = os.path.join(self.base_dir, '1', '2', '3', 'testfile')
+        os.makedirs(os.path.dirname(file1))
+        open(file1, 'a').close()
+        files_to_move = [file1]
+        mock_get_file_tenant_and_user_name.return_value = 'hello', 'world'
+
+        self.file_mover.conf[WatcherConst.STAGING_DIR] = self.base_dir
+        moved = self.file_mover.move_files_local(files_to_move)
+        self.assertEqual(1, moved)
+        self.assertTrue(os.path.exists(os.path.join(self.base_dir, 'hello', 'world', 'testfile')))

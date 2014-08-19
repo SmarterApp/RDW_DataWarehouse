d06993a8ba80ec66339f1dbedd99e0ba1a3d3639
Takashi Osako <tosako@amplify.com>
2014-08-19 16:04:02 -0400

    support multiple file-grabber
    Former-commit-id: 6f0ea61c22854ca820c11154a65c7b3ca1e00a4d

--- edudl2/edudl2/udl2_util/file_grabber.py
+++ edudl2/edudl2/udl2_util/file_grabber.py
@@ -6,10 +6,12 @@ Created on Aug 18, 2014
 import argparse
 import logging
 import logging.config
-from edcore.utils.utils import read_ini, get_config_from_ini, run_cron_job,\
+from edcore.utils.utils import read_ini, get_config_from_ini, run_cron_job, \
     create_daemon
 from edudl2.udl2_util.rsync import rsync
 import time
+import json
+import copy
 
 
 def main():
@@ -23,17 +25,32 @@ def main():
 
     args = parser.parse_args()
     file = args.ini_file
+    file = '/Users/tosako/development/edware/config/development.ini'
     logging.config.fileConfig(file)
     ini_file = read_ini(file)
-    d = get_config_from_ini(ini_file, '')
+    config = get_config_from_ini(ini_file, '')
     daemon_mode = args.daemon
     pid_file = args.pidfile
     if daemon_mode:
         create_daemon(pid_file)
-    # setup cron
-    run_cron_job(d, 'udl2_rsync.', rsync)
-    while True:
-        time.sleep(1)
+    # get file-grabber and reassembly config file
+    config_for_grabber = copy.deepcopy(config)
+    file_grabber_configs = {}
+    for key in config_for_grabber.keys():
+        key_values = key.split('.')
+        if key_values[0] == 'file-grabber':
+            key_values.pop(0)
+            name = key_values.pop(0)
+            file_grabber_config = file_grabber_configs.get(name, {})
+            file_grabber_config['file-grabber.' + '.'.join(key_values)] = config['.'.join(['file-grabber', name]) + '.' + '.'.join(key_values)]
+            file_grabber_configs[name] = file_grabber_config
+
+    if file_grabber_configs:
+        # setup cron
+        for file_grabber_config in file_grabber_configs:
+            run_cron_job(file_grabber_configs[file_grabber_config], 'file-grabber.', rsync)
+        while True:
+            time.sleep(1)
 
 if __name__ == "__main__":
     main()

5b4baae260caef912429309662885222c1b58076
Takashi Osako <tosako@amplify.com>
2014-08-27 14:17:23 -0400

    fix error code
    Former-commit-id: 4221c87e134c8a00be04c5ed015c2b177d201444

--- smarter_score_batcher/smarter_score_batcher/views.py
+++ smarter_score_batcher/smarter_score_batcher/views.py
@@ -3,7 +3,7 @@ Created on Aug 7, 2014
 
 @author: tosako
 '''
-from pyramid.httpexceptions import HTTPPreconditionFailed
+from pyramid.httpexceptions import HTTPPreconditionFailed, HTTPNotFound
 from pyramid.view import view_config
 
 
@@ -13,3 +13,8 @@ def precondition_failed_redirect(request):
     Errors get redirected here
     '''
     return HTTPPreconditionFailed()
+
+
+@view_config(route_name='error')
+def tsb_error(request):
+    return HTTPNotFound()

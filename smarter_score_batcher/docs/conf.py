a982b4a3830e66a1c0c7f775d105dfc410f4cdca
Anna Grebneva <agrebneva@gmail.com>
2014-09-08 13:43:44 -0400

    fixinf pep8 for docs
    Former-commit-id: 1bd625aed4798f44bd3c03966046039bf1135631

--- smarter_score_batcher/docs/conf.py
+++ smarter_score_batcher/docs/conf.py
@@ -193,23 +193,12 @@ htmlhelp_basename = 'ScoreBatcherdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', 'ScoreBatcher.tex', 'Score Batcher Documentation',
-   'Amplify', 'manual'),
-]
+latex_documents = [('index', 'ScoreBatcher.tex', 'Score Batcher Documentation', 'Amplify', 'manual')]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -250,11 +239,7 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', 'ScoreBatcher', 'Score Batcher Documentation',
-   'Amplify', 'ScoreBatcher', 'One line description of project.',
-   'Miscellaneous'),
-]
+texinfo_documents = [('index', 'ScoreBatcher', 'Score Batcher Documentation', 'Amplify', 'ScoreBatcher', 'One line description of project.', 'Miscellaneous')]
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []

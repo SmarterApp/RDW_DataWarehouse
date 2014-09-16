00f3922eae3c4d7946e98e36abd12750fa007939
Takashi Osako <tosako@amplify.com>
2014-09-16 17:58:20 -0400

    US15504: fix UT
    Former-commit-id: 1156dc5022fa662e07d1cba834e0d24aedfad6e3

--- edcore/edcore/tests/notification/test_notification.py
+++ edcore/edcore/tests/notification/test_notification.py
@@ -8,9 +8,15 @@ from edcore.notification.notification import create_notification_body, \
     send_notification
 from edcore.database.utils.constants import LoadType, UdlStatsConstants
 from unittest.mock import patch
+import httpretty
+from edcore.notification.constants import Constants
+from edcore.notification.callback import post_notification
 
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.udl2_conf = {'notification_retries': 5, 'notification_timeout': 1}
 
     @patch('edcore.notification.notification.send_notification_email')
     @patch('edcore.notification.notification.post_notification')
@@ -51,6 +57,94 @@ class Test(unittest.TestCase):
         self.assertEqual(body['status'], 'Success')
         self.assertIn('Job completed successfully', body['message'])
 
+
+    @httpretty.activate
+    def test_post_notification_success_no_retries(self):
+        # Create the notification request body.
+        notification_body = {'status': Constants.SUCCESS, 'id': 'aaa-bbb-ccc', 'testRegistrationId': '111-222-333', 'message': ''}
+
+        # Send the status.
+        callback_url = self.register_url([201])
+        notification_status, notification_error = post_notification(callback_url, 1, notification_body)
+
+        # Verify results.
+        self.assertEquals(Constants.SUCCESS, notification_status)
+        self.assertEquals(None, notification_error)
+
+    @httpretty.activate
+    def test_post_notification_pending(self):
+        # Create the notification request body.
+        notification_body = {'status': Constants.SUCCESS, 'id': 'aaa-bbb-ccc', 'testRegistrationId': '111-222-333', 'message': ''}
+
+        # Send the status.
+        callback_url = self.register_url([408])
+        notification_status, notification_error = post_notification(callback_url, 1, notification_body)
+
+        # Verify results.
+        self.assertEquals(Constants.PENDING, notification_status)
+        self.assertEquals('408 Client Error: Request a Timeout', notification_error)
+
+    @httpretty.activate
+    def test_post_notification_failure(self):
+        # Create the notification request body.
+        notification_body = {'status': Constants.SUCCESS, 'id': 'aaa-bbb-ccc', 'testRegistrationId': '111-222-333', 'message': ''}
+
+        # Send the status.
+        callback_url = self.register_url([401])
+        notification_status, notification_error = post_notification(callback_url, 1, notification_body)
+
+        # Verify results.
+        self.assertEquals('401 Client Error: Unauthorized', notification_error)
+        self.assertEquals(Constants.FAILURE, notification_status)
+
+    @httpretty.activate
+    def test_post_notification_pending_connection_error(self):
+        # Create the notification request body.
+        notification_body = {'status': Constants.SUCCESS, 'id': 'aaa-bbb-ccc', 'testRegistrationId': '111-222-333', 'message': ''}
+
+        # Send the status.
+        callback_url = 'http://SomeBogusurl/SomeBogusEndpoint'
+        notification_status, notification_error = post_notification(callback_url, 1, notification_body)
+
+        # Verify results.
+        self.assertEquals(Constants.PENDING, notification_status)
+        self.assertRegex(notification_error, "Max retries exceeded with url")
+
+    def test_create_notification_body_success(self):
+
+        body = create_notification_body(LoadType.STUDENT_REGISTRATION, "guid_batch", "batch_table", "id", "test_reg_id", 100, UdlStatsConstants.MIGRATE_INGESTED, None, None)
+
+        self.assertEquals(body['rowCount'], 100)
+        self.assertEquals(body['id'], 'id')
+        self.assertEquals(body['testRegistrationId'], 'test_reg_id')
+        self.assertEquals(body['status'], 'Success')
+        self.assertEquals(body['message'], ['Job completed successfully'])
+
+    def test_create_notification_body_failure(self):
+
+        body = create_notification_body(LoadType.STUDENT_REGISTRATION, "guid_batch", "batch_table", "id", "test_reg_id", 100, UdlStatsConstants.UDL_STATUS_FAILED, None, 'ERROR 3000')
+
+        self.assertTrue('rowCount' not in body)
+        self.assertEquals(body['id'], 'id')
+        self.assertEquals(body['testRegistrationId'], 'test_reg_id')
+        self.assertEquals(body['status'], 'Failed')
+        self.assertEquals(body['message'], ['ERROR 3000'])
+
+    def register_url(self, return_statuses):
+        url = "http://MyTestUri/MyEndpoint"
+        responses = [httpretty.Response(body={}, status=return_status) for return_status in return_statuses]
+        httpretty.register_uri(httpretty.POST, url, responses=responses)
+        return url
+
+    def get_conf(self):
+        conf = {'guid_batch': 'guid_batch',
+                'batch_table': 'batch_table',
+                'student_reg_guid': 'student_reg_guid',
+                'reg_system_id': 'reg_system_id',
+                'total_rows_loaded': 'total_rows_loaded',
+                'callback_url': 'callback_url',
+                'notification_timeout_interval': 'notification_timeout_interval'}
+        return conf
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']

b8720cbaf7f69798c362868ee150ce225bb8b360
Anna Grebneva <agrebneva@gmail.com>
2014-09-23 11:30:58 -0400

    - performance metadata for TSB
    Former-commit-id: 88f29527ba039f65da0dbb01b98f745003cc9761

--- smarter_score_batcher/smarter_score_batcher/templates/asmt_template_manager.py
+++ smarter_score_batcher/smarter_score_batcher/templates/asmt_template_manager.py
@@ -9,6 +9,8 @@ import json
 from smarter_score_batcher.exceptions import MetadataException
 from zope import interface, component
 from zope.interface.declarations import implementer
+import fnmatch
+from smarter_score_batcher.utils.merge import deep_merge
 
 
 class MetadataTemplate:
@@ -19,43 +21,74 @@ class MetadataTemplate:
         self.asmt_data_json = asmt_data_json
 
     def get_asmt_metadata_template(self):
-        return self.asmt_data_json
+        return self.asmt_data_json.copy()
 
     def get_asmt_subject(self):
         return self.asmt_data_json['Identification']['Subject']
 
 
 class IMetadataTemplateManager(interface.Interface):
-    def get_template(self, subject):
+    def get_template(self, key):
         pass
 
 
-@implementer(IMetadataTemplateManager)
+def get_template_key(year, asmt_type, grade, subject):
+    return '_'.join([str(year), asmt_type, str(grade), subject])
+
+
 class MetadataTemplateManager:
     '''
     Loads and manages asmt templates by asmt SUBJECT
     '''
-    def __init__(self, asmt_meta_rel_dir=None):
+    def __init__(self, asmt_meta_dir=None):
         self.templates = {}
-        asmt_meta_location = self._get_template_location(asmt_meta_rel_dir)
-        for file in os.listdir(asmt_meta_location):
-            if file.endswith(".static_asmt_metadata.json"):
-                full_path = os.path.join(asmt_meta_location, file)
+        self.asmt_meta_location = self._get_template_location(asmt_meta_dir)
+        self._load_templates(pattern='*.static_asmt_metadata.json', storer=self._store_by_subject)
+
+    def _load_templates(self, pattern='.static_asmt_metadata.json', storer=None):
+        for root, _, filenames in os.walk(self.asmt_meta_location):
+            for file in fnmatch.filter(filenames, pattern):
+                full_path = os.path.join(root, file)
                 with open(full_path, 'r+') as f:
-                    sm = MetadataTemplate(json.load(f))
-                    subject = sm.get_asmt_subject()
-                    self.templates[subject.lower()] = sm
+                    storer(root, MetadataTemplate(json.load(f)))
+
+    def _store_by_subject(self, path, metadata_template):
+        subject = metadata_template.get_asmt_subject()
+        self.templates[subject.lower()] = metadata_template
 
     def _get_template_location(self, asmt_meta_location=None):
         if not asmt_meta_location is None and os.path.isabs(asmt_meta_location):
             return asmt_meta_location
         here = os.path.abspath(os.path.dirname(__file__))
-        return os.path.join(here, conf.get('smarter_score_batcher.resources.path', '../../resources/') if asmt_meta_location is None else asmt_meta_location)
+        return os.path.join(here, self._get_configured_path() if asmt_meta_location is None else asmt_meta_location)
 
-    def get_template(self, subject):
-        sm = self.templates.get(subject.lower())
+    def _get_configured_path(self):
+        return conf.get('smarter_score_batcher.metadata.static', '../../resources/meta/static')
+
+    def get_template(self, key):
+        sm = self.templates.get(key.lower())
         if sm is None:
-            raise MetadataException("Unable to load metadata for subject {0}".format(subject))
-        return sm.asmt_data_json.copy()
+            raise MetadataException("Unable to load metadata for subject {0}".format(key))
+        return sm.get_asmt_metadata_template()
 
-component.provideUtility(MetadataTemplateManager(), IMetadataTemplateManager)
+
+@implementer(IMetadataTemplateManager)
+class PerfMetadataTemplateManager(MetadataTemplateManager):
+    '''
+    Loads and manages asmt templates by asmt SUBJECT
+    '''
+    def __init__(self, asmt_meta_dir=None, static_asmt_meta_dir=None):
+        self.templates = {}
+        self.asmt_meta_location = self._get_template_location(asmt_meta_dir)
+        self.meta_template_mgr = MetadataTemplateManager(asmt_meta_dir=static_asmt_meta_dir)
+        self._load_templates(pattern='*.json', storer=self._store)
+
+    def _get_configured_path(self):
+        return conf.get('smarter_score_batcher.metadata.static', '../../resources/meta/performance')
+
+    def _store(self, path, metadata_template):
+        key = path[len(self.asmt_meta_location) + 1:].replace(os.path.sep, '_')
+        key = key + '_' + metadata_template.get_asmt_subject()
+        self.templates[key.lower()] = MetadataTemplate(deep_merge(self.meta_template_mgr.get_template(metadata_template.get_asmt_subject()), metadata_template.get_asmt_metadata_template()))
+
+component.provideUtility(PerfMetadataTemplateManager(), IMetadataTemplateManager)
--- smarter_score_batcher/smarter_score_batcher/tests/templates/test_metadata_template_manager.py
+++ smarter_score_batcher/smarter_score_batcher/tests/templates/test_metadata_template_manager.py
@@ -4,16 +4,26 @@ Created on Sep 22, 2014
 @author: agrebneva
 '''
 import unittest
-from smarter_score_batcher.templates.asmt_template_manager import MetadataTemplateManager
+from smarter_score_batcher.templates.asmt_template_manager import MetadataTemplateManager,\
+    PerfMetadataTemplateManager, get_template_key
 import os
 from smarter_score_batcher.exceptions import MetadataException
 
 
-class TestJSONMetadata(unittest.TestCase):
+class TestAsmtMetadata(unittest.TestCase):
 
-    def test_metadata_manager(self):
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../resources/')
-        mdm = MetadataTemplateManager(asmt_meta_rel_dir=path)
+    def test_static_metadata_manager(self):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../resources/meta/static')
+        mdm = MetadataTemplateManager(asmt_meta_dir=path)
         self.assertIsNotNone(mdm.get_template('ALIEN'), 'It should find ALIEN template')
         with self.assertRaises(MetadataException):
             mdm.get_template('MATH')
+
+    def test_performance_metadata_manager(self):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../resources/meta/performance')
+        static_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../resources/meta/static')
+        mdm = PerfMetadataTemplateManager(asmt_meta_dir=path, static_asmt_meta_dir=static_path)
+        self.assertIsNotNone(mdm.get_template(get_template_key(2015, 'summative', 3, 'ALIEN')), 'It should find ALIEN template for 3rd grade')
+        self.assertIsNotNone(mdm.get_template(get_template_key(2015, 'summative', 8, 'ALIEN')), 'It should find ALIEN template for 8th grade')
+        with self.assertRaises(MetadataException):
+            mdm.get_template(get_template_key(2015, 'summative', 3, 'MATH'))

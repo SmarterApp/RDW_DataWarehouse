4fb1c52c97bad3cb43ad8f8fd15223781f550a99
Takashi Osako <tosako@amplify.com>
2014-10-01 11:15:16 -0400

    Revert "fix task"
    This reverts commit d9ea20800636972a1f54d30a37cc5225a17cd262.
    
    
    Former-commit-id: bdcda62ec859f095150d035e381256eadb9e8e5e

--- edudl2/edudl2/udl2/udl2_pipeline.py
+++ edudl2/edudl2/udl2/udl2_pipeline.py
@@ -31,8 +31,8 @@ def get_pipeline_chain(archive_file, load_type='Unknown', file_parts=4, guid_bat
                            W_file_expander.task.s(),
                            W_get_load_type.task.s(),
                            W_get_params.task.s(),
-                           W_load_err_file.task.s(),
                            W_simple_file_validator.task.s(),
+                           W_load_err_file.task.s(),
                            W_file_splitter.task.s(),
                            # The end tasks are in the W_determine_end_chain.py file until we resolve the awkward 3 way dependency
                            W_determine_end_chain.task.s())

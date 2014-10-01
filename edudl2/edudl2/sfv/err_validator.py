f778a4e546c0f3256caaecd1480de7241a6045a6
Takashi Osako <tosako@amplify.com>
2014-10-01 11:17:11 -0400

    fix validation
    Former-commit-id: 2524b2397c7fef96ee219e41546cc6e6e1e1bc2b

--- edudl2/edudl2/sfv/err_validator.py
+++ edudl2/edudl2/sfv/err_validator.py
@@ -3,24 +3,11 @@ Created on Sep 29, 2014
 
 @author: tosako
 '''
-import os
-import json
-from edudl2.exceptions.errorcodes import ErrorCode
 
 
 class ErrorValidator():
-
+    '''
+    fake validation for err
+    '''
     def execute(self, dir_path, file_name, batch_sid):
-        file_path = os.path.join(dir_path, file_name)
-        try:
-            with open(file_path) as f:
-                j = f.read()
-                json_data = json.loads(j)
-                content = json_data['content']
-                if content == 'error':
-                    tsb_error = json_data['tsb_error']
-                    if type(tsb_error) is list:
-                        return []
-        except:
-            pass
-        return (ErrorCode.SRC_JSON_INVALID_STRUCTURE, dir_path, file_name, batch_sid)
+        return []

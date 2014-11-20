b5ba92e862c97257c5e6a7ab0f8c1efc5ce0221c
dip <dip@wgen.net>
2014-11-20 14:37:35 -0500

    Tech debt: context security for fact_block and clean up
    Former-commit-id: 23594ec309ae2a8cf4cb8430d46750bbf327291f

--- smarter/smarter/security/roles/pii.py
+++ smarter/smarter/security/roles/pii.py
@@ -4,8 +4,6 @@ Created on May 9, 2013
 @author: dip
 '''
 from sqlalchemy.sql.expression import and_, or_
-
-from smarter.reports.helpers.constants import Constants
 from smarter.security.roles.default import BaseRole
 from smarter.security.roles.base import verify_context
 from smarter.security.context_role_map import ContextRoleMap
@@ -18,23 +16,6 @@ class PII(BaseRole):
 
     def __init__(self, connector, name):
         super().__init__(connector, name)
-
-    @verify_context
-    def get_context(self, tenant, user):
-        '''
-        Returns a sqlalchemy binary expression representing section_guid that user has context to
-        If Context is an empty list, return none, which will return Forbidden Error
-        '''
-        fact_asmt_outcome_vw = self.connector.get_table(Constants.FACT_ASMT_OUTCOME_VW)
-        context = user.get_context().get_all_context(tenant, self.name)
-        if not context:
-            # context returned is empty, therefore no context
-            return None
-        expr = []
-        for k, v in context.items():
-            if v:
-                expr.append(and_(fact_asmt_outcome_vw.c[k].in_(v)))
-        return expr
 
     @verify_context
     def add_context(self, tenant, user, query):
@@ -56,7 +37,4 @@ class PII(BaseRole):
         '''
         Given a list of student guids, return true if user guid has access to those students
         '''
-        query = super().get_students(tenant, student_ids)
-        query = query.where(or_(*self.get_context(tenant, user)))
-        results = self.connector.get_result(query)
-        return len(student_ids) == len(results)
+        return super().check_context(tenant, user, student_ids)
--- smarter/smarter/security/roles/state_level.py
+++ smarter/smarter/security/roles/state_level.py
@@ -4,8 +4,6 @@ Created on May 9, 2013
 @author: dip
 '''
 from sqlalchemy.sql.expression import or_
-
-from smarter.reports.helpers.constants import Constants
 from smarter.security.roles.base import BaseRole, verify_context
 from smarter.security.context_role_map import ContextRoleMap
 from smarter_common.security.constants import RolesConstants
@@ -17,19 +15,6 @@ class StateLevel(BaseRole):
 
     def __init__(self, connector, name):
         super().__init__(connector, name)
-
-    @verify_context
-    def get_context(self, tenant, user):
-        '''
-        Returns a sqlalchemy binary expression representing state_code that user has context to
-        '''
-        student_reg = self.connector.get_table(Constants.STUDENT_REG)
-        context = user.get_context().get_states(tenant, self.name)
-        # context of none means that user has no access
-        if context is None:
-            return None
-        else:
-            return [student_reg.c.state_code.in_(context)] if context else []
 
     @verify_context
     def add_context(self, tenant, user, query):
@@ -49,7 +34,4 @@ class StateLevel(BaseRole):
         '''
         Returns true if it has context to the list of student guids
         '''
-        query = super().get_students(tenant, student_ids)
-        query = query.where(or_(*self.get_context(tenant, user)))
-        results = self.connector.get_result(query)
-        return len(student_ids) == len(results)
+        return super().check_context(tenant, user, student_ids)

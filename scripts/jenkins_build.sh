b3a509d0be3f68a64efae4b29c8c692e01c47bc9
Nidhi Paroha <nparoha@wgen.net>
2014-11-21 11:56:44 -0500

    temp only run frontend_tests to debug jenkins failure #3
    Former-commit-id: cbc1bb0b6df4f3bbfec7dd23b30caef18cc283b6

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -236,7 +236,7 @@ function run_functional_tests {
        nosetests -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
     else
        cd frontend_tests
-       nosetests --verbose --logging-level=DEBUG --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
+       nosetests test_grade_view_list_of_students.py --verbose --logging-level=DEBUG --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
        #nosetests --exclude-dir=e2e_tests --exclude-dir=hpz -v --with-xunit --xunit-file=$WORKSPACE/nosetests.xml
        generate_docs edware_test/edware_test/functional_tests
     fi

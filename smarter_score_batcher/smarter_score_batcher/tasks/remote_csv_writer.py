5217918cd70ef9cedc50fb4591f5b4359e171a7c
dip <dip@wgen.net>
2014-12-04 16:41:52 -0500

    US29886 move metadata generator task one level up
    Former-commit-id: 73370a54a744d59575e0ad511cd4111efc884c34

--- smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
+++ smarter_score_batcher/smarter_score_batcher/tasks/remote_csv_writer.py
@@ -2,7 +2,6 @@ from smarter_score_batcher.celery import celery
 from smarter_score_batcher.processing.file_processor import generate_csv_from_xml
 from smarter_score_batcher.error.exceptions import TSBException
 from smarter_score_batcher.error.error_handler import handle_error
-from smarter_score_batcher.utils.metadata_generator import metadata_generator_task
 
 
 @celery.task(name="tasks.tsb.remote_csv_writer")
@@ -15,7 +14,6 @@ def remote_csv_generator(meta, csv_file_path, xml_file_path, work_dir, metadata_
     '''
     rtn = False
     try:
-        metadata_generator_task.apply_async(args=(xml_file_path,), queue=metadata_queue)
         mode = 0o700
         rtn = generate_csv_from_xml(meta, csv_file_path, xml_file_path, work_dir, metadata_queue, mode=mode)
     except TSBException as e:

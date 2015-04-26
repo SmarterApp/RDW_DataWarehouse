0873e83b55dd6eff2c8a3f9e45507f3614cb0d3b
Takashi Osako <tosako@amplify.com>
2015-04-26 11:18:55 -0400

    update jinja2 version
    Former-commit-id: 7b75305f6a3e4fbbb7a7e09ee5446a684a5c8fe1

--- hpz/setup.py
+++ hpz/setup.py
@@ -14,7 +14,7 @@ requires = [
     'SQLAlchemy == 0.8.3',
     'py-postgresql == 1.1.0',
     'zope.sqlalchemy==0.7.3',
-    'Jinja2==2.7.2',
+    'Jinja2==2.7.3',
     'config',
     'edschema',
     'edauth',

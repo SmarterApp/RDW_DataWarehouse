210d8272f973e199e2447b24e8f52e64a96aa1fa
Takashi Osako <tosako@amplify.com>
2015-06-08 19:03:23 -0400

    updated rpm
    Former-commit-id: ca32906d35aa162754704c90ba68af28229b0f0f

--- edudl2/edudl2/report/report_generator.py
+++ edudl2/edudl2/report/report_generator.py
@@ -186,7 +186,7 @@ def generate_report_for_cron(settings):
     today = datetime.datetime.today().strftime('%Y-%m-%d ' + str(report_hour) + ':00:00')
     start_date = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:00:00')
     end_date = today
-    generate_report(settings['uid'], settings['mail_to'], start_date, end_date, settings['mail_from'], settings['subject'])
+    generate_report(settings['mail_to'], start_date, end_date, settings['mail_from'], settings['subject'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate udl report')

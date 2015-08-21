495df885c63c1693830406682bdd489f4428f8e7
Anna Grebneva <agrebneva@amplify.com>
2015-08-21 11:57:17 -0400

    UDL changes for complete and status
    Former-commit-id: bb9b6b2739025cc434971afd30d56dd17a08b967

--- config/config/ref_table_data.py
+++ config/config/ref_table_data.py
@@ -142,6 +142,8 @@ ref_table_conf = {
         ('1', 'lz_csv', 'AccommodationSpeechToText', 'stg_sbac_asmt_outcome', 'AccommodationSpeechToText', 'clean', None),
         ('1', 'lz_csv', 'AccommodationStreamlineMode', 'stg_sbac_asmt_outcome', 'AccommodationStreamlineMode', 'clean', None),
         ('1', 'lz_csv', 'AccommodationNoiseBuffer', 'stg_sbac_asmt_outcome', 'AccommodationNoiseBuffer', 'clean', None),
+        ('1', 'lz_csv', 'assessmentstatus', 'stg_sbac_asmt_outcome', 'assessmentstatus', 'clean', None),
+        ('1', 'lz_csv', 'completestatus', 'stg_sbac_asmt_outcome', 'completestatus', 'clean', None),
         ('1', 'lz_csv', 'Op', 'stg_sbac_asmt_outcome', 'op', 'option', None),
         # Staging to Integration
         ('3', 'stg_sbac_asmt_outcome', 'record_sid', 'int_sbac_asmt_outcome', 'record_sid', None, None),
@@ -252,6 +254,9 @@ ref_table_conf = {
         ('3', 'stg_sbac_asmt_outcome', 'AccommodationSpeechToText', 'int_sbac_asmt_outcome', 'acc_speech_to_text_nonembed', None, "to_number({src_column}, '99999')"),
         ('3', 'stg_sbac_asmt_outcome', 'AccommodationStreamlineMode', 'int_sbac_asmt_outcome', 'acc_streamline_mode', None, "to_number({src_column}, '99999')"),
         ('3', 'stg_sbac_asmt_outcome', 'AccommodationNoiseBuffer', 'int_sbac_asmt_outcome', 'acc_noise_buffer_nonembed', None, "to_number({src_column}, '99999')"),
+        ('3', 'stg_sbac_asmt_outcome', 'assessmentstatus', 'int_sbac_asmt_outcome', 'asmt_status', 'clean', "coalesce({src_column}, 'OK')"),
+        ('3', 'stg_sbac_asmt_outcome', 'completestatus', 'int_sbac_asmt_outcome', 'complete', 'clean', "coalesce({src_column}, 'True')"),
+        
         # Integration to Target
         ('4', 'int_sbac_asmt', 'record_sid', 'dim_asmt', 'asmt_rec_id', None, None),
         ('4', 'int_sbac_asmt', 'guid_asmt', 'dim_asmt', 'asmt_guid', None, None),
@@ -476,6 +481,7 @@ ref_table_conf = {
         ('4', 'int_sbac_asmt_outcome', 'op', 'fact_asmt_outcome', 'rec_status', None, None),
         ('4', 'int_sbac_asmt_outcome', 'guid_batch', 'fact_asmt_outcome', 'batch_guid', None, None),
         ('4', 'int_sbac_asmt_outcome', 'asmt_status', 'fact_asmt_outcome_vw', 'asmt_status', None, None),
+        ('4', 'int_sbac_asmt_outcome', 'complete', 'fact_asmt_outcome_vw', 'complete', None, None),
         # Interim Assessment Blocks
         ('4', 'int_sbac_asmt_outcome', 'record_sid', 'fact_block_asmt_outcome', 'asmt_outcome_rec_id', None, None),
         ('4', 'int_sbac_asmt_outcome', '-1', 'fact_block_asmt_outcome', 'asmt_rec_id', None, None),
@@ -535,6 +541,7 @@ ref_table_conf = {
         ('4', 'int_sbac_asmt_outcome', 'op', 'fact_block_asmt_outcome', 'rec_status', None, None),
         ('4', 'int_sbac_asmt_outcome', 'guid_batch', 'fact_block_asmt_outcome', 'batch_guid', None, None),
         ('4', 'int_sbac_asmt_outcome', 'asmt_status', 'fact_block_asmt_outcome', 'asmt_status', None, None),
+        ('4', 'int_sbac_asmt_outcome', 'complete', 'fact_block_asmt_outcome', 'complete', None, None),
         # Used only in reporting app, smarter.  udl should never pick this up
         ('1000', 'int_sbac_asmt_outcome', 'guid_asmt', 'dim_asmt', 'asmt_guid', None, None),
     ]

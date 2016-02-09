81233fbdb35144d79a4847007304c7ed63dd85e3
defyrlt <defyrlt@users.noreply.github.com>
2016-02-09 12:28:55 +0200

    Removed filter on "summative" assessment type from `pre_pdf` query
    Removed that filter so all of the assessments can be processed in
    `pre_pdf_generator`. As for now, we don't even have "SUMMATIVE" type in
    `dim_asmt` table and it was decided that we can allow all of them.
    
    TA43185

--- smarter/smarter/tests/trigger/test_pre_pdf_generator_with_smarter.py
+++ smarter/smarter/tests/trigger/test_pre_pdf_generator_with_smarter.py
@@ -4,13 +4,18 @@ Created on Jun 24, 2013
 @author: dip
 '''
 import unittest
+
+from zope import component
+
 from edcore.tests.utils.unittest_with_edcore_sqlite import Unittest_with_edcore_sqlite,\
     get_unittest_tenant_name
-from smarter.trigger.pre_pdf_generator import prepare_pre_pdf, trigger_pre_pdf
-import services
-from zope import component
+
 from edauth.security.session_backend import ISessionBackend, SessionBackend
+
+import services
 from services.celery import setup_celery
+
+from smarter.trigger.pre_pdf_generator import prepare_pre_pdf, trigger_pre_pdf
 
 
 class TestPrePdfGenerator(Unittest_with_edcore_sqlite):
@@ -23,7 +28,7 @@ class TestPrePdfGenerator(Unittest_with_edcore_sqlite):
 
     def test_prepare_pre_pdf(self):
         results = prepare_pre_pdf(self.tenant, 'NC', '90901b70-ddaa-11e2-a95d-68a86d3c2f82')
-        self.assertEqual(388, len(results))
+        self.assertEqual(822, len(results))
 
     def test_prepare_pre_pdf_with_future_date(self):
         results = prepare_pre_pdf(self.tenant, 'NC', 'd1d7d814-ddb1-11e2-b3dd-68a86d3c2f82')
@@ -44,17 +49,24 @@ class TestPrePdfGenerator(Unittest_with_edcore_sqlite):
                     'celery.CELERY_ALWAYS_EAGER': True,
                     'pdf.minimum_file_size': 0,
                     'cache.regions': 'public.data, session',
-                    'cache.type': 'memory'
-                    }
+                    'cache.type': 'memory'}
         component.provideUtility(SessionBackend(settings), ISessionBackend)
         services.tasks.pdf.pdf_procs = ['echo', 'dummy']
         setup_celery(settings, "celery")
 
-        results = [{'school_id': '242', 'district_id': '228', 'effective_date': '20151213', 'asmt_type': 'SUMMATIVE', 'asmt_period_year': '2015', 'asmt_grade': '3', 'student_id': '34140997-8949-497e-bbbb-5d72aa7dc9cb'}]
+        results = [{
+            'school_id': '242',
+            'district_id': '228',
+            'effective_date': '20151213',
+            'asmt_type': 'SUMMATIVE',
+            'asmt_period_year': '2015',
+            'asmt_grade': '3',
+            'student_id': '34140997-8949-497e-bbbb-5d72aa7dc9cb'
+        }]
         triggered = trigger_pre_pdf(settings, 'NC', self.tenant, results)
         self.assertTrue(triggered)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
